104f2416a73e8a0118b26a048b5ba0935b15cfc2
Visser <jannisvisser@redcross.nl>
2019-05-15 17:07:47 +0200

    AB#173 unstage dist-folder

--- services/programs-service/dist/option/dto/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var create_option_dto_1 = require("./create-option.dto");
-exports.CreateOptionDto = create_option_dto_1.CreateOptionDto;
-//# sourceMappingURL=index.js.map
