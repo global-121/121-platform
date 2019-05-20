75d1be18b954ae595dc8c530ecc355c314ba49a8
Elwin Schmitz <eschmitz@redcross.nl>
2019-05-20 16:34:11 +0200

    AB#87 Rename "list"-page to "programs"-page
    To test the testing-pipeline

--- interfaces/HO-Portal/src/app/app-routing.module.ts
+++ interfaces/HO-Portal/src/app/app-routing.module.ts
@@ -12,8 +12,8 @@ const routes: Routes = [
     loadChildren: './home/home.module#HomePageModule'
   },
   {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
+    path: 'programs',
+    loadChildren: './programs/programs.module#ProgramsPageModule'
   }
 ];
 
