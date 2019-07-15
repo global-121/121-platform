2f82b35b96c4e4b12d37962d7af2f75cc9002435
RubenGeo <rubenavalk@gmail.com>
2019-07-15 11:56:27 +0200

    Fixed dto error

--- services/programs-service/src/program/dto/create-options.dto.ts
+++ services/programs-service/src/program/dto/create-options.dto.ts
@@ -1,12 +1,7 @@
 import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 
 export class CreateOptionsDto {
-  @IsNotEmpty()
-  @IsNumber()
   public readonly id: number;
-  @IsNotEmpty()
-  @IsString()
   public readonly option: string;
-  @IsNotEmpty()
   public readonly name: JSON;
 }
