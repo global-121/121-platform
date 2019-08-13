2911b3daf9e7dde6b3a459f86e12ee3f4e973057
Elwin Schmitz <eschmitz@redcross.nl>
2019-08-13 15:23:53 +0200

    Replace hard-coded conversation with dynamic one

--- interfaces/PA-App/src/app/tab2/tab2.page.ts
+++ interfaces/PA-App/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 import { environment } from 'src/environments/environment';
 import { ProgramsServiceApiService } from '../services/programs-service-api.service';
 
@@ -9,10 +10,19 @@ import { ProgramsServiceApiService } from '../services/programs-service-api.serv
   providers: [ProgramsServiceApiService]
 })
 export class Tab2Page {
+  @ViewChild(IonContent)
+  public ionContent: IonContent;
+
   public isDebug: boolean = !environment.production;
+
   public countries: any = null;
   public countryChoice: number = null;
   public programs: any = null;
+
+  public conversation = [];
+
+  myTurn = false;
+
   constructor(
     public programsService: ProgramsServiceApiService,
   ) { }
@@ -39,5 +49,38 @@ export class Tab2Page {
     });
   }
 
+  getActor(isMe: boolean = false) {
+    return isMe ? 'self' : 'system';
+  }
 
+  getRandomHello() {
+    const hellos = [
+      'hello',
+      'hallo',
+      'hey',
+      'hi',
+      'ola',
+      'allo',
+    ];
+
+    return hellos[Math.floor(Math.random() * hellos.length)];
+  }
+
+  addTurn(theActor: string, theContent: any) {
+    this.conversation.push({
+      actor: theActor,
+      content: theContent,
+    });
+  }
+
+  public sayHello() {
+    console.log('Hello!');
+    this.addTurn(this.getActor(this.myTurn), this.getRandomHello());
+    this.myTurn = !this.myTurn;
+
+    // Wait for the elements to be added to the DOM before scrolling down
+    setTimeout(() => {
+      this.ionContent.scrollToBottom();
+    }, 100);
+  }
 }
