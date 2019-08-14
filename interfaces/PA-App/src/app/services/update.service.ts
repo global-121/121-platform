0ae233134bb936f80738804e8c4801f3dd0c8eae
RubenGeo <rubenavalk@gmail.com>
2019-08-14 13:08:03 +0200

    Created sepparate functions to get and set in local storage

--- interfaces/PA-App/src/app/services/update.service.ts
+++ interfaces/PA-App/src/app/services/update.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { timer } from 'rxjs';
 import { ProgramsServiceApiService } from './programs-service-api.service';
+import { InclusionStorage } from '../models/local-storage/inclusion-storage.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,13 @@ import { ProgramsServiceApiService } from './programs-service-api.service';
 export class UpdateService {
   constructor(public programsService: ProgramsServiceApiService) { }
   checkInclusion(programId: number): void {
-    const allInclusion = JSON.parse(localStorage.getItem('inclusion'));
+    let allInclusion: InclusionStorage[];
+    allInclusion = this.getLocalStorageArray('inclusion');
+    for (const inclusion of allInclusion) {
+      if (inclusion.programId === programId || inclusion.programId in ['excluded', 'included']) {
+        return;
+      }
+    }
     this.programsService.getInclusionStatus(programId).subscribe(response => {
       if (response.status === 'unavailable') {
         const secondsWait = 3;
@@ -19,18 +26,35 @@ export class UpdateService {
           this.checkInclusion(programId);
         }, secondsWait * 1000);
       } else {
-        const inclusionState = {
+        const inclusionState: InclusionStorage = {
           programId: programId,
           status: response.status
         };
-
         allInclusion.push(inclusionState);
         console.log(allInclusion);
-        window.localStorage.setItem(
-          'inclusion',
-          JSON.stringify(allInclusion)
-        );
+        this.setLocalStorage('inclusion', allInclusion)
+
       }
     });
+  }
+
+  getLocalStorageArray(itemKey: string): [any] {
+    let value: [object];
+    const valueString = localStorage.getItem(itemKey);
+    if (valueString) {
+      value = JSON.parse(valueString);
+    } else {
+      value = [new Object()];
+      localStorage.setItem(itemKey, JSON.stringify(value));
+    }
+    return value;
+  }
+
+  setLocalStorage(itemKey: string, itemValue: object): void {
+    const itemString = JSON.stringify(itemValue);
+    window.localStorage.setItem(
+      itemKey,
+      itemString
+    );
   }
 }
