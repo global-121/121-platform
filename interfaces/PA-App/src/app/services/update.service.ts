dfcc093d85fffb4f461b4d5b0ae26d27c8f4faf6
RubenGeo <rubenavalk@gmail.com>
2019-08-15 11:45:15 +0200

    Moved all strigs to the class

--- interfaces/PA-App/src/app/services/update.service.ts
+++ interfaces/PA-App/src/app/services/update.service.ts
@@ -4,20 +4,34 @@ import { InclusionStorage } from '../models/local-storage/inclusion-storage.mode
 import { InclusionStatus } from '../models/inclusion-status.model';
 import { catchError } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class UpdateService {
-  updateSpeedMs = 3000;
+  public updateSpeedMs = 3000;
+  public inclusionStatus = {
+    included: 'included',
+    excluded: 'excluded',
+    unavailable: 'unavailable'
+  };
+  public receivedStatus = {
+    received: 'received',
+    unavailable: 'unavailable'
+  };
+  public inclusionStatusStorage = 'inclusionStatus';
+  public credentialStatusStorage = 'credentialStatus';
+  public didStorage = 'did';
 
   constructor(public programsService: ProgramsServiceApiService) { }
 
   checkInclusion(programId: number): void {
-    const allInclusion: InclusionStorage[] = this.getLocalStorageArray('inclusionStatus');
+    const allInclusion: InclusionStorage[] = this.getLocalStorageArray(this.inclusionStatusStorage);
     for (const inclusion of allInclusion) {
       console.log(inclusion);
-      if (inclusion.programId === programId && (inclusion.status === 'included' || inclusion.status === 'excluded')) {
+      if (inclusion.programId === programId &&
+        (inclusion.status === this.inclusionStatus.included || inclusion.status === this.inclusionStatus.included)) {
         return;
       }
     }
@@ -25,35 +39,33 @@ export class UpdateService {
   }
 
   checkCredential(programId: number): void {
-    const allCredentialState = this.getLocalStorageArray('credentialStatus');
+    const allCredentialState = this.getLocalStorageArray(this.credentialStatusStorage);
     console.log(allCredentialState);
     for (const credentialState of allCredentialState) {
-      if (credentialState.programId === programId && credentialState.status === 'received') {
-        console.log(credentialState);
+      if (credentialState.programId === programId && credentialState.status === this.receivedStatus.received) {
         return;
       }
     }
-    this.listenForCredential(programId, allCredentialState)
+    this.listenForCredential(programId, allCredentialState);
   }
 
   listenForInclusion(programId: number, allInclusion: InclusionStorage[]): void {
-    const did = localStorage.getItem('did');
+    const did = localStorage.getItem(this.didStorage);
     this.programsService.getInclusionStatus(programId, did).subscribe(response => {
-      console.log('response', response.status);
-      if (response.status === 'unavailable') {
+      if (response.status === this.inclusionStatus.unavailable) {
         setTimeout(() => {
           this.listenForInclusion(programId, allInclusion);
         }, this.updateSpeedMs);
-      } else if (response.status === 'included' || response.status === 'excluded') {
-        this.storeStatus(response.status, programId, allInclusion, 'inclusionStatus');
+      } else if (response.status === this.inclusionStatus.included || response.status === this.inclusionStatus.excluded) {
+        this.storeStatus(response.status, programId, allInclusion, this.inclusionStatusStorage);
       }
     });
   }
 
   listenForCredential(programId: number, allCredentialState: any): void {
-    const did = localStorage.getItem('did');
+    const did = localStorage.getItem(this.didStorage);
     this.programsService.getCredential(did).subscribe(response => {
-      this.storeStatus('received', programId, allCredentialState, 'credentialStatus');
+      this.storeStatus(this.receivedStatus.received, programId, allCredentialState, this.credentialStatusStorage);
     }, err => {
       setTimeout(() => {
         console.log('error', err);
@@ -67,7 +79,6 @@ export class UpdateService {
       programId,
       status,
     };
-    console.log('bablabl', state);
     allState.push(state);
     this.setLocalStorage(storageName, allState);
   }
