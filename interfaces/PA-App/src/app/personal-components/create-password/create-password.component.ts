d1ff337c90080c1c269f116cb53d25c32de0411c
Visser <jannisvisser@redcross.nl>
2019-09-04 16:04:35 +0200

    AB#626 create PA-account + store wallet-name

--- interfaces/PA-App/src/app/personal-components/create-password/create-password.component.ts
+++ interfaces/PA-App/src/app/personal-components/create-password/create-password.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { PersonalComponent } from '../personal-component.interface';
 
 import { ConversationService } from 'src/app/services/conversation.service';
+import { PaAccountApiService } from 'src/app/services/pa-account-api.service';
+import { UserImsApiService } from 'src/app/services/user-ims-api.service';
 
 @Component({
   selector: 'app-create-password',
@@ -17,6 +19,8 @@ export class CreatePasswordComponent implements PersonalComponent {
 
   constructor(
     public conversationService: ConversationService,
+    public paAccountApiService: PaAccountApiService,
+    public userImsApiService: UserImsApiService,
   ) { }
 
   ngOnInit() {
@@ -29,9 +33,57 @@ export class CreatePasswordComponent implements PersonalComponent {
       return;
     }
 
+    const paAccount = this.createPaAccount(create);
+    const paWalletName = this.storeWalletName(paAccount.paAccountUsername);
+    // this.createWallet(paWalletName, paAccount.paAccountPassword);
+
     this.passwordCreated = true;
 
     this.complete();
+  }
+
+  makeRandomUsername(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  createPaAccount(create) {
+    const paAccountUsername = this.makeRandomUsername(16);
+    const paAccountPassword = create;
+    this.paAccountApiService.create(paAccountUsername, paAccountPassword).subscribe((responseCreate) => {
+      console.log('response: ', responseCreate);
+    });
+    return { paAccountUsername, paAccountPassword };
+  }
+
+  storeWalletName(paAccountUsername) {
+    const paWalletName = this.makeRandomUsername(16);
+    this.paAccountApiService.store(paAccountUsername, 'walletName', paWalletName).subscribe((responseStore) => {
+      console.log('response: ', responseStore);
+    });
+    return paWalletName;
+  }
+
+
+  createWallet(paWalletName, paWalletPassword) {
+    const wallet = {
+      id: paWalletName,
+      passKey: paWalletPassword,
+    }
+    const correlation = {
+      correlationID: 'test'
+    }
+    this.userImsApiService.createWallet(
+      JSON.parse(JSON.stringify(wallet)),
+      JSON.parse(JSON.stringify(correlation))
+    ).subscribe((responseWallet) => {
+      console.log('response: ', responseWallet);
+    })
   }
 
   getNextSection() {
---
+++ interfaces/PA-App/src/app/services/user-ims-api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, tap, catchError } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserImsApiService {
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  createWallet(wallet: JSON, correlation: JSON): Observable<any> {
+    console.log('UserImsApiService : createWallet()');
+
+    return this.apiService
+      .post(
+        environment.url_user_ims_api,
+        '/wallet',
+        {
+          wallet,
+          correlation
+        },
+        true
+      )
+      .pipe(
+        tap(response => console.log('response: ', response)),
+        map(response => response)
+      );
+  }
+}
