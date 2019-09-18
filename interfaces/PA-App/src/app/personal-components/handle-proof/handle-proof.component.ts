608c4747ef42c481985dc011e7bb01d0e19bec84
RubenGeo <rubenavalk@gmail.com>
2019-09-18 11:01:13 +0200

    Cleaned up some unused variables, removed magic strings, cleaned up console.logs

--- interfaces/PA-App/src/app/personal-components/handle-proof/handle-proof.component.ts
+++ interfaces/PA-App/src/app/personal-components/handle-proof/handle-proof.component.ts
@@ -4,7 +4,13 @@ import { UpdateService } from 'src/app/services/update.service';
 import { PaAccountApiService } from 'src/app/services/pa-account-api.service';
 import { UserImsApiService } from 'src/app/services/user-ims-api.service';
 import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
-import { resolve } from 'url';
+
+enum InclusionStatusEnum {
+  included = 'inlcuded',
+  excluded = 'excluded',
+  unavailable = 'unavailable'
+}
+
 
 @Component({
   selector: 'app-handle-proof',
@@ -26,26 +32,23 @@ export class HandleProofComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    console.log('onint');
     this.handleProof();
   }
   async handleProof() {
+    console.log('handleProof');
     const proofRequest = await this.getProofRequest();
-    console.log('proofRequest', proofRequest);
     const proof = await this.getProof(proofRequest);
     const status = await this.sendProof(proof);
+
     let inclusionStatus;
     if (status === 'done') {
       inclusionStatus = await this.getInclusionStatus();
     }
-    if (inclusionStatus === 'included') {
-      console.log('jeuj');
+    if (inclusionStatus === InclusionStatusEnum.included) {
       this.inclusionStatusPositive = true;
-    } else if (inclusionStatus === 'excluded') {
+    } else if (inclusionStatus === InclusionStatusEnum.excluded) {
       this.inclusionStatusNegative = true;
-      console.log('nooh');
     }
-
   }
 
   async getProofRequest(): Promise<string> {
@@ -56,7 +59,7 @@ export class HandleProofComponent implements OnInit {
   }
 
   async getProof(proofRequest: string): Promise<string> {
-    console.log('getting proof from wallet');
+    console.log('getProof');
     const proofRequestJson = JSON.stringify(proofRequest);
     const wallet = JSON.parse(await this.paRetrieveData('wallet'));
     const correlation = JSON.parse(await this.paRetrieveData('correlation'));
@@ -66,18 +69,18 @@ export class HandleProofComponent implements OnInit {
   }
 
   async sendProof(proof: string): Promise<string> {
+    console.log('sendProof');
     const did = await this.paRetrieveData('did');
     const programId = Number(await this.paRetrieveData('programId'));
-    console.log('sending', proof, did, programId);
     const response = await this.programService.postIncludeMe(did, programId, proof).toPromise();
     return response.status;
   }
 
   async getInclusionStatus(): Promise<string> {
+    console.log('getInclusionStatus');
     const did = await this.paRetrieveData('did');
     const programId = await this.paRetrieveData('programId');
     const response = await this.programService.postInclusionStatus(did, programId).toPromise();
-    console.log('Inclusie status', response);
     return response.status;
   }
 
