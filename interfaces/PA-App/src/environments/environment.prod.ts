8c81dbbe11e4ad5945d0c52378ffc6b3e25d3072
Elwin Schmitz <eschmitz@redcross.nl>
2019-09-23 22:08:12 +0200

    Add staging-server as API endpoints

--- interfaces/PA-App/src/environments/environment.prod.ts
+++ interfaces/PA-App/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   useAnimation: true, // Use animations and delays in the interface
 
   // APIs:
-  url_121_service_api: '',
-  url_pa_account_service_api: '',
-  url_user_ims_api: '',
+  url_121_service_api: 'http://137.117.210.255/api',
+  url_pa_account_service_api: 'http://137.117.210.255:3001/api',
+  url_user_ims_api: 'http://137.117.210.255:50003/api',
 };
