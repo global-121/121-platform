5fb441efe8ac07b459d368769c132e9ff592b628
Visser <jannisvisser@redcross.nl>
2019-09-24 21:15:04 +0200

    AB#662 apps work with server-api

--- interfaces/AW-App/src/environments/environment.ts
+++ interfaces/AW-App/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  url_121_service_api: 'http://localhost:3000/api',
+  // url_121_service_api: 'http://localhost:3000/api',
+  url_121_service_api: 'http://23.97.236.236/121-service/api',
 };
 
 /*
