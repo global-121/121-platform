49a8f0258817d4288845b27399e4688f89f3f4b8
Elwin Schmitz <eschmitz@redcross.nl>
2019-10-02 14:07:24 +0200

    Make submit-button only appear after choice of credential-type
    + Simplify component logic - move into template

--- interfaces/PA-App/src/app/personal-components/choose-credential-type/choose-credential-type.component.ts
+++ interfaces/PA-App/src/app/personal-components/choose-credential-type/choose-credential-type.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { PersonalComponent } from '../personal-component.class';
 import { PersonalComponents } from '../personal-components.enum';
 
-import { Storage } from '@ionic/storage';
-import { TranslateService } from '@ngx-translate/core';
 import { ConversationService } from 'src/app/services/conversation.service';
 
 @Component({
@@ -12,58 +10,24 @@ import { ConversationService } from 'src/app/services/conversation.service';
   styleUrls: ['./choose-credential-type.component.scss'],
 })
 export class ChooseCredentialTypeComponent extends PersonalComponent {
-  public credentialTypes: any;
   public credentialTypeChoice: string;
   public typeChosen: boolean;
   public programChosen: boolean;
-  public credentialTypeChoiceNew: string;
 
   constructor(
-    public storage: Storage,
-    public translate: TranslateService,
     public conversationService: ConversationService,
   ) {
     super();
   }
 
   ngOnInit() {
-    this.credentialTypes = [
-      {
-        id: 'apply-to-program',
-        credentialType: this.translate.instant('personal.choose-credential-type.option1'),
-        disabled: false,
-      },
-      {
-        id: 'create-id',
-        credentialType: this.translate.instant('personal.choose-credential-type.option2'),
-        disabled: false,
-      },
-      {
-        id: 'delete-id',
-        credentialType: this.translate.instant('personal.choose-credential-type.option3'),
-        disabled: true,
-      },
-    ];
   }
 
-  private storeCredentialType(credentialTypeChoice: any) {
-    this.storage.set('credentialTypeChoice', credentialTypeChoice);
-  }
-
-  public changeCredentialType($event, iteration: string) {
-    if (iteration === 'apply-to-program') {
-      const credentialTypeChoice = $event.detail.value;
-      this.credentialTypeChoice = credentialTypeChoice;
-      this.storeCredentialType(credentialTypeChoice);
-    } else if (iteration === 'create-id') {
-      const credentialTypeChoiceNew = $event.detail.value;
-      this.credentialTypeChoiceNew = credentialTypeChoiceNew;
-      this.storeCredentialType(credentialTypeChoiceNew);
-    }
+  public changeCredentialType(value: string) {
+    this.credentialTypeChoice = value;
   }
 
   public submitCredentialType() {
-    console.log('Chosen credential type: ', this.credentialTypeChoice);
     this.typeChosen = true;
     this.conversationService.scrollToEnd();
 
@@ -78,7 +42,6 @@ export class ChooseCredentialTypeComponent extends PersonalComponent {
   }
 
   public submitCredentialTypeNew() {
-    console.log('Chosen credential type: ', this.credentialTypeChoiceNew);
     this.complete();
   }
 
