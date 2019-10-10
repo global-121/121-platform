a144670f7e17bbba5cb2e97c295488ef83e57c01
Elwin Schmitz <eschmitz@redcross.nl>
2019-10-10 12:52:56 +0200

    Refactor StorageService
    - Sort methods by 'type' of storage
    - add NotAvailable return value(s)
    - Make handling feature-toggles easier in the future
    - Rename things

--- interfaces/PA-App/src/app/services/storage.service.ts
+++ interfaces/PA-App/src/app/services/storage.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 
 import { environment } from 'src/environments/environment';
 
+import { Storage } from '@ionic/storage';
 import { PaAccountApiService } from './pa-account-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  private useLocalStorage: boolean;
 
   public type = {
     did: 'did',
@@ -19,39 +21,56 @@ export class StorageService {
   };
 
   constructor(
-    public paAccountApiService: PaAccountApiService,
-  ) { }
+    private ionStorage: Storage,
+    private paAccountApi: PaAccountApiService,
+  ) {
+    this.useLocalStorage = environment.localStorage;
 
-  async createAccount(username: string, password: string): Promise<any> {
-    if (environment.localStorage) {
-      // use local sotrage
-    } else {
-      return this.paAccountApiService.createAccount(username, password);
-    }
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+  // ALL types of storage:
+  /////////////////////////////////////////////////////////////////////////////
+
   async store(type: string, data: string): Promise<any> {
-    if (environment.localStorage) {
-      // use local sotrage
-    } else {
-      return this.paAccountApiService.store(type, data);
+    if (this.useLocalStorage) {
+      return this.ionStorage.set(type, data);
     }
+
+    return this.paAccountApi.store(type, data);
   }
 
   async retrieve(type: string): Promise<any> {
-    if (environment.localStorage) {
-      // use local sotrage
-    } else {
-      return this.paAccountApiService.retrieve(type);
+    if (this.useLocalStorage) {
+      return this.ionStorage.get(type);
     }
+
+    return this.paAccountApi.retrieve(type);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // ONLY for WEB users:
+  /////////////////////////////////////////////////////////////////////////////
+  private featureNotAvailable(): Promise<any>  {
+    return new Promise((resolve, reject) => {
+      return reject('Not available with local storage');
+    });
+  }
+
+  async createAccount(username: string, password: string): Promise<any> {
+    if (this.useLocalStorage) {
+      return this.featureNotAvailable();
+    }
+
+    return this.paAccountApi.createAccount(username, password);
   }
 
   async login(username: string, password: string): Promise<any> {
-    if (environment.localStorage) {
-      // use local sotrage
-    } else {
-      return this.paAccountApiService.login(username, password);
+    if (this.useLocalStorage) {
+      return this.featureNotAvailable();
     }
+
+    return this.paAccountApi.login(username, password);
   }
 
 }
