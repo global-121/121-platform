d31af188ad048fbc963e45065dcb3a615dcc363a
Elwin Schmitz <eschmitz@redcross.nl>
2019-10-15 12:50:41 +0200

    Cleanup environment files

--- interfaces/PA-App/src/environments/environment.ts
+++ interfaces/PA-App/src/environments/environment.ts
@@ -12,12 +12,9 @@ export const environment = {
   useAnimation: false, // Use animations and delays in the interface
 
   // APIs:
-  // url_121_service_api: 'http://localhost:3000/api',
-  // url_pa_account_service_api: 'http://localhost:3001/api',
-  // url_user_ims_api: 'http://localhost:50003/api',
+  url_121_service_api: 'http://localhost:3000/api',
+  url_pa_account_service_api: 'http://localhost:3001/api',
+  url_user_ims_api: 'http://localhost:50003/api',
 
-  url_121_service_api: 'http://23.97.236.236/121-service/api',
-  url_pa_account_service_api: 'http://23.97.236.236/PA-accounts/api',
-  url_user_ims_api: 'http://23.97.236.236/UserIMS/api',
 
 };
