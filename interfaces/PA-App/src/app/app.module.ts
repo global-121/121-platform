d8f5123d171c7950e282e31a45bf526a786eb963
RubenGeo <rubenavalk@gmail.com>
2019-10-22 15:22:13 +0200

    trying out firebase

--- interfaces/PA-App/src/app/app.module.ts
+++ interfaces/PA-App/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicStorageModule } from '@ionic/storage';
+import { FCM } from '@ionic-native/fcm/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -43,6 +44,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   providers: [
     StatusBar,
     SplashScreen,
+    FCM,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
