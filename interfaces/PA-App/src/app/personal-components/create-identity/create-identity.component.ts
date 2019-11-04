cccf9b4583e8e820e6862de3928f11ee1dc68ea7
Visser <jannisvisser@redcross.nl>
2019-11-04 14:54:11 +0100

    AB#944 moved initial-needs to after signup/signin

--- interfaces/PA-App/src/app/personal-components/create-identity/create-identity.component.ts
+++ interfaces/PA-App/src/app/personal-components/create-identity/create-identity.component.ts
@@ -117,7 +117,7 @@ export class CreateIdentityComponent extends PersonalComponent {
   }
 
   getNextSection() {
-    return PersonalComponents.selectCountry;
+    return PersonalComponents.initialNeeds;
   }
 
   complete() {
