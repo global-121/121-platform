809717b93c03574d51f9b9b6066ee9fede8f3694
Elwin Schmitz <eschmitz@redcross.nl>
2019-11-05 17:27:27 +0100

    Add mock dependencies to fix tests

--- interfaces/PA-App/src/app/personal-components/initial-needs/initial-needs.component.spec.ts
+++ interfaces/PA-App/src/app/personal-components/initial-needs/initial-needs.component.spec.ts
@@ -3,18 +3,27 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InitialNeedsComponent } from './initial-needs.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { ConversationService } from 'src/app/services/conversation.service';
 
 describe('InitialNeedsComponent', () => {
   let component: InitialNeedsComponent;
   let fixture: ComponentFixture<InitialNeedsComponent>;
 
   beforeEach(async(() => {
+    const conversationService = jasmine.createSpy();
+
     TestBed.configureTestingModule({
       declarations: [InitialNeedsComponent],
       imports: [
         TranslateModule.forRoot(),
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: ConversationService,
+          useValue: conversationService,
+        },
+      ],
     })
       .compileComponents();
   }));
--- interfaces/PA-App/src/app/personal-components/signup-signin/signup-signin.component.spec.ts
+++ interfaces/PA-App/src/app/personal-components/signup-signin/signup-signin.component.spec.ts
@@ -3,18 +3,27 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupSigninComponent } from './signup-signin.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { ConversationService } from 'src/app/services/conversation.service';
 
 describe('SignupSigninComponent', () => {
   let component: SignupSigninComponent;
   let fixture: ComponentFixture<SignupSigninComponent>;
 
   beforeEach(async(() => {
+    const conversationService = jasmine.createSpy();
+
     TestBed.configureTestingModule({
       declarations: [SignupSigninComponent],
       imports: [
         TranslateModule.forRoot(),
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: ConversationService,
+          useValue: conversationService,
+        },
+      ],
     })
       .compileComponents();
   }));
