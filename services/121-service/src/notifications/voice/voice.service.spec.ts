a102a0609bc1f8441211718fc7b1bd4a6d0d49d0
RubenGeo <rubenavalk@gmail.com>
2019-11-05 16:19:01 +0100

    Fixed test

--- services/121-service/src/notifications/voice/voice.service.spec.ts
+++ services/121-service/src/notifications/voice/voice.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VoiceService } from './voice.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TwilioMessageEntity } from '../twilio.entity';
+import { repositoryMockFactory } from '../../mock/repositoryMock.factory';
 
 describe('VoiceService', () => {
   let service: VoiceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VoiceService],
+      providers: [
+        VoiceService,
+        {
+          provide: getRepositoryToken(TwilioMessageEntity),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<VoiceService>(VoiceService);
