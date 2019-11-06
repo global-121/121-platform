69911a201fca4e9ff7cc13afe8855a9e96130bdf
Visser <jannisvisser@redcross.nl>
2019-11-06 11:57:16 +0100

    AB#973 fix unit test

--- interfaces/PA-App/src/app/personal-components/meeting-reminder/meeting-reminder.component.spec.ts
+++ interfaces/PA-App/src/app/personal-components/meeting-reminder/meeting-reminder.component.spec.ts
@@ -2,6 +2,11 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MeetingReminderComponent } from './meeting-reminder.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { QRCodeModule } from 'angularx-qrcode';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Storage } from '@ionic/storage';
+import { MockIonicStorage } from 'src/app/mocks/ionic.storage.mock';
 
 describe('MeetingReminderComponent', () => {
   let component: MeetingReminderComponent;
@@ -9,10 +14,21 @@ describe('MeetingReminderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MeetingReminderComponent ],
+      declarations: [MeetingReminderComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        HttpClientTestingModule,
+        QRCodeModule
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: Storage,
+          useValue: MockIonicStorage,
+        },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
