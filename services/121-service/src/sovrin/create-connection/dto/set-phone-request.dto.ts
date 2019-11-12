7a32926dff13ddd174cda51da0843ccdd96350f0
RubenGeo <rubenavalk@gmail.com>
2019-11-12 09:50:20 +0100

    Added preferred language to phone post request AB#1025

--- services/121-service/src/sovrin/create-connection/dto/set-phone-request.dto.ts
+++ services/121-service/src/sovrin/create-connection/dto/set-phone-request.dto.ts
@@ -1,12 +1,17 @@
-import { Length, IsNotEmpty, IsString } from 'class-validator';
+import { Length, IsNotEmpty, IsString, ValidateIf } from 'class-validator';
 import { ApiModelProperty } from '@nestjs/swagger';
 
 export class SetPhoneRequestDto {
-  @ApiModelProperty({ example: 'did:sov:2wJPyULfLLnYTEFYzByfUR' })
-  @Length(29, 30)
-  public readonly did: string;
-  @ApiModelProperty({ example: '0031600000000' })
-  @IsNotEmpty()
-  @IsString()
-  public readonly phonenumber: string;
-}
+         @ApiModelProperty({ example: 'did:sov:2wJPyULfLLnYTEFYzByfUR' })
+         @Length(29, 30)
+         public readonly did: string;
+         @ApiModelProperty({ example: '0031600000000' })
+         @ValidateIf(o => o.length > 0)
+         @IsString()
+         public readonly phonenumber: string;
+         @ValidateIf(o => o.length > 0)
+         @ApiModelProperty({ example: 'en' })
+         @IsString()
+         @Length(2, 5)
+         public readonly language: string;
+       }
