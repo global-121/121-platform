46eed0304b020bc801ffc354c4fc7dec1b924d9f
Elwin Schmitz <eschmitz@redcross.nl>
2019-11-12 11:38:27 +0100

    Refactor + support init-from-history of "enroll-in-program"-component

--- interfaces/PA-App/src/app/personal-components/enroll-in-program/enroll-in-program.component.ts
+++ interfaces/PA-App/src/app/personal-components/enroll-in-program/enroll-in-program.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PersonalComponent } from '../personal-component.class';
 import { PersonalComponents } from '../personal-components.enum';
 
@@ -16,14 +16,17 @@ import { PaDataService } from 'src/app/services/padata.service';
   styleUrls: ['./enroll-in-program.component.scss'],
 })
 export class EnrollInProgramComponent extends PersonalComponent {
+  @Input()
+  public data: any;
+
   public languageCode: string;
   public fallbackLanguageCode: string;
 
-  public programDetails: any;
-  public programTitle: string;
-
-  private credDefId: string;
   private programId: number;
+  private currentProgram: Program;
+  private credDefId: string;
+
+  public programDetails: any;
 
   public questions: Question[];
   public answerTypes = AnswerType;
@@ -42,51 +45,68 @@ export class EnrollInProgramComponent extends PersonalComponent {
     public conversationService: ConversationService,
   ) {
     super();
+  }
 
+  ngOnInit() {
     this.fallbackLanguageCode = this.translate.getDefaultLang();
     this.languageCode = this.translate.currentLang;
+
+    if (this.data) {
+      this.initHistory();
+      return;
+    }
+
+    this.initNew();
+  }
+
+  initHistory() {
+    this.isDisabled = true;
+    this.currentProgram = this.data.currentProgram;
+    this.prepareProgramDetails(this.data.currentProgram);
+    this.checkAllQuestionsShown(this.questions, Object.keys(this.data.answers));
+    this.answers = this.data.answers;
+    this.hasAnswered = true;
+  }
+
+  initNew() {
     this.getProgramDetails();
   }
 
-  private getProgramDetails() {
+  private async getProgramDetails() {
     this.conversationService.startLoading();
 
-    this.paData.retrieve(this.paData.type.programId).then(value => {
-      this.programId = Number(value);
-      this.getProgramDetailsById(value);
-    });
+    this.programId = Number(await this.paData.retrieve(this.paData.type.programId));
+    this.currentProgram = await this.programsService.getProgramById(this.programId);
+    this.prepareProgramDetails(this.currentProgram);
+    this.paData.saveProgram(this.programId, this.currentProgram);
+
+    this.conversationService.stopLoading();
   }
 
-  public getProgramDetailsById(programId: string) {
-    this.programsService.getProgramById(programId).subscribe((response: Program) => {
-      this.programTitle = this.mapLabelByLanguageCode(response.title);
-      this.credDefId = response.credDefId;
+  public prepareProgramDetails(program: Program) {
+    this.credDefId = program.credDefId;
 
-      this.programDetails = this.buildDetails(response);
-      this.questions = this.buildQuestions(response.customCriteria);
-
-      this.paData.saveProgram(response.id, response);
-
-      this.conversationService.stopLoading();
-    });
+    this.programDetails = this.buildDetails(program);
+    this.questions = this.buildQuestions(program.customCriteria);
   }
 
   private buildDetails(response: Program) {
     const programDetails = [];
     const details = [
       'ngo',
+      'title',
       'description',
       'meetingDocuments',
     ];
     for (const detail of details) {
       let value = this.mapLabelByLanguageCode(response[detail]);
 
-      if (typeof value === 'undefined') {
-        value = response[detail];
+      if (detail === 'meetingDocuments' && typeof value === 'string') {
+        value = this.buildDocumentsList(value);
       }
 
-      if (detail === 'meetingDocuments') {
-        value = this.buildDocumentsList(value);
+      if (typeof value === 'undefined') {
+        value = response[detail];
       }
 
       programDetails[detail] = value;
@@ -140,6 +160,10 @@ export class EnrollInProgramComponent extends PersonalComponent {
 
     if (!label) {
       label = property[this.fallbackLanguageCode];
+    }
+
+    if (!label) {
+      label = property;
     }
 
     return label;
@@ -216,21 +240,17 @@ export class EnrollInProgramComponent extends PersonalComponent {
   }
 
   public submit() {
-
-    this.paData.saveAnswers(this.programId, this.answers);
-
     if (!this.answers.dob) {
       this.dobFeedback = true;
-    } else {
-      this.hasAnswered = true;
-      this.dobFeedback = false;
-      this.conversationService.scrollToEnd();
+      return;
     }
+    this.hasAnswered = true;
+    this.dobFeedback = false;
+    this.conversationService.scrollToEnd();
+    this.paData.saveAnswers(this.programId, this.answers);
   }
 
   public async submitConfirm() {
-    console.log('submitConfirm()');
-
     this.conversationService.startLoading();
     this.isDisabled = true;
     await this.executeSovrinFlow();
@@ -299,7 +319,15 @@ export class EnrollInProgramComponent extends PersonalComponent {
     this.conversationService.onSectionCompleted({
       name: PersonalComponents.enrollInProgram,
       data: {
-        programDetails: this.programDetails,
+        currentProgram: {
+          id: this.currentProgram.id,
+          ngo: this.currentProgram.ngo,
+          title: this.currentProgram.title,
+          description: this.currentProgram.description,
+          meetingDocuments: this.currentProgram.meetingDocuments,
+          customCriteria: this.currentProgram.customCriteria,
+          credDefId: this.currentProgram.credDefId,
+        },
         answers: this.answers,
       },
       next: this.getNextSection(),
--- interfaces/PA-App/src/app/services/programs-service-api.service.ts
+++ interfaces/PA-App/src/app/services/programs-service-api.service.ts
@@ -47,13 +47,14 @@ export class ProgramsServiceApiService {
       .toPromise();
   }
 
-  getProgramById(programId: string): Observable<Program> {
+  getProgramById(programId: number): Promise<Program> {
     return this.apiService
       .get(environment.url_121_service_api, '/programs/' + programId)
       .pipe(
         tap(response => console.log('response: ', response)),
         map(response => response)
-      );
+      )
+      .toPromise();
   }
 
   getConnectionRequest(): Promise<any> {
