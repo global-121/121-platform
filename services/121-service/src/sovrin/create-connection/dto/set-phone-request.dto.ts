7df552d971974e0c4c91a05db331fd73c0b8027a
RubenGeo <rubenavalk@gmail.com>
2019-11-12 10:02:34 +0100

    Syling fix

--- services/121-service/src/sovrin/create-connection/dto/set-phone-request.dto.ts
+++ services/121-service/src/sovrin/create-connection/dto/set-phone-request.dto.ts
@@ -2,16 +2,16 @@ import { Length, IsNotEmpty, IsString, ValidateIf } from 'class-validator';
 import { ApiModelProperty } from '@nestjs/swagger';
 
 export class SetPhoneRequestDto {
-         @ApiModelProperty({ example: 'did:sov:2wJPyULfLLnYTEFYzByfUR' })
-         @Length(29, 30)
-         public readonly did: string;
-         @ApiModelProperty({ example: '0031600000000' })
-         @ValidateIf(o => o.phonenumber)
-         @IsString()
-         public readonly phonenumber: string;
-         @ValidateIf(o => o.language)
-         @ApiModelProperty({ example: 'en' })
-         @IsString()
-         @Length(2, 5)
-         public readonly language: string;
-       }
+  @ApiModelProperty({ example: 'did:sov:2wJPyULfLLnYTEFYzByfUR' })
+  @Length(29, 30)
+  public readonly did: string;
+  @ApiModelProperty({ example: '0031600000000' })
+  @ValidateIf(o => o.phonenumber)
+  @IsString()
+  public readonly phonenumber: string;
+  @ValidateIf(o => o.language)
+  @ApiModelProperty({ example: 'en' })
+  @IsString()
+  @Length(2, 5)
+  public readonly language: string;
+}
