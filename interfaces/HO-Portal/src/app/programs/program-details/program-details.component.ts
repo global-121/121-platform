b2953572a92db78e92b859f70c08de6b0b64100c
Visser <jannisvisser@redcross.nl>
2019-11-20 11:24:01 +0100

    AB#1077 make program-view list somewhat nicer

--- interfaces/HO-Portal/src/app/programs/program-details/program-details.component.ts
+++ interfaces/HO-Portal/src/app/programs/program-details/program-details.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
 import { Program } from 'src/app/models/program.model';
 import { ModalController } from '@ionic/angular';
 import { ProgramModalPage } from '../program-modal/program-modal.page';
 import { OverlayEventDetail } from '@ionic/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-program-details',
@@ -12,16 +13,28 @@ import { OverlayEventDetail } from '@ionic/core';
   styleUrls: ['./program-details.component.scss'],
 })
 export class ProgramDetailsComponent implements OnInit {
-  public program: any;
+  public languageCode: string;
+  public fallbackLanguageCode: string;
 
   public program: Program;
   public programTitle: string;
   public programArray: any;
+
+
+
   constructor(
     private route: ActivatedRoute,
+    private programsService: ProgramsServiceApiService,
     public modalController: ModalController,
+    public translate: TranslateService,
+  ) {
+
+  }
 
   ngOnInit() {
+    this.fallbackLanguageCode = this.translate.getDefaultLang();
+    this.languageCode = this.translate.currentLang;
+
     const id = this.route.snapshot.paramMap.get('id');
     this.programsService.getProgramById(id).subscribe((response) => {
       this.program = response;
@@ -30,7 +43,41 @@ export class ProgramDetailsComponent implements OnInit {
     });
   }
 
+
+
   public generateArray(obj) {
+    const result = [];
+    for (const key in obj) {
+
+        const keyNew = this.translate.instant('page.programs.program-details.' + key);
+        const valueNew = this.mapLabelByLanguageCode(obj[key]);
+
+        let isArray = false;
+        if (valueNew instanceof Array) {
+          if (typeof valueNew[0] === 'object') {
+            isArray = true;
+            // Enter code here to visualize array-properties (like Criteria/Aidworkers) differently
+          }
+        }
+        result.push(({ key: keyNew, value: valueNew, isArray }));
+    }
+    return result;
+  }
+
+  private mapLabelByLanguageCode(property: any) {
+    let label = property[this.languageCode];
+
+    if (!label) {
+      label = property[this.fallbackLanguageCode];
+    }
+
+    if (!label) {
+      label = property;
+    }
+
+    return label;
+  }
+
   async openModal() {
     const modal: HTMLIonModalElement =
       await this.modalController.create({
