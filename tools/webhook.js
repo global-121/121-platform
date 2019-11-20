4bb635126aaa4a2186f7cffdfa7af445324d3d63
Elwin Schmitz <ik@elwin.nu>
2019-11-20 14:42:41 +0100

    Revert "remove 121-indy plugin from config.xml"

--- tools/webhook.js
+++ tools/webhook.js
@@ -3,7 +3,6 @@ var secrets = require("./secrets");
 var secret = secrets.secret;
 var repo_services = "/home/121-platform/services";
 var repo_pa = "/home/121-platform/interfaces/PA-App";
-var repo_ho = "/home/121-platform/interfaces/HO-portal";
 
 let http = require('http');
 let crypto = require('crypto');
@@ -27,10 +26,7 @@ http.createServer(function (req, res) {
                         ' && sudo docker-compose up -d --build' +
                         ' && cd ' + repo_pa +
                         ' && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /PA-app/' +
-                        ' && sudo rm -rf /var/www/121-platform/PA-app && sudo cp -r www/ /var/www/121-platform/PA-app' +
-                        ' && cd ' + repo_ho +
-                        ' && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /HO-portal/' +
-                        ' && sudo rm -rf /var/www/121-platform/HO-portal && sudo cp -r www/ /var/www/121-platform/HO-portal'
+                        ' && sudo rm -rf /var/www/121-platform/PA-app && sudo cp -r www/ /var/www/121-platform/PA-app'
                 , function(error, stdout, stderr) {
                 if (error) {
                         console.log(stderr);
