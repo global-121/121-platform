bfbbfadd0fe375badc014b8c794e5cd3ad4e570c
Visser <jannisvisser@redcross.nl>
2019-11-25 16:12:35 +0100

    AB#1051 help-page in HO-portal

---
+++ interfaces/HO-Portal/src/app/help/help.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { HelpPage } from './help.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HelpPage
+      }
+    ]),
+    TranslateModule.forChild()
+  ],
+  declarations: [HelpPage]
+})
+export class HelpPageModule { }
