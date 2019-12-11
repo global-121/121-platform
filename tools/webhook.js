afdedf8bcd6db2b9d48f405fe80c515149842d4f
Elwin Schmitz <eschmitz@redcross.nl>
2019-12-11 14:41:38 +0100

    Cleanup syntax using Prettier

--- tools/webhook.js
+++ tools/webhook.js
@@ -6,49 +6,65 @@ var repo_pa = "/home/121-platform/interfaces/PA-App";
 var repo_ho = "/home/121-platform/interfaces/HO-Portal";
 var repo_aw = "/home/121-platform/interfaces/AW-App";
 
-let http = require('http');
-let crypto = require('crypto');
-const exec = require('child_process').exec;
+let http = require("http");
+let crypto = require("crypto");
+const exec = require("child_process").exec;
 
-http.createServer(function (req, res) {
+http
+  .createServer(function(req, res) {
     let body = [];
-    req.on('data', function(chunk) {
-        body.push(chunk);
-	console.log('BODY: ',body);
+    req.on("data", function(chunk) {
+      body.push(chunk);
+      console.log("BODY: ", body);
     });
-    req.on('end', function() {
-        let str = Buffer.concat(body).toString();
-	let sig = "sha1=" + crypto.createHmac('sha1', secret).update(str).digest('hex');
-	let payload = JSON.parse(str);
+    req.on("end", function() {
+      let str = Buffer.concat(body).toString();
+      let sig =
+        "sha1=" +
+        crypto
+          .createHmac("sha1", secret)
+          .update(str)
+          .digest("hex");
+      let payload = JSON.parse(str);
 
-        if (req.headers['x-hub-signature'] == sig && payload.action == 'closed' && payload.pull_request.merged) {
-            exec(
-                        'cd ' + repo_services +
-                        ' && sudo git pull ' +
-                        ' && sudo docker-compose up -d --build' +
-                        ' && cd ' + repo_pa +
-                        ' && export NG_SUB_DIR_PATH="PA-app/"' +
-                        ' && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /PA-app/' +
-                        ' && sudo rm -rf /var/www/121-platform/PA-app && sudo cp -r www/ /var/www/121-platform/PA-app' +
-                        ' && cd ' + repo_ho +
-                        ' && export NG_SUB_DIR_PATH="HO-portal/"' +
-                        ' && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /HO-portal/' +
-                        ' && sudo rm -rf /var/www/121-platform/HO-portal && sudo cp -r www/ /var/www/121-platform/HO-portal' + 
-                        ' && cd ' + repo_aw +
-                        ' && export NG_SUB_DIR_PATH="AW-app/"' +
-                        ' && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /AW-app/' +
-                        ' && sudo rm -rf /var/www/121-platform/AW-app && sudo cp -r www/ /var/www/121-platform/AW-app'
-                , function(error, stdout, stderr) {
-                if (error) {
-                        console.log(stderr);
-                } else {
-			console.log('Execution completed');
-                        console.log(stdout);
-                }
-            });
-        }
+      if (
+        req.headers["x-hub-signature"] == sig &&
+        payload.action == "closed" &&
+        payload.pull_request.merged
+      ) {
+        exec(
+          "cd " +
+            repo_services +
+            " && sudo git pull " +
+            " && sudo docker-compose up -d --build" +
+            " && cd " +
+            repo_pa +
+            ' && export NG_SUB_DIR_PATH="PA-app/"' +
+            " && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /PA-app/" +
+            " && sudo rm -rf /var/www/121-platform/PA-app && sudo cp -r www/ /var/www/121-platform/PA-app" +
+            " && cd " +
+            repo_ho +
+            ' && export NG_SUB_DIR_PATH="HO-portal/"' +
+            " && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /HO-portal/" +
+            " && sudo rm -rf /var/www/121-platform/HO-portal && sudo cp -r www/ /var/www/121-platform/HO-portal" +
+            " && cd " +
+            repo_aw +
+            ' && export NG_SUB_DIR_PATH="AW-app/"' +
+            " && sudo npm ci --unsafe-perm && sudo npm run build -- --prod --base-href /AW-app/" +
+            " && sudo rm -rf /var/www/121-platform/AW-app && sudo cp -r www/ /var/www/121-platform/AW-app",
+          function(error, stdout, stderr) {
+            if (error) {
+              console.log(stderr);
+            } else {
+              console.log("Execution completed");
+              console.log(stdout);
+            }
+          }
+        );
+      }
     });
     res.end();
-}).listen(3099);
+  })
+  .listen(3099);
 
-console.log('Listening on port 3099');
+console.log("Listening on port 3099");
