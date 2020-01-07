de07d10c10d02ae956eb7df179d25e8c7dd5e870
Visser <jannisvisser@redcross.nl>
2020-01-07 11:46:48 +0100

    AB#1145 save score & split minScore<>highestScores

--- interfaces/PA-App/src/app/services/programs-service-api.service.ts
+++ interfaces/PA-App/src/app/services/programs-service-api.service.ts
@@ -204,7 +204,7 @@ export class ProgramsServiceApiService {
   checkInclusionStatus(
     did: string,
     programId: number,
-  ): Promise<any> {
+  ): Observable<any> {
     return this.apiService
       .post(
         environment.url_121_service_api,
@@ -218,8 +218,27 @@ export class ProgramsServiceApiService {
         tap(response => console.log('response: ', response)),
         map(response => response.status)
       )
-      .toPromise();
   }
+
+  // checkInclusionStatus(
+  //   did: string,
+  //   programId: number,
+  // ): Promise<any> {
+  //   return this.apiService
+  //     .post(
+  //       environment.url_121_service_api,
+  //       '/programs/inclusionStatus/' + programId,
+  //       {
+  //         did
+  //       },
+  //       false
+  //     )
+  //     .pipe(
+  //       tap(response => console.log('response: ', response)),
+  //       map(response => response.status)
+  //     )
+  //     .toPromise();
+  // }
 
   getTimeslots(programId: number): Promise<Timeslot[]> {
     return this.apiService.get(
