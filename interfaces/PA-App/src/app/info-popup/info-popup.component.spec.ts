12748f3f9e0fa5cd7f9eb89401d042b2729b3714
Elwin Schmitz <eschmitz@redcross.nl>
2020-01-21 14:04:32 +0100

    Add test-scenarios

--- interfaces/PA-App/src/app/info-popup/info-popup.component.spec.ts
+++ interfaces/PA-App/src/app/info-popup/info-popup.component.spec.ts
@@ -34,4 +34,47 @@ describe('InfoPopupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the provided message as the modal contents', () => {
+    const customContent = 'CUSTOM-CONTENT';
+
+    component.message = customContent;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('ion-content').textContent).toContain(customContent);
+  });
+
+  it('should show the provided message rendered with HTML as the modal contents', () => {
+    const customContent = 'CUSTOM-CONTENT<p><strong>CUSTOM-CONTENT</strong><br>CUSTOM-CONTENT</p>';
+
+    component.message = customContent;
+    fixture.detectChanges();
+
+    const customContentRendered = `CUSTOM-CONTENT
+
+CUSTOM-CONTENT
+CUSTOM-CONTENT`;
+
+    expect(fixture.nativeElement.querySelector('ion-content').innerHTML).toContain(customContent);
+    expect(fixture.nativeElement.querySelector('ion-content').innerText).toContain(customContentRendered);
+  });
+
+  it('should show the provided heading as the modal title', () => {
+    const customHeading = 'CUSTOM-HEADING';
+
+    component.heading = customHeading;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('ion-title').textContent).toContain(customHeading);
+  });
+
+  it('should show a generic heading as the modal title, when no custom heading is provided', () => {
+    const genericHeading = 'shared.more-info';
+
+    component.heading = null;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('ion-title').textContent).toContain(genericHeading);
+  });
+
 });
