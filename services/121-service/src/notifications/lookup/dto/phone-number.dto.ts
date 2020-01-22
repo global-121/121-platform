4c74d293378cd179606cbb7ac76db1d2342d1c4a
RubenGeo <rubenavalk@gmail.com>
2020-01-22 13:23:27 +0100

    Added lookup call to twillio AB#1316

--- services/121-service/src/notifications/lookup/dto/phone-number.dto.ts
+++ services/121-service/src/notifications/lookup/dto/phone-number.dto.ts
@@ -3,7 +3,7 @@ import { ApiModelProperty } from "@nestjs/swagger";
 import { Length } from "class-validator";
 
 export class PhoneNumberDto {
-  @ApiModelProperty({ example: '+000000000000' })
+  @ApiModelProperty({ example: '+15108675310' })
   @Length(8, 17)
   public readonly phoneNumber: string;
 }
--- services/121-service/src/notifications/lookup/lookup.service.ts
+++ services/121-service/src/notifications/lookup/lookup.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { twilioClient } from '../twilio.client';
+import { resolve } from 'path';
 
 @Injectable()
 export class LookupService {
   public constructor() {}
 
-  public async lookupPhoneNr(
-    phoneNumber: string,
-  ): Promise<void> {
-    console.log('lookup', phoneNumber)
+  public async lookupPhoneNr(phoneNumber: string): Promise<any> {
+    console.log('lookup', phoneNumber);
+
+    let numberCorrect: Boolean
+
+    try {
+      const result  = await twilioClient.lookups
+      .phoneNumbers(phoneNumber)
+      .fetch({ type: ['carrier'] })
+      console.log(result)
+      numberCorrect = true;
+    } catch(e) {
+      if (e.status === 404) {
+        numberCorrect = false;
+      }
+    }
+    return { result: numberCorrect }
   }
 }
