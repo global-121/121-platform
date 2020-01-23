3ed8eba73d03ac979bab38dbc7fca9c8e8dd524e
Elwin Schmitz <eschmitz@redcross.nl>
2020-01-23 15:37:47 +0100

    Add new component "PasswordToggleInput"

--- interfaces/PA-App/src/app/shared/shared.module.ts
+++ interfaces/PA-App/src/app/shared/shared.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { DialogueTurnComponent } from './dialogue-turn/dialogue-turn.component';
 import { MoreInfoButtonComponent } from './more-info-button/more-info-button.component';
+import { PasswordToggleInputComponent } from './password-toggle-input/password-toggle-input.component';
 
 @NgModule({
   declarations: [
     DialogueTurnComponent,
-    MoreInfoButtonComponent
+    MoreInfoButtonComponent,
+    PasswordToggleInputComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
     IonicModule,
     TranslateModule.forChild(),
   ],
   exports: [
     DialogueTurnComponent,
     MoreInfoButtonComponent,
+    PasswordToggleInputComponent,
     TranslateModule,
   ],
 })
