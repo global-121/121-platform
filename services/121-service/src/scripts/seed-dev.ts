3154f89f64a7cd63c8d3a90bdb5ae9fb133acf7b
Elwin Schmitz <eschmitz@redcross.nl>
2020-01-27 15:49:51 +0100

    Remove features from DEV already present in MVP seed-script

--- services/121-service/src/scripts/seed-dev.ts
+++ services/121-service/src/scripts/seed-dev.ts
@@ -1,21 +1,11 @@
-import { SeedHelper } from './seed-helper';
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { InterfaceScript } from './scripts.module';
 
 import { AppointmentEntity } from '../schedule/appointment/appointment.entity';
 import { ConnectionEntity } from '../sovrin/create-connection/connection.entity';
-import { CountryEntity } from '../programs/country/country.entity';
 import { CredentialAttributesEntity } from '../sovrin/credential/credential-attributes.entity';
-import { UserEntity } from '../user/user.entity';
-import { ProtectionServiceProviderEntity } from '../programs/program/protection-service-provider.entity';
-
-import { SeedInit } from './seed-init';
-
-import programBasicExample from '../../examples/program-basic.json';
-import programAnonymousExample from '../../examples/program-anonymous1.json';
 import SeedPublish from './seed-publish';
-import { FinancialServiceProviderEntity } from '../programs/fsp/financial-service-provider.entity';
 
 const EXAMPLE_DID = 'did:sov:1wJPyULfLLnYTEFYzByfUR';
 
@@ -23,27 +13,9 @@ const EXAMPLE_DID = 'did:sov:1wJPyULfLLnYTEFYzByfUR';
 export class SeedDev implements InterfaceScript {
   public constructor(private connection: Connection) { }
 
-  private readonly seedHelper = new SeedHelper(this.connection);
   private readonly seedPublish = new SeedPublish();
 
   public async run(): Promise<void> {
-    const seedInit = await new SeedInit(this.connection);
-    await seedInit.run();
-
-    // ***** CREATE COUNTRIES *****
-    const countryRepository = this.connection.getRepository(CountryEntity);
-    await countryRepository.save([{ country: 'Country A' }]);
-    await countryRepository.save([{ country: 'Country B' }]);
-
-    // ***** CREATE FINANCIAL SERVICE PROVIDERS *****
-    const financialServiceProviderRepository = this.connection.getRepository(FinancialServiceProviderEntity);
-    await financialServiceProviderRepository.save([{ fsp: 'Bank A' }]);
-    await financialServiceProviderRepository.save([{ fsp: 'Mobile Money Provider B' }]);
-
-    // ***** CREATE PROTECTION SERVICE PROVIDERS *****
-    const protectionServiceProviderRepository = this.connection.getRepository(ProtectionServiceProviderEntity);
-    await protectionServiceProviderRepository.save([{ psp: 'Protection Service Provider A' }]);
-    await protectionServiceProviderRepository.save([{ psp: 'Protection Service Provider B' }]);
 
     // ***** CREATE A CONNECTION *****
     const connectionRepository = this.connection.getRepository(
@@ -58,22 +30,6 @@ export class SeedDev implements InterfaceScript {
         programsExcluded: [],
       },
     ]);
-
-    // ***** CREATE A PROGRAM WITH CUSTOM CRITERIA *****
-    const userRepository = this.connection.getRepository(UserEntity);
-
-    const examplePrograms = [
-      programAnonymousExample,
-      programBasicExample,
-    ];
-
-    await this.seedHelper.addPrograms(examplePrograms, 1);
-
-    // ***** ASSIGN AIDWORKER TO PROGRAM *****
-
-    await this.seedHelper.assignAidworker(2, 1);
-    await this.seedHelper.assignAidworker(2, 2);
-
 
     // ***** CREATE APPOINTMENT *****
     const appointmentRepository = this.connection.getRepository(
