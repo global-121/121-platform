aa52a4a0f84555cebdb0dd32b604158de93ca61f
RubenGeo <rubenavalk@gmail.com>
2020-01-29 13:38:53 +0100

    Removed some console.logs

--- services/121-service/src/roles.guard.ts
+++ services/121-service/src/roles.guard.ts
@@ -24,28 +24,24 @@ export class RolesGuard implements CanActivate {
     console.log('roles: ', roles);
 
     if (!roles) {
-      console.log('Everyone can access')
       return true;
     }
     // This line allows the Admin role to access every controller
     roles.push(UserRole.Admin)
 
-    console.log('Roles guardd1!!!', roles);
     const request = context.switchToHttp().getRequest()
     const authHeaders = request.headers.authorization;
-    console.log('authHeaders: ', authHeaders);
-    // console.log('authHeaders: ', authHeaders);
     if (authHeaders && (authHeaders as string).split(' ')[1]) {
       const token = (authHeaders as string).split(' ')[1];
       const decoded: any = jwt.verify(token, SECRET);
       const user = await this.userService.findById(decoded.id);
-      console.log('user: ', user);
 
       hasAccess = roles.includes(user.user.role);
     } else {
       hasAccess = false;
     }
     if (hasAccess === false) {
+
       // Add this to stay consitent with the old auth middeleware which returns 401
       // If you remove this an unautherized request return 403
       throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
