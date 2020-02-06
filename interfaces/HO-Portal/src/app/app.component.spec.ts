41bc57439ae375c4c495ca7fbd98aa20a4674cb6
Elwin Schmitz <eschmitz@redcross.nl>
2020-02-06 16:37:53 +0100

    Fix tests

--- interfaces/HO-Portal/src/app/app.component.spec.ts
+++ interfaces/HO-Portal/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { of } from 'rxjs';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 
@@ -9,6 +10,10 @@ import { AuthService } from './auth/auth.service';
 
 describe('AppComponent', () => {
 
+  const authServiceMock = {
+    authenticationState$: of(null),
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
@@ -16,6 +21,7 @@ describe('AppComponent', () => {
       providers: [
         {
           provide: AuthService,
+          useValue: authServiceMock,
         },
       ],
       imports: [
@@ -31,33 +37,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have menu items', async () => {
-    const fixture = await TestBed.createComponent(AppComponent);
-    await fixture.detectChanges();
-    const app = fixture.nativeElement;
-    const menuItems = app.querySelectorAll('ion-menu ion-list ion-label');
-    expect(menuItems.length).toEqual(4);
-  });
-
-  it('should have menu labels', async () => {
-    const fixture = await TestBed.createComponent(AppComponent);
-    await fixture.detectChanges();
-    const app = fixture.nativeElement;
-    const menuItems = app.querySelectorAll('ion-menu ion-list ion-label');
-    expect(menuItems[0].textContent).toContain('page.home.pageTitle');
-    expect(menuItems[1].textContent).toContain('page.programs.program-list.pageTitle');
-    expect(menuItems[2].textContent).toContain('page.help.pageTitle');
-    expect(menuItems[3].textContent).toContain('LOGOUT');
-  });
-
-  it('should have urls', async () => {
+  it('should have 1 menu item (when logged out)', async () => {
     const fixture = await TestBed.createComponent(AppComponent);
     await fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-item');
-    expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/home');
-    expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/programs');
-    expect(menuItems[2].getAttribute('ng-reflect-router-link')).toEqual('/help');
+    const menuLabels = app.querySelectorAll('ion-menu ion-list ion-label');
+    expect(menuItems.length).toEqual(1);
+    expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/login');
+    expect(menuLabels[0].textContent).toContain('LOGIN');
   });
 
 });
