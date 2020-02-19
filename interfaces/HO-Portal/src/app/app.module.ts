b84fccd53a45968492636a93a266b37ae77feaaf
Jannis Visser <jannisvisser@redcross.nl>
2020-02-19 10:32:07 +0100

    AB#1481 solve translation-bug in 3 apps

--- interfaces/HO-Portal/src/app/app.module.ts
+++ interfaces/HO-Portal/src/app/app.module.ts
@@ -15,6 +15,27 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ProgramsModule } from './programs/programs.module';
 
+import { Injector, APP_INITIALIZER } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LOCATION_INITIALIZED } from '@angular/common';
+
+export function appInitializerFactory(translate: TranslateService, injector: Injector) {
+  return () => new Promise<any>((resolve: any) => {
+    const locationInitialized = injector.get(LOCATION_INITIALIZED, Promise.resolve(null));
+    locationInitialized.then(() => {
+      const langToSet = 'en';
+      translate.setDefaultLang('en');
+      translate.use(langToSet).subscribe(() => {
+        console.log(`Successfully initialized '${langToSet}' language.`);
+      }, (err) => {
+        console.log(`Problem with '${langToSet}' language initialization: `, err);
+      }, () => {
+        resolve(null);
+      });
+    });
+  });
+}
+
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/');
 }
@@ -45,6 +66,12 @@ export function HttpLoaderFactory(http: HttpClient) {
       provide: RouteReuseStrategy,
       useClass: IonicRouteStrategy,
     },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializerFactory,
+      deps: [TranslateService, Injector],
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
