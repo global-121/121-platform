1fcc92c8ff453706995cead3474871cda70798e5
Jannis Visser <jannisvisser@redcross.nl>
2020-02-19 16:08:12 +0100

    AB#1492 language-setup

--- interfaces/AW-App/src/app/app.component.ts
+++ interfaces/AW-App/src/app/app.component.ts
@@ -4,9 +4,6 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { TranslateService } from '@ngx-translate/core';
-// import { UpdateService } from './services/update.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -16,12 +13,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private translate: TranslateService,
-    // private updateService: UpdateService,
   ) {
     this.initializeApp();
-    this.initializeLanguages();
-    this.initializeUpdateChecker();
   }
 
   initializeApp() {
@@ -31,14 +24,5 @@ export class AppComponent {
     });
   }
 
-  initializeLanguages() {
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
-  }
 
-  initializeUpdateChecker() {
-    localStorage.setItem('did', 'did:sov:1wJPyULfLLnYTEFYzByfUR');
-    // this.updateService.checkInclusion(1);
-    // this.updateService.checkCredential(1);
-  }
 }
