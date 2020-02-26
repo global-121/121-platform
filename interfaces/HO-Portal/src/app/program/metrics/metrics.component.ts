5dbd87d8c8b5bed016e22461d4c29390cd1b68b2
Elwin Schmitz <eschmitz@redcross.nl>
2020-02-26 17:47:22 +0100

    Create Metrics-component
    Related AB#1465
    Related AB#1496

---
+++ interfaces/HO-Portal/src/app/program/metrics/metrics.component.ts
@@ -0,0 +1,111 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { formatDate, formatCurrency } from '@angular/common';
+
+import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
+
+import { Program } from 'src/app/models/program.model';
+import { ProgramMetrics, MetricRow, MetricGroup } from 'src/app/models/program-metrics.model';
+
+@Component({
+  selector: 'app-metrics',
+  templateUrl: './metrics.component.html',
+  styleUrls: ['./metrics.component.scss'],
+})
+export class MetricsComponent implements OnChanges {
+  @Input()
+  public program: Program;
+
+  private locale: string;
+  private programMetrics: ProgramMetrics;
+  public metricList: Map<string, MetricRow> = new Map();
+  public lastUpdated: string;
+
+  constructor(
+    private translate: TranslateService,
+    private programService: ProgramsServiceApiService,
+  ) {
+    this.locale = this.translate.getBrowserCultureLang();
+  }
+
+  async ngOnChanges(changes: SimpleChanges) {
+    if (changes.program && typeof changes.program.currentValue === 'object') {
+      this.update();
+    }
+  }
+
+  public async update() {
+    this.programMetrics = await this.programService.getMetricsById(this.program.id);
+
+    this.render();
+  }
+
+  private render() {
+    this.renderUpdated();
+
+    this.renderPaMetrics();
+    this.renderFundsMetrics();
+  }
+
+  private renderUpdated() {
+    this.lastUpdated = formatDate(this.programMetrics.updated, 'full', this.locale);
+  }
+
+  private renderPaMetrics() {
+    const metrics = this.programMetrics.pa;
+    const group = MetricGroup.pa;
+
+    this.metricList.set('pendingVerification', {
+      group,
+      icon: 'people',
+      label: 'page.program.metrics.pa.pending-verification',
+      value: metrics.pendingVerification,
+    });
+    this.metricList.set('verifiedAwaitingDecision', {
+      group,
+      icon: 'person',
+      label: 'page.program.metrics.pa.verified-awaiting-decision',
+      value: metrics.verifiedAwaitingDecision,
+    });
+    this.metricList.set('included', {
+      group,
+      icon: 'person-add',
+      label: 'page.program.metrics.pa.included',
+      value: metrics.included,
+    });
+    this.metricList.set('excluded', {
+      group,
+      icon: 'person',
+      label: 'page.program.metrics.pa.excluded',
+      value: metrics.excluded,
+    });
+  }
+
+  private renderFundsMetrics() {
+    const metrics = this.programMetrics.funding;
+    const group = MetricGroup.funds;
+    const currencyCode = this.program.currency;
+    const symbol = `${currencyCode} `;
+    const locale = this.locale;
+
+    this.metricList.set('totalRaised', {
+      group,
+      icon: 'cash',
+      label: 'page.program.metrics.funds.raised',
+      value: formatCurrency(metrics.totalRaised, locale, symbol, currencyCode),
+    });
+    this.metricList.set('totalTransferred', {
+      group,
+      icon: 'cash',
+      label: 'page.program.metrics.funds.transferred',
+      value: formatCurrency(metrics.totalTransferred, locale, symbol, currencyCode),
+    });
+    this.metricList.set('totalAvailable', {
+      group,
+      icon: 'cash',
+      label: 'page.program.metrics.funds.available',
+      value: formatCurrency(metrics.totalAvailable, locale, symbol, currencyCode),
+    });
+
+  }
+}
