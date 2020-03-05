8aa0bfe6b29a209c30ee6cf73469c740873ca141
Elwin Schmitz <eschmitz@redcross.nl>
2020-03-05 13:33:19 +0100

    Cleanup use of magic strings + update imgPath-use

--- interfaces/PA-App/src/app/shared/dialogue-turn/dialogue-turn.component.ts
+++ interfaces/PA-App/src/app/shared/dialogue-turn/dialogue-turn.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
+enum Actor {
+  system = 'system',
+  self = 'self',
+}
+
 @Component({
   selector: 'dialogue-turn',
   templateUrl: './dialogue-turn.component.html',
   styleUrls: ['./dialogue-turn.component.scss'],
 })
 export class DialogueTurnComponent implements OnInit {
-  public imgPath: string;
-
   @Input()
   isSpoken = false;
 
   @Input()
-  actor = 'system';
+  actor = Actor.system;
 
   @Input()
   moment: Date;
@@ -21,15 +24,17 @@ export class DialogueTurnComponent implements OnInit {
   @Input()
   isConnected = false;
 
-  isSelf = false;
+  isSelf: boolean;
+  isSystem: boolean;
+
   animate = environment.useAnimation;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.imgPath = '../../..' + environment.subDirPath + '/assets/avatar--121.svg';
-    this.isSelf = (this.actor === 'self');
+    this.isSelf = (this.actor === Actor.self);
+    this.isSystem = (this.actor === Actor.system);
     this.moment = new Date();
   }
 
