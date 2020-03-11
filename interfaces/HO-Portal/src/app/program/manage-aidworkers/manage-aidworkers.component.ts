aa8a708df640450689afb1e41ed0459efb19d896
Jannis Visser <jannisvisser@redcross.nl>
2020-03-11 10:48:40 +0100

    AB#1625 create list of assigned aidworkers

--- interfaces/HO-Portal/src/app/program/manage-aidworkers/manage-aidworkers.component.ts
+++ interfaces/HO-Portal/src/app/program/manage-aidworkers/manage-aidworkers.component.ts
@@ -1,10 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { ProgramPhase } from 'src/app/models/program.model';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-manage-aidworkers',
   templateUrl: './manage-aidworkers.component.html',
   styleUrls: ['./manage-aidworkers.component.scss'],
 })
+export class ManageAidworkersComponent implements OnChanges {
+  @Input()
+  public selectedPhase: string;
+  @Input()
+  public aidworkers: any[];
+
   public componentVisible: boolean;
   private presentInPhases = [
     ProgramPhase.design,
@@ -15,14 +25,58 @@ import { Component, OnInit } from '@angular/core';
     ProgramPhase.evaluation,
   ];
 
+  public columns = [
+    {
+      prop: 'email',
+      name: this.translate.instant('page.program.manage-aidworkers.column-email'),
+      draggable: false,
+      resizeable: false,
+      hidePhases: []
+    },
+    {
+      prop: 'created',
+      name: this.translate.instant('page.program.manage-aidworkers.column-created'),
+      draggable: false,
+      resizeable: false,
+      hidePhases: []
+    }
+  ]
+
+  public tableMessages: any;
+  private locale: string;
+  private dateFormat = 'yyyy-MM-dd, hh:mm';
+
+  constructor(
+    public translate: TranslateService
+  ) {
+    this.locale = this.translate.getBrowserCultureLang();
+    this.tableMessages = {
+      emptyMessage: this.translate.instant('common.table.no-data'),
+      totalMessage: this.translate.instant('common.table.total'),
+      selectedMessage: this.translate.instant('common.table.selected'),
+    };
+  }
+
+  ngOnInit() { }
+
   async ngOnChanges(changes: SimpleChanges) {
     if (changes.selectedPhase && typeof changes.selectedPhase.currentValue === 'string') {
       this.checkVisibility(this.selectedPhase);
     }
+    if (changes.aidworkers && typeof changes.aidworkers.currentValue === 'object' && changes.aidworkers.currentValue) {
+      this.loadData();
+    }
+  }
+
   public checkVisibility(phase) {
     this.componentVisible = this.presentInPhases.includes(phase);
   }
 
-  ngOnInit() {}
+  public loadData() {
+    this.aidworkers.forEach((aidworker) => {
+      aidworker.email = aidworker.email;
+      aidworker.created = formatDate(aidworker.created, this.dateFormat, this.locale);
+    });
+  }
 
 }
