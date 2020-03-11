73343694c0dfc2f7eaad553dc0e6621a4ac5e54e
Jannis Visser <jannisvisser@redcross.nl>
2020-03-11 11:03:35 +0100

    fix unit test

--- interfaces/HO-Portal/src/app/program/manage-aidworkers/manage-aidworkers.component.spec.ts
+++ interfaces/HO-Portal/src/app/program/manage-aidworkers/manage-aidworkers.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ManageAidworkersComponent } from './manage-aidworkers.component';
 
@@ -9,10 +10,13 @@ describe('ManageAidworkersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ManageAidworkersComponent ],
+      declarations: [ManageAidworkersComponent],
+      imports: [
+        TranslateModule.forRoot(),
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
