15fc66f544939292cbe3ee5c3801c6939b44244c
Elwin Schmitz <eschmitz@redcross.nl>
2020-04-29 15:47:00 +0200

    AB#1800 Add QR-Scanner component

---
+++ interfaces/PA-App/src/app/shared/qr-scanner/qr-scanner.component.spec.ts
@@ -0,0 +1,37 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TranslateModule } from '@ngx-translate/core';
+import { ModalController } from '@ionic/angular';
+
+import { QrScannerComponent } from './qr-scanner.component';
+describe('QrScannerComponent', () => {
+  let component: QrScannerComponent;
+  let fixture: ComponentFixture<QrScannerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QrScannerComponent ],
+      imports: [
+        TranslateModule.forRoot(),
+      ],
+      providers: [
+        {
+          provide: ModalController,
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QrScannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
