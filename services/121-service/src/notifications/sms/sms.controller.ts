e1a0d2527917f381d349eebfd9cc48d646219c6f
RubenGeo <rubenavalk@gmail.com>
2020-05-06 11:27:19 +0200

    AB#1840 Added test endpoints for notifications  with admin auth

--- services/121-service/src/notifications/sms/sms.controller.ts
+++ services/121-service/src/notifications/sms/sms.controller.ts
@@ -1,6 +1,8 @@
 import { SmsService } from './sms.service';
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ApiResponse, ApiUseTags, ApiImplicitParam } from '@nestjs/swagger';
+import { UserRole } from '../../user-role.enum';
+import { Roles } from '../../roles.decorator';
 
 @ApiUseTags('notifications')
 @Controller('notifications/sms')
@@ -8,6 +10,22 @@ export class SmsController {
   private readonly smsService: SmsService;
   public constructor(smsService: SmsService) {
     this.smsService = smsService;
+  }
+
+  @Roles(UserRole.Admin)
+  @ApiResponse({
+    status: 200,
+    description: 'Test controller to test sending sms',
+  })
+  @ApiImplicitParam({ name: 'number' })
+  @Get(':number')
+  public async sendSms(@Param() params): Promise<void> {
+    return await this.smsService.notifyBySms(
+      params.number,
+      'en',
+      'included',
+      1,
+    );
   }
 
   @Post('status')
