a3656de347f6a4038fb45c20ef97c8c67e96c266
RubenGeo <rubenavalk@gmail.com>
2020-05-12 14:32:41 +0200

    Changed seed dev to be editable

--- services/121-service/src/scripts/seed-dev.ts
+++ services/121-service/src/scripts/seed-dev.ts
@@ -14,54 +14,8 @@ const EXAMPLE_DID = 'did:sov:1wJPyULfLLnYTEFYzByfUR';
 export class SeedDev implements InterfaceScript {
   public constructor(private connection: Connection) {}
 
-  private readonly seedPublish = new SeedPublish();
-
   public async run(): Promise<void> {
-    // ***** CREATE A CONNECTION *****
-    const connectionRepository = this.connection.getRepository(
-      ConnectionEntity,
-    );
-    await connectionRepository.save([
-      {
-        did: EXAMPLE_DID,
-        phoneNumber: '0031600000000',
-        programsEnrolled: [1],
-        programsIncluded: [1],
-        programsExcluded: [],
-      },
-    ]);
-
-    // ***** CREATE APPOINTMENT *****
-    const appointmentRepository = this.connection.getRepository(
-      AppointmentEntity,
-    );
-
-    const appointment = new AppointmentEntity();
-    appointment.timeslotId = 1;
-    appointment.did = EXAMPLE_DID;
-    appointment.status = 'waiting';
-    await appointmentRepository.save(appointment);
-
-    // ***** CREATE PREFILLED ANSWERS *****
-    const credentialAttributesRepository = this.connection.getRepository(
-      CredentialAttributesEntity,
-    );
-
-    let credential1 = new CredentialAttributesEntity();
-    credential1.did = EXAMPLE_DID;
-    credential1.programId = 1;
-    credential1.attributeId = 1;
-    credential1.attribute = 'nr_of_children';
-    credential1.answer = '2';
-    await credentialAttributesRepository.save(credential1);
-
-    let credential2 = new CredentialAttributesEntity();
-    credential2.did = EXAMPLE_DID;
-    credential2.programId = 1;
-    credential2.attributeId = 2;
-    credential2.attribute = 'roof_type';
-    credential2.answer = '0';
-    await credentialAttributesRepository.save(credential2);
+    // *****  ADD DATA YOU WANT TO SEED*****
 
     await this.connection.close();
   }
