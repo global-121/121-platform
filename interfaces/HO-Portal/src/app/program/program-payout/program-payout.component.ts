a20a3bbbbd7918e07e17b40623f9484dbcf97f41
RubenGeo <rubenavalk@gmail.com>
2020-06-17 14:43:40 +0200

    Fixed prettier

--- interfaces/HO-Portal/src/app/program/program-payout/program-payout.component.ts
+++ interfaces/HO-Portal/src/app/program/program-payout/program-payout.component.ts
@@ -210,9 +210,7 @@ export class ProgramPayoutComponent implements OnChanges {
         (err) => {
           console.log('err: ', err);
           if (err.error.errors) {
-            this.actionResult(
-              err.error.errors
-            );
+            this.actionResult(err.error.errors);
           }
           this.cancelPayout(installment);
         },
