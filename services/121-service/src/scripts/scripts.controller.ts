418b491d206ada0086ce848bf97a72599f25ddb0
RubenGeo <rubenavalk@gmail.com>
2020-06-17 17:08:20 +0200

    Added demo program AB#1532

--- services/121-service/src/scripts/scripts.controller.ts
+++ services/121-service/src/scripts/scripts.controller.ts
@@ -1,14 +1,18 @@
+import { SeedMultiProgram } from './seed-multi-program';
 import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { ApiOperation, ApiModelProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { Connection } from 'typeorm';
 import { SeedSingleProgram } from './seed-single-program';
+import SeedDemoProgram from './seed-demo-program';
 
 class ResetDto {
   @ApiModelProperty({ example: 'fill_in_secret' })
   @IsNotEmpty()
   @IsString()
   public readonly secret: string;
+  @ApiModelProperty({ example: 'single, multi or demo' })
+  public readonly script: string;
 }
 
 @Controller('scripts')
@@ -17,12 +21,19 @@ export class ScriptsController {
 
   @ApiOperation({ title: 'Reset database' })
   @Post('/reset')
-  public async resetDb(@Body() secret: ResetDto, @Res() res): Promise<string> {
-    if (secret.secret !== process.env.RESET_SECRET) {
+  public async resetDb(@Body() body: ResetDto, @Res() res): Promise<string> {
+    if (body.secret !== process.env.RESET_SECRET) {
       return res.status(HttpStatus.FORBIDDEN).send('Not allowed');
     }
-    const seedSingleProgram = new SeedSingleProgram(this.connection);
-    await seedSingleProgram.run();
+    let seed;
+    if (body.script == 'demo') {
+      seed = new SeedDemoProgram(this.connection);
+    } else if (body.script == 'multi') {
+      seed = new SeedMultiProgram(this.connection);
+    } else {
+      seed = new SeedSingleProgram(this.connection);
+    }
+    await seed.run();
     return res
       .status(HttpStatus.ACCEPTED)
       .send('Request received. The reset can take a minute.');
