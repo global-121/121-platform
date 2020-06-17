c112b6ae491d1f172cd828359523e7f13f04dd58
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-17 16:21:06 +0200

    Add AidWorkersPage

---
+++ interfaces/HO-Portal/src/app/pages/aid-workers/aid-workers.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AidWorkersPage } from './aid-workers.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AidWorkersPage,
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SharedModule,
+    RouterModule.forChild(routes),
+  ],
+  declarations: [AidWorkersPage],
+})
+export class AidWorkersPageModule {}
---
+++ interfaces/HO-Portal/src/app/pages/aid-workers/aid-workers.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aid-workers',
+  templateUrl: './aid-workers.page.html',
+  styleUrls: ['./aid-workers.page.scss'],
+})
+export class AidWorkersPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
