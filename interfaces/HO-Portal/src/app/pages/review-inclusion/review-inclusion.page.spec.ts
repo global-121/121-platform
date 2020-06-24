a26b28e39e39ffd901bc37b3ef93c0b93c1ba473
Elwin Schmitz <eschmitz@redcross.nl>
2020-06-24 16:32:05 +0200

    Fix test

--- interfaces/HO-Portal/src/app/pages/review-inclusion/review-inclusion.page.spec.ts
+++ interfaces/HO-Portal/src/app/pages/review-inclusion/review-inclusion.page.spec.ts
@@ -3,16 +3,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { ReviewInclusionPage } from './review-inclusion.page';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UserRole } from 'src/app/auth/user-role.enum';
+import { AuthService } from 'src/app/auth/auth.service';
 
 describe('ReviewInclusionPage', () => {
   let component: ReviewInclusionPage;
   let fixture: ComponentFixture<ReviewInclusionPage>;
+
+  const mockUserRole = UserRole.ProjectOfficer;
+  const mockAuthService = jasmine.createSpyObj('AuthService', ['getUserRole']);
+  mockAuthService.getUserRole.and.returnValue(mockUserRole);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ReviewInclusionPage],
       imports: [TranslateModule.forRoot(), RouterTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compileComponents();
   }));
 
