a9f9db9549f0cf69f7b1e9039a939a34e25a8968
Jannis Visser <jannisvisser@redcross.nl>
2020-07-22 14:56:38 +0200

    AB#2677 unit test

--- interfaces/HO-Portal/src/app/program/notification/notification.component.spec.ts
+++ interfaces/HO-Portal/src/app/program/notification/notification.component.spec.ts
@@ -1,14 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotificationComponent } from './notification.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserRole } from 'src/app/auth/user-role.enum';
 
 describe('NotificationComponent', () => {
   let component: NotificationComponent;
   let fixture: ComponentFixture<NotificationComponent>;
+  
+  const mockProgramId = 1;
+  const mockUserRole = UserRole.ProjectOfficer;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [NotificationComponent],
+      imports: [TranslateModule.forRoot(), HttpClientTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
