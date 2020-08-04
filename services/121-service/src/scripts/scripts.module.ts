f7bbf56a7f1ae2d6bd5d4567e7bec55b07cbfca0
Jannis Visser <jannisvisser@redcross.nl>
2020-08-04 16:09:27 +0200

    AB#2826 hotfix: initial seed Kenya

--- services/121-service/src/scripts/scripts.module.ts
+++ services/121-service/src/scripts/scripts.module.ts
@@ -10,6 +10,7 @@ import { SeedHelper } from './seed-helper';
 import { SeedMultiProgram } from './seed-program-multi';
 import { SeedSingleProgram } from './seed-program-single';
 import { SeedPilotNLProgram } from './seed-program-pilot-nl';
+import { SeedPilotKenProgram } from './seed-program-pilot-ken';
 import { SeedDemoProgram } from './seed-program-demo';
 import { SeedPublish } from './seed-publish';
 
@@ -29,6 +30,7 @@ import { SeedPublish } from './seed-publish';
     SeedSingleProgram,
     SeedMultiProgram,
     SeedPilotNLProgram,
+    SeedPilotKenProgram,
     SeedDemoProgram,
   ],
   controllers: [ScriptsController],
