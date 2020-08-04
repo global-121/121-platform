8386b534bfdb7d136499e5d8f1ec96cb01bb7e83
Jannis Visser <jannisvisser@redcross.nl>
2020-08-04 10:48:55 +0200

    AB#2802 AW load right 1st validation-component

--- interfaces/AW-App/src/app/services/conversation.service.ts
+++ interfaces/AW-App/src/app/services/conversation.service.ts
@@ -58,7 +58,7 @@ export class ConversationService {
   }
 
   startNewConversation() {
-    this.addSection(ValidationComponents.validateProgram);
+    this.addSection(ValidationComponents.mainMenu);
   }
 
   private addSection(sectionName) {
