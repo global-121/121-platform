032d3aca4a698c07a162057c6f54769a46e49351
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-08-18 12:16:51 +0200

    AB#2938 feat: help page

--- interfaces/Referral-App/src/app/help/help.module.ts
+++ interfaces/Referral-App/src/app/help/help.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { HelpPage } from './help.page';
 
 const routes: Routes = [
@@ -18,6 +19,7 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
+    SharedModule,
   ],
   declarations: [HelpPage],
 })
