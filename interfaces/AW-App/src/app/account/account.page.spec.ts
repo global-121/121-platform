08bcb67caca48d0e98db18041bedbf502f15f2f4
Omendra Manhar <omendra.manhar@gmail.com>
2020-08-19 17:15:02 +0200

    AB#2926 Unit Test Samples

--- interfaces/AW-App/src/app/account/account.page.spec.ts
+++ interfaces/AW-App/src/app/account/account.page.spec.ts
@@ -8,6 +8,7 @@ import { AccountPage } from './account.page';
 describe('AccountPage', () => {
   let component: AccountPage;
   let fixture: ComponentFixture<AccountPage>;
+  let event;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,6 +20,14 @@ describe('AccountPage', () => {
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
+
+    event = {
+      preventDefault: function () {},
+      target: { elements: {
+        create: { value: ""},
+        confirm: { value: ""}
+      }}
+    }
   }));
 
   beforeEach(() => {
@@ -30,4 +39,47 @@ describe('AccountPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('ngOnInit: should set up variables', () => {
+    expect(component.isLoggedIn).toBeDefined();
+  });
+
+  it('doLogin: should call login of authService', () => {
+    expect(component.isLoggedIn).toBeDefined();
+    spyOn(event, "preventDefault");
+    component.doLogin(event);
+    expect(event.preventDefault).toHaveBeenCalled();
+  });
+
+  it('logout: should create toastController', () => {
+    spyOn(component, "createToast");
+    component.logout();
+    expect(component.createToast).toHaveBeenCalled();
+  });
+
+  // async methods
+  it('doChangePassword: should call changePassword if create == confirm', (done) => {
+    spyOn(component, "createToast");
+    let spy = spyOn(component.programsService, "changePassword").and.returnValue(Promise.resolve(true));
+
+    component.doChangePassword(event);
+
+    spy.calls.mostRecent().returnValue.then(() => {
+        expect(component.createToast).toHaveBeenCalledWith(component.changedPassword);
+        expect(component.createToast).toHaveBeenCalled();
+        done();
+    });
+  });
+
+
+  it('doChangePassword: should not call changePassword if create != confirm', () => {
+    spyOn(component, "createToast");
+    spyOn(component.programsService, "changePassword");
+    event.target.elements.create = "none";
+
+    component.doChangePassword(event);
+    expect(component.programsService.changePassword).not.toHaveBeenCalled();
+    expect(component.createToast).toHaveBeenCalled();
+  });
+
 });
