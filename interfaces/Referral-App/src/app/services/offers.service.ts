8a5edde585d57ad1b43f0d80dd24f454201889a4
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2020-09-01 14:28:34 +0200

    AB#3165 feat: use spreadsheet as data source

--- interfaces/Referral-App/src/app/services/offers.service.ts
+++ interfaces/Referral-App/src/app/services/offers.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import apiCategoriesMock from 'src/app/mocks/api.categories.mock';
-import apiOffersMock from 'src/app/mocks/api.offers.mock';
-import apiSubCategoriesMock from 'src/app/mocks/api.sub-categories.mock';
 import { Category } from 'src/app/models/category.model';
 import { Offer } from 'src/app/models/offer.model';
 import { SubCategory } from 'src/app/models/sub-category.model';
@@ -10,17 +7,87 @@ import { SubCategory } from 'src/app/models/sub-category.model';
   providedIn: 'root',
 })
 export class OffersService {
+  private spreadsheetURL = 'https://sheets.googleapis.com/v4/spreadsheets';
+  private spreadsheetId = '1OANMuGJxGBQ2ba3xVfKvTpD0EGQM-ukUrgRyN9tzMbQ';
+  private spreadsheetKey = 'AIzaSyCJW0xxT4kgdUIzVGBVu3X39QxYrYBLzdY';
+  private categorySheetName = 'Categories';
+  private subCategorySheetName = 'Sub-Categories';
+  private offerSheetName = 'Offers';
+  private spreadsheetRange = 'A2:H';
+
   constructor() {}
 
+  convertCategoryRowToCategoryObject(categoryRow): Category {
+    return {
+      categoryID: parseInt(categoryRow[0]),
+      categoryName: {
+        en: categoryRow[1],
+      },
+      categoryIcon: categoryRow[2],
+      categoryDescription: {
+        en: categoryRow[3],
+      },
+    };
+  }
+
   getCategories(): Promise<Category[]> {
-    return Promise.resolve(apiCategoriesMock);
+    return fetch(
+      `${this.spreadsheetURL}/${this.spreadsheetId}/values/${this.categorySheetName}!${this.spreadsheetRange}?key=${this.spreadsheetKey}`,
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return response.values.map(this.convertCategoryRowToCategoryObject);
+      });
+  }
+
+  convertCategoryRowToSubCategoryObject(subCategoryRow): SubCategory {
+    return {
+      subCategoryID: parseInt(subCategoryRow[0]),
+      subCategoryName: {
+        en: subCategoryRow[1],
+      },
+      subCategoryIcon: subCategoryRow[2],
+      subCategoryDescription: {
+        en: subCategoryRow[3],
+      },
+      categoryID: parseInt(subCategoryRow[4]),
+    };
   }
 
   getSubCategories(): Promise<SubCategory[]> {
-    return Promise.resolve(apiSubCategoriesMock);
+    return fetch(
+      `${this.spreadsheetURL}/${this.spreadsheetId}/values/${this.subCategorySheetName}!${this.spreadsheetRange}?key=${this.spreadsheetKey}`,
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return response.values.map(this.convertCategoryRowToSubCategoryObject);
+      });
+  }
+
+  convertCategoryRowToOfferObject(offerRow): Offer {
+    return {
+      offerID: parseInt(offerRow[0]),
+      offerName: {
+        en: offerRow[1],
+      },
+      offerIcon: offerRow[2],
+      offerDescription: {
+        en: offerRow[3],
+      },
+      offerLink: offerRow[4],
+      offerImage: offerRow[5],
+      subCategoryID: parseInt(offerRow[6]),
+      categoryID: parseInt(offerRow[7]),
+    };
   }
 
   getOffers(): Promise<Offer[]> {
-    return Promise.resolve(apiOffersMock);
+    return fetch(
+      `${this.spreadsheetURL}/${this.spreadsheetId}/values/${this.offerSheetName}!${this.spreadsheetRange}?key=${this.spreadsheetKey}`,
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return response.values.map(this.convertCategoryRowToOfferObject);
+      });
   }
 }
