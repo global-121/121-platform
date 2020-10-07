00463a8b284fd0a05c63b6ef06198d1c34c297fc
Omendra Manhar <omendra.manhar@gmail.com>
2020-10-07 14:59:17 +0200

    AB#3728

--- interfaces/HO-Portal/src/app/app.component.spec.ts
+++ interfaces/HO-Portal/src/app/app.component.spec.ts
@@ -3,11 +3,13 @@ import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
+import { LoggingService } from './services/logging.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [LoggingService],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [TranslateModule.forRoot(), RouterTestingModule],
     }).compileComponents();
--- interfaces/HO-Portal/src/app/app.component.ts
+++ interfaces/HO-Portal/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { LoggingService } from './services/logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private loggingService: LoggingService) {
+    if (this.loggingService.appInsightsEnabled) {
+      this.loggingService.logPageView();
+    }
+  }
 }
