8db9fe0b1049604c56b6e2cc08639aa19290c453
Jannis Visser <jannisvisser@redcross.nl>
2020-12-02 11:53:49 +0100

    AB#5007 call payout-endpoint for 1 PA

--- services/121-service/src/programs/program/dto/payout.dto.ts
+++ services/121-service/src/programs/program/dto/payout.dto.ts
@@ -1,6 +1,6 @@
 import { ApiModelProperty } from '@nestjs/swagger';
 
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional, IsString, Length } from 'class-validator';
 
 export class PayoutDto {
   @ApiModelProperty({ example: 1 })
@@ -12,4 +12,9 @@ export class PayoutDto {
   @ApiModelProperty({ example: 10 })
   @IsNumber()
   public readonly amount: number;
+  @ApiModelProperty({ example: 'did:sov:2wJPyULfLLnYTEFYzByfUR' })
+  @Length(29, 30)
+  @IsString()
+  @IsOptional()
+  public readonly did: string;
 }
