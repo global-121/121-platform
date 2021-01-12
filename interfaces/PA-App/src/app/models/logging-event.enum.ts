98ef4093b27ca404ffecdb74ae84d6030a07b5d4
Elwin Schmitz <eschmitz@redcross.nl>
2021-01-13 00:01:29 +0100

    Add logs for language selection

--- interfaces/PA-App/src/app/models/logging-event.enum.ts
+++ interfaces/PA-App/src/app/models/logging-event.enum.ts
@@ -1,10 +1,12 @@
 export enum LoggingEventCategory {
   progress = 'progress',
+  input = 'input',
   error = 'error',
 }
 
 export enum LoggingEvent {
   error = 'error',
   exception = 'exception',
+  languageChosen = 'language-chosen',
   sectionCompleted = 'section-completed',
 }
