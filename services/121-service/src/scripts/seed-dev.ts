d975a1de60ee18f3336b6a993f18032ffd48bcc5
Elwin Schmitz <eschmitz@redcross.nl>
2021-03-08 12:00:32 +0100

    AB#6502 Add manual 'migration'-step in seed-script

--- services/121-service/src/scripts/seed-dev.ts
+++ services/121-service/src/scripts/seed-dev.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { InterfaceScript } from './scripts.module';
+import { UserRoleEntity } from '../user/user-role.entity';
+import { UserRole } from '../user-role.enum';
 
 @Injectable()
 export class SeedDev implements InterfaceScript {
@@ -8,6 +10,13 @@ export class SeedDev implements InterfaceScript {
 
   public async run(): Promise<void> {
     // *****  ADD DATA YOU WANT TO SEED*****
+    const userRoleRepository = this.connection.getRepository(UserRoleEntity);
+    await userRoleRepository.save([
+      {
+        role: UserRole.View,
+        label: 'Only view data, including Personally Identifiable Information',
+      },
+    ]);
 
     await this.connection.close();
   }
