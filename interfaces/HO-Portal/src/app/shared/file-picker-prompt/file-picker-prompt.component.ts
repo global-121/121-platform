242044ec41fcfb3ab26ade1bde6cfc252a549250
jannisvisser <jannisvisser@redcross.nl>
2021-03-10 14:19:36 +0100

    fix: rm tried content AB#6462

--- interfaces/HO-Portal/src/app/shared/file-picker-prompt/file-picker-prompt.component.ts
+++ interfaces/HO-Portal/src/app/shared/file-picker-prompt/file-picker-prompt.component.ts
@@ -5,7 +5,6 @@ import {
   Input,
 } from '@angular/core';
 import { NgModel } from '@angular/forms';
-import { FileChooser } from '@ionic-native/file-chooser/ngx';
 import { ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -29,50 +28,16 @@ export class FilePickerPromptComponent implements AfterViewInit {
   public filePickerProps: FilePickerProps;
   public filePickerModel: NgModel;
 
-  // public readMode = ReadMode.dataURL;
-  // public picked: ReadFile | null = null;
-  // public status: string | null = null;
-
-  // @ViewChild('myFilePicker')
-  // private ngxFilePicker: FilePickerDirective;
-
   constructor(
     public translate: TranslateService,
     private modalController: ModalController,
     private changeDetector: ChangeDetectorRef,
-    private fileChooser: FileChooser,
   ) {}
 
   ngAfterViewInit() {
     // Required to settle the value of a dynamic property in the template:
     this.changeDetector.detectChanges();
   }
-
-  open() {
-    this.fileChooser
-      .open()
-      .then((uri) => console.log(uri))
-      .catch((e) => console.log(e));
-  }
-
-  // ignoreTooBigFile(file: File): boolean {
-  //   return file.size < 100000;
-  // }
-
-  // onReadStart(fileCount: number) {
-  //   this.status = `Reading ${fileCount} file(s)...`;
-  // }
-
-  // onFilePicked(file: ReadFile) {
-  //   this.picked = file;
-  // }
-
-  // onReadEnd(fileCount: number) {
-  //   this.status = `Read ${fileCount} file(s) on ${new Date().toLocaleTimeString()}.`;
-  //   if (this.filePicker !== null) {
-  //     this.filePicker.reset();
-  //   }
-  // }
 
   public checkOkDisabled() {
     if (!this.filePickerProps) {
