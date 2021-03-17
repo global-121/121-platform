28cf84b9243ad3abcdbaff38ca1476665f20ba40
jannisvisser <jannisvisser@redcross.nl>
2021-03-17 15:01:28 +0100

    clean up seed-dev script AB#6598

--- services/121-service/src/scripts/seed-dev.ts
+++ services/121-service/src/scripts/seed-dev.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { InterfaceScript } from './scripts.module';
-import { UserRoleEntity } from '../user/user-role.entity';
-import { UserRole } from '../user-role.enum';
 
 @Injectable()
 export class SeedDev implements InterfaceScript {
@@ -10,13 +8,6 @@ export class SeedDev implements InterfaceScript {
 
   public async run(): Promise<void> {
     // *****  ADD DATA YOU WANT TO SEED*****
-    const userRoleRepository = this.connection.getRepository(UserRoleEntity);
-    await userRoleRepository.save([
-      {
-        role: UserRole.View,
-        label: 'Only view data, including Personally Identifiable Information',
-      },
-    ]);
 
     await this.connection.close();
   }
