c024816a215cf90b3d1d72f28df0272a05ed3a72
jannisvisser <jannisvisser@redcross.nl>
2021-04-22 15:58:09 +0200

    refactor: remove left over sovrin terms AB#7230

--- interfaces/PA-App/src/app/models/pa-statuses.enum.ts
+++ interfaces/PA-App/src/app/models/pa-statuses.enum.ts
@@ -1,9 +1,3 @@
-export enum PaCredentialStatus {
-  done = 'done',
-  pending = 'pending',
-  noValidation = 'no-validation',
-}
-
 export enum PaInclusionStates {
   included = 'included',
   rejected = 'rejected',
