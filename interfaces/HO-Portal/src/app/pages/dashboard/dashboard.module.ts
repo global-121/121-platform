1f054fa86be366f98a2e8cc49b9767f4d5f8e456
Elwin Schmitz <eschmitz@redcross.nl>
2021-07-13 15:29:20 +0200

    AB#8517 Add metrics-component for (more) PA-states

--- interfaces/HO-Portal/src/app/pages/dashboard/dashboard.module.ts
+++ interfaces/HO-Portal/src/app/pages/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { MetricsStatesComponent } from 'src/app/program/metrics-states/metrics-states.component';
 import { MetricsComponent } from 'src/app/program/metrics/metrics.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardPage } from './dashboard.page';
@@ -22,6 +23,6 @@ const routes: Routes = [
     SharedModule,
     RouterModule.forChild(routes),
   ],
-  declarations: [DashboardPage, MetricsComponent],
+  declarations: [DashboardPage, MetricsComponent, MetricsStatesComponent],
 })
 export class DashboardPageModule {}
--- services/121-service/src/programs/program/dto/pa-metrics.dto.ts
+++ services/121-service/src/programs/program/dto/pa-metrics.dto.ts
@@ -1,8 +1,13 @@
 import { PaStatus } from '../../../models/pa-status.model';
 
 export interface PaMetrics {
+  [PaStatus.imported]?: number;
+  [PaStatus.invited]?: number;
+  [PaStatus.noLongerEligible]?: number;
   [PaStatus.created]: number;
   [PaStatus.registered]: number;
+  [PaStatus.registeredWhileNoLongerEligible]?: number;
+  [PaStatus.selectedForValidation]?: number;
   [PaStatus.validated]: number;
   [PaStatus.included]: number;
   [PaStatus.inclusionEnded]: number;
