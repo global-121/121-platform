4a7d667f8e863f7853827d07a33773a0ef85b417
jannisvisser <jannisvisser@redcross.nl>
2021-08-31 17:08:40 +0200

    migration: first draft of all tables ready

--- services/121-service/src/scripts/migrate-registrations-refactor.ts
+++ services/121-service/src/scripts/migrate-registrations-refactor.ts
@@ -8,14 +8,7 @@ import { ProgramEntity } from './../programs/program.entity';
 import { InstanceEntity } from './../instance/instance.entity';
 import { FinancialServiceProviderEntity } from './../fsp/financial-service-provider.entity';
 import { Injectable } from '@nestjs/common';
-import {
-  Connection,
-  createConnection,
-  createConnections,
-  getConnection,
-  getRepository,
-  Repository,
-} from 'typeorm';
+import { Connection, createConnection, Repository } from 'typeorm';
 import { ColumnMetadata } from 'typeorm/metadata/ColumnMetadata';
 import { ProgramAidworkerAssignmentEntity } from '../programs/program-aidworker.entity';
 import { UserRole } from '../user-role.enum';
@@ -24,7 +17,17 @@ import { UserType } from '../user/user-type-enum';
 import { UserEntity } from '../user/user.entity';
 import { InterfaceScript } from './scripts.module';
 import { FspAttributeEntity } from '../fsp/fsp-attribute.entity';
-import { FspCallLogEntity } from '../fsp/fsp-call-log.entity';
+import { PersonAffectedAppDataEntity } from '../people-affected/person-affected-app-data.entity';
+import { RegistrationEntity } from '../registration/registration.entity';
+import {
+  RegistrationStatusEnum,
+  RegistrationStatusTimestampField,
+} from '../registration/enum/registration-status.enum';
+import { RegistrationStatusChangeEntity } from '../registration/registration-status-change.entity';
+import { ProgramAnswerEntity } from '../registration/program-answer.entity';
+import { ImageCodeExportVouchersEntity } from '../notifications/imagecode/image-code-export-vouchers.entity';
+import { IntersolveBarcodeEntity } from '../fsp/intersolve-barcode.entity';
+import { TransactionEntity } from '../programs/transactions.entity';
 
 // docker exec -it 121-service npx ts-node src/scripts migrate-registrations-refactor
 
@@ -33,6 +36,13 @@ export class MigrateRefactor implements InterfaceScript {
   public constructor(private connection: Connection) {}
   private oldConnection;
   private programId = 1;
+  private userRepository;
+  private programRepository;
+  private fspRepository;
+  private registrationRepository;
+  private registrationStatusChangeRepository;
+  private programQuestionRepository;
+  private intersolveBarcodeRepository;
 
   public async run(): Promise<void> {
     // await this.connection.query(`
@@ -49,12 +59,31 @@ export class MigrateRefactor implements InterfaceScript {
       name: 'oldConnection',
       type: 'postgres',
       host: '121db',
-      port: 5432,
+      port: 5438,
       username: process.env.ORMCONFIG_121_SERVICE_USERNAME,
       password: process.env.ORMCONFIG_121_SERVICE_PASSWORD,
-      database: 'global121-old',
+
+      database: 'global121',
       schema: '121-service',
     });
+    this.userRepository = this.connection.getRepository(UserEntity);
+    this.programRepository = this.connection.getRepository(ProgramEntity);
+    this.fspRepository = this.connection.getRepository(
+      FinancialServiceProviderEntity,
+    );
+    this.registrationRepository = this.connection.getRepository(
+      RegistrationEntity,
+    );
+    this.registrationStatusChangeRepository = this.connection.getRepository(
+      RegistrationStatusChangeEntity,
+    );
+    this.programQuestionRepository = this.connection.getRepository(
+      ProgramQuestionEntity,
+    );
+    this.intersolveBarcodeRepository = this.connection.getRepository(
+      IntersolveBarcodeEntity,
+    );
+
     await this.migrateUsers();
     await this.migrateFsp();
     await this.migrateInstance();
@@ -64,7 +93,12 @@ export class MigrateRefactor implements InterfaceScript {
     await this.migrateIntersolveInstruction();
     await this.migrateIntersolveRequests();
     await this.migrateImageCode();
-    await this.migrateFspCallLog();
+    await this.migrateIntersolveBarcode();
+    await this.migratePaUsers();
+    await this.migrateRegistrations();
+    await this.migrateProgramAnswers();
+    await this.migrateImageCodeExportVouchers();
+    await this.migrateTransactions();
   }
 
   public async migrateUsers(): Promise<void> {
@@ -102,18 +136,16 @@ export class MigrateRefactor implements InterfaceScript {
         ur_ur."userRoleId" = ur.id
     `);
 
-    const userRepository = this.connection.getRepository(UserEntity);
-    await this.disableAutoIncrementId(userRepository);
+    await this.disableAutoIncrementId(this.userRepository);
     for (const oldUser of oldUsers) {
-      let newUser = await userRepository.findOne(oldUser.id);
+      let newUser = await this.userRepository.findOne(oldUser.id);
       if (!newUser) {
-        console.log('oldUser: ', oldUser);
         newUser = new UserEntity();
         newUser.id = oldUser.userId;
         newUser.username = oldUser.email;
         newUser.password = 'temporary password';
         newUser.userType = UserType.aidWorker;
-        await userRepository.save(newUser);
+        await this.userRepository.save(newUser);
       }
       // This is needed to ensure that the old password is not double hashed
       await this.connection
@@ -128,29 +160,73 @@ export class MigrateRefactor implements InterfaceScript {
       );
       const userRoleRepository = this.connection.getRepository(UserRoleEntity);
       newUser.id;
-      console.log('newUser.id: ', newUser.id);
       const roles = await userRoleRepository.find({
         where: {
           role: oldUser.role,
         },
       });
-      console.log('roles: ', roles);
       await assignmentRepository.save({
         user: { id: newUser.id },
         program: { id: this.programId },
         roles: roles,
       });
     }
-    await this.enableAutoIncrementId(userRepository);
+    await this.enableAutoIncrementId(this.userRepository);
+  }
+
+  private async migratePaUsers(): Promise<void> {
+    const oldUsers = await this.oldConnection.query(`SELECT
+        *
+      FROM
+        "pa-accounts"."user"
+    `);
+    for await (const oldUser of oldUsers) {
+      const newUser = new UserEntity();
+      newUser.username = oldUser.username;
+      newUser.password = 'temporary password';
+      newUser.userType = UserType.personAffected;
+      await this.userRepository.save(newUser);
+
+      // This is needed to ensure that the old password is not double hashed
+      await this.connection
+        .createQueryBuilder()
+        .update(UserEntity)
+        .set({ password: oldUser.password })
+        .where('username = :username', { username: oldUser.username })
+        .execute();
+
+      const user = await this.userRepository.findOne({
+        where: { username: newUser.username },
+      });
+      await this.migratePaDataStorage(user);
+    }
+  }
+
+  private async migratePaDataStorage(user: UserEntity): Promise<void> {
+    const query = `SELECT 
+    *
+  FROM
+    "pa-accounts"."data-storage" ds
+  LEFT JOIN "pa-accounts".user u ON ds."userId" = u.id
+  WHERE username = '${user.username}'
+`;
+    const oldStorage = await this.oldConnection.query(query);
+    const paDataStorageRepository = this.connection.getRepository(
+      PersonAffectedAppDataEntity,
+    );
+    for await (const oldRecord of oldStorage) {
+      const newRecord = new PersonAffectedAppDataEntity();
+      newRecord.type = oldRecord.type;
+      newRecord.data = oldRecord.data;
+      newRecord.user = user;
+      await paDataStorageRepository.save(newRecord);
+    }
   }
 
   private async migrateFsp(): Promise<void> {
     console.log('migrateFsp: ');
-    const fspRepository = this.connection.getRepository(
-      FinancialServiceProviderEntity,
-    );
     const fspAttrRepository = this.connection.getRepository(FspAttributeEntity);
-    await this.disableAutoIncrementId(fspRepository);
+    await this.disableAutoIncrementId(this.fspRepository);
     await this.disableAutoIncrementId(fspAttrRepository);
 
     const oldFsps = await this.oldConnection
@@ -158,8 +234,7 @@ export class MigrateRefactor implements InterfaceScript {
     LEFT JOIN "121-service".fsp_attribute fa
     ON	f.id = fa."fspId"`);
     for (const oldFsp of oldFsps) {
-      console.log('oldFsp: ', oldFsp);
-      const newFsp = await fspRepository.save({
+      const newFsp = await this.fspRepository.save({
         id: oldFsp.fspid,
         fsp: oldFsp.fsp,
         fspDisplayName: oldFsp.fspDisplayName,
@@ -177,6 +252,157 @@ export class MigrateRefactor implements InterfaceScript {
           fsp: newFsp,
         });
       }
+    }
+  }
+
+  private async migrateRegistrations(): Promise<void> {
+    await this.disableAutoIncrementId(this.registrationRepository);
+
+    const oldConnections = await this.oldConnection.query(
+      `SELECT c.*,u.username 
+      FROM "121-service".connection c
+      LEFT JOIN "pa-accounts".user u
+      ON c."referenceId" = u."referenceId"
+      `,
+    );
+    const program = await this.programRepository.findOne(this.programId);
+    for await (const oldConnection of oldConnections) {
+      const newRegistration = new RegistrationEntity();
+      newRegistration.id = oldConnection.id;
+      newRegistration.registrationStatus = this.getPaStatus(
+        oldConnection,
+        this.programId,
+      );
+      newRegistration.qrIdentifier = oldConnection.qrIdentifier;
+      newRegistration.referenceId = oldConnection.referenceId;
+      newRegistration.customData = oldConnection.customData;
+      newRegistration.phoneNumber = oldConnection.phoneNumber;
+      newRegistration.preferredLanguage = oldConnection.preferredLanguage;
+      newRegistration.inclusionScore = oldConnection.inclusionScore;
+      newRegistration.namePartnerOrganization =
+        oldConnection.namePartnerOrganization;
+      newRegistration.paymentAmountMultiplier =
+        oldConnection.paymentAmountMultiplier;
+      newRegistration.note = oldConnection.note;
+      newRegistration.noteUpdated = oldConnection.noteUpdated;
+      newRegistration.program = program;
+      newRegistration.user = await this.userRepository.findOne({
+        where: { username: oldConnection.username },
+      });
+      newRegistration.fsp = await this.fspRepository.findOne(
+        oldConnection.fspId,
+      );
+
+      const registration = await this.registrationRepository.save(
+        newRegistration,
+      );
+
+      for await (let status of Object.values(RegistrationStatusEnum)) {
+        const dateColumn = this.getDateColumPerStatus(status);
+        if (!!oldConnection[dateColumn]) {
+          await this.migrateRegistrationStatusChanges(
+            registration,
+            status,
+            oldConnection[dateColumn],
+          );
+        }
+      }
+    }
+    await this.enableAutoIncrementId(this.registrationRepository);
+  }
+
+  private async migrateRegistrationStatusChanges(
+    registration: RegistrationEntity,
+    status: RegistrationStatusEnum,
+    timestamp: Date,
+  ): Promise<void> {
+    const registrationStatusChange = new RegistrationStatusChangeEntity();
+    registrationStatusChange.registration = registration;
+    registrationStatusChange.registrationStatus = status;
+    registrationStatusChange.created = timestamp;
+    await this.registrationStatusChangeRepository.save(
+      registrationStatusChange,
+    );
+  }
+
+  public getDateColumPerStatus(filterStatus: RegistrationStatusEnum): any {
+    switch (filterStatus) {
+      case RegistrationStatusEnum.imported:
+        return RegistrationStatusTimestampField.importedDate;
+      case RegistrationStatusEnum.invited:
+        return RegistrationStatusTimestampField.invitedDate;
+      case RegistrationStatusEnum.noLongerEligible:
+        return RegistrationStatusTimestampField.noLongerEligibleDate;
+      case RegistrationStatusEnum.startedRegistration:
+        return 'accountCreatedDate'; //RegistrationStatusTimestampField.startedRegistrationDate;
+      case RegistrationStatusEnum.registered:
+        return 'appliedDate'; //RegistrationStatusTimestampField.registeredDate;
+      case RegistrationStatusEnum.selectedForValidation:
+        return RegistrationStatusTimestampField.selectedForValidationDate;
+      case RegistrationStatusEnum.validated:
+        return RegistrationStatusTimestampField.validationDate;
+      case RegistrationStatusEnum.included:
+        return RegistrationStatusTimestampField.inclusionDate;
+      case RegistrationStatusEnum.inclusionEnded:
+        return RegistrationStatusTimestampField.inclusionEndDate;
+      case RegistrationStatusEnum.rejected:
+        return RegistrationStatusTimestampField.rejectionDate;
+    }
+  }
+
+  private getPaStatus(
+    connection: any,
+    programId: number,
+  ): RegistrationStatusEnum {
+    let paStatus: RegistrationStatusEnum;
+    if (connection.programsIncluded.includes(programId)) {
+      paStatus = RegistrationStatusEnum.included;
+    } else if (connection.inclusionEndDate) {
+      paStatus = RegistrationStatusEnum.inclusionEnded;
+    } else if (connection.programsRejected.includes(programId)) {
+      paStatus = RegistrationStatusEnum.rejected;
+    } else if (connection.appliedDate && connection.noLongerEligibleDate) {
+      paStatus = RegistrationStatusEnum.registeredWhileNoLongerEligible;
+    } else if (connection.validationDate) {
+      paStatus = RegistrationStatusEnum.validated;
+    } else if (connection.selectedForValidationDate) {
+      paStatus = RegistrationStatusEnum.selectedForValidation;
+    } else if (connection.appliedDate) {
+      paStatus = RegistrationStatusEnum.registered;
+    } else if (connection.accountCreatedDate) {
+      paStatus = RegistrationStatusEnum.startedRegistration;
+    } else if (connection.noLongerEligibleDate) {
+      paStatus = RegistrationStatusEnum.noLongerEligible;
+    } else if (connection.invitedDate) {
+      paStatus = RegistrationStatusEnum.invited;
+    } else if (connection.importedDate) {
+      paStatus = RegistrationStatusEnum.imported;
+    } else if (connection.created) {
+      paStatus = RegistrationStatusEnum.startedRegistration;
+    }
+    return paStatus;
+  }
+
+  public async migrateProgramAnswers(): Promise<void> {
+    console.log('migrateProgramAnswers');
+    const programAnswerRepository = this.connection.getRepository(
+      ProgramAnswerEntity,
+    );
+    const oldValidationData = await this.oldConnection.query(`SELECT
+        *
+      FROM
+        "121-service"."validation_data_attributes" v;
+      `);
+    for (const oldRecord of oldValidationData) {
+      const newRecord = new ProgramAnswerEntity();
+      newRecord.programAnswer = oldRecord.answer;
+      newRecord.programQuestion = await this.programQuestionRepository.findOne({
+        where: { name: oldRecord.attribute },
+      });
+      newRecord.registration = await this.registrationRepository.findOne({
+        where: { referenceId: oldRecord.referenceId },
+      });
+      await programAnswerRepository.save(newRecord);
     }
   }
 
@@ -202,9 +428,7 @@ export class MigrateRefactor implements InterfaceScript {
         "121-service"."program" i;
       `)
     )[0];
-    const pRepo = this.connection.getRepository(ProgramEntity);
-    const newProgram = await pRepo.save(oldProgram);
-    const pQRepo = this.connection.getRepository(ProgramQuestionEntity);
+    const newProgram = await this.programRepository.save(oldProgram);
     const crits = await this.oldConnection.query(`SELECT
         *
       FROM
@@ -213,9 +437,8 @@ export class MigrateRefactor implements InterfaceScript {
     for (let c of crits) {
       c.name = c.criterium;
       c.questionType = c.criteriumType;
-      console.log('c: ', c);
       c.program = newProgram;
-      pQRepo.save(c);
+      this.programQuestionRepository.save(c);
     }
   }
 
@@ -281,17 +504,65 @@ export class MigrateRefactor implements InterfaceScript {
     await repo.save(i);
   }
 
-  private async migrateFspCallLog(): Promise<void> {
-    const repo = this.connection.getRepository(FspCallLogEntity);
-    const logs = await this.oldConnection.query(`SELECT
+  private async migrateIntersolveBarcode(): Promise<void> {
+    console.log('migrateIntersolveBarcode: ');
+    const i = await this.oldConnection.query(`SELECT
         *
       FROM
-        "121-service"."fsp_call_log" a;
+        "121-service"."intersolve_barcode" a;
       `);
-    for (const log of logs) {
-      log.fsp = { id: log.fspId };
-      log.created = log.timestamp;
-      repo.save(log);
+    await this.intersolveBarcodeRepository.save(i);
+  }
+
+  private async migrateImageCodeExportVouchers(): Promise<void> {
+    console.log('migrateImageCodeExportVouchers: ');
+    const imageCodeExportVouchersRepository = this.connection.getRepository(
+      ImageCodeExportVouchersEntity,
+    );
+    const oldRecords = await this.oldConnection.query(`SELECT
+        *
+      FROM
+        "121-service"."imagecode_export_vouchers" a;
+      `);
+    for await (let oldRecord of oldRecords) {
+      const newRecord = new ImageCodeExportVouchersEntity();
+      newRecord.image = oldRecord.image;
+      newRecord.registration = await this.registrationRepository.findOne(
+        oldRecord.connectionId,
+      );
+      newRecord.barcode = await this.intersolveBarcodeRepository.findOne(
+        oldRecord.barcodeId,
+      );
+
+      await imageCodeExportVouchersRepository
+        .createQueryBuilder()
+        .insert()
+        .values(newRecord)
+        .execute();
+    }
+  }
+
+  private async migrateTransactions(): Promise<void> {
+    console.log('migrateTransactions: ');
+    const transactionRepository = this.connection.getRepository(
+      TransactionEntity,
+    );
+    const program = await this.programRepository.findOne(this.programId);
+    const oldRecords = await this.oldConnection.query(`SELECT
+        *
+      FROM
+        "121-service"."transaction";
+      `);
+    for await (let oldRecord of oldRecords) {
+      const newRecord: TransactionEntity = oldRecord;
+      newRecord.registration = await this.registrationRepository.findOne(
+        oldRecord.connectionId,
+      );
+      newRecord.program = program;
+      newRecord.financialServiceProvider = await this.fspRepository.findOne(
+        oldRecord.financialServiceProviderId,
+      );
+      await transactionRepository.save(newRecord);
     }
   }
 
@@ -326,9 +597,9 @@ export class MigrateRefactor implements InterfaceScript {
     }
 
     await repo.query(`
-      ALTER TABLE "121-service"."${repo.metadata.tableName}" ALTER COLUMN id SET DEFAULT nextval('${repo.metadata.tableName}_id_seq');`);
+      ALTER TABLE "121-service"."${repo.metadata.tableName}" ALTER COLUMN id SET DEFAULT nextval('"121-service".${repo.metadata.tableName}_id_seq');`);
     await repo.query(
-      `UPDATE "121-service"."${repo.metadata.tableName}" SET id = nextval('${repo.metadata.tableName}_id_seq')`,
+      `UPDATE "121-service"."${repo.metadata.tableName}" SET id = nextval('"121-service".${repo.metadata.tableName}_id_seq')`,
     );
   }
 }
