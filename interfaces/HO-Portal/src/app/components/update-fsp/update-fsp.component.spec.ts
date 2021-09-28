9da817dc1f2e246ed6dd191aca850262c5f79461
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2021-09-28 16:52:28 +0200

    Fix testing errors

--- interfaces/HO-Portal/src/app/components/update-fsp/update-fsp.component.spec.ts
+++ interfaces/HO-Portal/src/app/components/update-fsp/update-fsp.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
@@ -13,6 +14,7 @@ describe('UpdateFspComponent', () => {
       declarations: [UpdateFspComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [FormsModule, TranslateModule.forRoot()],
+      providers: [HttpClient],
     }).compileComponents();
   }));
 
