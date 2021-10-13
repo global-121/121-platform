693013fde24aaa6600752697e091791da4a81cde
Sowjanya Bysani <sbysani@rodekruis.nl>
2021-10-13 19:29:17 +0530

    AB#10158 Updated api

--- interfaces/HO-Portal/src/app/services/programs-service-api.service.ts
+++ interfaces/HO-Portal/src/app/services/programs-service-api.service.ts
@@ -513,7 +513,7 @@ export class ProgramsServiceApiService {
     return this.apiService
       .post(
         environment.url_121_service_api,
-        `/programs/update/programs/${programId}`,
+        `/programs/update/${programId}`,
         updateBody,
       )
       .toPromise();
