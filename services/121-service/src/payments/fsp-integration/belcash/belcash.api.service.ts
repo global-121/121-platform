179a4498e9191b88c5877caa081c3e4f1e1929fa
jannisvisser <jannisvisser@redcross.nl>
2021-10-20 16:19:36 +0200

    feat: call belcash api (NOT WORKING) AB#10282

--- services/121-service/src/payments/fsp-integration/belcash/belcash.api.service.ts
+++ services/121-service/src/payments/fsp-integration/belcash/belcash.api.service.ts
@@ -1,57 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
 import { FspName } from '../../../fsp/financial-service-provider.entity';
 import { StatusEnum } from '../../../shared/enum/status.enum';
 import { PaTransactionResultDto } from '../../dto/payment-transaction-result.dto';
 
 @Injectable()
 export class BelcashApiService {
-  public constructor() {}
+  public constructor(private readonly httpService: HttpService) {}
 
-  // NOTE: ALL BELOW IS COPIED FROM AFRICAS-TALKING. THIS MUST BE ADJUSTED TO BELCASH STILL.
-
-  public async sendPaymentPerPa(payload): Promise<PaTransactionResultDto> {
-    // A timeout of 123ms to not overload africa's talking server
-    await new Promise(r => setTimeout(r, 123));
-    const credentials = {
-      apiKey: process.env.AFRICASTALKING_API_KEY,
-      username: process.env.AFRICASTALKING_USERNAME,
+  private async authenticate(): Promise<any> {
+    const payload = {
+      principal: process.env.BELCASH_LOGIN,
+      system: process.env.BELCASH_SYSTEM,
+      credentials: process.env.BELCASH_PASSWORD,
     };
-    const Belcash = require('africastalking')(credentials);
-    const payments = Belcash.PAYMENTS;
+    const authenticationResult = await this.httpService
+      .post(`${process.env.BELCASH_API_URL}/authenticate`, payload)
+      .toPromise();
+    const headers = {
+      // 'Content-Type': 'application/json',
+      Authorization: `Bearer ${authenticationResult.data.token}`,
+    };
+    return headers;
+  }
+
+  public async sendPaymentPerPa(
+    payload: any,
+    referenceId: string,
+  ): Promise<PaTransactionResultDto> {
+    // A timeout of 100ms to not overload belcash server
+    await new Promise(r => setTimeout(r, 100));
+    const authenticationHeaders = await this.authenticate();
 
     const paTransactionResult = new PaTransactionResultDto();
     paTransactionResult.fspName = FspName.belcash;
-    paTransactionResult.referenceId =
-      payload.recipients[0].metadata.referenceId;
+    paTransactionResult.referenceId = referenceId;
     paTransactionResult.date = new Date();
-    paTransactionResult.calculatedAmount =
-      payload.recipients[0].metadata.amount;
+    paTransactionResult.calculatedAmount = payload.amount;
 
-    let result;
-    await payments
-      .mobileB2C(payload)
-      .then((response: any) => {
-        result = { response: response };
+    // this.httpService
+    //   .post(`${process.env.BELCASH_API_URL}/transfers`, payload, {
+    //     headers: authenticationHeaders,
+    //   })
+    //   .subscribe(response => {
+    //     console.log('response: ', response);
+    //   });
+
+    const paymentRequestResult = await this.httpService
+      .post(`${process.env.BELCASH_API_URL}/transfers`, payload, {
+        headers: authenticationHeaders,
       })
-      .catch((error: any) => {
-        console.log('error: ', error);
-        result = { error: error };
-      });
+      .toPromise();
+    console.log('paymentRequestResult: ', paymentRequestResult);
 
-    if (result.response?.entries[0]?.errorMessage) {
-      paTransactionResult.status = StatusEnum.error;
-      paTransactionResult.message = result.response.entries[0].errorMessage;
-    } else if (result.response?.errorMessage) {
-      paTransactionResult.status = StatusEnum.error;
-      paTransactionResult.message = result.response.errorMessage;
-    } else if (result.error) {
-      paTransactionResult.status = StatusEnum.error;
-      paTransactionResult.message = result.error;
-    } else {
-      paTransactionResult.status = StatusEnum.waiting;
-      paTransactionResult.message =
-        'No notification of payment status received yet.';
-    }
+    // if (result.response?.entries[0]?.errorMessage) {
+    //   paTransactionResult.status = StatusEnum.error;
+    //   paTransactionResult.message = result.response.entries[0].errorMessage;
+    // } else if (result.response?.errorMessage) {
+    //   paTransactionResult.status = StatusEnum.error;
+    //   paTransactionResult.message = result.response.errorMessage;
+    // } else if (result.error) {
+    //   paTransactionResult.status = StatusEnum.error;
+    //   paTransactionResult.message = result.error;
+    // } else {
+    //   paTransactionResult.status = StatusEnum.waiting;
+    //   paTransactionResult.message =
+    //     'No notification of payment status received yet.';
+    // }
     return paTransactionResult;
   }
 }
--- services/121-service/src/payments/fsp-integration/belcash/belcash.service.ts
+++ services/121-service/src/payments/fsp-integration/belcash/belcash.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { FspName } from '../../../fsp/financial-service-provider.entity';
+import { ProgramEntity } from '../../../programs/program.entity';
 import { PaPaymentDataDto } from '../../dto/pa-payment-data.dto';
 import { FspTransactionResultDto } from '../../dto/payment-transaction-result.dto';
 import { TransactionsService } from '../../transactions/transactions.service';
@@ -7,12 +10,12 @@ import { BelcashApiService } from './belcash.api.service';
 
 @Injectable()
 export class BelcashService {
+  @InjectRepository(ProgramEntity)
+  private readonly programRepository: Repository<ProgramEntity>;
   public constructor(
     private readonly belcashApiService: BelcashApiService,
     private readonly transactionsService: TransactionsService,
   ) {}
-
-  // NOTE: ALL BELOW IS COPIED FROM AFRICAS-TALKING. THIS MUST BE ADJUSTED TO BELCASH STILL.
 
   public async sendPayment(
     paymentList: PaPaymentDataDto[],
@@ -24,17 +27,20 @@ export class BelcashService {
     fspTransactionResult.paList = [];
     fspTransactionResult.fspName = FspName.belcash;
 
+    const program = await this.programRepository.findOne(programId);
+
     for (let payment of paymentList) {
       const calculatedAmount = amount * (payment.paymentAmountMultiplier || 1);
       const payload = this.createPayloadPerPa(
         payment,
-        programId,
         paymentNr,
         calculatedAmount,
+        program.currency,
       );
 
       const paymentRequestResultPerPa = await this.belcashApiService.sendPaymentPerPa(
         payload,
+        payment.referenceId,
       );
       fspTransactionResult.paList.push(paymentRequestResultPerPa);
     }
@@ -49,32 +55,20 @@ export class BelcashService {
 
   public createPayloadPerPa(
     paymentData: PaPaymentDataDto,
-    programId: number,
-    payment: number,
+    paymentNr: number,
     amount: number,
+    currency: string,
   ): object {
     const payload = {
-      username: process.env.AFRICASTALKING_USERNAME,
-      productName: process.env.AFRICASTALKING_PRODUCT_NAME,
-      recipients: [],
-    };
-
-    const recipient = {
-      phoneNumber: paymentData.paymentAddress,
-      currencyCode: process.env.AFRICASTALKING_CURRENCY_CODE,
       amount: amount,
-      metadata: {
-        programId: String(programId),
-        payment: String(payment),
-        referenceId: String(paymentData.referenceId),
-        amount: String(amount),
-      },
+      to: `+${paymentData.paymentAddress}`,
+      description: 'Test description',
+      currency: currency,
+      referenceid: `${paymentData.referenceId}-payment-${paymentNr}`,
+      tracenumber: 'abc',
+      notifyfrom: true,
+      notifyto: true,
     };
-    if (process.env.AFRICASTALKING_PROVIDER_CHANNEL) {
-      recipient['providerChannel'] =
-        process.env.AFRICASTALKING_PROVIDER_CHANNEL;
-    }
-    payload.recipients.push(recipient);
 
     return payload;
   }
