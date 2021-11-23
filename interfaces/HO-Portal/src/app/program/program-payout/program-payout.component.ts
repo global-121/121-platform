e16c95052f8fcf10e95a28f0df5425d65aebe3ca
jannisvisser <jannisvisser@redcross.nl>
2021-11-23 15:37:04 +0100

    unit test

--- interfaces/HO-Portal/src/app/program/program-payout/program-payout.component.ts
+++ interfaces/HO-Portal/src/app/program/program-payout/program-payout.component.ts
@@ -250,7 +250,7 @@ export class ProgramPayoutComponent implements OnInit {
 
   private async checkIntersolve(): Promise<boolean> {
     this.program = await this.programsService.getProgramById(this.programId);
-    for (const fsp of this.program.financialServiceProviders) {
+    for (const fsp of this.program.financialServiceProviders || []) {
       if (fsp.fsp.toLowerCase().includes('intersolve')) {
         return true;
       }
@@ -260,7 +260,7 @@ export class ProgramPayoutComponent implements OnInit {
 
   async checkFspWithCsvIntegration() {
     this.program = await this.programsService.getProgramById(this.programId);
-    for (const fsp of this.program.financialServiceProviders) {
+    for (const fsp of this.program.financialServiceProviders || []) {
       if (fsp.integrationType === FspIntegrationType.csv) {
         return true;
       }
