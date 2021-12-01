df5f86bd81037ce7ae4612b775b05c522517db83
Elwin Schmitz <eschmitz@redcross.nl>
2021-12-01 12:22:33 +0100

    fix: allow more types of pattern to be used on OnlyAllowedInput-directive
    Related #10993
    Related #11004

--- interfaces/PA-App/src/app/directives/only-allowed-input.directive.ts
+++ interfaces/PA-App/src/app/directives/only-allowed-input.directive.ts
@@ -11,18 +11,78 @@ export class OnlyAllowedInputDirective {
 
   @HostListener('keypress', ['$event'])
   onKeyPress(event: KeyboardEvent | any) {
-    return new RegExp(event.target.pattern).test(event.key);
+    let pattern = event.target.pattern;
+
+    if (!pattern) {
+      return true;
+    }
+
+    // Strip quantifiers from pattern, to match single characters
+    pattern = this.stripQuantifiers(pattern);
+
+    return new RegExp(pattern).test(event.key);
   }
 
   @HostListener('paste', ['$event'])
   onPaste(event: ClipboardEvent | any) {
+    if (!event.target.pattern) {
+      return;
+    }
     event.preventDefault();
-    const invertPattern = new RegExp(
-      `${event.target.pattern.replace('[', '[^')}`,
-      'g',
-    );
-    const pasteData = event.clipboardData.getData('text/plain');
-    const stripped = pasteData.replace(invertPattern, '');
+
+    let pattern = event.target.pattern;
+
+    pattern = this.stripStartEnd(pattern);
+    pattern = this.stripQuantifiers(pattern);
+    pattern = this.invertCharacterRanges(pattern);
+    pattern = this.invertDigits(pattern);
+
+    let invertedPattern = new RegExp('');
+    try {
+      invertedPattern = new RegExp(pattern, 'g');
+    } catch (error) {}
+
+    const pasteData: string = event.clipboardData.getData('text/plain');
+    const stripped = pasteData.replace(invertedPattern, '');
     document.execCommand('insertText', false, stripped);
+  }
+
+  private stripQuantifiers(patternValue: string): string {
+    let pattern = patternValue;
+
+    const quantifiersPattern = new RegExp(/\{[0-9]+\}/, 'g');
+    if (quantifiersPattern.test(pattern)) {
+      pattern = pattern.replace(quantifiersPattern, '');
+    }
+
+    return pattern;
+  }
+
+  private stripStartEnd(patternValue: string): string {
+    let pattern = patternValue;
+
+    pattern = pattern.replace(/^\^/, '');
+    pattern = pattern.replace(/\$$/, '');
+
+    return pattern;
+  }
+
+  private invertCharacterRanges(patternValue: string): string {
+    let pattern = patternValue;
+
+    if (new RegExp(/\[/).test(pattern)) {
+      pattern = pattern.replace('[', '[^');
+    }
+
+    return pattern;
+  }
+  private invertDigits(patternValue: string): string {
+    let pattern = patternValue;
+
+    if (new RegExp(/\\d/).test(pattern)) {
+      pattern = pattern.replace(/d/g, 'D');
+    }
+
+    return pattern;
   }
 }
