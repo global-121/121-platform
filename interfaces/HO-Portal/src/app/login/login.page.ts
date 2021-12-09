0ddc2e463769ec7131afb5ca30e3c20fecc754fa
arsforza <asforza@redcross.nl>
2021-12-09 15:26:36 +0100

    Create login page notification toast

--- interfaces/HO-Portal/src/app/login/login.page.ts
+++ interfaces/HO-Portal/src/app/login/login.page.ts
@@ -1,20 +1,30 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
   @ViewChild('loginForm', { static: true })
   public loginForm: NgForm;
 
   public email: any;
   public password: any;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    public toastController: ToastController,
+    private translate: TranslateService,
+  ) {}
+  ngOnInit(): void {
+    this.presentToast();
+  }
 
   public async doLogin() {
     console.log('doLogin()');
@@ -27,5 +37,22 @@ export class LoginPage {
       // Remove credentials from interface-state to prevent re-use after log-out:
       this.loginForm.resetForm();
     });
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: this.translate.instant('page.login.toast-message'),
+      cssClass: 'login-notification ion-text-center',
+      position: 'top',
+      color: 'tertiary',
+      buttons: [
+        {
+          side: 'end',
+          icon: 'close',
+          role: 'cancel',
+        },
+      ],
+    });
+    await toast.present();
   }
 }
