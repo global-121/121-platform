6ed3ad7582c8b34cd92dc1749c57e5f4207800a8
jannisvisser <jannisvisser@redcross.nl>
2021-12-14 10:54:53 +0100

    fix: always use + in phonenr lookup AB#11268

--- services/121-service/src/notifications/lookup/lookup.service.ts
+++ services/121-service/src/notifications/lookup/lookup.service.ts
@@ -10,8 +10,11 @@ export class LookupService {
   ): Promise<{ result: boolean | undefined }> {
     let numberCorrect: boolean;
     try {
+      // Add additional sanitizing (incl NL-specific) because user is given no opportunity to correct here
+      const updatedPhone = this.sanitizePhoneNrExtra(phoneNumber);
+
       await twilioClient.lookups
-        .phoneNumbers(phoneNumber)
+        .phoneNumbers(updatedPhone)
         .fetch({ type: ['carrier'] });
       numberCorrect = true;
     } catch (e) {
@@ -65,14 +68,18 @@ export class LookupService {
   }
 
   private sanitizePhoneNrExtra(phoneNumber: string): string {
-    return phoneNumber.substr(0, 2) == '00'
-      ? phoneNumber.substr(2, phoneNumber.length - 2)
-      : phoneNumber.substr(0, 2) == '06'
-      ? '31' + phoneNumber
-      : phoneNumber.substr(0, 3) == '+00'
-      ? phoneNumber.substr(3, phoneNumber.length - 3)
-      : phoneNumber.substr(0, 2) == '+0'
-      ? phoneNumber.substr(2, phoneNumber.length - 2)
-      : phoneNumber;
+    const sanitizedPhoneNr =
+      phoneNumber.substr(0, 2) == '00'
+        ? phoneNumber.substr(2, phoneNumber.length - 2)
+        : phoneNumber.substr(0, 2) == '06'
+        ? '31' + phoneNumber
+        : phoneNumber.substr(0, 3) == '+00'
+        ? phoneNumber.substr(3, phoneNumber.length - 3)
+        : phoneNumber.substr(0, 2) == '+0'
+        ? phoneNumber.substr(2, phoneNumber.length - 2)
+        : phoneNumber.substr(0, 1) == '+'
+        ? phoneNumber.substr(1, phoneNumber.length - 1)
+        : phoneNumber;
+    return `+${sanitizedPhoneNr}`;
   }
 }
