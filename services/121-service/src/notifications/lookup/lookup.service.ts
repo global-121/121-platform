48a0429ff287d0b498f2dafe56c13bdabdebc184
Elwin Schmitz <eschmitz@redcross.nl>
2021-12-14 11:09:09 +0100

    Merge pull request #2607 from global-121/fix.lebanese-phonenr-check
    fix: always use + in phonenr lookup AB#11268

--- services/121-service/src/notifications/lookup/lookup.service.ts
+++ services/121-service/src/notifications/lookup/lookup.service.ts
@@ -10,8 +10,10 @@ export class LookupService {
   ): Promise<{ result: boolean | undefined }> {
     let numberCorrect: boolean;
     try {
+      const sanitizedNumber = this.sanitizePhoneNrExtra(phoneNumber);
+
       await twilioClient.lookups
-        .phoneNumbers(phoneNumber)
+        .phoneNumbers(sanitizedNumber)
         .fetch({ type: ['carrier'] });
       numberCorrect = true;
     } catch (e) {
@@ -65,14 +67,18 @@ export class LookupService {
   }
 
   private sanitizePhoneNrExtra(phoneNumber: string): string {
-    return phoneNumber.substr(0, 2) == '00'
-      ? phoneNumber.substr(2, phoneNumber.length - 2)
-      : phoneNumber.substr(0, 2) == '06'
-      ? '31' + phoneNumber
-      : phoneNumber.substr(0, 3) == '+00'
-      ? phoneNumber.substr(3, phoneNumber.length - 3)
-      : phoneNumber.substr(0, 2) == '+0'
-      ? phoneNumber.substr(2, phoneNumber.length - 2)
-      : phoneNumber;
+    const sanitizedPhoneNr =
+      phoneNumber.substr(0, 2) == '00'
+        ? phoneNumber.substr(2, phoneNumber.length - 2)
+        : phoneNumber.substr(0, 2) == '06'
+        ? '31' + phoneNumber
+        : phoneNumber.substr(0, 3) == '+00'
+        ? phoneNumber.substr(3, phoneNumber.length - 3)
+        : phoneNumber.substr(0, 2) == '+0'
+        ? phoneNumber.substr(2, phoneNumber.length - 2)
+        : phoneNumber.substr(0, 1) == '+'
+        ? phoneNumber.substr(1, phoneNumber.length - 1)
+        : phoneNumber;
+    return `+${sanitizedPhoneNr}`;
   }
 }
