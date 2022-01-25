312860f551946de3c1dc379abf82a2a9a6b2f55b
Elwin Schmitz <eschmitz@redcross.nl>
2022-01-25 12:33:58 +0100

    test: Include mocks for dependencies

--- interfaces/HO-Portal/src/app/pages/registration-validation/registration-validation.page.spec.ts
+++ interfaces/HO-Portal/src/app/pages/registration-validation/registration-validation.page.spec.ts
@@ -5,6 +5,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { provideMagicalMock } from 'src/app/mocks/helpers';
+import { ProgramPhaseService } from 'src/app/services/program-phase.service';
 import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
 import { RegistrationValidationPage } from './registration-validation.page';
 
@@ -24,6 +25,7 @@ describe('RegistrationValidationPage', () => {
       providers: [
         provideMagicalMock(AuthService),
         provideMagicalMock(ProgramsServiceApiService),
+        provideMagicalMock(ProgramPhaseService),
       ],
     }).compileComponents();
   }));
