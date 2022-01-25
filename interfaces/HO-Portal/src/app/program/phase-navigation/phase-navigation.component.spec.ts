9c99e30408c98ac22506ad08a96267717cebac5d
Elwin Schmitz <eschmitz@redcross.nl>
2022-01-25 11:06:41 +0100

    test: Include mocks for dependencies

--- interfaces/HO-Portal/src/app/program/phase-navigation/phase-navigation.component.spec.ts
+++ interfaces/HO-Portal/src/app/program/phase-navigation/phase-navigation.component.spec.ts
@@ -2,18 +2,21 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
+import apiProgramsMock from 'src/app/mocks/api.programs.mock';
 import { provideMagicalMock } from 'src/app/mocks/helpers';
 import { ProgramPhase } from 'src/app/models/program.model';
 import {
   Phase,
   ProgramPhaseService,
 } from 'src/app/services/program-phase.service';
+import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
 import { PhaseNavigationComponent } from './phase-navigation.component';
 
 describe('PhaseNavigationComponent', () => {
   let component: PhaseNavigationComponent;
   let fixture: ComponentFixture<PhaseNavigationComponent>;
 
+  const mockProgramId = 1;
   const mockProgramPhase: Phase = {
     id: 1,
     name: ProgramPhase.design,
@@ -26,13 +29,23 @@ describe('PhaseNavigationComponent', () => {
     TestBed.configureTestingModule({
       declarations: [PhaseNavigationComponent],
       imports: [TranslateModule.forRoot(), RouterTestingModule],
-      providers: [provideMagicalMock(ProgramPhaseService)],
+      providers: [
+        provideMagicalMock(ProgramsServiceApiService),
+        provideMagicalMock(ProgramPhaseService),
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
+  let mockProgramsApi: jasmine.SpyObj<any>;
   let mockProgramPhaseService: jasmine.SpyObj<any>;
+
   beforeEach(() => {
+    mockProgramsApi = TestBed.inject(ProgramsServiceApiService);
+    mockProgramsApi.getProgramById.and.returnValue(
+      new Promise((r) => r(apiProgramsMock.programs[mockProgramId])),
+    );
+
     mockProgramPhaseService = TestBed.inject(ProgramPhaseService);
     mockProgramPhaseService.getActivePhase.and.returnValue(mockProgramPhase);
 
