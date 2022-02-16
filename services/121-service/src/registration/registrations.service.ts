59b644b0a998c0abae294dae301a81f940fb00e0
jannisvisser <jannisvisser@redcross.nl>
2022-02-16 15:38:58 +0100

    fix: export false if null for boolean AB#12412

--- services/121-service/src/registration/registrations.service.ts
+++ services/121-service/src/registration/registrations.service.ts
@@ -1283,9 +1283,15 @@ export class RegistrationsService {
     programCustomAttributes: ProgramCustomAttributeEntity[],
   ): object {
     for (const programCustomAttribute of programCustomAttributes) {
-      row[programCustomAttribute.name] =
-        customData[programCustomAttribute.name];
+      if (programCustomAttribute.type === CustomAttributeType.boolean) {
+        row[programCustomAttribute.name] =
+          customData[programCustomAttribute.name] || false;
+      } else {
+        row[programCustomAttribute.name] =
+          customData[programCustomAttribute.name];
+      }
     }
+    console.log('row: ', row);
     return row;
   }
 }
