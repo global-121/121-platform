2a7beb57a97a86b2041e475b8395d33606aaa2ec
Smallenbroek,Peter P.J.M <peter.smallenbroek@gmail.com>
2022-02-28 15:47:49 +0100

    Feat: Remove Review inclusion AB#12605

--- services/121-service/src/shared/enum/program-phase.model.ts
+++ services/121-service/src/shared/enum/program-phase.model.ts
@@ -2,7 +2,6 @@ export enum ProgramPhase {
   design = 'design',
   registrationValidation = 'registrationValidation',
   inclusion = 'inclusion',
-  reviewInclusion = 'reviewInclusion',
   payment = 'payment',
   evaluation = 'evaluation',
 }
