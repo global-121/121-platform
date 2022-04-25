f5c6e8ee60e59b96fefa3f610d0e9e36229c579c
Smallenbroek,Peter P.J.M <peter.smallenbroek@gmail.com>
2022-04-25 10:08:22 +0200

    Fix: Lint AB#13487

--- interfaces/PA-App/src/app/mocks/swupdate.mock.ts
+++ interfaces/PA-App/src/app/mocks/swupdate.mock.ts
@@ -3,7 +3,7 @@ import { Observable, Subject } from 'rxjs';
 export class SwUpdateMock {
   public available: Observable<any> = new Subject();
   public activated: Observable<any> = new Subject();
-  public isEnabled: boolean = false;
+  public isEnabled = false;
 
   public checkForUpdate(): Promise<void> {
     return new Promise((resolve) => resolve());
