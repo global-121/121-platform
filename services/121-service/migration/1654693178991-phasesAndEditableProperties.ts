c8de7babf5a1f53110551f72cb6ddb7c2019b9d8
RubenGeo <rubenavalk@gmail.com>
2022-06-15 17:32:51 +0200

    Always allow editing of fsp question answers AB#14234

--- services/121-service/migration/1654693178991-phasesAndEditableProperties.ts
+++ services/121-service/migration/1654693178991-phasesAndEditableProperties.ts
@@ -1,6 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class PhasesAndEditableProperties1654693178991 implements MigrationInterface {
+export class PhasesAndEditableProperties1654693178991
+  implements MigrationInterface {
   name = 'PhasesAndEditableProperties1654693178991';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -8,14 +9,17 @@ export class PhasesAndEditableProperties1654693178991 implements MigrationInterf
       `ALTER TABLE "121-service"."program_custom_attribute" DROP COLUMN "export"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "121-service"."fsp_attribute" ADD "editableInPortal" boolean NOT NULL DEFAULT false`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "121-service"."program_question" ADD "editableInPortal" boolean NOT NULL DEFAULT false`,
     );
-    await queryRunner.query(`ALTER TABLE "121-service"."fsp_attribute" ADD "phases" json NOT NULL DEFAULT '[]'`);
-    await queryRunner.query(`ALTER TABLE "121-service"."program_question" ADD "phases" json NOT NULL DEFAULT '[]'`);
-    await queryRunner.query(`ALTER TABLE "121-service"."program_custom_attribute" ADD "phases" json NOT NULL DEFAULT '[]'`);
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."fsp_attribute" ADD "phases" json NOT NULL DEFAULT '[]'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program_question" ADD "phases" json NOT NULL DEFAULT '[]'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program_custom_attribute" ADD "phases" json NOT NULL DEFAULT '[]'`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -23,13 +27,16 @@ export class PhasesAndEditableProperties1654693178991 implements MigrationInterf
       `ALTER TABLE "121-service"."program_question" DROP COLUMN "editableInPortal"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "121-service"."fsp_attribute" DROP COLUMN "editableInPortal"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "121-service"."program_custom_attribute" ADD "export" json NOT NULL DEFAULT '["all-people-affected","included","selected-for-validation", "payment"]'`,
     );
-    await queryRunner.query(`ALTER TABLE "121-service"."program_custom_attribute" DROP COLUMN "phases"`);
-    await queryRunner.query(`ALTER TABLE "121-service"."program_question" DROP COLUMN "phases"`);
-    await queryRunner.query(`ALTER TABLE "121-service"."fsp_attribute" DROP COLUMN "phases"`);
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program_custom_attribute" DROP COLUMN "phases"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program_question" DROP COLUMN "phases"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."fsp_attribute" DROP COLUMN "phases"`,
+    );
   }
 }
