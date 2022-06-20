fbfce9281cb1bce1812dbf05795f157bfec7afe4
Elwin Schmitz <eschmitz@redcross.nl>
2022-06-20 10:20:32 +0200

    ci: undo verbose logging

--- interfaces/HO-Portal/src/karma.conf.js
+++ interfaces/HO-Portal/src/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     captureConsole: true,
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     restartOnFileChange: true,
     browsers: ['Chrome'],
