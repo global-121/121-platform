2091e818a81bfed55c35f2cbb611f8799df15238
Elwin Schmitz <eschmitz@redcross.nl>
2022-06-20 10:10:51 +0200

    ci: WIP add more logging-output

--- interfaces/HO-Portal/src/karma.conf.js
+++ interfaces/HO-Portal/src/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     captureConsole: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     restartOnFileChange: true,
     browsers: ['Chrome'],
