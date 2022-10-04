35ed4668688bf8470729321e47e7e73a84ba5160
jannisvisser <jannisvisser@redcross.nl>
2022-10-04 14:50:01 +0200

    fix: forgotten constraint drop AB#15247

--- services/121-service/src/programs/program-custom-attribute.entity.ts
+++ services/121-service/src/programs/program-custom-attribute.entity.ts
@@ -10,14 +10,12 @@ import {
 import { ProgramEntity } from './program.entity';
 import { Base121Entity } from '../base.entity';
 import { CustomAttributeType } from './dto/create-program-custom-attribute.dto';
-import { ExportType } from '../export-metrics/dto/export-details';
 import { RegistrationDataEntity } from '../registration/registration-data.entity';
 
 @Unique('programCustomAttributeUnique', ['name', 'programId'])
 @Entity('program_custom_attribute')
 export class ProgramCustomAttributeEntity extends Base121Entity {
   @Column()
-  @Index({ unique: true })
   public name: string;
 
   @Column()
