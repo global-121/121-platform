160c8e5e21a586c17547e3efc7baabaad793e27c
arsforza <asforza@redcross.nl>
2022-10-06 11:54:44 +0200

    fix: tests

--- interfaces/HO-Portal/src/app/components/program-card/program-card.component.spec.ts
+++ interfaces/HO-Portal/src/app/components/program-card/program-card.component.spec.ts
@@ -1,21 +1,31 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
 import { ProgramCardComponent } from './program-card.component';
 
 describe('ProgramCardComponent', () => {
   let component: ProgramCardComponent;
   let fixture: ComponentFixture<ProgramCardComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProgramCardComponent],
-      imports: [IonicModule.forRoot()],
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ProgramCardComponent],
+        imports: [
+          IonicModule.forRoot(),
+          HttpClientTestingModule,
+          TranslateModule.forRoot(),
+          RouterTestingModule,
+        ],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(ProgramCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(ProgramCardComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }),
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
