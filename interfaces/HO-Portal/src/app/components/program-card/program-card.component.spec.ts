7a3b10b6888d454f56dfbbb4d8ced9488aee5b93
arsforza <asforza@redcross.nl>
2022-10-06 11:59:52 +0200

    fix: syntax

--- interfaces/HO-Portal/src/app/components/program-card/program-card.component.spec.ts
+++ interfaces/HO-Portal/src/app/components/program-card/program-card.component.spec.ts
@@ -9,23 +9,21 @@ describe('ProgramCardComponent', () => {
   let component: ProgramCardComponent;
   let fixture: ComponentFixture<ProgramCardComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [ProgramCardComponent],
-        imports: [
-          IonicModule.forRoot(),
-          HttpClientTestingModule,
-          TranslateModule.forRoot(),
-          RouterTestingModule,
-        ],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProgramCardComponent],
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+        RouterTestingModule,
+      ],
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(ProgramCardComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    }),
-  );
+    fixture = TestBed.createComponent(ProgramCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
