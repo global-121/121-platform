fab56b5b0e2c13e0295b6b5701b7e692623cd035
RubenGeo <rubenavalk@gmail.com>
2022-11-08 10:43:20 +0100

    Remove id from update monitoring question AB#16077

--- services/121-service/src/instance/dto/update-monitoring-question.dto.ts
+++ services/121-service/src/instance/dto/update-monitoring-question.dto.ts
@@ -1,14 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { IsOptional } from 'class-validator';
 import { CreateOptionsDto } from '../../programs/dto/create-options.dto';
 
 export class UpdateMonitoringQuestionDto {
-  @ApiProperty({ example: 1 })
-  @IsNotEmpty()
-  @IsNumber()
-  public readonly id: number;
-
   @ApiProperty({ example: { en: 'intro text' } })
   @IsOptional()
   public intro: JSON;
