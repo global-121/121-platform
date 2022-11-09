31447a89d372bcb9b21c14f9083f7a32220eb730
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2022-11-09 15:10:24 +0100

    Migrate id to registrationProgramId AB#16197

--- services/121-service/migration/1667984005227-add-registrationProgramId-column.ts
+++ services/121-service/migration/1667984005227-add-registrationProgramId-column.ts
@@ -5,6 +5,7 @@ export class addRegistrationProgramIdColumn1667984005227 implements MigrationInt
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "121-service"."registration" ADD "registrationProgramId" integer`);
+        await queryRunner.query(`UPDATE "121-service"."registration" SET "registrationProgramId" = "id"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
