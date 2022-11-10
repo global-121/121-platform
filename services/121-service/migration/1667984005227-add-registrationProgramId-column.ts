d50fec9e810fc052a92b549bc2e5711a90101bc3
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2022-11-10 09:05:33 +0100

    Feat: UNIQUE index on programId & registrationProgramId AB#16198

--- services/121-service/migration/1667984005227-add-registrationProgramId-column.ts
+++ services/121-service/migration/1667984005227-add-registrationProgramId-column.ts
@@ -1,15 +1,27 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class addRegistrationProgramIdColumn1667984005227 implements MigrationInterface {
-    name = 'addRegistrationProgramIdColumn1667984005227'
+export class addRegistrationProgramIdColumn1667984005227
+  implements MigrationInterface {
+  name = 'addRegistrationProgramIdColumn1667984005227';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "121-service"."registration" ADD "registrationProgramId" integer`);
-        await queryRunner.query(`UPDATE "121-service"."registration" SET "registrationProgramId" = "id"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."registration" ADD "registrationProgramId" integer`,
+    );
+    await queryRunner.query(
+      `UPDATE "121-service"."registration" SET "registrationProgramId" = "id"`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX IDX_unique_registrationProgramIds ON "121-service"."registration"("programId", "registrationProgramId")`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "121-service"."registration" DROP COLUMN "registrationProgramId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."registration" DROP COLUMN "registrationProgramId"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "121-service"."IDX_unique_registrationProgramIds"`,
+    );
+  }
 }
