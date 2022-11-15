be06d3d5cc751afe3630a3518328c487b25407b4
jannisvisser <jannisvisser@redcross.nl>
2022-11-15 14:06:51 +0100

    fix: migration AB#16277

--- services/121-service/migration/1667984005227-add-registrationProgramId-column.ts
+++ services/121-service/migration/1667984005227-add-registrationProgramId-column.ts
@@ -6,10 +6,13 @@ export class addRegistrationProgramIdColumn1667984005227
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "121-service"."registration" ADD "registrationProgramId" integer NOT NULL`,
+      `ALTER TABLE "121-service"."registration" ADD "registrationProgramId" integer`,
     );
     await queryRunner.query(
       `UPDATE "121-service"."registration" SET "registrationProgramId" = "id"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."registration" ALTER COLUMN "registrationProgramId" SET NOT NULL`,
     );
     await queryRunner.query(
       `ALTER TABLE "121-service"."registration" ADD CONSTRAINT "registrationProgramUnique" UNIQUE ("programId", "registrationProgramId")`,
