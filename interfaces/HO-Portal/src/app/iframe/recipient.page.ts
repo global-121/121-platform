057862b11aaf9d9bd416045c1eefedbc484aef70
Elwin Schmitz <eschmitz@redcross.nl>
2022-11-16 15:51:57 +0100

    feat: Add mock-data for Recipients AB#16285

--- interfaces/HO-Portal/src/app/iframe/recipient.page.ts
+++ interfaces/HO-Portal/src/app/iframe/recipient.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import RegistrationStatus from '../enums/registration-status.enum';
+import { Person } from '../models/person.model';
 
 @Component({
   selector: 'app-recipient-page',
@@ -6,7 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipient.page.scss'],
 })
 export class RecipientPage implements OnInit {
+  public recipients: Person[];
+  private queryParamPhonenumber = '+15005550002';
+
   constructor() {}
 
-  ngOnInit() {}
+  async ngOnInit() {
+    this.recipients = await this.getPhoneNumberDetails(
+      this.queryParamPhonenumber,
+    );
+  }
+
+  private async getPhoneNumberDetails(phoneNumber: string): Promise<Person[]> {
+    console.log('phoneNumber: ', phoneNumber);
+    return [
+      {
+        id: 102,
+        phoneNumber: '+15005550002',
+        referenceId: '',
+        programId: 1,
+        status: RegistrationStatus.included,
+        registrationProgramId: 1,
+      },
+      {
+        id: 103,
+        phoneNumber: '+15005550003',
+        referenceId: '',
+        programId: 1,
+        status: RegistrationStatus.included,
+        registrationProgramId: 2,
+      },
+      {
+        id: 104,
+        phoneNumber: '+15005550002',
+        referenceId: '',
+        programId: 2,
+        status: RegistrationStatus.included,
+        registrationProgramId: 1,
+      },
+    ];
+  }
 }
