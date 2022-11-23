52465a3b47d08cd9bf4991c0389b9c5fe13c26c8
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2022-11-23 16:01:28 +0100

    Feat: Banner with 'PA found' count AB#16362

---
+++ interfaces/HO-Portal/src/app/components/banner/banner.component.spec.ts
@@ -0,0 +1,31 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { BannerComponent } from './banner.component';
+
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [BannerComponent],
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+        RouterTestingModule,
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
