15ef3dbf464067f5eb4319151f30c5de8ae4d6b4
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2022-11-28 11:20:35 +0100

    WIP: Cypress test fix AB#16407

--- interfaces/tests/cypress/e2e/HO-Portal/src/pa-details-iframe.cy.ts
+++ interfaces/tests/cypress/e2e/HO-Portal/src/pa-details-iframe.cy.ts
@@ -22,17 +22,25 @@ describe('Pa details iframe page', () => {
         cy.get('ion-label').contains(registration.nameFirst);
         cy.get('ion-label').contains(registration.nameLast);
         cy.get('ion-label').contains(programLVV.titlePortal.en);
-        cy.get('ion-label').contains(registration.nameFirst);
-        cy.get('ion-label').contains(registration.nameLast);
-        cy.get('ion-label').contains(programLVV.titlePortal.en);
+        // Banner with status
+        cy.get('app-banner').contains(registration.status)
+
+        // Personal: ID, name, phonenumber, whatsappphonenumber, pref. language
+        // cy.get('app-recipient-details').contains(registration.registrationProgramId);
+        // cy.get('app-recipient-details').contains(registration.name);
         cy.get('app-recipient-details').contains(registration.phoneNumber);
         cy.get('app-recipient-details').contains(registration.whatsappPhoneNumber);
-        cy.get('app-recipient-details').contains(registration.vnumber);
-        cy.get('app-recipient-details').contains(registration.fspName);
         cy.get('app-recipient-details').contains(registration.preferredLanguage);
-        // for (const [_key, value] of Object.entries(portalEn['recipient-details'])) {
-        //   cy.get('app-recipient-details').contains(value);
-        // }
+
+        // Notes: note or -
+        cy.get('app-recipient-details').contains(registration.note || '-');
+
+        // Status history: timestamps of PA status changes
+        // TODO: add timestamp checks
+
+        // Payment history: FSP name & if exists: transaction rows
+        cy.get('app-recipient-details').contains(registration.fspName);
+        cy.get('app-recipient-details').contains(registration.paymentAmountMultiplier);
       })
     });
   });
