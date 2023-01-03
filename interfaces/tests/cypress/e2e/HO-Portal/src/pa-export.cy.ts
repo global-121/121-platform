50a035a0a6ac6693b9dcc343a7b65d17e2b87e3d
RubenGeo <rubenavalk@gmail.com>
2023-01-03 16:20:33 +0100

    AB#17035 Cypress test for PA export'

---
+++ interfaces/tests/cypress/e2e/HO-Portal/src/pa-export.cy.ts
@@ -0,0 +1,78 @@
+import portalEn from "../../../../../../interfaces/HO-Portal/src/assets/i18n/en.json";
+import { ProgramPhase } from "../../../../../../services/121-service/src/shared/enum/program-phase.model";
+import programLVV from "../../../../../../services/121-service/seed-data/program/program-pilot-nl.json";
+
+
+describe("Regustration phase", () => {
+  beforeEach(() => {
+    const programId = 1;
+    cy.seedDatabase();
+    cy.loginApi();
+    cy.loginPortal();
+    cy.moveToSpecifiedPhase(1, ProgramPhase.registrationValidation);
+    cy.moveToSpecifiedPhase(1, ProgramPhase.payment);
+    cy.importRegistrations(1);
+    let arr = [];
+    cy.getAllPeopleAffected(programId).then((response) => {
+      for (const pa of response.body) {
+        arr.push(pa.referenceId);
+      }
+      cy.includePeopleAffected(programId, arr);
+      cy.doPayment(1, arr, 1, 10);
+      cy.doPayment(1, arr, 2, 10);
+    });
+  });
+
+  it('Export with a PA', function () {
+    // Wait for the twilio mock to fake an incomming message
+    cy.wait(1000);
+    cy.fixture("pa-export").then((page) => {
+      cy.setHoPortal();
+      cy.visit(page.url);
+      cy.get('[data-cy="export-list-all-pa"]').click();
+      cy.get(
+        '[data-cy="input-prompt-ok"]'
+      ).click();
+      cy.verifyDownload('.xlsx', { contains: true });
+      const date = new Date();
+      const filename = `all-people-affected-${date.getFullYear()}-${date.getMonth() + 1
+        }-${date.getDate()}.xlsx`;
+      cy.verifyDownload(filename, { contains: true });
+      cy.fixture("registration-nlrc").then((seededRegistration) => {
+
+        // Checks if read excel contains original seeded values
+        cy.readXlsx(filename, 'data').then((excelData) => {
+          for (const excelRow of excelData) {
+            console.log('row: ', excelRow);
+            for (let [key, value] of Object.entries(seededRegistration)) {
+              if (key === 'fspName') {
+                key = 'financialserviceprovider';
+              }
+              if (key === 'status') {
+                value = 'included';
+              }
+              expect(String(excelRow[key])).to.equal(String(value));
+            }
+
+            // Checks if read excel contains proper status changes
+            const dateString = new Date().toISOString().split('T')[0];
+            expect(excelRow).to.have.any.keys('registeredDate', 'inclusionDate');
+            expect(String(excelRow['registeredDate'])).to.include(dateString);
+            expect(String(excelRow['inclusionDate'])).to.include(dateString);
+
+            // Checks if read excel contains payments data
+            const paymentNumbers = [1, 2]
+            for (const payment of paymentNumbers) {
+              const keyPaymentAmount = `payment${payment}_amount`;
+              expect(String(excelRow[keyPaymentAmount])).to.equal(String(10));
+              const keyPaymentDate = `payment${payment}_date`;
+              expect(String(excelRow[keyPaymentDate])).to.include(dateString);
+              const keyPaymentVoucherClaimed = `payment${payment}_voucherClaimed_date`;
+              expect(String(excelRow[keyPaymentVoucherClaimed])).to.include(dateString);
+            }
+          }
+        });
+      });
+    });
+  });
+});
