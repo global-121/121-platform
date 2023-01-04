93496502072128f768f366ef7e514f8ff3f18b47
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2023-01-04 12:42:21 +0100

    Fix: Removed index from migration

--- services/121-service/migration/1672829862684-add-contenttype-to-twilio-message.ts
+++ services/121-service/migration/1672829862684-add-contenttype-to-twilio-message.ts
@@ -4,7 +4,6 @@ export class addContenttypeToTwilioMessage1672829862684 implements MigrationInte
     name = 'addContenttypeToTwilioMessage1672829862684'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "121-service"."IDX_ea52d8a2faad81796097568a41"`);
         await queryRunner.query(`CREATE TYPE "121-service"."twilio_message_contenttype_enum" AS ENUM('registered', 'invitation', 'included', 'inclusion-ended', 'rejected', 'invited', 'custom', 'generic-templated', 'payment-templated')`);
         await queryRunner.query(`ALTER TABLE "121-service"."twilio_message" ADD "contentType" "121-service"."twilio_message_contenttype_enum" NOT NULL DEFAULT 'custom'`);
         await queryRunner.query(`CREATE TYPE "121-service"."whatsapp_pending_message_contenttype_enum" AS ENUM('registered', 'invitation', 'included', 'inclusion-ended', 'rejected', 'invited', 'custom', 'generic-templated', 'payment-templated')`);
@@ -16,7 +15,6 @@ export class addContenttypeToTwilioMessage1672829862684 implements MigrationInte
         await queryRunner.query(`DROP TYPE "121-service"."whatsapp_pending_message_contenttype_enum"`);
         await queryRunner.query(`ALTER TABLE "121-service"."twilio_message" DROP COLUMN "contentType"`);
         await queryRunner.query(`DROP TYPE "121-service"."twilio_message_contenttype_enum"`);
-        await queryRunner.query(`CREATE INDEX "IDX_ea52d8a2faad81796097568a41" ON "121-service"."transaction" ("payment") `);
     }
 
 }
