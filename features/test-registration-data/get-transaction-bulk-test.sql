4edf4c103caf611e77811ed4ab51ccaf861d6b51
RubenGeo <rubenavalk@gmail.com>
2023-01-10 15:59:34 +0100

    Get transactions mock data in bulk AB#17223

---
+++ features/test-registration-data/get-transaction-bulk-test.sql
@@ -0,0 +1,111 @@
+-- You can create a dataset of 1024 PAs with 64 payments:
+
+--     1. reset to nlrc-multiple
+--     2. Register & include 1 pa
+--     3. Do 1 payment for pa #1
+--     4. Run the below queries in dbeaver
+
+DO $$ DECLARE i record;
+
+BEGIN FOR i IN 1..10 LOOP
+INSERT
+	INTO
+	"121-service".registration (
+	SELECT
+		id + (
+		SELECT
+			max(id)
+		FROM
+			"121-service".registration),
+		created,
+		"registrationStatus",
+		concat("referenceId", md5(random()::text)),
+		"phoneNumber",
+		"preferredLanguage",
+		"inclusionScore",
+		"paymentAmountMultiplier",
+		note,
+		"noteUpdated",
+		"programId",
+		"userId",
+		"fspId",
+		updated,
+		"registrationProgramId" + (
+		SELECT
+			max("registrationProgramId")
+		FROM
+			"121-service"."registration")
+	FROM
+		"121-service".registration);
+END LOOP;
+END;
+
+$$ ;
+
+DO $$ DECLARE i record;
+
+BEGIN FOR i IN 1..10 LOOP
+INSERT
+	INTO
+	"121-service"."transaction" (
+	SELECT
+		id + (
+		SELECT
+			max(id)
+		FROM
+			"121-service"."transaction"),
+		created,
+		status,
+		"errorMessage",
+		payment,
+		"customData",
+		"transactionStep",
+		"programId",
+		"financialServiceProviderId",
+		"registrationId" + (
+		SELECT
+			max("registrationId")
+		FROM
+			"121-service"."transaction"),
+		amount,
+		updated
+	FROM
+		"121-service"."transaction"
+	WHERE
+		payment = 1);
+END LOOP;
+END;
+
+$$ ;
+
+DO $$ DECLARE i record;
+
+BEGIN FOR i IN 1..63 LOOP
+INSERT
+	INTO
+	"121-service"."transaction" (
+	SELECT
+		id + (
+		SELECT
+			count(id)
+		FROM
+			"121-service"."transaction"),
+		created,
+		status,
+		"errorMessage",
+		i + 1,
+		"customData",
+		"transactionStep",
+		"programId",
+		"financialServiceProviderId",
+		"registrationId",
+		amount,
+		updated
+	FROM
+		"121-service"."transaction"
+	WHERE
+		payment = i);
+END LOOP;
+END;
+
+$$ ;
