1d1ccffaa89a3d1bac50b39e302f81d7deebb66a
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2023-01-23 10:25:26 +0100

    Fix: type check on numericNullable AB#17471

--- services/121-service/src/registration/validator/registration-data-type.validator.ts
+++ services/121-service/src/registration/validator/registration-data-type.validator.ts
@@ -67,11 +67,11 @@ export class RegistrationDataTypeValidator
     type: string,
     options: any[],
     attribute: string,
-  ): boolean {
-    let isValid = false;
+    ): boolean {
+      let isValid = false;
     if (type === AnswerTypes.date) {
       const datePattern =
-        /^(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[0-2])-(19[2-9][0-9]|20[0-1][0-9])$/;
+      /^(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[0-2])-(19[2-9][0-9]|20[0-1][0-9])$/;
       isValid = datePattern.test(value);
     } else if (type === AnswerTypes.dropdown) {
       isValid = this.optionIsValid(value, options);
@@ -79,7 +79,7 @@ export class RegistrationDataTypeValidator
       isValid = this.multiSelectIsValid(value, options);
     } else if (type === AnswerTypes.numeric) {
       isValid = !isNaN(+value);
-    } else if (AnswerTypes.numericNullable) {
+    } else if (type === AnswerTypes.numericNullable) {
       isValid = !isNaN(+value) || null;
     } else if (type === AnswerTypes.tel) {
       // Potential refactor: put lookup code here
