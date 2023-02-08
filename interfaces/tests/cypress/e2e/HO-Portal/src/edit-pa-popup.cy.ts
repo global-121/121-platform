30f93b021568c0c8b9e25b33d4de4c947dcc6366
Elwin Schmitz <eschmitz@redcross.nl>
2023-02-08 11:40:56 +0100

    refactor/tests: Add specific timeouts instead of generic waits

--- interfaces/tests/cypress/e2e/HO-Portal/src/edit-pa-popup.cy.ts
+++ interfaces/tests/cypress/e2e/HO-Portal/src/edit-pa-popup.cy.ts
@@ -11,27 +11,27 @@ describe('Edit person affect pop-up', () => {
   it('Should edit person affect in pop-up', function () {
     const programId = 1;
     cy.moveToSpecifiedPhase(programId, ProgramPhase.registrationValidation);
+
     cy.fixture('edit-pa-popup').then((fixture) => {
       cy.importRegistrations(programId, [fixture.registration]);
       cy.setHoPortal();
       cy.visit(fixture.url);
-      cy.get('#edit-button').as('btn').click();
 
-      cy.wait(300);
+      cy.get('[data-cy="pa-edit-button"]', { timeout: 8000 }).click();
 
-      cy.get('app-update-property-item').each(($el) => {
-        const el = $el[0];
-        const type = el.attributes.getNamedItem('ng-reflect-type')?.textContent;
-        const label =
-          el.attributes.getNamedItem('ng-reflect-label')?.textContent;
-        validateInput(type, label);
-      });
+      cy.get('app-edit-person-affected-popup')
+        .get('app-update-property-item')
+        .each(($el) => {
+          const type = $el.attr('ng-reflect-type');
+          const label = $el.attr('ng-reflect-label');
+          validateInput(type, label);
+        });
 
       cy.get('app-update-fsp').click();
-      cy.contains(
-        '.select-interface-option',
-        FspName.intersolveNoWhatsapp,
-      ).click();
+      cy.get('ion-popover')
+        .contains('.select-interface-option', FspName.intersolveNoWhatsapp)
+        .click();
+
       clickSaveAndConfirm('Financial Service Provider');
     });
   });
@@ -82,11 +82,13 @@ describe('Edit person affect pop-up', () => {
 
   function clickSaveAndConfirm(label: string) {
     cy.intercept('**/attribute').as('updateAttribute');
-    cy.get(`[ng-reflect-label="${label}"] ion-button`).click();
+
+    cy.get(`[ng-reflect-label="${label}"] ion-button[type="submit"]`).click();
+
     cy.wait('@updateAttribute')
       .its('response.statusCode')
       .should('not.be.oneOf', [400, 500]);
-    cy.get('.alert-button').click();
-    cy.wait(100);
+
+    cy.get('button.alert-button', { timeout: 5000 }).click();
   }
 });
