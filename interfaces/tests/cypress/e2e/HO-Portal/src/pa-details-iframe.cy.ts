968ce5ca563182f589f22f184e4b84e1dc08ae3a
Elwin Schmitz <eschmitz@redcross.nl>
2023-02-14 12:30:57 +0100

    refactor: Restructure iframe-test + be more specific in selectors + add shoulds

--- interfaces/tests/cypress/e2e/HO-Portal/src/pa-details-iframe.cy.ts
+++ interfaces/tests/cypress/e2e/HO-Portal/src/pa-details-iframe.cy.ts
@@ -10,18 +10,28 @@ describe('Pa details iframe page', () => {
 
   it('Visits the PA details iframe page and finds 1 PA and checks details', function () {
     cy.importRegistrations(1);
+
     cy.fixture('pa-details-iframe').then((page) => {
       cy.fixture('registration-nlrc').then((registration) => {
         cy.setHoPortal();
+
         cy.visit(page.url, {
           qs: {
             phonenumber: registration.phoneNumber,
           },
         });
         cy.url().should('include', 'iframe');
-        cy.get('ion-label').contains(registration.nameFirst);
-        cy.get('ion-label').contains(registration.nameLast);
-        cy.get('ion-label').contains(programLVV.titlePortal.en);
+        cy.get('app-recipient-page').should('be.visible');
+
+        cy.get(
+          'app-recipient-page *:not(app-recipient-details) ion-label',
+        ).contains(registration.nameFirst);
+        cy.get(
+          'app-recipient-page *:not(app-recipient-details) ion-label',
+        ).contains(registration.nameLast);
+        cy.get(
+          'app-recipient-page *:not(app-recipient-details) ion-label',
+        ).contains(programLVV.titlePortal.en);
         cy.get('app-banner').contains(registration.status);
         cy.get('app-recipient-details').contains(registration.phoneNumber);
         cy.get('app-recipient-details').contains(
@@ -45,17 +55,23 @@ describe('Pa details iframe page', () => {
       const whatsappPhoneNumber = 14155238887;
       registration.whatsappPhoneNumber = whatsappPhoneNumber;
       cy.importRegistrations(1, [registration]);
+
       cy.fixture('pa-details-iframe').then((page) => {
         cy.setHoPortal();
+
         cy.visit(page.url, {
           qs: {
             phonenumber: whatsappPhoneNumber,
           },
         });
         cy.url().should('include', 'iframe');
-        cy.get('ion-label').contains(registration.nameFirst);
-        cy.get('ion-label').contains(registration.nameLast);
-        cy.get('ion-label').contains(programLVV.titlePortal.en);
+        cy.get('app-recipient-page').should('be.visible');
+
+        cy.get('app-recipient-page ion-label').contains(registration.nameFirst);
+        cy.get('app-recipient-page ion-label').contains(registration.nameLast);
+        cy.get('app-recipient-page ion-label').contains(
+          programLVV.titlePortal.en,
+        );
       });
     });
   });
@@ -66,19 +82,33 @@ describe('Pa details iframe page', () => {
         registration2.phoneNumber = registration1.phoneNumber;
         cy.importRegistrations(1, [registration1]);
         cy.importRegistrations(2, [registration2]);
+
         cy.fixture('pa-details-iframe').then((page) => {
           cy.setHoPortal();
+
           cy.visit(page.url, {
             qs: {
               phonenumber: registration1.phoneNumber,
             },
           });
           cy.url().should('include', 'iframe');
-          cy.get('ion-label').contains(registration1.nameFirst);
-          cy.get('ion-label').contains(registration1.nameLast);
-          cy.get('ion-label').contains(programLVV.titlePortal.en);
-          cy.get('ion-label').contains(registration2.nameFirst);
-          cy.get('ion-label').contains(registration2.nameLast);
+          cy.get('app-recipient-page').should('be.visible');
+
+          cy.get('app-recipient-page ion-label').contains(
+            registration1.nameFirst,
+          );
+          cy.get('app-recipient-page ion-label').contains(
+            registration1.nameLast,
+          );
+          cy.get('app-recipient-page ion-label').contains(
+            programLVV.titlePortal.en,
+          );
+          cy.get('app-recipient-page ion-label').contains(
+            registration2.nameFirst,
+          );
+          cy.get('app-recipient-page ion-label').contains(
+            registration2.nameLast,
+          );
         });
       });
     });
@@ -86,14 +116,18 @@ describe('Pa details iframe page', () => {
 
   it('Visits the PA details iframe page and finds no PAs with provided phonenumber', function () {
     cy.importRegistrations(1);
+
     cy.fixture('pa-details-iframe').then((page) => {
       cy.setHoPortal();
+
       cy.visit(page.url, {
         qs: {
           phonenumber: '12313123123',
         },
       });
       cy.url().should('include', 'iframe');
+      cy.get('app-recipient-page').should('be.visible');
+
       const text = portalEn.page.iframe.recipient['no-recipients-found'];
       cy.get('ion-content').contains(text);
     });
@@ -101,10 +135,14 @@ describe('Pa details iframe page', () => {
 
   it('Visits the PA details iframe page with no phonenumber provided', function () {
     cy.importRegistrations(1);
+
     cy.fixture('pa-details-iframe').then((page) => {
       cy.setHoPortal();
+
       cy.visit(page.url);
       cy.url().should('include', 'iframe');
+      cy.get('app-recipient-page').should('be.visible');
+
       const text = portalEn.page.iframe.recipient['no-phone-number'];
       cy.get('ion-content').contains(text);
     });
