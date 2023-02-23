ca6c0b3f06311e9f5b8c227ebfa78b6052d67540
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2023-02-23 17:33:53 +0100

    Feat: update registration from webhook

--- services/121-service/src/espocrm/espocrm.service.ts
+++ services/121-service/src/espocrm/espocrm.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PatchRegistrationDto } from '../registration/dto/patch-registration.dto';
+import { RegistrationsService } from '../registration/registrations.service';
 
 @Injectable()
 export class EspocrmService {
+  public constructor(
+    private readonly registrationsService: RegistrationsService,
+  ) {}
 
-  public async patchRegistration(patchRegistrations: PatchRegistrationDto[]): Promise<void> {
-    console.log('patchRegistrations: ', patchRegistrations);
+  public async patchRegistration(
+    patchRegistrations: PatchRegistrationDto[],
+  ): Promise<void> {
+    for (const patchRegistration of patchRegistrations) {
+      const refId = patchRegistration.id;
+      for (const key in patchRegistration) {
+        if (key !== 'id') {
+          const value = patchRegistration[key];
+          try {
+            await this.registrationsService.setAttribute(refId, key, value);
+          } catch (error) {
+            if (error.name !== 'RegistrationDataSaveError') {
+              console.warn('Unknown error: ', error);
+              console.log(
+                `Failed updating '${key}' with value: ${value} (referenceId: ${refId})`,
+              );
+            }
+          }
+        }
+      }
+    }
     return;
   }
 
