8aadc72086d972c7132a065ae095c9ec21a16d2e
jannisvisser <jannisvisser@redcross.nl>
2023-03-08 15:06:05 +0100

    fix: split migration file in old and new AB#18238

--- services/121-service/migration/1676992303536-addIntersolveVisaFsp.ts
+++ services/121-service/migration/1676992303536-addIntersolveVisaFsp.ts
@@ -1,5 +1,4 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
-
 export class addIntersolveVisaFsp1676992303536 implements MigrationInterface {
   name = 'addIntersolveVisaFsp1676992303536';
 
@@ -17,13 +16,13 @@ export class addIntersolveVisaFsp1676992303536 implements MigrationInterface {
       `CREATE INDEX "IDX_98ffc5f840df6cfb2b02bf2600" ON "121-service"."intersolve_visa_request" ("endpoint") `,
     );
     await queryRunner.query(
-      `CREATE TABLE "121-service"."intersolve_visa_wallet" ("id" SERIAL NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "tokenCode" character varying, "tokenBlocked" boolean, "status" character varying, "type" character varying, CONSTRAINT "UQ_5859fe005b0a7fb606f6f191c50" UNIQUE ("tokenCode"), CONSTRAINT "PK_ad0d5b893e3726602f7822c6f29" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "121-service"."intersolve_visa_card" ("id" SERIAL NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "success" boolean, "tokenCode" character varying, "tokenBlocked" boolean, "expiresAt" character varying, "status" character varying, CONSTRAINT "UQ_5859fe005b0a7fb606f6f191c50" UNIQUE ("tokenCode"), CONSTRAINT "PK_ad0d5b893e3726602f7822c6f29" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_571e8d4fb00bd19be71ec359c9" ON "121-service"."intersolve_visa_wallet" ("created") `,
+      `CREATE INDEX "IDX_571e8d4fb00bd19be71ec359c9" ON "121-service"."intersolve_visa_card" ("created") `,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_5859fe005b0a7fb606f6f191c5" ON "121-service"."intersolve_visa_wallet" ("tokenCode") `,
+      `CREATE INDEX "IDX_5859fe005b0a7fb606f6f191c5" ON "121-service"."intersolve_visa_card" ("tokenCode") `,
     );
     await queryRunner.query(
       `CREATE TABLE "121-service"."intersolve_visa_customer" ("id" SERIAL NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "blocked" boolean, "holderId" character varying, "registrationId" integer, "visaCardId" integer, CONSTRAINT "REL_ad00c730226a462624de94041e" UNIQUE ("registrationId"), CONSTRAINT "REL_70d1231a6094f1151ec92303a8" UNIQUE ("visaCardId"), CONSTRAINT "PK_4e102a56ae70e53cfbfbe428d93" PRIMARY KEY ("id"))`,
@@ -41,7 +40,7 @@ export class addIntersolveVisaFsp1676992303536 implements MigrationInterface {
       `ALTER TABLE "121-service"."intersolve_visa_customer" ADD CONSTRAINT "FK_ad00c730226a462624de94041ec" FOREIGN KEY ("registrationId") REFERENCES "121-service"."registration"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
-      `ALTER TABLE "121-service"."intersolve_visa_customer" ADD CONSTRAINT "FK_70d1231a6094f1151ec92303a83" FOREIGN KEY ("visaCardId") REFERENCES "121-service"."intersolve_visa_wallet"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+      `ALTER TABLE "121-service"."intersolve_visa_customer" ADD CONSTRAINT "FK_70d1231a6094f1151ec92303a83" FOREIGN KEY ("visaCardId") REFERENCES "121-service"."intersolve_visa_card"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
   }
 
@@ -70,9 +69,7 @@ export class addIntersolveVisaFsp1676992303536 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX "121-service"."IDX_571e8d4fb00bd19be71ec359c9"`,
     );
-    await queryRunner.query(
-      `DROP TABLE "121-service"."intersolve_visa_wallet"`,
-    );
+    await queryRunner.query(`DROP TABLE "121-service"."intersolve_visa_card"`);
     await queryRunner.query(
       `DROP INDEX "121-service"."IDX_98ffc5f840df6cfb2b02bf2600"`,
     );
