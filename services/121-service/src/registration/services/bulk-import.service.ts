053b594a90d2f25cbb6ff6e7419fad506395a499
RubenGeo <rubenavalk@gmail.com>
2023-03-09 11:12:04 +0100

    AB#18286 Import without isPhysicalCardVisa broke the portal

--- services/121-service/src/registration/services/bulk-import.service.ts
+++ services/121-service/src/registration/services/bulk-import.service.ts
@@ -167,7 +167,9 @@ export class BulkImportService {
       return string;
     }
     if (string === undefined) {
-      return defaultValue || undefined;
+      return this.isValueUdefinedOrNull(defaultValue)
+        ? undefined
+        : defaultValue;
     }
     switch (string.toLowerCase().trim()) {
       case 'true':
@@ -181,8 +183,14 @@ export class BulkImportService {
       case null:
         return false;
       default:
-        return defaultValue || undefined;
+        return this.isValueUdefinedOrNull(defaultValue)
+          ? undefined
+          : defaultValue;
     }
+  }
+
+  private isValueUdefinedOrNull(value: any): boolean {
+    return value === undefined || value === null;
   }
 
   public async getImportRegistrationsTemplate(
