b40a597b5478f71100834be6f3ab62b63d99e1b9
Elwin Schmitz <eschmitz@redcross.nl>
2023-03-20 17:24:57 +0100

    fix: auto-upgrade changes syntax linting

--- interfaces/PA-App/src/app/app-routing.module.ts
+++ interfaces/PA-App/src/app/app-routing.module.ts
@@ -15,8 +15,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
-}),
+      preloadingStrategy: PreloadAllModules,
+    }),
   ],
   exports: [RouterModule],
 })
