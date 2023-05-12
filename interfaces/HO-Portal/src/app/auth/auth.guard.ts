f013b62bce784e7cc8342c83e23191b34d43ba53
arsforza <asforza@redcross.nl>
2023-05-12 16:48:12 +0200

    feat: update angular HO AB#19372

--- interfaces/HO-Portal/src/app/auth/auth.guard.ts
+++ interfaces/HO-Portal/src/app/auth/auth.guard.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppRoutes } from '../app-routes.enum';
 import { AuthService } from './auth.service';
@@ -12,7 +7,7 @@ import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
