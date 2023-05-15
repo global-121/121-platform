cbeb2a7710469f9b3e24cb3a5f73f2633fdded5a
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2023-05-15 09:54:33 +0200

    Feat: create & read AB#19294

--- services/121-service/src/programs/fsp-configuration/fsp-configuration.controller.ts
+++ services/121-service/src/programs/fsp-configuration/fsp-configuration.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Admin } from '../../guards/admin.decorator';
+import { CreateProgramFspConfigurationDto } from '../dto/create-program-fsp-configuration.dto';
 import { AdminAuthGuard } from './../../guards/admin.guard';
 import { PermissionsGuard } from './../../guards/permissions.guard';
 import { ProgramFspConfigurationService } from './fsp-configuration.service';
@@ -17,16 +27,52 @@ export class ProgramFspConfigurationController {
     this.programFspConfigurationService = programFspConfigurationService;
   }
 
-  @ApiOperation({ summary: 'Get ProgramFspConfigurationEntity by program id' })
-  @ApiParam({ name: 'programId', required: true, type: 'integer' })
-  @ApiResponse({ status: 200, description: 'Return program by id.' })
   @Admin()
+  @ApiOperation({ summary: 'Get programFspConfigurationEntity by program id' })
+  @ApiParam({ name: 'programId', required: true, type: 'integer' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return programFspConfigurationEntity by program id.',
+  })
   @Get(':programId/fsp-configuration')
   public async findByProgramId(
     @Param() params,
   ): Promise<ProgramFspConfigurationEntity[]> {
+    if (isNaN(params.programId)) {
+      throw new HttpException(
+        'Program ID is not a number',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     return this.programFspConfigurationService.findByProgramId(
       params.programId,
+    );
+  }
+
+  @Admin()
+  @ApiOperation({ summary: 'Create ProgramFspConfigurationEntity' })
+  @ApiParam({ name: 'programId', required: true, type: 'integer' })
+  @ApiResponse({
+    status: 201,
+    description:
+      'The programFspConfigurationEntity has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @Post(':programId/fsp-configuration')
+  public async create(
+    @Body() programFspConfigurationData: CreateProgramFspConfigurationDto,
+    @Param() params,
+  ): Promise<number> {
+    if (isNaN(params.programId)) {
+      throw new HttpException(
+        'Program ID is not a number',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return await this.programFspConfigurationService.create(
+      params.programId,
+      programFspConfigurationData,
     );
   }
 }
--- services/121-service/src/programs/fsp-configuration/fsp-configuration.service.ts
+++ services/121-service/src/programs/fsp-configuration/fsp-configuration.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateProgramFspConfigurationDto } from '../dto/create-program-fsp-configuration.dto';
 import { ProgramFspConfigurationEntity } from './program-fsp-configuration.entity';
 
 @Injectable()
@@ -13,9 +14,40 @@ export class ProgramFspConfigurationService {
   ): Promise<ProgramFspConfigurationEntity[]> {
     const programFspConfigurations =
       await this.programFspConfigurationRepository.find({
-        where: { id: programId },
+        where: { programId: programId },
       });
 
     return programFspConfigurations;
+  }
+
+  public async create(
+    programId: number,
+    programFspConfigurationDto: CreateProgramFspConfigurationDto,
+  ): Promise<number> {
+    const programFspConfiguration = new ProgramFspConfigurationEntity();
+    programFspConfiguration.programId = programId;
+    programFspConfiguration.fspId = programFspConfigurationDto.fspId;
+    programFspConfiguration.name = programFspConfigurationDto.name;
+    programFspConfiguration.value = programFspConfigurationDto.value;
+
+    try {
+      const resProgramFspConfiguration =
+        await this.programFspConfigurationRepository.save(
+          programFspConfiguration,
+        );
+      return resProgramFspConfiguration.id;
+    } catch (error) {
+      if (error['code'] === '23505') {
+        throw new HttpException(
+          `Conflict on unique constraint: ${error['constraint']}`,
+          HttpStatus.CONFLICT,
+        );
+      } else {
+        throw new HttpException(
+          'Internal server error',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
   }
 }
