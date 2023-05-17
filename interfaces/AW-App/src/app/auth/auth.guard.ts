e3019e9c8748b7060da4444b46f19605375f6ed4
arsforza <asforza@redcross.nl>
2023-05-17 15:21:38 +0200

    feat: update angular AW AB#19379

--- interfaces/AW-App/src/app/auth/auth.guard.ts
+++ interfaces/AW-App/src/app/auth/auth.guard.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
