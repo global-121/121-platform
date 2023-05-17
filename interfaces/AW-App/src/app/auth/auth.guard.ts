42a9101c728f19d88cb4bac3ac62efb19a787ad4
arsforza <asforza@redcross.nl>
2023-05-17 16:19:24 +0200

    fix: lint AB#19379

--- interfaces/AW-App/src/app/auth/auth.guard.ts
+++ interfaces/AW-App/src/app/auth/auth.guard.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard  {
+export class AuthGuard {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
