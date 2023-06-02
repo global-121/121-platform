e08ac05d653e52509e8ac3aa340fbc1fb795ce3b
Kaspars Grinbergs <kgrinbergs@redcross.nl>
2023-06-02 13:21:12 +0300

    Secrets moved to .env

--- services/121-service/src/payments/fsp-integration/safaricom/safaricom.service.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/safaricom.service.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 @Injectable()
 export class SafaricomService {
-  private readonly consumerKey: string = '3fZlPYA1RFQQ6eeGG1MXc4QxOqtUVPFY';
-  private readonly consumerSecret: string = 'IMPDlynkPHWhg6ZS';
-  private readonly accessTokenUrl: string = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  private readonly consumerKey: string = process.env.SAFARICOM_CONSUMER_KEY;
+  private readonly consumerSecret: string = process.env.SAFARICOM_CONSUMER_SECRET;
+  private readonly accessTokenUrl: string = process.env.SAFARICOM_CONSUMER_ACCESS_TOKEN_URL + '/oauth/v1/generate?grant_type=client_credentials';
 
   public async getAccessToken(): Promise<string> {
     const auth = Buffer.from(`${this.consumerKey}:${this.consumerSecret}`).toString('base64');
