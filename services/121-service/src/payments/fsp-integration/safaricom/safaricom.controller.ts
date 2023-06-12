69a2d664a89b8d71108f5b3f1d45b5b02602c4d3
root <root@LVAGPLTP6523.dir.svc.accenture.com>
2023-06-12 16:51:27 +0300

    WIP: resolving merge conflicts

--- services/121-service/src/payments/fsp-integration/safaricom/safaricom.controller.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/safaricom.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SafaricomService } from './safaricom.service';
+import { SafaricomPaymentPayloadDto } from './dto/safaricom-payment-payload.dto';
 
+@ApiTags('payments/safaricom')
 @Controller('safaricom')
 export class SafaricomController {
-  constructor(private readonly SafaricomService: SafaricomService) {}
+  constructor(private readonly safaricomService: SafaricomService) {}
 
-  @Get('/access-token')
-  public async getAccessToken(): Promise<string> {
-    const accessToken = await this.SafaricomService.getAccessToken();
-    console.log("Access Token: " + accessToken);
-    return accessToken;
+  @ApiBody({ isArray: true, type: SafaricomPaymentPayloadDto })
+  @Post('make-payment')
+  @ApiOperation({ summary: 'Make Safaricom payment' })
+  async makePayment(@Body() payload: SafaricomPaymentPayloadDto): Promise<any> {
+    console.log("TEST");
+    try {
+      console.log("TEST");
+      const paymentResponse = await this.safaricomService.makePayment(payload);
+      return paymentResponse;
+    } catch (error) {
+
+      throw new Error('Failed to make payment');
+    }
   }
 }
--- services/121-service/src/payments/fsp-integration/safaricom/safaricom.service.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/safaricom.service.ts
@@ -1,23 +1,89 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+// import { SafaricomPaymentPayloadDto } from './dto/safaricom-payment-payload.dto';
+import { FspName } from '../../../fsp/enum/fsp-name.enum';
+import { StatusEnum } from '../../../shared/enum/status.enum';
+import { PaPaymentDataDto } from '../../dto/pa-payment-data.dto';
+import {
+  FspTransactionResultDto,
+  PaTransactionResultDto,
+} from '../../dto/payment-transaction-result.dto';
+import { TransactionEntity } from '../../transactions/transaction.entity';
+import { TransactionsService } from '../../transactions/transactions.service';
 
 @Injectable()
 export class SafaricomService {
-  private readonly consumerKey: string = process.env.SAFARICOM_CONSUMER_KEY;
-  private readonly consumerSecret: string = process.env.SAFARICOM_CONSUMER_SECRET;
-  private readonly accessTokenUrl: string = process.env.SAFARICOM_CONSUMER_ACCESS_TOKEN_URL;
+  public constructor(
+    private readonly transactionsService: TransactionsService,
+  ) {}
 
-  public async getAccessToken(): Promise<string> {
-    const auth = Buffer.from(`${this.consumerKey}:${this.consumerSecret}`).toString('base64');
+  public async sendPayment(
+    paymentList: PaPaymentDataDto[],
+    programId: number,
+    paymentNr: number,
+    amount: number,
+  ): Promise<FspTransactionResultDto> {
+    const fspTransactionResult = new FspTransactionResultDto();
+    fspTransactionResult.paList = [];
+    fspTransactionResult.fspName = FspName.vodacash;
 
-    const { data } = await axios.get(this.accessTokenUrl, {
+    for (const payment of paymentList) {
+      const calculatedAmount = amount * (payment.paymentAmountMultiplier || 1);
+
+      const paTransactionResult = {
+        fspName: FspName.vodacash,
+        referenceId: payment.referenceId,
+        date: new Date(),
+        calculatedAmount: calculatedAmount,
+        status: StatusEnum.waiting,
+        message: null,
+      };
+
+      // Storing the per payment so you can continiously seed updates of transactions in HO-Portal
+      this.transactionsService.storeTransactionUpdateStatus(
+        paTransactionResult,
+        programId,
+        paymentNr,
+      );
+    }
+    return fspTransactionResult;
+  }
+
+  public async makePayment(payload: any): Promise<any> {
+    console.log("TEST Safaricom Service " + process.env.SAFARICOM_B2C_PAYMENTREQUEST_URL);
+    try {
+      const paymentUrl = process.env.SAFARICOM_B2C_PAYMENTREQUEST_URL;
+      const consumerKey = process.env.SAFARICOM_CONSUMER_KEY;
+      const consumerSecret = process.env.SAFARICOM_CONSUMER_SECRET;
+
+      const accessToken = await this.getAccessToken(consumerKey, consumerSecret);
+      console.log(accessToken);
+      const response = await axios.post(paymentUrl, payload, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to make Safaricom B2C payment API call');
+    }
+  }
+
+  async getAccessToken(consumerKey: string, consumerSecret: string): Promise<string> {
+    const accessTokenUrl = process.env.SAFARICOM_CONSUMER_ACCESS_TOKEN_URL;
+
+    const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
+    console.log("TEST");
+    const { data } = await axios.get(accessTokenUrl, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Basic ${auth}`,
       },
     });
 
-    console.log("Access Token: " + data.access_token);
     return data.access_token;
   }
 }
