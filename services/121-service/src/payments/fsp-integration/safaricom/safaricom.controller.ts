90626e15f454abba9a2d05a6e4ae2baa03071937
TatevAccenture <tatev.kostanyan@accenture.com>
2023-06-16 17:41:39 +0300

    changing callback

--- services/121-service/src/payments/fsp-integration/safaricom/safaricom.controller.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/safaricom.controller.ts
@@ -2,24 +2,36 @@ import { ApiOperation, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SafaricomService } from './safaricom.service';
 import { SafaricomApiService } from './safaricom.api.service';
-import { SafaricomTransferPayload } from './safaricom-transfer-payload.dto';
-
+//import { SafaricomTransferPayload } from './safaricom-transfer-payload.dto';
+import { SafaricomTransferPayloadDto } from './safaricom-transfer-payload.dto';
 @ApiTags('payments/safaricom')
 @Controller('safaricom')
 export class SafaricomController {
   constructor(private readonly safaricomService: SafaricomService, private readonly safaricomApiService: SafaricomApiService) {}
-  @ApiBody({ isArray: true, type: SafaricomTransferPayload })
-  @Post('make-payment')
+  @ApiBody({ isArray: true, type: SafaricomTransferPayloadDto })
   @ApiOperation({ summary: 'Make Safaricom payment' })
-  async makePayment(@Body() payload: SafaricomTransferPayload): Promise<any> {
-    console.log("TEST");
-    try {
-      console.log("TEST");
-      const paymentResponse = await this.safaricomApiService.makePayment(payload);
-      return paymentResponse;
-    } catch (error) {
+  @ApiResponse({ status: 201, description: 'Notified' })
+  @Post('make-payment')
+  
+  public async makePayment(
+    
+    @Body() 
+//     payload: SafaricomTransferPayload): Promise<any> {
+//     console.log("TEST");
+//     try {
+//       console.log("TESTSuccess");
+//       const paymentResponse = await this.safaricomApiService.makePayment(payload);
+//       return paymentResponse;
+//     } catch (error) {
 
-      throw new Error('Failed to make payment');
-    }
+//       console.log("TESTError");
+//       throw new Error('Failed to make payment');
+//     }
+//   }
+// }
+belcashCallbackData: SafaricomTransferPayloadDto,
+  ): Promise<void> {
+    await this.safaricomApiService.makePayment(belcashCallbackData);
+    ;
   }
 }
