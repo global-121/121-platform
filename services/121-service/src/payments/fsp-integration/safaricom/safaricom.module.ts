fbbe9f2308fde58e7860ecc6d1d0fe13e6747036
TatevAccenture <tatev.kostanyan@accenture.com>
2023-06-16 15:15:48 +0300

    fixing dependansies

--- services/121-service/src/payments/fsp-integration/safaricom/safaricom.module.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/safaricom.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { SafaricomController } from './safaricom.controller';
 import { SafaricomService } from './safaricom.service';
+import { SafaricomApiService } from './safaricom.api.service';
+import { TransactionsModule } from '../../transactions/transactions.module';
+import { CustomHttpService } from '../../../shared/services/custom-http.service';
 
 @Module({
+ imports: [ TransactionsModule, HttpModule ],
   controllers: [SafaricomController],
-  providers: [SafaricomService],
+  providers: [SafaricomService, 
+              SafaricomApiService,
+              CustomHttpService,],
 })
 export class SafaricomModule {}
