43a4caf18d452af858cc4cbaa735b8f6c42a06e7
agnesesaulite <a.saulite@accenture.com>
2023-06-22 10:30:34 +0300

    fixes

--- interfaces/HO-Portal/src/app/models/payment.model.ts
+++ interfaces/HO-Portal/src/app/models/payment.model.ts
@@ -1,64 +1,65 @@
 import { Transaction } from './transaction.model';
 
 export class PaymentData {
-  id: number;
-  paymentDate: Date;
+id: number;
+paymentDate: Date;
 }
 
 export class Payment extends PaymentData {
-  statusOpen?: boolean;
-  isExportAvailable?: boolean;
+statusOpen?: boolean;
+isExportAvailable?: boolean;
 }
 
 export class LastPaymentResults {
-  error: number;
-  success: number;
-  waiting: number;
+error: number;
+success: number;
+waiting: number;
 }
 
 export class PaymentColumnDetail {
-  text: string;
-  paymentIndex?: number;
-  amount?: string;
-  hasMessageIcon?: boolean;
-  hasMoneyIconTable?: boolean;
-  errorMessage?: string;
-  status?: string;
+text: string;
+paymentIndex?: number;
+amount?: string;
+hasMessageIcon?: boolean;
+hasMoneyIconTable?: boolean;
+errorMessage?: string;
+status?: string;
 }
 
 export class PaymentRowDetail {
-  text: string;
-  paymentIndex?: number;
-  amount?: string;
-  hasMessageIcon?: boolean;
-  hasMoneyIconTable?: boolean;
-  transaction?: Transaction;
-  errorMessage?: string;
-  waiting?: boolean;
-  fsp?: string;
-  status?: string;
+text: string;
+additionalText?: string;
+paymentIndex?: number;
+amount?: string;
+hasMessageIcon?: boolean;
+hasMoneyIconTable?: boolean;
+transaction?: Transaction;
+errorMessage?: string;
+waiting?: boolean;
+fsp?: string;
+status?: string;
 }
 
 export class PopupPayoutDetails {
-  programId: number;
-  payment: number;
-  amount: number;
-  referenceId: string;
-  paNr: number;
-  currency: string;
+programId: number;
+payment: number;
+amount: number;
+referenceId: string;
+paNr: number;
+currency: string;
 }
 
 export class SinglePayoutDetails {
-  paNr: number;
-  amount: number;
-  currency: string;
-  multiplier: number;
-  programId: number;
-  payment: number;
-  referenceId: string;
+paNr: number;
+amount: number;
+currency: string;
+multiplier: number;
+programId: number;
+payment: number;
+referenceId: string;
 }
 
 export class TotalTransferAmounts {
-  public registrations: number;
-  public transferAmounts: number;
+public registrations: number;
+public transferAmounts: number;
 }
