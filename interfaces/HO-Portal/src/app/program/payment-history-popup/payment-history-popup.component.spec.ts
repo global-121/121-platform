3d444d79f9b1e633e12e38886906b2a356c45097
agnesesaulite <a.saulite@accenture.com>
2023-08-01 15:44:17 +0300

    fixes

--- interfaces/HO-Portal/src/app/program/payment-history-popup/payment-history-popup.component.spec.ts
+++ interfaces/HO-Portal/src/app/program/payment-history-popup/payment-history-popup.component.spec.ts
@@ -1,38 +1,24 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { ModalController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { provideMagicalMock } from 'src/app/mocks/helpers';
-import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
+import { PaymentHistoryAccordionComponent } from '../payment-history-accordion/payment-history-accordion.component';
 import { PaymentHistoryPopupComponent } from './payment-history-popup.component';
 
 describe('PaymentHistoryPopupComponent', () => {
   let component: PaymentHistoryPopupComponent;
   let fixture: ComponentFixture<PaymentHistoryPopupComponent>;
 
-  beforeEach(waitForAsync(() => {
-    const modalSpy = jasmine.createSpyObj('Modal', ['present']);
-    const modalCtrlSpy = jasmine.createSpyObj('ModalController', ['create']);
-    modalCtrlSpy.create.and.callFake(() => modalSpy);
-
-    TestBed.configureTestingModule({
-      declarations: [PaymentHistoryPopupComponent],
-      imports: [TranslateModule.forRoot(), HttpClientTestingModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [
-        {
-          provide: ModalController,
-          useValue: modalCtrlSpy,
-        },
-        provideMagicalMock(ProgramsServiceApiService),
-        {
-          provide: ModalController,
-          useValue: modalCtrlSpy,
-        },
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        IonicModule,
+        CommonModule,
+        TranslateModule,
+        PaymentHistoryAccordionComponent,
       ],
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PaymentHistoryPopupComponent);
