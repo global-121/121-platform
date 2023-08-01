01059736590c3447ed354e947740542b9792f222
agnesesaulite <a.saulite@accenture.com>
2023-08-01 15:59:42 +0300

    fixes

--- interfaces/HO-Portal/src/app/program/payment-history-popup/payment-history-popup.component.spec.ts
+++ interfaces/HO-Portal/src/app/program/payment-history-popup/payment-history-popup.component.spec.ts
@@ -1,8 +1,9 @@
-import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { PaymentHistoryAccordionComponent } from '../payment-history-accordion/payment-history-accordion.component';
+import { provideMagicalMock } from 'src/app/mocks/helpers';
+import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
 import { PaymentHistoryPopupComponent } from './payment-history-popup.component';
 
 describe('PaymentHistoryPopupComponent', () => {
@@ -10,12 +11,22 @@ describe('PaymentHistoryPopupComponent', () => {
   let fixture: ComponentFixture<PaymentHistoryPopupComponent>;
 
   beforeEach(async () => {
+    const modalSpy = jasmine.createSpyObj('Modal', ['present']);
+    const modalCtrlSpy = jasmine.createSpyObj('ModalController', ['create']);
+    modalCtrlSpy.create.and.callFake(() => modalSpy);
+
     await TestBed.configureTestingModule({
-      imports: [
-        IonicModule,
-        CommonModule,
-        TranslateModule,
-        PaymentHistoryAccordionComponent,
+      imports: [TranslateModule.forRoot(), HttpClientTestingModule],
+      providers: [
+        {
+          provide: ModalController,
+          useValue: modalCtrlSpy,
+        },
+        provideMagicalMock(ProgramsServiceApiService),
+        {
+          provide: ModalController,
+          useValue: modalCtrlSpy,
+        },
       ],
     }).compileComponents();
   });
