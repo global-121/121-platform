b6f683fe7fa07af2368b76fe5d84f6b42cae1512
agnesesaulite <a.saulite@accenture.com>
2023-08-01 16:53:04 +0300

    fixes

--- interfaces/HO-Portal/src/app/program/payment-history-popup/payment-history-popup.component.spec.ts
+++ interfaces/HO-Portal/src/app/program/payment-history-popup/payment-history-popup.component.spec.ts
@@ -1,19 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { provideMagicalMock } from 'src/app/mocks/helpers';
+import { PastPaymentsService } from 'src/app/services/past-payments.service';
+import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
 import { PaymentHistoryPopupComponent } from './payment-history-popup.component';
 
 describe('PaymentHistoryPopupComponent', () => {
   let component: PaymentHistoryPopupComponent;
   let fixture: ComponentFixture<PaymentHistoryPopupComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [IonicModule, FormsModule, SharedModule, CommonModule],
+  beforeEach(waitForAsync(() => {
+    const modalSpy = jasmine.createSpyObj('Modal', ['present']);
+    const modalCtrlSpy = jasmine.createSpyObj('ModalController', ['create']);
+    modalCtrlSpy.create.and.callFake(() => modalSpy);
+
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot(),
+        HttpClientTestingModule,
+        PaymentHistoryPopupComponent,
+        IonicModule,
+        CommonModule,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: ModalController,
+          useValue: modalCtrlSpy,
+        },
+        provideMagicalMock(ProgramsServiceApiService),
+        provideMagicalMock(TranslateService),
+        provideMagicalMock(PastPaymentsService),
+      ],
     }).compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PaymentHistoryPopupComponent);
