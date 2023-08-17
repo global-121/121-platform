8634ae26f286867836828c202b16b48b7ebe2cbd
arsforza <asforza@redcross.nl>
2023-08-17 16:35:44 +0200

    fix: map language enum to labels AB#23209

---
+++ interfaces/HO-Portal/src/app/services/enum.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EnumService {
+  constructor(private translate: TranslateService) {}
+
+  private enumerableAttributes = ['preferredLanguage'];
+
+  public isEnumerableAttribute(attributeName: string): boolean {
+    console.log('=== isEnumerableAttribute: ', attributeName);
+    return this.enumerableAttributes.includes(attributeName);
+  }
+
+  public getEnumLabel(attributeName: string, value: string): string {
+    switch (attributeName) {
+      case 'preferredLanguage':
+        return this.getPreferredLanguageLabel(value);
+    }
+  }
+
+  private getPreferredLanguageLabel(value: string): string {
+    if (!value || value === '-') {
+      return '-';
+    }
+    return this.translate.instant(
+      'page.program.program-people-affected.language.' + value,
+    );
+  }
+}
