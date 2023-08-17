808dccb161dd509c760eb75a62d876b9a7d7978b
Vladimir.Petkov <vp55329@bose.com>
2023-08-17 10:04:54 +0300

    fix: response message body

--- services/121-service/src/payments/fsp-integration/commercial-bank-ethiopia/commercial-bank-ethiopia.api.service.ts
+++ services/121-service/src/payments/fsp-integration/commercial-bank-ethiopia/commercial-bank-ethiopia.api.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { SoapService } from '../../../utils/soap/soap.service';
 import { CommercialBankEthiopiaMockService } from './commercial-bank-ethiopia.mock';
 import { CommercialBankEthiopiaTransactionResponse } from './dto/commercial-bank-ethiopia-transaction-response.dto';
-import { CommercialBankEthiopiaTransferResponse } from './dto/commercial-bank-ethiopia-transfer-response.dto';
 import { CommercialBankEthiopiaSoapElements } from './enum/commercial-bank-ethiopia.enum';
 
 @Injectable()
@@ -25,101 +24,9 @@ export class CommercialBankEthiopiaApiService {
           );
 
       console.log(responseBody, 'responseBody');
-      // Map the response to the CommercialBankEthiopiaTransferResponse DTO
-      const result: CommercialBankEthiopiaTransferResponse = {
-        resultCode: responseBody.IssueCardResponse.ResultCode._text,
-        resultDescription:
-          responseBody.IssueCardResponse.ResultDescription._text,
-        transactionId: responseBody.IssueCardResponse.TransactionId?._text,
-        successIndicator:
-          responseBody.IssueCardResponse.successIndicator?._text,
-        FundsTtransferType:
-          responseBody.IssueCardResponse.FUNDSTRANSFERType?._text,
-        transactionType: responseBody.IssueCardResponse.TRANSACTIONTYPE?._text,
-        debitAcctNo: responseBody.IssueCardResponse.DEBITACCTNO?._text,
-        currencyMktDr: responseBody.IssueCardResponse.CURRENCYMKTDR?._text,
-        debitCurrency: responseBody.IssueCardResponse.DEBITCURRENCY?._text,
-        debitAmount: responseBody.IssueCardResponse.DEBITAMOUNT?._text,
-        debitValueDate: responseBody.IssueCardResponse.DEBITVALUEDATE?._text,
-        debitTheirRef: {
-          reloadable:
-            responseBody.IssueCardResponse.DEBITTHEIRREF?.Reloadable?.map(
-              (item) => item._text,
-            ) ?? [],
-          refundable:
-            responseBody.IssueCardResponse.DEBITTHEIRREF?.Refundable?.map(
-              (item) => item._text,
-            ) ?? [],
-        },
-        creditAcctNo: responseBody.IssueCardResponse.CREDITACCTNO?._text,
-        currencyMktCr: responseBody.IssueCardResponse.CURRENCYMKTCR?._text,
-        creditCurrency: responseBody.IssueCardResponse.CREDITCURRENCY?._text,
-        creditValueDate: responseBody.IssueCardResponse.CREDITVALUEDATE?._text,
-        treasuryRate: responseBody.IssueCardResponse.TREASURYRATE?._text,
-        processingDate: responseBody.IssueCardResponse.PROCESSINGDATE?._text,
-        gOrderingCust: {
-          orderingCust:
-            responseBody.IssueCardResponse.gORDERINGCUST?.ORDERINGCUST?._text,
-        },
-        chargeComDisplay:
-          responseBody.IssueCardResponse.CHARGECOMDISPLAY?._text,
-        commissionCode: responseBody.IssueCardResponse.COMMISSIONCODE?._text,
-        chargeCode: responseBody.IssueCardResponse.CHARGECODE?._text,
-        baseCurrency: responseBody.IssueCardResponse.BASECURRENCY?._text,
-        profitCenterCust:
-          responseBody.IssueCardResponse.PROFITCENTRECUST?._text,
-        returnToDept: responseBody.IssueCardResponse.RETURNTODEPT?._text,
-        fedFunds: responseBody.IssueCardResponse.FEDFUNDS?._text,
-        positionType: responseBody.IssueCardResponse.POSITIONTYPE?._text,
-        amountDebited: responseBody.IssueCardResponse.AMOUNTDEBITED?._text,
-        amountCredited: responseBody.IssueCardResponse.AMOUNTCREDITED?._text,
-        customerRate: responseBody.IssueCardResponse.CUSTOMERRATE?._text,
-        gDeliveryOutRef: {
-          deliveryOutRef:
-            responseBody.IssueCardResponse.gDELIVERYOUTREF?.DELIVERYOUTREF?.map(
-              (item) => item._text,
-            ) ?? [],
-        },
-        creditCompCode: responseBody.IssueCardResponse.CREDITCOMPCODE?._text,
-        debitCompCode: responseBody.IssueCardResponse.DEBITCOMPCODE?._text,
-        locAmtDebited: responseBody.IssueCardResponse.LOCAMTDEBITED?._text,
-        locAmtCredited: responseBody.IssueCardResponse.LOCAMTCREDITED?._text,
-        custGroupLevel: responseBody.IssueCardResponse.CUSTGROUPLEVEL?._text,
-        debitCustomer: responseBody.IssueCardResponse.DEBITCUSTOMER?._text,
-        creditCustomer: responseBody.IssueCardResponse.CREDITCUSTOMER?._text,
-        drAdviceReqDyn: responseBody.IssueCardResponse.DRADVICEREQDYN?._text,
-        crAdviceReqDyn: responseBody.IssueCardResponse.CRADVICEREQDYN?._text,
-        chargedCustomer: responseBody.IssueCardResponse.CHARGEDCUSTOMER?._text,
-        totRecComm: responseBody.IssueCardResponse.TOTRECCOMM?._text,
-        totRecCommLcl: responseBody.IssueCardResponse.TOTRECCOMMLCL?._text,
-        totRecChg: responseBody.IssueCardResponse.TOTRECCHG?._text,
-        totRecChgLcl: responseBody.IssueCardResponse.TOTRECCHGLCL?._text,
-        rateFixing: responseBody.IssueCardResponse.RATEFIXING?._text,
-        totRecChgCrCcy: responseBody.IssueCardResponse.TOTRECCHGCRCCY?._text,
-        totSndChgCrCcy: responseBody.IssueCardResponse.TOTSNDCHGCRCCY?._text,
-        authDate: responseBody.IssueCardResponse.AUTHDATE?._text,
-        roundType: responseBody.IssueCardResponse.ROUNDTYPE?._text,
-        gStmtNos: {
-          stmtNos:
-            responseBody.IssueCardResponse.gSTMTNOS?.STMTNOS?.map(
-              (item) => item._text,
-            ) ?? [],
-        },
-        currNo: responseBody.IssueCardResponse.CURRNO?._text,
-        gInputter: {
-          inputter: responseBody.IssueCardResponse.gINPUTTER?.INPUTTER?._text,
-        },
-        gDateTime: {
-          dateTime: responseBody.IssueCardResponse.gDATETIME?.DATETIME?._text,
-        },
-        authoriser: responseBody.IssueCardResponse.AUTHORISER?._text,
-        coCode: responseBody.IssueCardResponse.COCODE?._text,
-        deptCode: responseBody.IssueCardResponse.DEPTCODE?._text,
-        remName: responseBody.IssueCardResponse.REMNAME?._text,
-        benName: responseBody.IssueCardResponse.BENNAME?._text,
-      };
+      console.log(responseBody.Status.messages, 'messages');
 
-      return result;
+      return responseBody;
     } catch (error) {
       console.log(error, 'creditTransfer');
       // Handle errors here
