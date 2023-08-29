c4388a6491916b33cd108ea01a27cf171f4f04fa
agnesesaulite <a.saulite@accenture.com>
2023-08-29 15:24:47 +0300

    payment accordion UI updates

--- interfaces/HO-Portal/src/app/enums/date-format.enum.ts
+++ interfaces/HO-Portal/src/app/enums/date-format.enum.ts
@@ -1,7 +1,7 @@
 export enum DateFormat {
   dateOnly = 'dd-MM-yyyy',
   dateOnlyReverse = 'yyyy-MM-dd',
-  dayAndTime = 'yyyy-MM-dd, HH:mm',
+  dayAndTime = 'dd-MM-yyyy, HH:mm',
   dayAndTimeAlternative = 'dd-MM-yy, HH:mm',
   monthOnly = 'yyyy-MM',
   withDayName = 'EEEE, dd-MM-yyyy, HH:mm',
