dc8c35d6ac5b29017d290a17e14e01c1ae089f50
agnesesaulite <a.saulite@accenture.com>
2023-09-04 17:24:36 +0300

    table status & UI

--- interfaces/HO-Portal/src/app/users/users-table/users-table.component.ts
+++ interfaces/HO-Portal/src/app/users/users-table/users-table.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { DateFormat } from 'src/app/enums/date-format.enum';
 import { User } from 'src/app/models/user.model';
 import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -14,6 +15,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
   styleUrls: ['./users-table.component.scss'],
 })
 export class UsersTableComponent implements OnInit {
+  DateFormat = DateFormat;
   rows = [];
 
   constructor(private programsService: ProgramsServiceApiService) {}
