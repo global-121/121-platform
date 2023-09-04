a21b9bcf1dc1f2fdd9c62879ca45721b6f05c861
agnesesaulite <a.saulite@accenture.com>
2023-09-04 13:20:45 +0300

    PR fixes

--- interfaces/HO-Portal/src/app/enums/date-format.enum.ts
+++ interfaces/HO-Portal/src/app/enums/date-format.enum.ts
@@ -1,7 +1,8 @@
 export enum DateFormat {
   dateOnly = 'dd-MM-yyyy',
   dateOnlyReverse = 'yyyy-MM-dd',
-  dayAndTime = 'dd-MM-yyyy, HH:mm',
+  dayAndTime = 'yyyy-MM-dd, HH:mm',
+  dayAndTimeReverse = 'dd-MM-yyyy, HH:mm',
   dayAndTimeAlternative = 'dd-MM-yy, HH:mm',
   monthOnly = 'yyyy-MM',
   withDayName = 'EEEE, dd-MM-yyyy, HH:mm',
