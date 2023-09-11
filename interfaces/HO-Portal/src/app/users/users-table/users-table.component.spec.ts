a4b83c38e14eb8a3f722bb1d40e9dafff2b11ade
agnesesaulite <a.saulite@accenture.com>
2023-09-11 10:56:23 +0300

    fix unit test

--- interfaces/HO-Portal/src/app/users/users-table/users-table.component.spec.ts
+++ interfaces/HO-Portal/src/app/users/users-table/users-table.component.spec.ts
@@ -1,15 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { UsersTableComponent } from './users-table.component';
 
 describe('UsersTableComponent', () => {
   let component: UsersTableComponent;
   let fixture: ComponentFixture<UsersTableComponent>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [UsersTableComponent],
-    });
+      imports: [
+        UsersTableComponent,
+        CommonModule,
+        IonicModule,
+        SharedModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+      ],
+      providers: [ProgramsServiceApiService],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(UsersTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
