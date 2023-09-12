b5f89bda5a6d4956792904033ac29f323d6ca880
RubenGeo <rubenavalk@gmail.com>
2023-09-12 14:46:13 +0200

    AB#23447 Search phone api test

---
+++ services/121-service/test/registrations/search-for-phone.test.ts
@@ -0,0 +1,90 @@
+import { HttpStatus } from '@nestjs/common';
+import { SeedScript } from '../../src/scripts/seed-script.enum';
+import { ProgramPhase } from '../../src/shared/enum/program-phase.model';
+import { changePhase } from '../helpers/program.helper';
+import {
+  importRegistrations,
+  searchRegistrationByPhoneNumber,
+} from '../helpers/registration.helper';
+import { getAccessToken, resetDB } from '../helpers/utility.helper';
+import { registrationVisa } from '../visa-card/visa-card.data';
+
+describe('Load PA table', () => {
+  describe('using the "attributes" query-parameter', () => {
+    let accessToken: string;
+    const programId = 3;
+    const registration1 = { ...registrationVisa };
+    const registration2 = { ...registrationVisa };
+    registration2.referenceId = '63e62864557597sade0d';
+    registration2.phoneNumber = '14155238888';
+    const registration3 = { ...registrationVisa };
+    registration3.referenceId = '63e62864557597e0as';
+
+    beforeEach(async () => {
+      await resetDB(SeedScript.nlrcMultiple);
+      accessToken = await getAccessToken();
+
+      await changePhase(2, ProgramPhase.registrationValidation, accessToken);
+
+      await changePhase(3, ProgramPhase.registrationValidation, accessToken);
+      // Arrange
+      await importRegistrations(
+        programId,
+        [registration1, registration2, registration3],
+        accessToken,
+      );
+    });
+
+    it('should only return return 1 registration', async () => {
+      // Act
+      const searchResponse = await searchRegistrationByPhoneNumber(
+        registration2.phoneNumber,
+        accessToken,
+      );
+
+      // Assert
+      expect(searchResponse.statusCode).toBe(HttpStatus.OK);
+      expect(searchResponse.body.length).toBe(1);
+    });
+
+    it('should only return return 2 registrations', async () => {
+      // Act
+      const searchResponse = await searchRegistrationByPhoneNumber(
+        registration1.phoneNumber,
+        accessToken,
+      );
+
+      // Assert
+      expect(searchResponse.statusCode).toBe(HttpStatus.OK);
+      expect(searchResponse.body.length).toBe(2);
+    });
+
+    it('should find 1 registration with phonenumber + notation', async () => {
+      // Act
+      const searchResponse = await searchRegistrationByPhoneNumber(
+        `+${registration2.phoneNumber}`,
+        accessToken,
+      );
+
+      // Assert
+      expect(searchResponse.statusCode).toBe(HttpStatus.OK);
+      expect(searchResponse.body.length).toBe(1);
+    });
+
+    it('should find registrations cross program', async () => {
+      //Arrange
+      const registration4 = { ...registrationVisa };
+      registration4.referenceId = '63e62864557597e0aasdas1123s';
+      await importRegistrations(2, [registration4], accessToken);
+      // Act
+      const searchResponse = await searchRegistrationByPhoneNumber(
+        `+${registration1.phoneNumber}`,
+        accessToken,
+      );
+
+      // Assert
+      expect(searchResponse.statusCode).toBe(HttpStatus.OK);
+      expect(searchResponse.body.length).toBe(3);
+    });
+  });
+});
