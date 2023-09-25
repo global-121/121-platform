ed9387797d09086678b2a40c44652b7da3746e2f
RubenGeo <rubenavalk@gmail.com>
2023-09-25 14:36:36 +0200

    Test max payment & payment completed AB#23733

---
+++ services/121-service/test/payment/payment-count-completed.test.ts
@@ -0,0 +1,113 @@
+import { HttpStatus } from '@nestjs/common';
+import { FspName } from '../../src/fsp/enum/fsp-name.enum';
+import { RegistrationStatusEnum } from '../../src/registration/enum/registration-status.enum';
+import { SeedScript } from '../../src/scripts/seed-script.enum';
+import { ProgramPhase } from '../../src/shared/enum/program-phase.model';
+import { StatusEnum } from '../../src/shared/enum/status.enum';
+import {
+  changePhase,
+  doPayment,
+  getTransactions,
+  waitForPaymentTransactionsToComplete,
+} from '../helpers/program.helper';
+import {
+  changePaStatus,
+  getRegistrations,
+  importRegistrations,
+} from '../helpers/registration.helper';
+import { getAccessToken, resetDB, waitFor } from '../helpers/utility.helper';
+
+describe('Do payment to 1 PA', () => {
+  const programId = 1;
+  const referenceIdAh = '63e62864557597e0d-AH';
+  const payment = 1;
+  const amount = 22;
+  const registrationAh = {
+    referenceId: referenceIdAh,
+    preferredLanguage: 'en',
+    paymentAmountMultiplier: 1,
+    nameFirst: 'John',
+    nameLast: 'Smith',
+    phoneNumber: '14155238886',
+    fspName: FspName.intersolveVoucherWhatsapp,
+    whatsappPhoneNumber: '14155238886',
+    maxPayments: 1,
+  };
+
+  describe('with FSP: Intersolve Voucher WhatsApp', () => {
+    let accessToken: string;
+
+    beforeEach(async () => {
+      await resetDB(SeedScript.nlrcMultiple);
+      accessToken = await getAccessToken();
+
+      await changePhase(
+        programId,
+        ProgramPhase.registrationValidation,
+        accessToken,
+      );
+      await changePhase(programId, ProgramPhase.inclusion, accessToken);
+      await changePhase(programId, ProgramPhase.payment, accessToken);
+    });
+
+    it('should set registration to complete', async () => {
+      // Arrange
+      await importRegistrations(programId, [registrationAh], accessToken);
+      await changePaStatus(programId, [referenceIdAh], 'include', accessToken);
+      const paymentReferenceIds = [referenceIdAh];
+
+      // Act
+      const doPaymentResponse = await doPayment(
+        programId,
+        payment,
+        amount,
+        paymentReferenceIds,
+        accessToken,
+      );
+
+      // Assert
+      await waitForPaymentTransactionsToComplete(
+        programId,
+        [referenceIdAh],
+        accessToken,
+        5000,
+      );
+
+      const getTransactionsRes = await getTransactions(
+        programId,
+        payment,
+        referenceIdAh,
+        accessToken,
+      );
+      const getTransactionsBody = getTransactionsRes.body;
+      // Wait for registration to be updated
+      const timeout = 60000; // Timeout in milliseconds
+      const interval = 1000; // Interval between retries in milliseconds
+      let elapsedTime = 0;
+      let getRegistration = null;
+      while (
+        (!getRegistration || getRegistration.paymentCount !== 1) &&
+        elapsedTime < timeout
+      ) {
+        const getRegistraitonRes = await getRegistrations(
+          programId,
+          null,
+          accessToken,
+        );
+        getRegistration = getRegistraitonRes.body.data[0];
+
+        await waitFor(interval);
+        elapsedTime += interval;
+      }
+      // Assert
+      expect(doPaymentResponse.status).toBe(HttpStatus.CREATED);
+      expect(doPaymentResponse.text).toBe(String(paymentReferenceIds.length));
+      expect(getTransactionsBody[0].status).toBe(StatusEnum.success);
+      expect(getTransactionsBody[0].errorMessage).toBe(null);
+
+      expect(getRegistration.status).toBe(RegistrationStatusEnum.completed);
+      expect(getRegistration.paymentCountRemaining).toBe(0);
+      expect(getRegistration.paymentCount).toBe(1);
+    });
+  });
+});
