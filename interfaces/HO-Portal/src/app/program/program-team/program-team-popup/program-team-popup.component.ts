ec73b360ad9ce56e702bf452d1f3522d88243b26
Kaspars Grinbergs <kgrinbergs@redcross.nl>
2023-09-27 10:54:06 +0300

    Search results displayed in popover

--- interfaces/HO-Portal/src/app/program/program-team/program-team-popup/program-team-popup.component.ts
+++ interfaces/HO-Portal/src/app/program/program-team/program-team-popup/program-team-popup.component.ts
@@ -4,6 +4,7 @@ import { IonicModule, ModalController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { ProgramsServiceApiService } from 'src/app/services/programs-service-api.service';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-program-team-popup',
@@ -12,17 +13,20 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     IonicModule,
     SharedModule,
-    TranslateModule
+    TranslateModule,
+    FormsModule
   ],
   templateUrl: './program-team-popup.component.html',
   styleUrls: ['./program-team-popup.component.scss'],
 })
 export class ProgramTeamPopupComponent {
   programId;
+  searchQuery: string = '';
 
   constructor(
     private modalController: ModalController,
-    private programsServiceApiService: ProgramsServiceApiService
+    private programsServiceApiService: ProgramsServiceApiService,
+    // private popoverController: PopoverController
   ){}
 
   searchResults: any[] = []; //TODO Should NOT be "any"
@@ -30,7 +34,23 @@ export class ProgramTeamPopupComponent {
   public async search(event: CustomEvent) {
     const searchTerm = event.detail.value.toLowerCase();
     this.searchResults = await this.programsServiceApiService.getUsersByName(this.programId, searchTerm);
+    // this.showPopoverOnSearch();
   }
+
+  updateSearchbarValue(selectedItem: string) {
+    this.searchQuery = selectedItem;
+  }
+
+  // async showPopoverOnSearch() {
+  //   // Create and display the popover when a search is performed
+  //   const popover = await this.popoverController.create({
+  //     component: ProgramTeamPopupComponent,
+  //     translucent: true,
+  //   });
+
+  //   return await popover.present();
+  // }
+
 
   public closeModal() {
     this.modalController.dismiss();
