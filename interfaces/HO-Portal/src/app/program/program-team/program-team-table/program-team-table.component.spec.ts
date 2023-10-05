c49549b5a348dd19303fd9036cefb1abdd6ec083
RubenGeo <rubenavalk@gmail.com>
2023-10-05 14:44:18 +0200

    Fix test  AB#24007

--- interfaces/HO-Portal/src/app/program/program-team/program-team-table/program-team-table.component.spec.ts
+++ interfaces/HO-Portal/src/app/program/program-team/program-team-table/program-team-table.component.spec.ts
@@ -5,6 +5,10 @@ import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProgramTeamTableComponent } from './program-team-table.component';
+import { provideMagicalMock } from '../../../mocks/helpers';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ProgramsServiceApiService } from '../../../services/programs-service-api.service';
+import { TranslatableStringService } from '../../../services/translatable-string.service';
 
 describe('ProgramTeamTableComponent', () => {
   let component: ProgramTeamTableComponent;
@@ -19,7 +23,13 @@ describe('ProgramTeamTableComponent', () => {
         SharedModule,
         TranslateModule.forRoot(),
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        provideMagicalMock(TranslatableStringService),
+        provideMagicalMock(ProgramsServiceApiService),
+      ],
     });
+
     fixture = TestBed.createComponent(ProgramTeamTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
