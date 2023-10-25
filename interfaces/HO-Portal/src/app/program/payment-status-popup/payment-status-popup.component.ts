c3c7fcac85ff312b12db98582cff69352a7a51f7
jannisvisser <jannisvisser@redcross.nl>
2023-10-25 16:09:21 +0200

    fix: handle error in single retry btn AB#24357

--- interfaces/HO-Portal/src/app/program/payment-status-popup/payment-status-popup.component.ts
+++ interfaces/HO-Portal/src/app/program/payment-status-popup/payment-status-popup.component.ts
@@ -233,7 +233,7 @@ export class PaymentStatusPopupComponent implements OnInit {
       },
       (err) => {
         console.log('err: ', err);
-        if (err && err.error && err.error.error) {
+        if (err && err.error && err.error.errors) {
           actionResult(this.alertController, this.translate, err.error.errors);
         }
         this.isInProgress = false;
