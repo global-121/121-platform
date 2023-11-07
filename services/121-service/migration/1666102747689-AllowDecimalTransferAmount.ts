462e98bf77f463e0f1e0716894ebc02da33ab84b
Elwin Schmitz <eschmitz@redcross.nl>
2023-11-07 11:20:01 +0100

    fix: Add missing awaits to query/save-calls

--- services/121-service/migration/1666102747689-AllowDecimalTransferAmount.ts
+++ services/121-service/migration/1666102747689-AllowDecimalTransferAmount.ts
@@ -32,7 +32,7 @@ export class AllowDecimalTransferAmount1666102747689
       `ALTER TABLE "121-service"."program" DROP COLUMN "fixedTransferValue_TEMP"`,
     );
 
-    queryRunner.query(
+    await queryRunner.query(
       `ALTER TABLE "121-service"."intersolve_barcode" RENAME COLUMN "amount" TO "amount_TEMP"`,
     );
     await queryRunner.query(
