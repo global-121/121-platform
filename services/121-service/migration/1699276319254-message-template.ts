e28b1feaf22b8aa8fda5b3d800c2ddee231a1ce6
jannisvisser <jannisvisser@redcross.nl>
2023-11-21 09:26:56 +0100

    chore: drop notifications col from program AB#24807

--- services/121-service/migration/1699276319254-message-template.ts
+++ services/121-service/migration/1699276319254-message-template.ts
@@ -1,18 +1,33 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class MessageTemplate1699276319254 implements MigrationInterface {
-    name = 'MessageTemplate1699276319254'
+  name = 'MessageTemplate1699276319254';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "121-service"."message_template" ("id" SERIAL NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "type" character varying NOT NULL, "language" character varying NOT NULL, "message" character varying NOT NULL, "isWhatsappTemplate" boolean NOT NULL, "programId" integer NOT NULL, CONSTRAINT "PK_616800da109c721fb4dd2019a9b" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE INDEX "IDX_927cc52d451d253f66fcc9d659" ON "121-service"."message_template" ("created") `);
-        await queryRunner.query(`ALTER TABLE "121-service"."message_template" ADD CONSTRAINT "FK_55ebe1d2e603be11a0cfc97372f" FOREIGN KEY ("programId") REFERENCES "121-service"."program"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "121-service"."message_template" ("id" SERIAL NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "type" character varying NOT NULL, "language" character varying NOT NULL, "message" character varying NOT NULL, "isWhatsappTemplate" boolean NOT NULL, "programId" integer NOT NULL, CONSTRAINT "PK_616800da109c721fb4dd2019a9b" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_927cc52d451d253f66fcc9d659" ON "121-service"."message_template" ("created") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."message_template" ADD CONSTRAINT "FK_55ebe1d2e603be11a0cfc97372f" FOREIGN KEY ("programId") REFERENCES "121-service"."program"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program" DROP COLUMN "notifications"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "121-service"."message_template" DROP CONSTRAINT "FK_55ebe1d2e603be11a0cfc97372f"`);
-        await queryRunner.query(`DROP INDEX "121-service"."IDX_927cc52d451d253f66fcc9d659"`);
-        await queryRunner.query(`DROP TABLE "121-service"."message_template"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."message_template" DROP CONSTRAINT "FK_55ebe1d2e603be11a0cfc97372f"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "121-service"."IDX_927cc52d451d253f66fcc9d659"`,
+    );
+    await queryRunner.query(`DROP TABLE "121-service"."message_template"`);
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program" ADD COLUMN IF NOT EXISTS "notifications" json`,
+    );
+  }
 }
