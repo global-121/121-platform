3ec6647678302dace39621733a19e4a051f74a8d
Ruben <vandervalk@geoit.nl>
2023-12-11 17:10:00 +0100

    Test import registration AB#25303

--- services/121-service/test/registrations/import-registration.test.ts
+++ services/121-service/test/registrations/import-registration.test.ts
@@ -4,29 +4,39 @@ import {
   importRegistrations,
   searchRegistrationByReferenceId,
 } from '../helpers/registration.helper';
-import { getAccessToken, resetDB } from '../helpers/utility.helper';
+import {
+  getAccessToken,
+  getAccessTokenScoped,
+  resetDB,
+} from '../helpers/utility.helper';
 import { waitFor } from '../../src/utils/waitFor.helper';
 import {
   referenceIdVisa,
   registrationVisa,
 } from '../../seed-data/mock/visa-card.data';
+import {
+  programIdOCW,
+  programIdPV,
+  referenceId1PV,
+  registration1PV,
+  registration2PV,
+} from './pagination/pagination-data';
+import { DebugScope } from '../../src/scripts/enum/debug-scope.enum';
 
 describe('Import a registration', () => {
-  const programId = 3;
-
   let accessToken: string;
 
   beforeEach(async () => {
     await resetDB(SeedScript.nlrcMultiple);
-    accessToken = await getAccessToken();
 
     await waitFor(2_000);
   });
 
   it('should import registrations', async () => {
+    accessToken = await getAccessToken();
     // Act
     const response = await importRegistrations(
-      programId,
+      programIdOCW,
       [registrationVisa],
       accessToken,
     );
@@ -35,7 +45,7 @@ describe('Import a registration', () => {
 
     const result = await searchRegistrationByReferenceId(
       referenceIdVisa,
-      programId,
+      programIdOCW,
       accessToken,
     );
     const registration = result.body.data[0];
@@ -50,5 +60,61 @@ describe('Import a registration', () => {
         expect(registration[key]).toBe(registrationVisa[key]);
       }
     }
+  });
+
+  it('should import registration scoped', async () => {
+    const accessToken = await getAccessTokenScoped(DebugScope.Zeeland);
+
+    // Act
+    const response = await importRegistrations(
+      programIdPV,
+      [registration1PV],
+      accessToken,
+    );
+
+    expect(response.statusCode).toBe(HttpStatus.CREATED);
+
+    const result = await searchRegistrationByReferenceId(
+      referenceId1PV,
+      programIdPV,
+      accessToken,
+    );
+    const registrationResult = result.body.data[0];
+    console.log(
+      '🚀 ~ file: import-registration.test.ts:81 ~ it ~ result.body:',
+      result.body,
+    );
+    for (const key in registrationVisa) {
+      if (key === 'fspName') {
+        // eslint-disable-next-line jest/no-conditional-expect
+        expect(registrationResult['financialServiceProvider']).toBe(
+          registration1PV[key],
+        );
+      } else {
+        // eslint-disable-next-line jest/no-conditional-expect
+        expect(registrationResult[key]).toBe(registration1PV[key]);
+      }
+    }
+  });
+
+  it('should not import any registration if one of them has different scope than user', async () => {
+    const accessToken = await getAccessTokenScoped(DebugScope.Zeeland);
+
+    // Act
+    const response = await importRegistrations(
+      programIdPV,
+      [registration1PV, registration2PV],
+      accessToken,
+    );
+
+    expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+
+    const result = await searchRegistrationByReferenceId(
+      referenceId1PV,
+      programIdPV,
+      accessToken,
+    );
+    const registrationsResult = result.body.data;
+    expect(registrationsResult).toHaveLength(0);
   });
 });
