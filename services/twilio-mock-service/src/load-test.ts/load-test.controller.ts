c07ad8fbafe015e3394a7029ff1a546de8f09619
jannisvisser <jannisvisser@redcross.nl>
2023-12-12 14:43:22 +0100

    chore: cleanup AB#25180

--- services/twilio-mock-service/src/load-test.ts/load-test.controller.ts
+++ services/twilio-mock-service/src/load-test.ts/load-test.controller.ts
@@ -1,16 +1,18 @@
-import {  Controller, Get } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import { Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoadTestService } from './load-test.service';
 
 @Controller()
 export class LoadTestController {
   public constructor(private readonly loadTestService: LoadTestService) {}
 
-  @ApiOperation({ summary: 'Load test' })
-  @Get('load-test/registrations')
+  @ApiTags('2. load-test')
+  @ApiOperation({
+    summary:
+      'Load test "import registrations" (Not actually related to Twilio, to be moved)',
+  })
+  @Post('load-test/import-registrations')
   public async loadTest(): Promise<void> {
     return await this.loadTestService.loadTest();
   }
-
-
 }
