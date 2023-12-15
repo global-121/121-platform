96f303789f2ba050c3e512d060f45d8d7449e41b
jannisvisser <jannisvisser@redcross.nl>
2023-12-15 13:49:12 +0100

    fix: make check more generic AB#25381

--- interfaces/PA-App/src/app/shared/phone-number-input/phone-number-input.component.ts
+++ interfaces/PA-App/src/app/shared/phone-number-input/phone-number-input.component.ts
@@ -89,13 +89,15 @@ export class PhoneNumberInputComponent {
       },
       (error) => {
         console.log('lookupPhoneNumber error: ', error);
-        if (error?.error?.message[0]?.constraints?.isLength !== undefined) {
-          // if the phone number is too short or too long, it is invalid
-          // NOTE: this check is specifically tied to the constraints in phone-number.dto.ts
-          isValid = false;
-        } else {
-          // Allow any input as valid in case of errors during lookup
-          isValid = this.defaultValidity;
+        for (const element of error?.error?.message ?? []) {
+          if (Object.keys(element?.constraints).length > 0) {
+            // if the call fails because of constraints on the 121 endpoint (e.g. too short or too long), it is invalid
+            isValid = false;
+            break; // break out of loop after 1st constraint, so that isValid is not overwritten to true again
+          } else {
+            // Allow any input as valid in case of other errors during lookup
+            isValid = this.defaultValidity;
+          }
         }
       },
     );
