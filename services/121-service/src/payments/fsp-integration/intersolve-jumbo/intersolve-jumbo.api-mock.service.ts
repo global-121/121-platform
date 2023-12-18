a391e04257ea94c075f5e9b546d17941ae9ecf91
arsforza <asforza@redcross.nl>
2023-12-18 11:17:49 +0100

    chore: fix prettier for service AB#25212

--- services/121-service/src/payments/fsp-integration/intersolve-jumbo/intersolve-jumbo.api-mock.service.ts
+++ services/121-service/src/payments/fsp-integration/intersolve-jumbo/intersolve-jumbo.api-mock.service.ts
@@ -32,9 +32,10 @@ export class IntersolveJumboApiMockService {
           IntersolveJumboResultCode.InvalidOrderLine;
         response[
           'tns:CreatePreOrderResponse'
-        ].WebserviceRequest.ResultDescription._cdata = `Error found in OrderLine ${
-          index + 1
-        }: Geconstateerde fouten: -STAD ongeldig! (Stad is niet ingevuld).`;
+        ].WebserviceRequest.ResultDescription._cdata =
+          `Error found in OrderLine ${
+            index + 1
+          }: Geconstateerde fouten: -STAD ongeldig! (Stad is niet ingevuld).`;
         return response;
       } else if (
         element.lastName?.toLowerCase().includes('mock-fail-create-order')
@@ -45,9 +46,10 @@ export class IntersolveJumboApiMockService {
           IntersolveJumboResultCode.InvalidOrderLine;
         response[
           'tns:CreatePreOrderResponse'
-        ].WebserviceRequest.ResultDescription._cdata = `Error found in OrderLine ${
-          index + 1
-        }: Mock failed the create-pre-order step`;
+        ].WebserviceRequest.ResultDescription._cdata =
+          `Error found in OrderLine ${
+            index + 1
+          }: Mock failed the create-pre-order step`;
         return response;
       }
     }
