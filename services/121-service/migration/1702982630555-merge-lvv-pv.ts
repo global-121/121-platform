b37872e11e21da71d8ecbb8a65f9e07ef0dbfd54
Ruben <vandervalk@geoit.nl>
2023-12-19 16:25:14 +0100

    Update latest transaction id AB#25451

--- services/121-service/migration/1702982630555-merge-lvv-pv.ts
+++ services/121-service/migration/1702982630555-merge-lvv-pv.ts
@@ -45,6 +45,20 @@ export class MergeLvvPv1702982630555 implements MigrationInterface {
         payment = payment + ${difference}
       where
         "programId" = 2;`);
+
+    // Update lasest transaction entity
+    await queryRunner.query(`
+      INSERT INTO "121-service"."latest_transaction" ("payment", "registrationId", "transactionId")
+      SELECT t.payment, t."registrationId", t.id AS transactionId
+      FROM (
+          SELECT payment, "registrationId", MAX(created) AS max_created
+          FROM "121-service"."transaction"
+          GROUP BY payment, "registrationId"
+      ) AS latest_transactions
+      INNER JOIN "121-service"."transaction" AS t
+          ON t.payment = latest_transactions.payment
+          AND t."registrationId" = latest_transactions."registrationId"
+          AND t.created = latest_transactions.max_created;`);
   }
 
   private async updateTemplates(queryRunner: QueryRunner) {
