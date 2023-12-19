26a9e45e9b82ef2ea055f8e996db26db321a7af6
Ruben <vandervalk@geoit.nl>
2023-12-19 10:48:35 +0100

    AB#25427 Also get in combi with filter

--- services/121-service/test/programs/registrations/scoped.test.ts
+++ services/121-service/test/programs/registrations/scoped.test.ts
@@ -77,5 +77,47 @@ describe('Registrations - [Scoped]', () => {
     const data = getRegistrationsResponse.body.data;
     expect(getRegistrationsResponse.status).toBe(HttpStatus.OK);
     expect(data.length).toBe(2);
+
+    const expectedReferenceIds = [
+      registrationScopedGoesPv.referenceId,
+      registrationScopedMiddelburgPv.referenceId,
+    ];
+
+    // Also check if the right referenceIds are in the transactions
+    expect(data.map((r) => r.referenceId).sort()).toEqual(
+      expectedReferenceIds.sort(),
+    );
+  });
+
+  it('should return all filtered registrations from 1 program within the scope of the requesting user', async () => {
+    // Arrange
+    const testScope = DebugScope.Zeeland;
+    accessToken = await getAccessTokenScoped(testScope);
+
+    // Act
+    // Act
+    // 8 registrations in total are registered
+    // 4 registrations are in include in program PV
+    // 2 registrations are in include in program PV and are in the scope (Zeeland) of the requesting user
+    // 1 of those 2 registrations has a nameFirst that has an 'a'
+    const getRegistrationsResponse = await getServer()
+      .get(`/programs/${PvProgramId}/registrations`)
+      .set('Cookie', [accessToken])
+      .query({
+        ['filter.nameFirst']: `$ilike:a`,
+      })
+      .send();
+
+    // Assert
+    const data = getRegistrationsResponse.body.data;
+    expect(getRegistrationsResponse.status).toBe(HttpStatus.OK);
+    expect(data.length).toBe(1);
+
+    const expectedReferenceIds = [registrationScopedGoesPv.referenceId];
+
+    // Also check if the right referenceIds are in the transactions
+    expect(data.map((r) => r.referenceId).sort()).toEqual(
+      expectedReferenceIds.sort(),
+    );
   });
 });
