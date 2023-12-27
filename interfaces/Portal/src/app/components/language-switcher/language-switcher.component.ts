4cb4ecd082ef9fe0154f1812c28692828fd534f7
arsforza <asforza@redcross.nl>
2023-12-27 16:36:32 +0100

    feat: create component AB#25508

---
+++ interfaces/Portal/src/app/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import {
+  LanguageOption,
+  LanguageService,
+} from '../../services/language.service';
+
+@Component({
+  selector: 'app-language-switcher',
+  templateUrl: './language-switcher.component.html',
+  styleUrls: ['./language-switcher.component.css'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+})
+export class LanguageSwitcherComponent implements OnInit {
+  public languages: LanguageOption[];
+
+  constructor(private languageService: LanguageService) {}
+  ngOnInit(): void {
+    if (!this.languageService) {
+      this.languages = [];
+      return;
+    }
+    this.languages = this.languageService.getLanguages();
+  }
+
+  public selectLanguage($event) {
+    this.languageService.changeLanguage($event.detail.value);
+  }
+}
