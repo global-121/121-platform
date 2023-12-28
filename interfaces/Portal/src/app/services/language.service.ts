967f7889aea70a9dbea5f15fddf07e870402ef57
arsforza <asforza@redcross.nl>
2023-12-28 14:16:13 +0100

    feat: keep language on refresh AB#25523

--- interfaces/Portal/src/app/services/language.service.ts
+++ interfaces/Portal/src/app/services/language.service.ts
@@ -11,10 +11,25 @@ export class LanguageOption {
   providedIn: 'root',
 })
 export class LanguageService {
-  private DEFAULT_LANGUAGE = 'en';
+  private DEFAULT_LANGUAGE_KEY = 'en';
+  private DEFAULT_LANGUAGE_LABEL = 'ENGLISH';
+
+  private selectedLanguage: string;
 
   constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang(this.DEFAULT_LANGUAGE);
+    if (!this.translate) {
+      return;
+    }
+    this.translate.setDefaultLang(this.DEFAULT_LANGUAGE_KEY);
+
+    if (!this.selectedLanguage && !this.getStoredLanguage()) {
+      this.selectedLanguage = this.DEFAULT_LANGUAGE_KEY;
+      this.storeLanguage();
+    }
+
+    this.selectedLanguage = this.getStoredLanguage();
+
+    this.translate.use(this.selectedLanguage);
   }
 
   public getLanguages(): LanguageOption[] {
@@ -23,7 +38,11 @@ export class LanguageService {
     }
     const allLocales = environment.locales.trim().split(/\s*,\s*/);
     if (!allLocales || allLocales.length === 0) {
-      return [this.getLanguageOption(this.DEFAULT_LANGUAGE)];
+      return [
+        this.getLanguageOption(this.DEFAULT_LANGUAGE_KEY, {
+          _languageLabel: this.DEFAULT_LANGUAGE_LABEL,
+        }),
+      ];
     }
 
     const languages = [];
@@ -32,8 +51,7 @@ export class LanguageService {
     for (const locale of allLocales) {
       this.translate.getTranslation(locale).subscribe({
         next: (localeObject) => {
-          languages.push(this.getLanguageOption(locale));
-          console.log(localeObject);
+          languages.push(this.getLanguageOption(locale, localeObject));
         },
         error: this.handleMissingFile,
       });
@@ -47,18 +65,32 @@ export class LanguageService {
       return;
     }
     this.translate.use(languageKey);
+    this.selectedLanguage = languageKey;
+    this.storeLanguage();
+    window.location.reload();
+    document.documentElement.dir = this.translate.instant('_dir');
   }
 
   private handleMissingFile(error) {
     console.warn(error);
   }
 
-  private getLanguageOption(locale: string) {
+  private getLanguageOption(locale: string, localeObject: any) {
     return {
       key: locale,
-      name: this.translate.instant(
-        `page.program.program-people-affected.language.${locale}`,
-      ),
+      name: localeObject['_languageLabel'],
     };
+  }
+
+  public getSelectedLanguage(): string {
+    return this.selectedLanguage;
+  }
+
+  private storeLanguage() {
+    localStorage.setItem('selectedLanguage', this.selectedLanguage);
+  }
+
+  private getStoredLanguage(): string {
+    return localStorage.getItem('selectedLanguage');
   }
 }
