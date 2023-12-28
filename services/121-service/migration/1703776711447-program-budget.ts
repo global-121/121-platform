6038c8dce6d6349497be1b87425fc495f87dc9ad
Ruben <vandervalk@geoit.nl>
2023-12-28 17:59:00 +0100

    lint

--- services/121-service/migration/1703776711447-program-budget.ts
+++ services/121-service/migration/1703776711447-program-budget.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ProgramBudget1703776711447 implements MigrationInterface {
-    name = 'ProgramBudget1703776711447'
+  name = 'ProgramBudget1703776711447';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "121-service"."program" ADD "budget" integer`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program" ADD "budget" integer`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "121-service"."program" DROP COLUMN "budget"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."program" DROP COLUMN "budget"`,
+    );
+  }
 }
