8927299e2fa0db657df2b5a28fe9d1990e5e3007
arsforza <asforza@redcross.nl>
2024-01-04 14:49:34 +0100

    feat: update styling AB#25585

--- interfaces/Portal/src/app/program/export-select/export-select.component.ts
+++ interfaces/Portal/src/app/program/export-select/export-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ExportType } from '../../models/export-type.model';
 import { ProgramPhase } from '../../models/program.model';
 
@@ -7,7 +7,7 @@ import { ProgramPhase } from '../../models/program.model';
   templateUrl: './export-select.component.html',
   styleUrls: ['./export-select.component.css'],
 })
-export class ExportSelectComponent {
+export class ExportSelectComponent implements OnInit {
   @Input()
   public programId: number;
 
@@ -16,15 +16,29 @@ export class ExportSelectComponent {
 
   public isPopoverOpen = false;
 
-  public options = [
-    ExportType.filteredTable,
-    ExportType.allPeopleAffected,
-    ExportType.duplicates,
-    ExportType.selectedForValidation,
-    ExportType.paDataChanges,
-  ];
+  private optionsPerPhase = {
+    [ProgramPhase.registrationValidation]: [
+      ExportType.filteredTable,
+      ExportType.allPeopleAffected,
+      ExportType.duplicates,
+      ExportType.selectedForValidation,
+      ExportType.paDataChanges,
+    ],
+    [ProgramPhase.inclusion]: [ExportType.filteredTable],
+    [ProgramPhase.payment]: [ExportType.filteredTable],
+  };
+
+  public options = [];
 
   constructor() {}
+
+  ngOnInit(): void {
+    if (!this.thisPhase) {
+      return;
+    }
+
+    this.options = this.optionsPerPhase[this.thisPhase];
+  }
 
   public togglePopover() {
     this.isPopoverOpen = !this.isPopoverOpen;
