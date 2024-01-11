a17971a869b202195645db329c1c37615fcbc9b4
Elwin Schmitz <ik@elwin.nu>
2024-01-11 13:23:02 +0100

    fix: Add fallback to phone-number value to make the ENV-variables' existence non-blocking/breaking

--- services/twilio-mock-service/src/twilio/twilio.dto.ts
+++ services/twilio-mock-service/src/twilio/twilio.dto.ts
@@ -114,7 +114,9 @@ export class TwilioIncomingCallbackDto {
   public WaId: string;
 
   @ApiProperty({
-    example: formatWhatsAppNumber(process.env.TWILIO_WHATSAPP_NUMBER),
+    example: formatWhatsAppNumber(
+      process.env.TWILIO_WHATSAPP_NUMBER || '31600000000',
+    ),
   })
   @IsString()
   @IsOptional()
