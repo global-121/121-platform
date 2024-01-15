02ad105c4da4eadebfffb6f17bbcd3e87e036e8b
Ruben <vandervalk@geoit.nl>
2024-01-15 09:36:59 +0100

    AB#25804 queue incoming whatsapp

--- services/121-service/src/notifications/message-incoming/message-incoming.module.ts
+++ services/121-service/src/notifications/message-incoming/message-incoming.module.ts
@@ -14,7 +14,9 @@ import { RegistrationEntity } from '../../registration/registration.entity';
 import { AzureLogService } from '../../shared/services/azure-log.service';
 import { UserModule } from '../../user/user.module';
 import { AuthMiddlewareTwilio } from '../auth.middlewareTwilio';
+import { QueueNameMessageCallBack } from '../enum/queue.names.enum';
 import { MessageTemplateModule } from '../message-template/message-template.module';
+import { MessageIncomingProcessor } from '../processors/message-incoming.processor';
 import { MessageStatusCallbackProcessor } from '../processors/message-status-callback.processor';
 import { QueueMessageModule } from '../queue-message/queue-message.module';
 import { TwilioMessageEntity } from '../twilio.entity';
@@ -42,7 +44,7 @@ import { MessageIncomingService } from './message-incoming.service';
     QueueMessageModule,
     MessageTemplateModule,
     BullModule.registerQueue({
-      name: 'messageStatusCallback',
+      name: QueueNameMessageCallBack.status,
       processors: [
         {
           path: 'src/notifications/processors/message-status-callback.processor.ts',
@@ -54,10 +56,24 @@ import { MessageIncomingService } from './message-incoming.service';
         duration: 1000, // per duration in milliseconds
       },
     }),
+    BullModule.registerQueue({
+      name: QueueNameMessageCallBack.incomingMessage,
+      processors: [
+        {
+          path: 'src/notifications/processors/message-incoming.processor.ts',
+          concurrency: 2,
+        },
+      ],
+      limiter: {
+        max: 10, // Max number of jobs processed
+        duration: 1000, // per duration in milliseconds
+      },
+    }),
   ],
   providers: [
     MessageIncomingService,
     MessageStatusCallbackProcessor,
+    MessageIncomingProcessor,
     AzureLogService,
   ],
   controllers: [MessageIncomingController],
---
+++ services/121-service/src/notifications/processors/message-incoming.processor.ts
@@ -0,0 +1,26 @@
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
+import { AzureLogService } from '../../shared/services/azure-log.service';
+import {
+  ProcessName,
+  QueueNameMessageCallBack,
+} from '../enum/queue.names.enum';
+import { MessageIncomingService } from '../message-incoming/message-incoming.service';
+@Processor(QueueNameMessageCallBack.incomingMessage)
+export class MessageIncomingProcessor {
+  constructor(
+    private readonly messageIncomingService: MessageIncomingService,
+    private readonly azureLogService: AzureLogService,
+  ) {}
+
+  @Process(ProcessName.whatsapp)
+  async handleIncomingWhatsapp(job: Job): Promise<void> {
+    const callbackData = job.data;
+    await this.messageIncomingService
+      .processIncomingWhatsapp(callbackData)
+      .catch((err) => {
+        this.azureLogService.logError(err, false);
+        throw err;
+      });
+  }
+}
