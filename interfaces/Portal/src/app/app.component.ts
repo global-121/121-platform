f785a4346bf9aaf8887799f8af24461ad24e650b
Elwin Schmitz <eschmitz@redcross.nl>
2024-01-17 16:27:19 +0100

    fix: Make sure to load Language-Loading/switching as early as possible AB#25844

--- interfaces/Portal/src/app/app.component.ts
+++ interfaces/Portal/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { LanguageService } from './services/language.service';
 import { LoggingService } from './services/logging.service';
 
 @Component({
@@ -7,7 +8,10 @@ import { LoggingService } from './services/logging.service';
   templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  constructor(private loggingService: LoggingService) {
+  constructor(
+    public languageService: LanguageService, // Required to load as early as possible in the lifecycle of the page to prevent incorrect languages shown in some components
+    private loggingService: LoggingService,
+  ) {
     if (this.loggingService.appInsightsEnabled) {
       this.loggingService.logPageView();
     }
