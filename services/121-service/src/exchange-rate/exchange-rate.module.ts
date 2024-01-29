86ab11f4b9a100ac7f6c50772d2f5335ea6560b1
arsforza <asforza@redcross.nl>
2024-01-29 14:41:31 +0100

    fix: lint + prettier

--- services/121-service/src/exchange-rate/exchange-rate.module.ts
+++ services/121-service/src/exchange-rate/exchange-rate.module.ts
@@ -1,9 +1,9 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { HttpModule } from '@nestjs/axios';
-import { CustomHttpService } from '../shared/services/custom-http.service';
 import { GuardsService } from '../guards/guards.service';
 import { ProgramEntity } from '../programs/program.entity';
+import { CustomHttpService } from '../shared/services/custom-http.service';
 import { UserModule } from '../user/user.module';
 import { ExchangeRateEntity } from './exchange-rate.entity';
 import { ExchangeRateService } from './exchange-rate.service';
