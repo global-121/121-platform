96c62f8bc6f2444736d3cf7986202eb70a42f3e4
arsforza <asforza@redcross.nl>
2024-01-31 17:29:04 +0100

    test: add queryBuilder mock helper AB#26152

---
+++ services/121-service/src/utils/createQueryBuilderMock.helper.ts
@@ -0,0 +1,32 @@
+export interface QueryBuilderMock {
+  where: () => QueryBuilderMock;
+  andWhere: () => QueryBuilderMock;
+  select: () => QueryBuilderMock;
+  addSelect: () => QueryBuilderMock;
+  leftJoin: () => QueryBuilderMock;
+  getMany?: () => any;
+  getRawMany?: () => any;
+  distinct?: () => QueryBuilderMock;
+}
+
+export function generateMockCreateQueryBuilder(
+  dbQueryResult?: any[],
+  options: any = {},
+): QueryBuilderMock {
+  const mock: QueryBuilderMock = {
+    select: () => mock,
+    addSelect: () => mock,
+    where: () => mock,
+    andWhere: () => mock,
+    leftJoin: () => mock,
+    distinct: () => mock,
+  };
+
+  if (options.useGetMany) {
+    mock.getMany = () => dbQueryResult;
+  } else {
+    mock.getRawMany = () => dbQueryResult;
+  }
+
+  return mock;
+}
