07d8ed89182fe50493285cff4f47f687101ef581
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-02-13 14:32:24 +0100

    Test: test mappers AB#26362

---
+++ services/121-service/src/actions/utils/action.mapper.spec.ts
@@ -0,0 +1,65 @@
+import { ProgramEntity } from '../../programs/program.entity';
+import { UserOwnerDto } from '../../user/dto/user-owner.dto';
+import { UserType } from '../../user/user-type-enum';
+import { UserEntity } from '../../user/user.entity';
+import { ActionEntity, AdditionalActionType } from '../action.entity';
+import { ActionReturnDto } from '../dto/action-return.dto';
+import { ActionMapper } from './action.mapper';
+
+describe('Action mapper', () => {
+  it('should map to correct object', async () => {
+    // Arrange
+    const userId = 1;
+    const actionId = 1;
+    const username = 'test@example.org';
+    const createdDate = new Date();
+    const user: UserEntity = {
+      id: userId,
+      username: username,
+      password: 'testPassword',
+      programAssignments: [],
+      actions: [],
+      registrations: [],
+      personAffectedAppData: [],
+      changes: [],
+      notes: [],
+      userType: UserType.aidWorker,
+      admin: false,
+      cascadeDelete: async () => {},
+      salt: 'salt',
+      active: true,
+      lastLogin: new Date(),
+      hashPassword: async () => {},
+      deleteAllOneToMany: async () => {},
+      deleteOneToMany: async () => {},
+      created: createdDate,
+      updated: new Date(),
+    };
+    const actionEntity: ActionEntity = {
+      id: actionId,
+      actionType: AdditionalActionType.importPeopleAffected,
+      user: user,
+      program: {} as ProgramEntity,
+      userId: userId,
+      created: createdDate,
+      updated: new Date(),
+    };
+
+    const expectedUserOwnerResult: UserOwnerDto = {
+      id: userId,
+      username: username,
+    };
+    const expectedResult: ActionReturnDto = {
+      id: actionId,
+      actionType: AdditionalActionType.importPeopleAffected,
+      user: expectedUserOwnerResult,
+      created: createdDate,
+    };
+
+    // Act
+    const mappedAction = ActionMapper.entityToActionReturnDto(actionEntity);
+
+    // Assert
+    expect(mappedAction).toEqual(expectedResult);
+  });
+});
---
+++ services/121-service/src/user/utils/user.mapper.spec.ts
@@ -0,0 +1,45 @@
+import { UserOwnerDto } from '../dto/user-owner.dto';
+import { UserType } from '../user-type-enum';
+import { UserEntity } from '../user.entity';
+import { UserMapper } from './user.mapper';
+
+describe('User mapper', () => {
+  it('should map to correct object', async () => {
+    // Arrange
+    const userId = 1;
+    const username = 'test@example.org';
+    const user: UserEntity = {
+      id: userId,
+      username: username,
+      password: 'testPassword',
+      programAssignments: [],
+      actions: [],
+      registrations: [],
+      personAffectedAppData: [],
+      changes: [],
+      notes: [],
+      userType: UserType.aidWorker,
+      admin: false,
+      cascadeDelete: async () => {},
+      salt: 'salt',
+      active: true,
+      lastLogin: new Date(),
+      hashPassword: async () => {},
+      deleteAllOneToMany: async () => {},
+      deleteOneToMany: async () => {},
+      created: new Date(),
+      updated: new Date(),
+    };
+
+    const expectedResult: UserOwnerDto = {
+      id: userId,
+      username: username,
+    };
+
+    // Act
+    const mappedUser = UserMapper.entityToOwner(user);
+
+    // Assert
+    expect(mappedUser).toEqual(expectedResult);
+  });
+});
