6c3189089ab3b505beaf660e0484f1960c100d71
Ruben <vandervalk@geoit.nl>
2024-02-15 16:00:30 +0100

    AB#26415 Event with registrationId

--- services/121-service/src/events/entities/event.entity.ts
+++ services/121-service/src/events/entities/event.entity.ts
@@ -7,10 +7,10 @@ import {
   OneToMany,
 } from 'typeorm';
 import { Base121AuditedEntity } from '../../base-audited.entity';
+import { RegistrationEntity } from '../../registration/registration.entity';
 import { UserEntity } from '../../user/user.entity';
 import { EventEnum } from '../event.enum';
 import { EventAttributeEntity } from './event-attribute.entity';
-import { RegistrationEntity } from '../../registration/registration.entity';
 
 @Entity('event')
 export class EventEntity extends Base121AuditedEntity {
@@ -31,4 +31,6 @@ export class EventEntity extends Base121AuditedEntity {
   @ManyToOne(() => RegistrationEntity)
   @JoinColumn({ name: 'registrationId' })
   public registration: RegistrationEntity;
+  @Column()
+  public registrationId: number;
 }
