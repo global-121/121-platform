0a134cd4f64d9a53202e150ca8cd473c4f786037
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-02-20 13:42:53 +0100

    Fix: broken enum AB#26419

--- interfaces/Portal/src/app/enums/event-type.enum.ts
+++ interfaces/Portal/src/app/enums/event-type.enum.ts
@@ -1,4 +1,4 @@
-import { EventEnum } from '../../../../../services/121-service/src/events/event.enum';
+import { EventEnum } from '../../../../../services/121-service/src/events/enum/event.enum';
 const EventType = EventEnum;
 type EventType = EventEnum;
 export default EventType;
