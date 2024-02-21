1ebc448dab7905795e05660aa31b9a1022d80338
jannisvisser <jannisvisser@redcross.nl>
2024-02-21 08:25:41 +0100

    fix: only add excel f not exists AB#26481

--- services/121-service/migration/1708010228200-AddFspExcelMainData.ts
+++ services/121-service/migration/1708010228200-AddFspExcelMainData.ts
@@ -1,16 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddFspExcelMainData1708010228200 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Add Excel FSP only if not exists yet
+    const exists = await queryRunner.query(
+      `SELECT * FROM "121-service"."fsp" WHERE "fsp" = 'Excel'`,
+    );
+    if (!exists || exists.length === 0) {
       await queryRunner.query(
-        `INSERT INTO "121-service"."fsp" ("fsp", "fspDisplayNamePaApp", "integrationType", "fspDisplayNamePortal") VALUES ('Excel', '{ "en": "Excel Payment Instructions" }', 'csv', 'Excel Payment Instructions')`,
+        `INSERT INTO "121-service"."fsp" ("fsp", "fspDisplayNamePaApp", "integrationType", "fspDisplayNamePortal","hasReconciliation") VALUES ('Excel', '{ "en": "Excel Payment Instructions" }', 'csv', 'Excel Payment Instructions', true)`,
       );
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(
-        `DELETE FROM "121-service"."fsp" WHERE "fsp" = 'Excel'`,
-      );
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DELETE FROM "121-service"."fsp" WHERE "fsp" = 'Excel'`,
+    );
+  }
 }
