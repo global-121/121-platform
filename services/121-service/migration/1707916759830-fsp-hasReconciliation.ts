25a1f3cd8b2f313739ee2b3c65ffe5478260bb79
jannisvisser <jannisvisser@redcross.nl>
2024-02-21 08:26:53 +0100

    fix: update hasReconciliation in migration AB#26481

--- services/121-service/migration/1707916759830-fsp-hasReconciliation.ts
+++ services/121-service/migration/1707916759830-fsp-hasReconciliation.ts
@@ -7,6 +7,11 @@ export class FspHasReconciliation1707916759830 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "121-service"."fsp" ADD "hasReconciliation" boolean NOT NULL DEFAULT false`,
     );
+
+    // Update VodaCash and Excel to have reconciliation
+    await queryRunner.query(
+      `UPDATE "121-service"."fsp" SET "hasReconciliation" = true WHERE "fsp" IN ('VodaCash','Excel')`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
