1a20c3489c7e64703ae7ff6e873b4881955e642c
Diderik van Wingerden <diderik@diderikvanwingerden.com>
2024-02-22 18:09:55 +0100

    Add test if fsp does not exist

--- services/121-service/test/program/update-program.test.ts
+++ services/121-service/test/program/update-program.test.ts
@@ -30,7 +30,6 @@ describe.only('Update program', () => {
 
     // Act
     // Call the update function
-    console.log('accessToken: ', accessToken);
     const updateProgramResponse = await patchProgram(2, program as UpdateProgramDto, accessToken);
 
     // Assert
@@ -55,5 +54,18 @@ describe.only('Update program', () => {
     expect(updateProgramResponse.statusCode).toBe(HttpStatus.OK);
     const hasSpecificKeyValue = updateProgramResponse.body.financialServiceProviders.some(fsp => fsp.fsp === FspName.excel);
     expect(hasSpecificKeyValue).toBeTruthy();
+  });
+
+  it('should not be able to add an fsp that does not exists to a program', async () => {
+    // Arrange
+    const program = {
+      financialServiceProviders: JSON.parse(JSON.stringify([{ fsp: 'non-existing-fsp' }])),
+    };
+
+    // Act
+    const updateProgramResponse = await patchProgram(2, program as UpdateProgramDto, accessToken);
+
+    // Assert
+    expect(updateProgramResponse.statusCode).toBe(HttpStatus.BAD_REQUEST);
   });
 });
