df5940896cc9966b2f321c7a74de45df2ef87398
Diderik van Wingerden <diderik@diderikvanwingerden.com>
2024-02-22 17:52:22 +0100

    WIP Add API Test

---
+++ services/121-service/test/program/update-program.test.ts
@@ -0,0 +1,59 @@
+/* eslint-disable jest/no-conditional-expect */
+import { HttpStatus } from '@nestjs/common';
+import programEth from '../../seed-data/program/program-joint-response-dorcas.json';
+import programOCW from '../../seed-data/program/program-nlrc-ocw.json';
+import { SeedScript } from '../../src/scripts/seed-script.enum';
+import {
+  assertArraysAreEqual,
+  assertObjectsAreEqual,
+} from '../helpers/assert.helper';
+import { getProgram, patchProgram, postProgram } from '../helpers/program.helper';
+import { getAccessToken, resetDB } from '../helpers/utility.helper';
+import { UpdateProgramDto } from '../../src/programs/dto/update-program.dto';
+import { FspName } from '../../src/fsp/enum/fsp-name.enum';
+
+describe.only('Update program', () => {
+  let accessToken: string;
+
+  beforeEach(async () => {
+    await resetDB(SeedScript.nlrcMultiple);
+    accessToken = await getAccessToken();
+  });
+
+  it('should update a program', async () => {
+    // Arrange
+    // TODO: Should this really contain all attributes of a program that can be updated?
+    const program = {
+      titlePortal: JSON.parse(JSON.stringify({ en: 'new title' })),
+      location: "new location",
+    };
+
+    // Act
+    // Call the update function
+    console.log('accessToken: ', accessToken);
+    const updateProgramResponse = await patchProgram(2, program as UpdateProgramDto, accessToken);
+
+    // Assert
+    // Check the response to see if the attributes were actually updated
+    expect(updateProgramResponse.statusCode).toBe(HttpStatus.OK);
+    expect(updateProgramResponse.body.location).toBe(program.location);
+    const keyToIgnore = [''];
+    assertObjectsAreEqual(updateProgramResponse.body.titlePortal, program.titlePortal, keyToIgnore);
+
+  });
+
+  it('should add an fsp to a program', async () => {
+    // Arrange
+    const program = {
+      financialServiceProviders: JSON.parse(JSON.stringify([{ fsp: FspName.excel }])),
+    };
+
+    // Act
+    const updateProgramResponse = await patchProgram(2, program as UpdateProgramDto, accessToken);
+
+    // Assert
+    expect(updateProgramResponse.statusCode).toBe(HttpStatus.OK);
+    const hasSpecificKeyValue = updateProgramResponse.body.financialServiceProviders.some(fsp => fsp.fsp === FspName.excel);
+    expect(hasSpecificKeyValue).toBeTruthy();
+  });
+});
