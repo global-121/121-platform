dbbb0b10cdde8db20a9fcd23056cc6f956535aa4
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-03-06 12:18:51 +0100

    Feat: create auth callback component AB#26838

---
+++ interfaces/Portal/src/app/auth/auth-callback/auth-callback.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthCallbackComponent } from './auth-callback.component';
+
+describe('AuthCallbackComponent', () => {
+  let component: AuthCallbackComponent;
+  let fixture: ComponentFixture<AuthCallbackComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AuthCallbackComponent]
+    });
+    fixture = TestBed.createComponent(AuthCallbackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ interfaces/Portal/src/app/auth/auth-callback/auth-callback.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-callback',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-callback.component.html',
+  styleUrls: ['./auth-callback.component.scss']
+})
+export class AuthCallbackComponent {
+
+}
