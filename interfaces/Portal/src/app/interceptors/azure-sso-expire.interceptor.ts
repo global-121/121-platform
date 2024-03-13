58b55856eef2cf002056cdcf6bc39071ef1f84d9
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-03-13 16:08:55 +0100

    Feat: login after 1 day AB#26779

---
+++ interfaces/Portal/src/app/interceptors/azure-sso-expire.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { from, lastValueFrom, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class AzureSsoExpireInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    return from(this.handle(req, next));
+  }
+
+  async handle(request: HttpRequest<any>, next: HttpHandler) {
+    if (request.url.includes(environment.url_121_service_api)) {
+      if (!request.url.includes('users/current')) {
+        await this.authService.checkExpirationDate();
+      }
+    }
+    return await lastValueFrom(next.handle(request));
+  }
+}
---
+++ interfaces/Portal/src/app/interceptors/msal-skip.interceptor.ts
@@ -0,0 +1,47 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MsalInterceptor } from '@azure/msal-angular';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { USER_KEY } from '../auth/auth.service';
+import { User } from '../models/user.model';
+
+@Injectable()
+export class MsalSkipInterceptor
+  extends MsalInterceptor
+  implements HttpInterceptor
+{
+  override intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    // Only potentially skip on 121-service API requests
+    if (request.url.includes(environment.url_121_service_api)) {
+      // Never skip on request to get current user, as we need it always to check if user is an Entra user
+      if (request.url.includes('users/current')) {
+        return super.intercept(request, next);
+      }
+
+      // Otherwise, get user from local storage
+      const rawUser = localStorage.getItem(USER_KEY);
+      if (!rawUser) {
+        // If no user found (this should never happen), then skip to avoid SSO-redirect, and let it fail somewhere else
+        return next.handle(request);
+      }
+      // If user found ..
+      const user = JSON.parse(rawUser) as User;
+      // .. skip if not an entra-user
+      if (user?.isEntraUser === false) {
+        return next.handle(request);
+      }
+      // .. otherwise don't skip
+      return super.intercept(request, next);
+    }
+    return next.handle(request);
+  }
+}
