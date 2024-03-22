602a2c8c8529e5e63141c43b46bd563833002582
piotrk39 <piotrk39@gmail.com>
2024-03-22 14:52:17 +0100

    AB#27140

--- services/121-service/test/registrations/mass-update-registration.test.ts
+++ services/121-service/test/registrations/mass-update-registration.test.ts
@@ -10,6 +10,7 @@ import {
   getAccessToken,
   resetDB,
 } from '../helpers/utility.helper';
+import { assertRegistrationImport } from '../helpers/assert.helper';
 
 describe('Update attribute of multiple PAs via Bulk update', () => {
   const programIdOcw = 3;
@@ -43,30 +44,20 @@ describe('Update attribute of multiple PAs via Bulk update', () => {
       programIdOcw,
       accessToken,
     );
+    expect(pa1result.statusCode).toBe(HttpStatus.OK);
 
     const pa1response = pa1result.body.data[0];
-
-    expect(pa1result.statusCode).toBe(HttpStatus.OK);
-    expect(pa1response.phoneNumber).toBe(PA1.phoneNumber);
-    expect(pa1response.lastName).toBe(PA1.lastName);
-    expect(pa1response.addressStreet).toBe(PA1.addressStreet);
-    expect(pa1response.addressHouseNumber).toBe(PA1.addressHouseNumber);
-    expect(pa1response.addressHouseNumberAddition).toBe(PA1.addressHouseNumberAddition);
+    assertRegistrationImport(pa1response, PA1);
 
     const pa2result = await searchRegistrationByReferenceId(
       '01dc9451-1273-484c-b2e8-ae21b51a96ab',
       programIdOcw,
       accessToken,
     );
+    expect(pa2result.statusCode).toBe(HttpStatus.OK);
 
     const pa2response = pa2result.body.data[0];
-
-    expect(pa2result.statusCode).toBe(HttpStatus.OK);
-    expect(pa2response.phoneNumber).toBe(PA2.phoneNumber);
-    expect(pa2response.lastName).toBe(PA2.lastName);
-    expect(pa2response.addressStreet).toBe(PA2.addressStreet);
-    expect(pa2response.addressHouseNumber).toBe(PA2.addressHouseNumber);
-    expect(pa2response.addressHouseNumberAddition).toBe(PA2.addressHouseNumberAddition);
+    assertRegistrationImport(pa2response, PA2);
   });
 
   it('Should bulk update and validate changed records', async () => {
@@ -97,13 +88,7 @@ describe('Update attribute of multiple PAs via Bulk update', () => {
     );
 
     const pa1patchedResponse = pa1patched.body.data[0];
-
-    expect(pa1patched.statusCode).toBe(HttpStatus.OK);
-    expect(pa1patchedResponse.phoneNumber).toBe(PA1Patch.phoneNumber);
-    expect(pa1patchedResponse.lastName).toBe(PA1Patch.lastName);
-    expect(pa1patchedResponse.addressStreet).toBe(PA1Patch.addressStreet);
-    expect(pa1patchedResponse.addressHouseNumber).toBe(PA1Patch.addressHouseNumber);
-    expect(pa1patchedResponse.addressHouseNumberAddition).toBe(PA1Patch.addressHouseNumberAddition);
+    assertRegistrationImport(pa1patchedResponse, PA1Patch);
 
     const pa2patched = await searchRegistrationByReferenceId(
       '01dc9451-1273-484c-b2e8-ae21b51a96ab',
@@ -112,13 +97,7 @@ describe('Update attribute of multiple PAs via Bulk update', () => {
     );
 
     const pa2patchedResponse = pa2patched.body.data[0];
-
-    expect(pa2patched.statusCode).toBe(HttpStatus.OK);
-    expect(pa2patchedResponse.phoneNumber).toBe(PA2Patch.phoneNumber);
-    expect(pa2patchedResponse.lastName).toBe(PA2Patch.lastName);
-    expect(pa2patchedResponse.addressStreet).toBe(PA2Patch.addressStreet);
-    expect(pa2patchedResponse.addressHouseNumber).toBe(PA2Patch.addressHouseNumber);
-    expect(pa2patchedResponse.addressHouseNumberAddition).toBe(PA2Patch.addressHouseNumberAddition);
+    assertRegistrationImport(pa2patchedResponse, PA2Patch);
 
     console.log('Bulk update and revalidate successful!');
   });
