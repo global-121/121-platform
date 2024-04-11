73d2383316c357630185ffc4e2564a755f8c695b
Elwin Schmitz <eschmitz@redcross.nl>
2024-04-11 12:46:33 +0200

    refactor: Use early-return(s) to reduce dept

--- interfaces/Portal/src/app/interceptors/msal-skip.interceptor.ts
+++ interfaces/Portal/src/app/interceptors/msal-skip.interceptor.ts
@@ -20,31 +20,32 @@ export class MsalSkipInterceptor
     request: HttpRequest<unknown>,
     next: HttpHandler,
   ): Observable<HttpEvent<unknown>> {
-    if (environment.use_sso_azure_entra) {
+    if (
+      !environment.use_sso_azure_entra ||
       // Only potentially skip on 121-service API requests
-      if (request.url.includes(environment.url_121_service_api)) {
-        // Never skip on request to get current user, as we need it always to check if user is an Entra user
-        if (request.url.includes(CURRENT_USER_ENDPOINT_PATH)) {
-          return super.intercept(request, next);
-        }
-
-        // Otherwise, get user from local storage
-        const rawUser = localStorage.getItem(USER_KEY);
-        if (!rawUser) {
-          // If no user found (this should never happen), then skip to avoid SSO-redirect, and let it fail somewhere else
-          return next.handle(request);
-        }
-        // If user found ..
-        const user = JSON.parse(rawUser) as User;
-        // .. skip if not an entra-user
-        if (user?.isEntraUser === false) {
-          return next.handle(request);
-        }
-        // .. otherwise don't skip
-        return super.intercept(request, next);
-      }
+      request.url.startsWith(environment.url_121_service_api)
+    ) {
       return next.handle(request);
     }
-    return next.handle(request);
+
+    // Never skip on request to get current user, as we need it always to check if user is an Entra user
+    if (request.url.includes(CURRENT_USER_ENDPOINT_PATH)) {
+      return super.intercept(request, next);
+    }
+
+    // Otherwise, get user from local storage
+    const rawUser = localStorage.getItem(USER_KEY);
+    if (!rawUser) {
+      // If no user found (this should never happen), then skip to avoid SSO-redirect, and let it fail somewhere else
+      return next.handle(request);
+    }
+    // If user found ..
+    const user: User = JSON.parse(rawUser);
+    // .. skip if not an entra-user
+    if (user?.isEntraUser === false) {
+      return next.handle(request);
+    }
+    // .. otherwise don't skip
+    return super.intercept(request, next);
   }
 }
