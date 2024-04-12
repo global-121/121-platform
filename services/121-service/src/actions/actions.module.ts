796b2b74ba59ddb122b0d46e8626b8cc99d2ed18
diderikvw <diderik@diderikvanwingerden.com>
2024-04-12 10:53:07 +0200

    Fix prettier more?

--- services/121-service/src/actions/actions.module.ts
+++ services/121-service/src/actions/actions.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProgramEntity } from '../programs/program.entity';
 import { UserEntity } from '../user/user.entity';
 import { UserModule } from '../user/user.module';
-import { ActionsController } from './actions.controller';
 import { ActionEntity } from './action.entity';
-import { ActionsService } from './actions.service'; 
+import { ActionsController } from './actions.controller';
+import { ActionsService } from './actions.service';
 
 @Module({
   imports: [
