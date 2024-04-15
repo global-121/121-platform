e728fce6892d30d332bbfeaa7fa2f0621c834157
Elwin Schmitz <eschmitz@redcross.nl>
2024-04-15 15:37:49 +0200

    refactor: Use consistent "development"-flag + condition

--- services/mock-service/src/config.ts
+++ services/mock-service/src/config.ts
@@ -1,12 +1,11 @@
 import packageJson = require('../package.json');
 
-export const DEBUG = process.env.NODE_ENV !== 'production';
-export const PORT = process.env.NODE_ENV === 'development' ? 3001 : 8080;
+export const DEVELOPMENT = process.env.NODE_ENV === 'development';
+export const PORT = DEVELOPMENT ? 3001 : 8080;
 
-export const ROOT_URL =
-  process.env.NODE_ENV === 'development'
-    ? `http://localhost:${PORT}/`
-    : process.env.MOCK_SERVICE_URL;
+export const ROOT_URL = DEVELOPMENT
+  ? `http://localhost:${PORT}/`
+  : process.env.MOCK_SERVICE_URL;
 
 // Configure Swagger UI appearance:
 // ---------------------------------------------------------------------------
@@ -39,10 +38,9 @@ export const API_PATHS = {
   whatsAppIncoming: 'notifications/whatsapp/incoming',
   safaricomCallback: 'payments/safaricom/transaction',
 };
-const rootApi121Service =
-  process.env.NODE_ENV === 'development'
-    ? `http://121-service:${process.env.PORT_121_SERVICE}/`
-    : process.env.EXTERNAL_121_SERVICE_URL;
+const rootApi121Service = DEVELOPMENT
+  ? `http://121-service:${process.env.PORT_121_SERVICE}/`
+  : process.env.EXTERNAL_121_SERVICE_URL;
 
 export const EXTERNAL_API = {
   rootApi: `${rootApi121Service}api`,
