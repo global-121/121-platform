7b3aaf7c6610d1bf4db9738de038072459759870
piotrk39 <piotrk39@gmail.com>
2024-04-16 15:04:58 +0200

    AB#27544 update comment for future reference

--- services/121-service/test/E2E/tests/121-Portal/ViewPaProfilePage/ViewPersonalInformationTable.spec.ts
+++ services/121-service/test/E2E/tests/121-Portal/ViewPaProfilePage/ViewPersonalInformationTable.spec.ts
@@ -44,7 +44,7 @@ test('[27492] View Personal information table', async ({ page }) => {
     await registration.validatePaProfileOpened();
     // Reload the page to make asynchronous data available
     // await page.reload();
-    // Reload should be removed after fixing the issue with the data not being available
+    // Reload should be removed after fixing the issue with the data not being available https://dev.azure.com/redcrossnl/121%20Platform/_workitems/edit/27568
     await registration.validatePersonalInformationTable("PA #1", "Registered", await helpers.getTodaysDate(), "English", "+14155238886", "Visa debit card");
   });
 });
