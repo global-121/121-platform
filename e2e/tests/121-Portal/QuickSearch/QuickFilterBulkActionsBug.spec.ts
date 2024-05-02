927ab7a7b6a1565c580027dec09a67e3b2e64fc8
piotrk39 <piotrk39@gmail.com>
2024-05-02 12:53:44 +0300

    Test in CI the bulkAction testCase

--- e2e/tests/121-Portal/QuickSearch/QuickFilterBulkActionsBug.spec.ts
+++ e2e/tests/121-Portal/QuickSearch/QuickFilterBulkActionsBug.spec.ts
@@ -32,8 +32,7 @@ test.beforeEach(async ({ page }) => {
   );
 });
 
-// XXX: disabling for now due to the bug in CI
-test.skip('[27614] Quick filter through available PAs and apply bulk action', async ({
+test('[27614] Quick filter through available PAs and apply bulk action', async ({
   page,
 }) => {
   const table = new TableModule(page);
