4d9f8bc31df4906415e52a786c6d9384f2ae355f
Ashish Basak <120180036+ashishkumarbasak-accenture@users.noreply.github.com>
2024-05-14 15:30:19 +0200

    Feat.move import button and other button (#5280)
    * feat: move import registration buttons and allow registration toggel option AB#27936
    
    * feat: align all the tabs in a single line tab option AB#27936
    
    * feat: fix lint issues AB#27936
    
    * fix: fix import registration button style AB#28039

--- interfaces/Portal/src/app/program-phase-order.ts
+++ interfaces/Portal/src/app/program-phase-order.ts
@@ -3,14 +3,22 @@ import { ProgramPhase } from './models/program.model';
 export const PROGRAM_PHASE_ORDER = [
   {
     id: 1,
-    name: ProgramPhase.design,
+    name: ProgramPhase.overview,
   },
   {
     id: 2,
+    name: ProgramPhase.team,
+  },
+  {
+    id: 3,
     name: ProgramPhase.peopleAffected,
   },
   {
     id: 4,
     name: ProgramPhase.payment,
+  },
+  {
+    id: 5,
+    name: ProgramPhase.monitoring,
   },
 ];
