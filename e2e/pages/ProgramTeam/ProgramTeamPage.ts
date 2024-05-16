62c122617d0611b48cb2e7f506c1f734866daaaf
piotrk39 <piotrk39@gmail.com>
2024-05-16 13:23:01 +0200

    AB#28094 add testids, newScreenshots & refactor

--- e2e/pages/ProgramTeam/ProgramTeamPage.ts
+++ e2e/pages/ProgramTeam/ProgramTeamPage.ts
@@ -5,6 +5,11 @@ class ProgramTeam {
   readonly page: Page;
   readonly addTeamMemberButton: Locator;
   readonly addTeamMemberPopUp: Locator;
+  readonly addTeamMemberRolesDropdown: Locator;
+  readonly addTeamMemberSearchbar: Locator;
+  readonly editTeamMemberButton: Locator;
+  readonly removeTeamMemberButton: Locator;
+  readonly removeUserButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -12,6 +17,17 @@ class ProgramTeam {
     this.addTeamMemberPopUp = this.page.getByTestId(
       'add-new-team-member-pop-up-content',
     );
+    this.addTeamMemberRolesDropdown = this.page.getByTestId(
+      'team-popup-roles-dropdown',
+    );
+    this.addTeamMemberSearchbar = this.page.getByTestId(
+      'team-popup-team-member-searchbar',
+    );
+    this.editTeamMemberButton = this.page.getByTestId(
+      'edit-team-member-button',
+    );
+    this.removeTeamMemberButton = this.page.getByTestId('remove-team-member');
+    this.removeUserButton = this.page.getByTestId('remove-team-member-button');
   }
 
   async openAddNewTeamMemberPoUp() {
@@ -21,6 +37,35 @@ class ProgramTeam {
   async validateAddTeamMemberPopUpIsPresent() {
     await this.page.waitForLoadState('domcontentloaded');
     await expect(this.addTeamMemberPopUp).toBeVisible();
+  }
+
+  async openRolesDropdown() {
+    await this.addTeamMemberRolesDropdown.click();
+  }
+
+  async fillInTeamMemberName({ name }: { name: string }) {
+    await this.page.getByPlaceholder('Select a team member').fill(name);
+    await this.page.getByRole('button', { name: name }).click();
+  }
+
+  async clickEditTeamMember({ noteIndex = 0 }: { noteIndex?: number }) {
+    await this.page.waitForLoadState('networkidle');
+    await this.editTeamMemberButton.nth(noteIndex).click();
+    await this.page.getByRole('button', { name: 'Edit' }).click();
+  }
+
+  async clickDeleteTeamMember({ noteIndex = 0 }: { noteIndex?: number }) {
+    await this.page.waitForLoadState('networkidle');
+    await this.page
+      .getByRole('row')
+      .getByLabel('Options')
+      .nth(noteIndex)
+      .click();
+    await this.removeTeamMemberButton.click();
+  }
+
+  async removeUser() {
+    await this.removeUserButton.click();
   }
 }
 
--- e2e/tests/UserManualScreenshots/userManualScreenshots.spec.ts
+++ e2e/tests/UserManualScreenshots/userManualScreenshots.spec.ts
@@ -6,52 +6,109 @@ import UsersAndRoles from '@121-e2e/pages/UsersAndRoles/UsersAndRolesPage';
 import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
 import { seedPaidRegistrations } from '@121-service/test/helpers/registration.helper';
 import { resetDB } from '@121-service/test/helpers/utility.helper';
-import {
-  programIdOCW,
-  registrationsOCW,
-} from '@121-service/test/registrations/pagination/pagination-data';
+import { registrationsOCW } from '@121-service/test/registrations/pagination/pagination-data';
 import { test } from '@playwright/test';
+import { Page } from 'playwright';
+
+const TIMEOUT_DURATION = 200;
+const PROGRAM_ID = 3;
+
+async function navigateAndScreenshot({
+  page,
+  helpers,
+  url,
+  fileName,
+}: {
+  page: Page;
+  helpers: Helpers;
+  url: string;
+  fileName: string;
+}) {
+  await page.goto(url);
+  await page.waitForTimeout(TIMEOUT_DURATION);
+  await helpers.takeFullScreenShot({ fileName });
+}
 
 test.beforeEach(async () => {
   await resetDB(SeedScript.nlrcMultiple);
-  const programIdOCW = 3;
-  const OcwProgramId = programIdOCW;
-  await seedPaidRegistrations(registrationsOCW, OcwProgramId);
+  await seedPaidRegistrations(registrationsOCW, PROGRAM_ID);
 });
 
-test('Open the edit PA popup', async ({ page }) => {
+test('Navigates to the portal and takes screenshots', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const helpers = new Helpers(page);
   const homePage = new HomePage(page);
   const usersAndRoles = new UsersAndRoles(page);
   const programTeam = new ProgramTeam(page);
 
-  await test.step('Navigates to the portal and takes full or partial screenshots of elements from the page', async () => {
-    await page.goto('/login');
-    await page.waitForLoadState('domcontentloaded');
-    await homePage.closeBrowserCompatibilityBanner();
-    await helpers.takeFullScreenShot({ fileName: 'loginScreen' });
-    await loginPage.login(
-      process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
-      process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
-    );
-    await homePage.validateNumberOfActivePrograms(2);
-    await helpers.takeFullScreenShot({ fileName: 'ProgramOverview' });
-    await homePage.openMenu();
-    await page.waitForTimeout(200);
-    await helpers.takeFullScreenShot({ fileName: 'HomeMenuPage' });
-    await page.goto('/user');
-    await helpers.takeFullScreenShot({ fileName: 'ChangePassword' });
-    await page.goto('/users');
-    await helpers.takeFullScreenShot({ fileName: 'ListofUsers' });
-    await usersAndRoles.navigateRolesTab();
-    await page.waitForTimeout(200);
-    await helpers.takeFullScreenShot({ fileName: 'UsersRoleTab' });
-    await page.goto(`/program/${programIdOCW}/team`);
-    await helpers.takeFullScreenShot({ fileName: 'ProgramTeam' });
-    await programTeam.openAddNewTeamMemberPoUp();
-    await programTeam.validateAddTeamMemberPopUpIsPresent();
-    await page.waitForTimeout(200);
-    await helpers.takeFullScreenShot({ fileName: 'AddTeamMember' });
+  await page.goto('/login');
+  await page.waitForLoadState('domcontentloaded');
+  await homePage.closeBrowserCompatibilityBanner();
+  await helpers.takeFullScreenShot({ fileName: 'loginScreen' });
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+  await homePage.validateNumberOfActivePrograms(2);
+  await helpers.takeFullScreenShot({ fileName: 'ProgramOverview' });
+  await homePage.openMenu();
+  await page.waitForTimeout(TIMEOUT_DURATION);
+  await helpers.takeFullScreenShot({ fileName: 'HomeMenuPage' });
+
+  await navigateAndScreenshot({
+    page: page,
+    helpers: helpers,
+    url: '/user',
+    fileName: 'ChangePassword',
+  });
+  await navigateAndScreenshot({
+    page,
+    helpers: helpers,
+    url: '/users',
+    fileName: 'ListofUsers',
+  });
+
+  await usersAndRoles.navigateRolesTab();
+  await page.waitForTimeout(TIMEOUT_DURATION);
+  await helpers.takeFullScreenShot({ fileName: 'UsersRoleTab' });
+
+  await navigateAndScreenshot({
+    page: page,
+    helpers: helpers,
+    url: `/program/${PROGRAM_ID}/team`,
+    fileName: 'ProgramTeam',
+  });
+
+  await programTeam.openAddNewTeamMemberPoUp();
+  await programTeam.validateAddTeamMemberPopUpIsPresent();
+  await page.waitForTimeout(TIMEOUT_DURATION);
+  await helpers.takeFullScreenShot({ fileName: 'AddTeamMember' });
+
+  await programTeam.fillInTeamMemberName({ name: 'cva-manager@example.org' });
+  await helpers.takeFullScreenShot({
+    fileName: 'ProgramTeamMultipurposeCashTeamTabAddTeamMember',
+  });
+
+  await programTeam.openRolesDropdown();
+  await page.waitForTimeout(TIMEOUT_DURATION);
+  await helpers.takeFullScreenShot({ fileName: 'ProgramUserManagementRoles' });
+
+  await navigateAndScreenshot({
+    page: page,
+    helpers: helpers,
+    url: `/program/${PROGRAM_ID}/team`,
+    fileName: 'ChangeRoleTeam',
+  });
+
+  await programTeam.clickDeleteTeamMember({});
+  await page.waitForTimeout(TIMEOUT_DURATION);
+  await helpers.takeFullScreenShot({
+    fileName: 'ProgramTeamDeleteRemoveUserActionBox',
+  });
+
+  await programTeam.removeUser();
+  await page.waitForTimeout(TIMEOUT_DURATION);
+  await helpers.takeFullScreenShot({
+    fileName: 'ProgramTeamDeleteRemoveUserValidation',
   });
 });
