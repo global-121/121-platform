4ae40ee67d31f415ced3539b7500b671cbad5886
diderikvw <diderik@diderikvanwingerden.com>
2024-05-28 13:00:06 +0200

    Code skeleton and comments for Intersolve Visa Re-implementation (#5303)
    * WIP comments
    
    * Refactor: add comments
    
    * More comments
    
    * Some comments
    
    * WIP commit before creating new module ProgramFinancialServiceProviderConfigurations
    
    * WIP continue code skeketon for do payment intersolve visa
    
    * Complete draft skeleton for do payment
    
    * Chore: more TODOs
    
    * WIP Add code skeleton and comments for process intersolvevisatransferjob
    
    * Finish draft code skeleton and comments for process intersolve visa transfer job
    
    * Add some refactor comments
    
    * WIP reissue card code dekeleton and comments
    
    * Finish draft code skeleton and comments for reissue card
    
    ---------
    
    Co-authored-by: PeterSmallenbroek <peter.smallenbroek@gmail.com>

--- services/121-service/src/payments/fsp-integration/intersolve-voucher/processors/intersolve-voucher.processor.ts
+++ services/121-service/src/payments/fsp-integration/intersolve-voucher/processors/intersolve-voucher.processor.ts
@@ -4,8 +4,8 @@ import {
 } from '@121-service/src/payments/enum/queue.names.enum';
 import { IntersolveVoucherService } from '@121-service/src/payments/fsp-integration/intersolve-voucher/intersolve-voucher.service';
 import {
-  REDIS_CLIENT,
   getRedisSetName,
+  REDIS_CLIENT,
 } from '@121-service/src/payments/redis-client';
 import { Process, Processor } from '@nestjs/bull';
 import { Inject } from '@nestjs/common';
