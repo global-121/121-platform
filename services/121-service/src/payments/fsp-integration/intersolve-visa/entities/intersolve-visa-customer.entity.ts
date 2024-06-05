ad54d483c14a04cc67e91c71e1f59128ffcd4e96
Peter Smallenbroek <PeterSmallenbroek@users.noreply.github.com>
2024-06-05 08:42:33 +0200

    Add new Visa entities (#5379)

--- services/121-service/src/payments/fsp-integration/intersolve-visa/entities/intersolve-visa-customer.entity.ts
+++ services/121-service/src/payments/fsp-integration/intersolve-visa/entities/intersolve-visa-customer.entity.ts
@@ -1,4 +1,5 @@
 import { CascadeDeleteEntity } from '@121-service/src/base.entity';
+import { IntersolveVisaParentWalletEntity } from '@121-service/src/payments/fsp-integration/intersolve-visa/entities/intersolve-visa-parent-wallet.entity';
 import { IntersolveVisaWalletEntity } from '@121-service/src/payments/fsp-integration/intersolve-visa/intersolve-visa-wallet.entity';
 import { RegistrationEntity } from '@121-service/src/registration/registration.entity';
 import {
@@ -12,18 +13,27 @@ import {
   Relation,
 } from 'typeorm';
 
+// TODO: Currently this is a duplicate entity, when the 'old' one is removed, this one should be fixed in the migration
 @Entity('intersolve_visa_customer')
 export class IntersolveVisaCustomerEntity extends CascadeDeleteEntity {
   @Index()
-  @Column({ type: 'character varying', nullable: true })
+  @Column({ type: 'character varying', nullable: true }) // TODO: Why is this nullable?
   public holderId: string | null;
 
   @OneToOne(() => RegistrationEntity)
   @JoinColumn({ name: 'registrationId' })
   public registration: Relation<RegistrationEntity>;
-  @Column({ type: 'int', nullable: true })
+  @Column({ type: 'int', nullable: true }) // TODO: Why is this nullable? An intersolve visa customer should always have a registration linked to it
   public registrationId: number | null;
 
+  @OneToOne(
+    (_type) => IntersolveVisaParentWalletEntity,
+    (intersolveVisaParentWallet) =>
+      intersolveVisaParentWallet.intersolveVisaCustomer,
+  )
+  public intersolveVisaParentWallet: Relation<IntersolveVisaParentWalletEntity>;
+
+  // TODO: Remove this when all references to this have been refactored.
   @OneToMany(
     (_type) => IntersolveVisaWalletEntity,
     (visaWallets) => visaWallets.intersolveVisaCustomer,
@@ -34,7 +44,7 @@ export class IntersolveVisaCustomerEntity extends CascadeDeleteEntity {
   public async cascadeDelete(): Promise<void> {
     await this.deleteAllOneToMany([
       {
-        entityClass: IntersolveVisaWalletEntity,
+        entityClass: IntersolveVisaParentWalletEntity,
         columnName: 'intersolveVisaCustomer',
       },
     ]);
