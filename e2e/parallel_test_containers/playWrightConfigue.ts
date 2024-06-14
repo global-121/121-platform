952d434b63279139b647081a70ad680cc39b74b5
Xiaoyu Sun <sunxiaoyu_2009@126.com>
2024-06-14 11:14:42 +0200

    change the execute time of test and remove unused file

--- e2e/parallel_test_containers/playWrightConfigue.ts
+++
@@ -1,9 +0,0 @@
-import { PlaywrightTestConfig } from '@playwright/test';
-
-const config: PlaywrightTestConfig = {
-  use: {
-    baseURL: 'http://localhost:8888',
-  },
-};
-
-export default config;
