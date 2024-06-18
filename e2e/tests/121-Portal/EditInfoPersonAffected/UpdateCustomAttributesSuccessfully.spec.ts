4d0a448de0233b006ea63c4e7986230e1130b580
Xiaoyu Sun <sunxiaoyu_2009@126.com>
2024-06-18 10:50:00 +0200

    optimize test

--- e2e/tests/121-Portal/EditInfoPersonAffected/UpdateCustomAttributesSuccessfully.spec.ts
+++ e2e/tests/121-Portal/EditInfoPersonAffected/UpdateCustomAttributesSuccessfully.spec.ts
@@ -7,27 +7,10 @@ import NLRCProgram from '@121-service/src/seed-data/program/program-nlrc-ocw.jso
 import { seedPaidRegistrations } from '@121-service/test/helpers/registration.helper';
 import { resetDB } from '@121-service/test/helpers/utility.helper';
 import { registrationsOCW } from '@121-service/test/registrations/pagination/pagination-data';
-import { test } from '@playwright/test';
 import englishTranslations from '../../../../interfaces/Portal/src/assets/i18n/en.json';
-import {
-  startEnvironment,
-  stopEnvironment,
-} from '../../../parallel_test_containers/testContainer_compose';
+import { test } from '../../../parallel_test_containers/test';
 
-const dynamicBaseURL = test.extend<{ baseURL: string }>({
-  // Define the modifier function
-  baseURL: async ({}, use: (url: string) => Promise<void>) => {
-    const port = await startEnvironment();
-    const dynamicURL = `http://localhost:${port}`;
-    await use(dynamicURL);
-  },
-});
-
-dynamicBaseURL.afterAll(async () => {
-  await stopEnvironment();
-});
-
-dynamicBaseURL.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await resetDB(SeedScript.nlrcMultiple);
   const programIdOCW = 3;
   const OcwProgramId = programIdOCW;
@@ -41,52 +24,43 @@ dynamicBaseURL.beforeEach(async ({ page }) => {
   );
 });
 
-dynamicBaseURL(
-  '[28043] Update custom attributes successfully',
-  async ({ page }) => {
-    const table = new TableModule(page);
-    const homePage = new HomePage(page);
-    const registration = new RegistrationDetails(page);
-    let oldAmount = '';
+test('[28043] Update custom attributes successfully', async ({ page }) => {
+  const table = new TableModule(page);
+  const homePage = new HomePage(page);
+  const registration = new RegistrationDetails(page);
+  let oldAmount = '';
 
-    await dynamicBaseURL.step('Navigate to PA table', async () => {
-      await homePage.navigateToProgramme(NLRCProgram.titlePortal.en);
+  await test.step('Navigate to PA table', async () => {
+    await homePage.navigateToProgramme(NLRCProgram.titlePortal.en);
+  });
+
+  await test.step('Open information pop-up', async () => {
+    await table.openPaPersonalInformation({});
+  });
+
+  await test.step('Update payment amount multiplier', async () => {
+    oldAmount = await registration.updatepaymentAmountMultiplier({
+      saveButtonName: englishTranslations.common.save,
+      okButtonName: englishTranslations.common.ok,
     });
+  });
 
-    await dynamicBaseURL.step('Open information pop-up', async () => {
-      await table.openPaPersonalInformation({});
-    });
+  await test.step('navigate to PA profile page in data changes table', async () => {
+    await page.reload();
+    await table.clickOnPaNumber(1);
+  });
 
-    await dynamicBaseURL.step('Update payment amount multiplier', async () => {
-      oldAmount = await registration.updatepaymentAmountMultiplier({
-        saveButtonName: englishTranslations.common.save,
-        okButtonName: englishTranslations.common.ok,
-      });
-    });
-
-    await dynamicBaseURL.step(
-      'navigate to PA profile page in data changes table',
-      async () => {
-        await page.reload();
-        await table.clickOnPaNumber(1);
-      },
+  await test.step('Validate the "Payments" tab on the PA Activity Overview table to Contain Payment notifications, correct status, userName and date', async () => {
+    await registration.validateHeaderToContainText(
+      englishTranslations['registration-details'].pageTitle,
     );
-
-    await dynamicBaseURL.step(
-      'Validate the "Payments" tab on the PA Activity Overview table to Contain Payment notifications, correct status, userName and date',
-      async () => {
-        await registration.validateHeaderToContainText(
-          englishTranslations['registration-details'].pageTitle,
-        );
-        await registration.openActivityOverviewTab('Data changes');
-        await registration.validateDataChangesTab({
-          dataChangesLabel:
-            englishTranslations['registration-details']['activity-overview']
-              .activities['data-changes'].label,
-          oldValue: oldAmount,
-          newValue: String(Number(oldAmount) + 1),
-        });
-      },
-    );
-  },
-);
+    await registration.openActivityOverviewTab('Data changes');
+    await registration.validateDataChangesTab({
+      dataChangesLabel:
+        englishTranslations['registration-details']['activity-overview']
+          .activities['data-changes'].label,
+      oldValue: oldAmount,
+      newValue: String(Number(oldAmount) + 1),
+    });
+  });
+});
