4d61c2127b434aaeb30159dea69514af28deb05d
Xiaoyu Sun <sunxiaoyu_2009@126.com>
2024-06-19 16:49:43 +0200

    AB#28668 e2e test

---
+++ e2e/tests/121-Portal/EditInfoPersonAffected/UpdateFinancialServiceProvider.spec.ts
@@ -0,0 +1,55 @@
+import HomePage from '@121-e2e/pages/Home/HomePage';
+import LoginPage from '@121-e2e/pages/Login/LoginPage';
+import RegistrationDetails from '@121-e2e/pages/RegistrationDetails/RegistrationDetailsPage';
+import TableModule from '@121-e2e/pages/Table/TableModule';
+import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
+import fspIntersolveJumbo from '@121-service/src/seed-data/fsp/fsp-intersolve-jumbo-physical.json';
+import visaFspIntersolve from '@121-service/src/seed-data/fsp/fsp-intersolve-visa.json';
+import NLRCProgram from '@121-service/src/seed-data/program/program-nlrc-ocw.json';
+import { seedPaidRegistrations } from '@121-service/test/helpers/registration.helper';
+import { resetDB } from '@121-service/test/helpers/utility.helper';
+import { registrationsOCW } from '@121-service/test/registrations/pagination/pagination-data';
+import { test } from '@playwright/test';
+import englishTranslations from '../../../../interfaces/Portal/src/assets/i18n/en.json';
+
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.nlrcMultiple);
+  const programIdOCW = 3;
+  const OcwProgramId = programIdOCW;
+
+  await seedPaidRegistrations(registrationsOCW, OcwProgramId);
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto('/login');
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+});
+
+test('[28048] Update chosen Finacial service provider', async ({ page }) => {
+  const table = new TableModule(page);
+  const homePage = new HomePage(page);
+  const registration = new RegistrationDetails(page);
+  await test.step('Navigate to PA table', async () => {
+    await homePage.navigateToProgramme(NLRCProgram.titlePortal.en);
+  });
+
+  await test.step('Open information pop-up', async () => {
+    await table.openPaPersonalInformation({ buttonIndex: 3 });
+  });
+
+  await test.step('Update Finacial service provider from Jumbo card to Visa debit card', async () => {
+    await registration.updatefinancialServiceProvider({
+      fspNewName: visaFspIntersolve.displayName.en,
+      fspOldName: fspIntersolveJumbo.displayName.en,
+      saveButtonName: englishTranslations.common.save,
+      okButtonName: englishTranslations.common.ok,
+    });
+  });
+
+  await test.step('Validate Finacial service provider be updated', async () => {
+    await registration.validateRowPATable(visaFspIntersolve.displayName.en);
+  });
+});
