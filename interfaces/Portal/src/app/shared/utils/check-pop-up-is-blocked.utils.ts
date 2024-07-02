cfbe8f6fe28b4c8244c42994eed2d62eb15a280a
vladimir <vladimirpetkov1990@gmail.com>
2024-07-02 17:37:39 +0300

    fix: isPopupBlocked method with about blank page AB#29028

--- interfaces/Portal/src/app/shared/utils/check-pop-up-is-blocked.utils.ts
+++ interfaces/Portal/src/app/shared/utils/check-pop-up-is-blocked.utils.ts
@@ -7,7 +7,12 @@
  * @returns {boolean} - Returns true if pop-ups are blocked, otherwise false.
  */
 export function isPopupBlocked(): boolean {
-  const testPopup = window.open('', '_blank', 'width=100,height=100');
+  const testPopup = window.open(
+    'about:blank',
+    '_blank',
+    'width=100,height=100',
+  );
+
   if (
     !testPopup ||
     testPopup.closed ||
