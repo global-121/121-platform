1c57a213593c893db8adf8149059d52f4dc53aac
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-07-03 13:47:58 +0200

    Remove old enum

--- services/121-service/src/payments/fsp-integration/intersolve-visa/enum/intersolve-visa-wallet-status.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum IntersolveVisaTokenStatus {
-  Active = 'ACTIVE',
-  Inactive = 'INACTIVE',
-  Redeemed = 'REDEEMED',
-  Substituted = 'SUBSTITUTED',
-  Expired = 'EXPIRED',
-  Disabled = 'DISABLED',
-}
