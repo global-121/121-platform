2f5fffcd28c407774627e0e2e6ef2b6b9d264e50
Ashish Basak <ashish.kumar.basak@accenture.com>
2024-07-04 10:51:00 +0200

    fix: updated to last 8 payments

--- interfaces/Portal/src/app/program/program-payout/program-payout.component.ts
+++ interfaces/Portal/src/app/program/program-payout/program-payout.component.ts
@@ -18,7 +18,7 @@ import { FspIntegrationType } from '../../models/fsp.model';
 import { PastPaymentsService } from '../../services/past-payments.service';
 import { actionResult } from '../../shared/action-result';
 
-const MAX_NUMBER_OF_PAYMENTS_TO_EXPORT = 7;
+const MAX_NUMBER_OF_PAYMENTS_TO_EXPORT = 8;
 
 @Component({
   selector: 'app-program-payout',
