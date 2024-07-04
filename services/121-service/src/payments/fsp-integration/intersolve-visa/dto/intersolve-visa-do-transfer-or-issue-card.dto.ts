677a9c5144b711d5541961c66ee714e6acc462bc
Peter Smallenbroek <PeterSmallenbroek@users.noreply.github.com>
2024-07-04 10:38:33 +0200

    Process feedback on re-issue card (#5527)
    * Process feedback + lint
    
    * Change reissue URL

--- services/121-service/src/payments/fsp-integration/intersolve-visa/dto/intersolve-visa-do-transfer-or-issue-card.dto.ts
+++ services/121-service/src/payments/fsp-integration/intersolve-visa/dto/intersolve-visa-do-transfer-or-issue-card.dto.ts
@@ -1,4 +1,4 @@
-import { ContactInformationDto } from './external/contact-information.dto';
+import { ContactInformationDto } from '@121-service/src/payments/fsp-integration/intersolve-visa/dto/external/contact-information.dto';
 
 export interface IntersolveVisaDoTransferOrIssueCardDto {
   // Used to find the IntersolveVisaCustomer Entity related to the Registration and continue from there.
