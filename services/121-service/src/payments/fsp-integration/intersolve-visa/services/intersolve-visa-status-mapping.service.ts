42aa0c8c7480e68236acf250b89ef7d4cef57633
diderikvw <diderik@diderikvanwingerden.com>
2024-07-04 10:36:25 +0200

    Update comment

--- services/121-service/src/payments/fsp-integration/intersolve-visa/services/intersolve-visa-status-mapping.service.ts
+++ services/121-service/src/payments/fsp-integration/intersolve-visa/services/intersolve-visa-status-mapping.service.ts
@@ -55,7 +55,7 @@ const VisaCardActionsMapping = {
   'Issue New Card': VisaCardAction.reissue,
 };
 
-// TODO: REFACTOR: IMO Move into IntersolveVisaApiService. Make sure it does not depend on anything outside of this Module.
+// TODO: REFACTOR: IMO Move into IntersolveVisaService. Make sure it does not depend on anything outside of this Module.
 export class IntersolveVisaStatusMappingService {
   private readonly mapping: VisaStatusMapInterface[] = [];
 
