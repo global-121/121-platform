73ff31852345c9fd2f4774463d3bd071beb2502b
Elwin Schmitz <eschmitz@redcross.nl>
2024-08-02 01:12:40 +0200

    fix: The "proper" way didn't work, so a directly0imported workaround

--- interfaces/Portalicious/src/app/models/program.model.ts
+++ interfaces/Portalicious/src/app/models/program.model.ts
@@ -1,9 +1,7 @@
-import { MetricsController } from '@121-service/src/metrics/metrics.controller';
+import { ProgramStats } from '@121-service/src/metrics/dto/program-stats.dto';
 import { ProgramController } from '@121-service/src/programs/programs.controller';
 import { Dto121Service } from '~/utils/dto-type';
 
 export type Program = Dto121Service<ProgramController['findOne']>;
 
-export type ProgramMetrics = Dto121Service<
-  MetricsController['getProgramStats']
->;
+export type ProgramMetrics = ProgramStats;
