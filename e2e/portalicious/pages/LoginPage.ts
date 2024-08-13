981a1c5530ce018881704ab1a24f807516e92bf4
piotrk39 <piotrk39@gmail.com>
2024-08-13 15:44:48 +0200

    AB#29646 fix url path validation

--- e2e/portalicious/pages/LoginPage.ts
+++ e2e/portalicious/pages/LoginPage.ts
@@ -27,7 +27,7 @@ class LoginPage {
     }
 
     await this.page.waitForURL((url) =>
-      url.pathname.startsWith('/en/projects'),
+      url.pathname.startsWith('/en/all-projects'),
     );
   }
 
