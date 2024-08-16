ea3c7d294f198ca6f529d7b38085b415bf88da37
piotrk39 <piotrk39@gmail.com>
2024-08-16 11:05:08 +0200

    AB#29621 create project with wrong token

---
+++ e2e/portalicious/tests/CreateNewProject/FillInInvalidToken.spec.ts
@@ -0,0 +1,35 @@
+import CreateProject from '@121-e2e/portalicious/pages/CreateProjectPage';
+import HomePage from '@121-e2e/portalicious/pages/HomePage';
+import LoginPage from '@121-e2e/portalicious/pages/LoginPage';
+import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
+import { resetDB } from '@121-service/test/helpers/utility.helper';
+import { test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.test);
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto('/');
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+});
+
+test('[29636] Fill in with invalid Token', async ({ page }) => {
+  const homePage = new HomePage(page);
+  const createProject = new CreateProject(page);
+
+  await test.step('Should navigate to main page and select "Create new project" button and fill in the form with wrong token', async () => {
+    await homePage.openCreateNewProject();
+    await createProject.fillInForm({
+      assetId: process.env.KOBO_ASSET_ID_TEST,
+      token: 'wrongToken',
+    });
+    await createProject.submitForm();
+    await createProject.validateFormError({
+      errorText: 'Something went wrong: Invalid token.',
+    });
+  });
+});
