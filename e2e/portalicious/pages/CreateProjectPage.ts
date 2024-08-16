be03c28a947c035f07bb8bda02d941009ac1fa9d
piotrk39 <piotrk39@gmail.com>
2024-08-16 11:01:03 +0200

    AB#29621 Create Project without assetid and token in env

--- e2e/portalicious/pages/CreateProjectPage.ts
+++ e2e/portalicious/pages/CreateProjectPage.ts
@@ -4,6 +4,8 @@ import BasePage from './BasePage';
 class CreateProject extends BasePage {
   page: Page;
   readonly submitButton: Locator;
+  readonly assetIdInput: Locator;
+  readonly tokenInput: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -11,10 +13,27 @@ class CreateProject extends BasePage {
     this.submitButton = this.page.getByRole('button', {
       name: 'Submit',
     });
+    this.assetIdInput = this.page.locator('[formcontrolname="assetId"]');
+    this.tokenInput = this.page.locator('[formcontrolname="token"]');
+  }
+
+  async fillInForm({
+    assetId = '',
+    token = '',
+  }: {
+    assetId?: string;
+    token?: string;
+  }) {
+    await this.assetIdInput.fill(assetId);
+    await this.tokenInput.fill(token);
   }
 
   async submitForm() {
     await this.submitButton.click();
+  }
+
+  async assertCreateProjectSuccessPopUp() {
+    await this.validateToastMessage('Project successfully created.');
   }
 }
 
---
+++ e2e/portalicious/tests/CreateNewProject/CreateNewProjectSuccessfully.spec.ts
@@ -0,0 +1,36 @@
+import CreateProject from '@121-e2e/portalicious/pages/CreateProjectPage';
+import HomePage from '@121-e2e/portalicious/pages/HomePage';
+import LoginPage from '@121-e2e/portalicious/pages/LoginPage';
+import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
+import { resetDB } from '@121-service/test/helpers/utility.helper';
+import { test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.test);
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto('/');
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+});
+
+test('[29635] Create project successfully', async ({ page }) => {
+  const homePage = new HomePage(page);
+  const createProject = new CreateProject(page);
+
+  await test.step('Should navigate to main page and select "Create new project" button and fill in the form', async () => {
+    await homePage.openCreateNewProject();
+    await createProject.fillInForm({
+      assetId: process.env.KOBO_ASSET_ID_TEST,
+      token: process.env.KOBO_TOKEN_TEST,
+    });
+    await createProject.submitForm();
+    await createProject.assertCreateProjectSuccessPopUp();
+    await page.waitForURL((url) =>
+      url.pathname.startsWith('/en/project/2/registrations'),
+    );
+  });
+});
