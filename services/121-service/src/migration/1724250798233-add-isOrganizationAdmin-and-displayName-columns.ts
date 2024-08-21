5dc38961221e1f947675b0ab49ca1463341984fa
vladimir <vladimirpetkov1990@gmail.com>
2024-08-21 17:44:13 +0300

    feat: create migration to add isOrganizationAdmin and displayName in user table AB#29823

---
+++ services/121-service/src/migration/1724250798233-add-isOrganizationAdmin-and-displayName-columns.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddIsOrganizationAdminAndDisplayNameColumns1724250798233
+  implements MigrationInterface
+{
+  name = 'AddIsOrganizationAdminAndDisplayNameColumns1724250798233';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."user" ADD "isOrganizationAdmin" boolean NOT NULL DEFAULT false`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."user" ADD "displayName" character varying`,
+    );
+
+    await queryRunner.query(`
+      UPDATE "121-service"."user"
+      SET "displayName" = substring("username" from '^[^@]+')
+    `);
+
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."user" ALTER COLUMN "displayName" SET NOT NULL`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."user" DROP COLUMN "isOrganizationAdmin"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."user" DROP COLUMN "displayName"`,
+    );
+  }
+}
