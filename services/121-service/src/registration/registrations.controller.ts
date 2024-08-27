41bf213d1d4b12a5800f7a215f734c4d9a2f265c
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-08-27 07:03:32 +0200

    Fix bugs & lint

--- services/121-service/src/registration/registrations.controller.ts
+++ services/121-service/src/registration/registrations.controller.ts
@@ -732,10 +732,14 @@ export class RegistrationsController {
   public async reissueCardAndSendMessage(
     @Param('programId', ParseIntPipe) programId: number,
     @Param('referenceId') referenceId: string,
+    @Req() req,
   ): Promise<void> {
+    const userId = req.user.id;
+
     await this.registrationsService.reissueCardAndSendMessage(
       referenceId,
       programId,
+      userId,
     );
   }
 
@@ -790,6 +794,7 @@ export class RegistrationsController {
       programId,
       tokenCode,
       pause,
+      userId,
     );
   }
 
