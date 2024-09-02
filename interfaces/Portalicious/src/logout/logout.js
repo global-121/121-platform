da063e769ba054ad2fe8cbd3848e74830902a4a3
Elwin Schmitz <eschmitz@redcross.nl>
2024-09-02 16:33:36 +0200

    fix: Align "logout override"-features between all Portals

--- interfaces/Portalicious/src/logout/logout.js
+++ interfaces/Portalicious/src/logout/logout.js
@@ -1,6 +1,24 @@
-window.setTimeout(function () {
-    window.localStorage.clear();
-    window.sessionStorage.clear();
-  
-    window.location.href = '/?forced=' + Date.now();
-  }, 1000);
+window.API_URL = `NG_URL_121_SERVICE_API`;
+
+function redirectToLogin() {
+  window.location.assign('/login?forced=' + Date.now());
+}
+
+window.setTimeout(async () => {
+  window.localStorage.clear();
+  window.sessionStorage.clear();
+
+  await window
+    .fetch(`${window.API_URL}/users/logout`, {
+      credentials: 'include',
+      method: 'POST',
+      mode: 'cors',
+    })
+    .then(() => {
+      redirectToLogin();
+    });
+}, 100);
+
+window.setTimeout(async () => {
+  redirectToLogin();
+}, 2000);
