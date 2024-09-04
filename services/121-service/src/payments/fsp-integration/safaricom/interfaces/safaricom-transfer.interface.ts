92850c26dc7fde9afb0369a8e2051ee414786a25
Ashish Basak <ashish.kumar.basak@accenture.com>
2024-09-04 13:25:50 +0200

    fix: Improvements based on PR review comments AB#30018

---
+++ services/121-service/src/payments/fsp-integration/safaricom/interfaces/safaricom-transfer.interface.ts
@@ -0,0 +1,9 @@
+export interface SafaricomTransferParams {
+  readonly transactionAmount: number;
+  readonly programId: number;
+  readonly paymentNr: number;
+  readonly userId: number;
+  readonly referenceId: string;
+  readonly phoneNumber: string;
+  readonly idNumber: string;
+}
