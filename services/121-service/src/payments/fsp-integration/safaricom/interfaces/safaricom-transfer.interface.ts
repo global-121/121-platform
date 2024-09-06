a0963e3584b147ddb90232fe4d7b59a1c23d34cd
jannisvisser <jannisvisser@redcross.nl>
2024-09-06 14:57:07 +0200

    fix: renaming + put registrationProgramId back

--- services/121-service/src/payments/fsp-integration/safaricom/interfaces/safaricom-transfer.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface SafaricomTransferParams {
-  readonly transactionAmount: number;
-  readonly programId: number;
-  readonly paymentNr: number;
-  readonly userId: number;
-  readonly referenceId: string;
-  readonly phoneNumber: string;
-  readonly idNumber: string;
-}
