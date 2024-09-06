596ee5616f997f00e4cee8b10ce2c4af4d870d30
piotrk39 <piotrk39@gmail.com>
2024-09-06 13:10:10 +0200

    AB#30150 add e2e test and new seed script

---
+++ e2e/portalicious/tests/ManageTeam/AllSystemUsersCanBeAddedToProjectTeam.spec.ts
@@ -0,0 +1,52 @@
+import BasePage from '@121-e2e/portalicious/pages/BasePage';
+import LoginPage from '@121-e2e/portalicious/pages/LoginPage';
+import ProjectTeam from '@121-e2e/portalicious/pages/ProjectTeam';
+import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
+import { resetDB } from '@121-service/test/helpers/utility.helper';
+import { test } from '@playwright/test';
+
+const expectedAssignedUsers = ['admin@example.org'];
+const expectedAvailablesystemUsers = [
+  'program-admin@example.org',
+  'view-user@example.org',
+  'kobo-user@example.org',
+  'cva-manager@example.org',
+  'cva-officer@example.org',
+  'finance-manager@example.org',
+  'finance-officer@example.org',
+  'view-no-pii@example.org',
+];
+
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.oneAdmin);
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto('/');
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+});
+
+test('[29758] All system-users are available to be added to a "project team"', async ({
+  page,
+}) => {
+  const basePage = new BasePage(page);
+  const manageTeam = new ProjectTeam(page);
+  const projectTitle = 'Cash program Warsaw';
+
+  await test.step('Select program and navigate to Manage team', async () => {
+    await basePage.selectProgram(projectTitle);
+    await basePage.navigateToProgramPage('Team');
+  });
+
+  await test.step('Validate assigned users are visible', async () => {
+    await manageTeam.validateAssignedTeamMembers(expectedAssignedUsers);
+  });
+
+  await test.step('Validate available system users are visible', async () => {
+    await manageTeam.navigateToAddTeamMemebers();
+    await manageTeam.validateAvailableSystemUsers(expectedAvailablesystemUsers);
+  });
+});
