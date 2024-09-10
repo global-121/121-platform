36f66da0df78f9ec9f054c197c038c9a9f9492e9
Ashish Basak <ashish.kumar.basak@accenture.com>
2024-09-10 17:16:15 +0200

    fix: remove id from SafaricomTransferEntity since it inherit from Base121Entity

--- services/121-service/src/payments/fsp-integration/safaricom/entities/safaricom-transfer.entity.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/entities/safaricom-transfer.entity.ts
@@ -1,18 +1,9 @@
 import { Base121Entity } from '@121-service/src/base.entity';
 import { TransactionEntity } from '@121-service/src/payments/transactions/transaction.entity';
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 
 @Entity('safaricom_transfer')
 export class SafaricomTransferEntity extends Base121Entity {
-  @PrimaryGeneratedColumn()
-  public id: number;
-
   @Column({ unique: true })
   public originatorConversationId: string;
 
