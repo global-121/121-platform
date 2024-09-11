c067c9394cf19238b1f5bff88a641024f872af0f
jannisvisser <jannisvisser@redcross.nl>
2024-09-11 10:53:09 +0200

    fix: remove occassion from transfer payload AB#30017

--- services/121-service/src/payments/fsp-integration/safaricom/interfaces/do-transfer.interface.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/interfaces/do-transfer.interface.ts
@@ -4,6 +4,5 @@ export interface DoTransferParams {
   readonly phoneNumber: string;
   readonly idNumber: string;
   readonly remarks: string;
-  readonly occasion: string; // Safaricom API has misspelled this field: occassion
   readonly originatorConversationId: string;
 }
