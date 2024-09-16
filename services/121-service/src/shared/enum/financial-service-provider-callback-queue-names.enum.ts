c78d5cf74071793472838cb47a94bb1af2b52e5c
Ashish Basak <120180036+ashishkumarbasak-accenture@users.noreply.github.com>
2024-09-16 14:24:46 +0200

    Feat.implement timeout callback (#5829)
    * feat: implement safaricom timeout callback AB#30017
    
    * fix: added phone number 254000000002 for time out call response in mock service AB#30337
    
    * fix: some improvements
    
    * fix: added a comment
    
    ---------
    
    Co-authored-by: jannisvisser <jannisvisser@redcross.nl>

--- services/121-service/src/shared/enum/financial-service-provider-callback-queue-names.enum.ts
+++ services/121-service/src/shared/enum/financial-service-provider-callback-queue-names.enum.ts
@@ -1,3 +1,4 @@
 export enum FinancialServiceProviderCallbackQueuesNames {
   safaricomTransferCallback = 'safaricomTransferCallback',
+  safaricomTransferTimeoutCallback = 'safaricomTransferTimeoutCallback',
 }
