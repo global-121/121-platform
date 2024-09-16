bd6b19938dda4556758c97c0b807a76c8bdf11d0
arsforza <asforza@redcross.nl>
2024-09-16 10:24:11 +0200

    WIP add personal info page AB#29985

---
+++ interfaces/Portalicious/src/app/domains/project/project.helper.ts
@@ -0,0 +1,6 @@
+export const ATTRIBUTE_LABELS: Record<string, string> = {
+  fspDisplayName: $localize`:@@attribute-label-fspDisplayName:FSP Display Name`,
+  paymentAmountMultiplier: $localize`:@@attribute-label-paymentAmountMultiplier:Transfer value multiplier`,
+  maxPayments: $localize`:@@attribute-label-paymentAmountMultiplier:Max. payments`,
+  paymentCountRemaining: $localize`:@@attribute-label-paymentAmountMultiplier:Remaining payments`,
+};
