b7135b9cf0a2bb036b1bc1a830f70dd3c3e91ff5
Piotr Krajewski <52535860+Piotrk39@users.noreply.github.com>
2024-09-18 13:00:22 +0200

    Add E2E tests for Safaricom refactor (#5817)
    * AB#30266 Add successful payment test
    
    * AB#30266 Remove sent messages check add FSP & value assertions
    
    * AB#30266 Add failed payment tests for wrong phone number
    
    * AB#30266 Add failed payment tests for empy national id
    
    * AB#30266 change test file names add retry payment test and patch file
    
    * AB#30266 fix lint issue on ci
    
    * Add a zero
    
    * fix: small cleanup AB#30266
    
    * removewrong number test
    
    * docs: add rely-comment AB#30266
    
    ---------
    
    Co-authored-by: jannisvisser <jannisvisser@redcross.nl>

---
+++ e2e/tests/121-Portal/MakeNewPayment/Safaricom/MissingNationalIdErrorSafaricom.spec.ts
@@ -0,0 +1,106 @@
+import { test } from '@playwright/test';
+
+import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
+import KRCSProgram from '@121-service/src/seed-data/program/program-krcs-turkana.json';
+import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
+import {
+  getAccessToken,
+  resetDB,
+} from '@121-service/test/helpers/utility.helper';
+import { registrationsSafaricom } from '@121-service/test/registrations/pagination/pagination-data';
+
+import HomePage from '@121-e2e/pages/Home/HomePage';
+import LoginPage from '@121-e2e/pages/Login/LoginPage';
+import NavigationModule from '@121-e2e/pages/Navigation/NavigationModule';
+import PaymentsPage from '@121-e2e/pages/Payments/PaymentsPage';
+import RegistrationDetails from '@121-e2e/pages/RegistrationDetails/RegistrationDetailsPage';
+import TableModule from '@121-e2e/pages/Table/TableModule';
+
+import { AppRoutes } from '../../../../../interfaces/Portal/src/app/app-routes.enum';
+import englishTranslations from '../../../../../interfaces/Portal/src/assets/i18n/en.json';
+
+const krcsProgramTitle = KRCSProgram.titlePortal.en;
+const paymentLabel = englishTranslations.page.program.tab.payment.label;
+const paymentStatus = englishTranslations.entity.payment.status.error;
+const paymentFilter =
+  englishTranslations['registration-details']['activity-overview'].filters
+    .payment;
+const paymentErrorMessages = 'Property idNumber is undefined';
+
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.krcsMultiple);
+  const programIdBHA = 2;
+  const bhaProgramId = programIdBHA;
+  registrationsSafaricom[0].nationalId = '';
+
+  const accessToken = await getAccessToken();
+  await seedIncludedRegistrations(
+    registrationsSafaricom,
+    bhaProgramId,
+    accessToken,
+  );
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto(AppRoutes.login);
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+});
+
+test('[30262] Safaricom: Error because of missing National ID', async ({
+  page,
+}) => {
+  const table = new TableModule(page);
+  const navigationModule = new NavigationModule(page);
+  const homePage = new HomePage(page);
+  const registrationPage = new RegistrationDetails(page);
+  const paymentsPage = new PaymentsPage(page);
+
+  const numberOfPas = registrationsSafaricom.length;
+  const defaultTransferValue = KRCSProgram.fixedTransferValue;
+  const defaultMaxTransferValue = registrationsSafaricom.reduce(
+    (output, pa) => {
+      return output + pa.paymentAmountMultiplier * defaultTransferValue;
+    },
+    0,
+  );
+  const currency = KRCSProgram.currency;
+
+  // Format the number
+  const formattedValue = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+  }).format(defaultMaxTransferValue);
+
+  await test.step('Navigate to PA table', async () => {
+    await homePage.navigateToProgramme(krcsProgramTitle);
+    await navigationModule.navigateToProgramTab(paymentLabel);
+  });
+
+  await test.step('Do payment #1', async () => {
+    await table.doPayment(1);
+    await paymentsPage.executeNonEurPayment({
+      numberOfPas,
+      defaultTransferValue,
+      defaultMaxTransferValue: formattedValue,
+      currency,
+    });
+  });
+
+  await test.step('Check PA payments and messages for failed payment and errors', async () => {
+    await table.openFspProfile({ shouldIncludeVisa: false });
+
+    await registrationPage.validateQuantityOfActivity({ quantity: 3 });
+
+    await registrationPage.openActivityOverviewTab(paymentFilter);
+    await registrationPage.validatePaymentsTab({
+      paymentLabel,
+      paymentNumber: 1,
+      statusLabel: paymentStatus,
+    });
+    await registrationPage.validatePaymentErrorMessages({
+      messageContent: paymentErrorMessages,
+    });
+  });
+});
