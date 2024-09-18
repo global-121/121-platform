fdca30546e6ab0471c61cf69642dcf6d208af0b2
jannisvisser <jannisvisser@redcross.nl>
2024-09-18 16:42:02 +0200

    fix: share code between ocw and safaricom payment tests AB#30282

--- k6/tests/payment100kRegistrationSafaricom.js
+++ k6/tests/payment100kRegistrationSafaricom.js
@@ -1,19 +1,12 @@
 import { check, sleep } from 'k6';
 
 import { registrationSafaricom } from '../helpers/registration-default.data.js';
-import loginModel from '../models/login.js';
-import paymentsModel from '../models/payments.js';
-import ProgramsModel from '../models/programs.js';
-import RegistrationsModel from '../models/registrations.js';
-import resetModel from '../models/reset.js';
+import InitializePaymentModel from '../models/initalize-payment.js';
 
-const paymentsPage = new paymentsModel();
-const resetPage = new resetModel();
-const loginPage = new loginModel();
-const registrationsPage = new RegistrationsModel();
-const programsPage = new ProgramsModel();
+const initializePayment = new InitializePaymentModel();
 
-const duplicateNumber = 17;
+const duplicateNumber = 17; // '17' leads to 131k registrations
+const resetScript = 'krcs-multiple';
 const programId = 3;
 const paymentId = 3;
 const maxTimeoutAttempts = 200;
@@ -29,71 +22,13 @@ export const options = {
 };
 
 export default function () {
-  // reset db
-  const reset = resetPage.resetDB();
-  check(reset, {
-    'Reset succesfull status was 202': (r) => r.status == 202,
-  });
-
-  // login
-  const login = loginPage.login();
-  check(login, {
-    'Login succesfull status was 200': (r) => r.status == 201,
-    'Login time is less than 200ms': (r) => {
-      if (r.timings.duration >= 200) {
-        console.log(`Login time was ${r.timings.duration}ms`);
-      }
-      return r.timings.duration < 200;
-    },
-  });
-
-  // Upload registration
-  const registrationImport = registrationsPage.importRegistrations(
+  const monitorPayment = initializePayment.initializePayment(
+    resetScript,
     programId,
     registrationSafaricom,
-  );
-  check(registrationImport, {
-    'Import of registration successful status was 201': (r) => r.status == 201,
-  });
-
-  // Duplicate registration to be more then 100k
-  const duplicateRegistration =
-    resetPage.duplicateRegistrations(duplicateNumber);
-  check(duplicateRegistration, {
-    'Duplication successful status was 201': (r) => r.status == 201,
-  });
-
-  // Change status of all PAs to included and check response
-  const responseIncluded = programsPage.updateRegistrationStatusAndLog(
-    programId,
-    'included',
-  );
-  check(responseIncluded, {
-    'Status successfully changed to included: 202': (r) => {
-      if (r.status != 202) {
-        console.log(r.body);
-      }
-      return r.status == 202;
-    },
-  });
-
-  // Do the payment
-  const doPayment = paymentsPage.createPayment(programId);
-  check(doPayment, {
-    'Payment successfully done status 202': (r) => {
-      if (r.status != 202) {
-        console.log(r.body);
-      }
-      return r.status == 202;
-    },
-  });
-
-  // Monitor that 10% of payments is successful and then stop the test
-  const monitorPayment = paymentsPage.getPaymentResults(
-    programId,
-    maxTimeoutAttempts,
-    paymentId,
     duplicateNumber,
+    paymentId,
+    maxTimeoutAttempts,
     minPassRatePercentage,
   );
   check(monitorPayment, {
