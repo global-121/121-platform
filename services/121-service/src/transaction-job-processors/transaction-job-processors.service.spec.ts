6d1d84430ae30b5b7cba6345519f67f0dc8a182f
Ashish Basak <120180036+ashishkumarbasak-accenture@users.noreply.github.com>
2024-09-18 11:22:35 +0200

    feat: added unit tests for processSafaricomTransactionJob AB#30341 (#5834)
    * feat: added unit tests for processSafaricomTransactionJob AB#30341
    
    * fix: clear up test name AB#30341
    
    ---------
    
    Co-authored-by: jannisvisser <jannisvisser@redcross.nl>

---
+++ services/121-service/src/transaction-job-processors/transaction-job-processors.service.spec.ts
@@ -0,0 +1,167 @@
+import { TestBed } from '@automock/jest';
+import { QueryFailedError, UpdateResult } from 'typeorm';
+
+import { FinancialServiceProviderName } from '@121-service/src/financial-service-providers/enum/financial-service-provider-name.enum';
+import { FinancialServiceProviderEntity } from '@121-service/src/financial-service-providers/financial-service-provider.entity';
+import { FinancialServiceProviderRepository } from '@121-service/src/financial-service-providers/repositories/financial-service-provider.repository';
+import { SafaricomService } from '@121-service/src/payments/fsp-integration/safaricom/safaricom.service';
+import { TransactionStatusEnum } from '@121-service/src/payments/transactions/enums/transaction-status.enum';
+import { LatestTransactionRepository } from '@121-service/src/payments/transactions/repositories/latest-transaction.repository';
+import { TransactionEntity } from '@121-service/src/payments/transactions/transaction.entity';
+import { ProgramEntity } from '@121-service/src/programs/program.entity';
+import { ProgramRepository } from '@121-service/src/programs/repositories/program.repository';
+import { RegistrationEntity } from '@121-service/src/registration/registration.entity';
+import { RegistrationScopedRepository } from '@121-service/src/registration/repositories/registration-scoped.repository';
+import { ScopedRepository } from '@121-service/src/scoped.repository';
+import { TransactionJobProcessorsService } from '@121-service/src/transaction-job-processors/transaction-job-processors.service';
+import { SafaricomTransactionJobDto } from '@121-service/src/transaction-queues/dto/safaricom-transaction-job.dto';
+import { getScopedRepositoryProviderName } from '@121-service/src/utils/scope/createScopedRepositoryProvider.helper';
+
+const mockedRegistration: RegistrationEntity = {
+  id: 1,
+  referenceId: 'a3d1f489-2718-4430-863f-5abc14523691',
+  registrationStatus: 'active',
+  paymentCount: 0,
+  preferredLanguage: 'en',
+} as unknown as RegistrationEntity;
+
+const mockedFinancialServiceProvider: FinancialServiceProviderEntity = {
+  id: 1,
+  name: FinancialServiceProviderName.safaricom,
+} as unknown as FinancialServiceProviderEntity;
+
+const mockedTransaction: TransactionEntity = {
+  id: 1,
+  amount: 25,
+  status: TransactionStatusEnum.waiting,
+  userId: 1,
+} as TransactionEntity;
+
+const mockedTransactionJob: SafaricomTransactionJobDto = {
+  programId: 3,
+  paymentNumber: 3,
+  referenceId: 'a3d1f489-2718-4430-863f-5abc14523691',
+  transactionAmount: 25,
+  isRetry: false,
+  userId: 1,
+  bulkSize: 10,
+  phoneNumber: '254708374149',
+  idNumber: 'nat-123',
+  originatorConversationId: 'originator-conversation-id',
+};
+
+const mockedProgram = {
+  titlePortal: { en: 'Example Title' },
+  published: false,
+  distributionDuration: 100,
+  fixedTransferValue: 500,
+  budget: 50000,
+  enableMaxPayments: true,
+};
+
+describe('TransactionJobProcessorsService', () => {
+  let safaricomService: SafaricomService;
+  let transactionJobProcessorsService: TransactionJobProcessorsService;
+
+  let programRepository: ProgramRepository;
+  let registrationScopedRepository: RegistrationScopedRepository;
+  let latestTransactionRepository: LatestTransactionRepository;
+  let transactionScopedRepository: ScopedRepository<TransactionEntity>;
+  let financialServiceProviderRepository: FinancialServiceProviderRepository;
+
+  beforeEach(async () => {
+    const { unit, unitRef } = TestBed.create(
+      TransactionJobProcessorsService,
+    ).compile();
+
+    transactionJobProcessorsService = unit;
+
+    safaricomService = unitRef.get<SafaricomService>(SafaricomService);
+
+    programRepository = unitRef.get<ProgramRepository>(ProgramRepository);
+    registrationScopedRepository = unitRef.get<RegistrationScopedRepository>(
+      RegistrationScopedRepository,
+    );
+
+    financialServiceProviderRepository =
+      unitRef.get<FinancialServiceProviderRepository>(
+        FinancialServiceProviderRepository,
+      );
+
+    transactionScopedRepository = unitRef.get<
+      ScopedRepository<TransactionEntity>
+    >(getScopedRepositoryProviderName(TransactionEntity));
+
+    latestTransactionRepository = unitRef.get<LatestTransactionRepository>(
+      LatestTransactionRepository,
+    );
+  });
+
+  it('should be defined', () => {
+    expect(transactionJobProcessorsService).toBeDefined();
+  });
+
+  it('[Idempotency] safaricom transaction job processing should fail when using same originatorConversationId', async () => {
+    jest
+      .spyOn(registrationScopedRepository, 'getByReferenceId')
+      .mockResolvedValueOnce(mockedRegistration);
+
+    jest
+      .spyOn(registrationScopedRepository, 'updateUnscoped')
+      .mockResolvedValueOnce({} as UpdateResult);
+
+    jest
+      .spyOn(financialServiceProviderRepository, 'getByName')
+      .mockResolvedValueOnce(mockedFinancialServiceProvider);
+
+    jest
+      .spyOn(programRepository, 'findByIdOrFail')
+      .mockResolvedValueOnce(mockedProgram as ProgramEntity);
+
+    jest
+      .spyOn(transactionScopedRepository, 'save')
+      .mockResolvedValueOnce([mockedTransaction]);
+
+    jest
+      .spyOn(latestTransactionRepository, 'insertOrUpdateFromTransaction')
+      .mockResolvedValueOnce();
+
+    const queryFailedError = new QueryFailedError(
+      'safaricom_transfer',
+      [],
+      new Error('duplicate key value violates unique constraint'),
+    );
+    (queryFailedError as any).code = '23505';
+
+    jest
+      .spyOn(safaricomService, 'doTransfer')
+      .mockRejectedValueOnce(queryFailedError);
+
+    jest
+      .spyOn(transactionScopedRepository, 'update')
+      .mockResolvedValueOnce({} as UpdateResult);
+
+    // Call the service method
+    await expect(
+      transactionJobProcessorsService.processSafaricomTransactionJob(
+        mockedTransactionJob,
+      ),
+    ).rejects.toThrow(QueryFailedError);
+
+    expect(registrationScopedRepository.getByReferenceId).toHaveBeenCalledWith({
+      referenceId: mockedTransactionJob.referenceId,
+    });
+    expect(financialServiceProviderRepository.getByName).toHaveBeenCalledWith(
+      FinancialServiceProviderName.safaricom,
+    );
+    expect(safaricomService.doTransfer).toHaveBeenCalledWith(
+      expect.objectContaining({
+        transferAmount: mockedTransactionJob.transactionAmount,
+        phoneNumber: mockedTransactionJob.phoneNumber,
+        idNumber: mockedTransactionJob.idNumber,
+        remarks: `Payment ${mockedTransactionJob.paymentNumber}`,
+        originatorConversationId: mockedTransactionJob.originatorConversationId,
+      }),
+    );
+  });
+});
