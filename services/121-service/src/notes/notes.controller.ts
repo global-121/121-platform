8c29625dbf2863168ebca894d8cfb5d7b50517f1
diderikvw <diderik@diderikvanwingerden.com>
2024-09-25 09:47:54 +0200

    Notes endpoints small refactors and refactor comments (#5847)
    * Small refactors and refactor comments
    
    * Fix to POST notes

--- services/121-service/src/notes/notes.controller.ts
+++ services/121-service/src/notes/notes.controller.ts
@@ -15,7 +15,7 @@ import { AuthenticatedUser } from '@121-service/src/guards/authenticated-user.de
 import { AuthenticatedUserGuard } from '@121-service/src/guards/authenticated-user.guard';
 import { CreateNoteDto } from '@121-service/src/notes/dto/note.dto';
 import { ResponseNoteDto } from '@121-service/src/notes/dto/response-note.dto';
-import { NoteService } from '@121-service/src/notes/notes.service';
+import { NotesService } from '@121-service/src/notes/notes.service';
 import { ScopedUserRequest } from '@121-service/src/shared/scoped-user-request';
 import { PermissionEnum } from '@121-service/src/user/enum/permission.enum';
 import { RequestHelper } from '@121-service/src/utils/request-helper/request-helper.helper';
@@ -24,9 +24,9 @@ import { RequestHelper } from '@121-service/src/utils/request-helper/request-hel
 @ApiTags('programs')
 @Controller('programs')
 export class NoteController {
-  private readonly noteService: NoteService;
-  public constructor(noteService: NoteService) {
-    this.noteService = noteService;
+  private readonly notesService: NotesService;
+  public constructor(notesService: NotesService) {
+    this.notesService = notesService;
   }
 
   @AuthenticatedUser({
@@ -49,16 +49,17 @@ export class NoteController {
   })
   @ApiParam({ name: 'programId', required: true, type: 'integer' })
   @ApiParam({ name: 'referenceId', required: true, type: 'string' })
-  @Post(':programId/registrations/:referenceId/note')
+  @Post(':programId/registrations/:referenceId/notes')
   public async createNote(
     @Req() req: ScopedUserRequest,
     @Param('programId', ParseIntPipe) programId: number,
     @Param('referenceId') referenceId: string,
     @Body() createNote: CreateNoteDto,
   ): Promise<void> {
+    // TODO: REFACTOR: Should return a representation of the created note in the response
     const userId = RequestHelper.getUserId(req);
 
-    await this.noteService.createNote(
+    await this.notesService.createNote(
       referenceId,
       createNote.text,
       userId,
@@ -81,6 +82,7 @@ export class NoteController {
     @Param('programId', ParseIntPipe) programId: number,
     @Param('referenceId') referenceId: string,
   ): Promise<ResponseNoteDto[]> {
-    return await this.noteService.retrieveNotes(referenceId, programId);
+    // TODO: REFACTOR: Should return the user as a nested object in the response, see e.g.: GET /api/programs/{programId}/actions
+    return await this.notesService.retrieveNotes(referenceId, programId);
   }
 }
