a523aba9c9d031ecb3de2698c534b7bde36ec658
Elwin Schmitz <eschmitz@redcross.nl>
2024-09-25 16:07:01 +0200

    fix: Translatable text IDs

--- interfaces/Portalicious/src/app/domains/project/project.helper.ts
+++ interfaces/Portalicious/src/app/domains/project/project.helper.ts
@@ -10,8 +10,8 @@ import { Attribute } from '~/domains/project/project.model';
 export const ATTRIBUTE_LABELS: Record<string, string | undefined> = {
   fspDisplayName: $localize`:@@attribute-label-fspDisplayName:FSP Display Name`,
   paymentAmountMultiplier: $localize`:@@attribute-label-paymentAmountMultiplier:Transfer value multiplier`,
-  maxPayments: $localize`:@@attribute-label-paymentAmountMultiplier:Max. payments`,
-  paymentCountRemaining: $localize`:@@attribute-label-paymentAmountMultiplier:Remaining payments`,
+  maxPayments: $localize`:@@attribute-label-maxPayments:Max. payments`,
+  paymentCountRemaining: $localize`:@@attribute-label-paymentCountRemaining:Remaining payments`,
 };
 
 export const attributeToDataListItem = (
