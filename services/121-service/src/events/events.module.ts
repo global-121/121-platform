d1ac8495e3588ee860a8738113d287794eee38bc
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-09-26 13:36:19 +0200

    refactor: event scoped repository

--- services/121-service/src/events/events.module.ts
+++ services/121-service/src/events/events.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventEntity } from '@121-service/src/events/entities/event.entity';
 import { EventAttributeEntity } from '@121-service/src/events/entities/event-attribute.entity';
+import { EventRepository } from '@121-service/src/events/event.repository';
 import { EventsController } from '@121-service/src/events/events.controller';
 import { EventsService } from '@121-service/src/events/events.service';
 import { UserModule } from '@121-service/src/user/user.module';
@@ -15,7 +16,11 @@ import { createScopedRepositoryProvider } from '@121-service/src/utils/scope/cre
     UserModule,
     TypeOrmModule.forFeature([EventEntity, EventAttributeEntity]),
   ],
-  providers: [EventsService, createScopedRepositoryProvider(EventEntity)],
+  providers: [
+    EventsService,
+    createScopedRepositoryProvider(EventEntity),
+    EventRepository,
+  ],
   controllers: [EventsController],
   exports: [EventsService],
 })
