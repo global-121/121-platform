3e624ec78adf63f199246dff931a6bb66ed992e4
diderikvw <diderik@diderikvanwingerden.com>
2024-10-04 15:35:46 +0200

    Visa chores, comments and small refactors (#5891)
    * Some refactoring
    
    Refactor query to custom repo, remove unncessary check for sending contact info
    
    * Remove comment
    
    * Fix test to assert 500
    
    * chore: switch job dto from class to interface + remove decorators AB#30445
    
    * fix: put back check on intersolve customer AB#30297
    
    ---------
    
    Co-authored-by: jannisvisser <jannisvisser@redcross.nl>

--- services/121-service/src/transaction-queues/dto/intersolve-visa-transaction-job.dto.ts
+++ services/121-service/src/transaction-queues/dto/intersolve-visa-transaction-job.dto.ts
@@ -1,58 +1,16 @@
-import {
-  IsBoolean,
-  IsNotEmpty,
-  IsNumber,
-  IsNumberString,
-  IsOptional,
-} from 'class-validator';
-
-// TODO: Make properties read-only according to DTO guidelines
-export class IntersolveVisaTransactionJobDto {
-  @IsNotEmpty()
-  @IsNumberString()
-  public readonly programId: number;
-
-  @IsNotEmpty()
-  @IsNumberString()
-  public readonly paymentNumber: number;
-
-  @IsNotEmpty()
-  public readonly referenceId: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  public readonly transactionAmountInMajorUnit: number; // This is in the major unit of the currency, for example whole euros
-
-  @IsNotEmpty()
-  @IsBoolean()
-  public readonly isRetry: boolean;
-
-  @IsNotEmpty()
-  @IsNumber()
-  public readonly userId: number;
-
-  @IsNotEmpty()
-  @IsNumber()
-  public readonly bulkSize: number;
-
-  @IsOptional()
-  public readonly name?: string;
-
-  @IsOptional()
-  public readonly addressStreet?: string;
-
-  @IsOptional()
-  public readonly addressHouseNumber?: string;
-
-  @IsOptional()
-  public readonly addressHouseNumberAddition?: string;
-
-  @IsOptional()
-  public readonly addressPostalCode?: string;
-
-  @IsOptional()
-  public readonly addressCity?: string;
-
-  @IsOptional()
-  public readonly phoneNumber?: string;
+export interface IntersolveVisaTransactionJobDto {
+  readonly programId: number;
+  readonly paymentNumber: number;
+  readonly referenceId: string;
+  readonly transactionAmountInMajorUnit: number; // This is in the major unit of the currency, for example whole euros
+  readonly isRetry: boolean;
+  readonly userId: number;
+  readonly bulkSize: number;
+  readonly name?: string;
+  readonly addressStreet?: string;
+  readonly addressHouseNumber?: string;
+  readonly addressHouseNumberAddition?: string;
+  readonly addressPostalCode?: string;
+  readonly addressCity?: string;
+  readonly phoneNumber?: string;
 }
