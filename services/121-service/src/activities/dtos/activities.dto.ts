50d5e1cef8b86c14623d6a0ff5e9941ca14b539c
Domenico Gemoli <domenicogemoli@gmail.com>
2024-10-09 16:53:34 +0200

    feat.activities: dto mapping to endpoint
    AB#30664

--- services/121-service/src/activities/dtos/activities.dto.ts
+++ services/121-service/src/activities/dtos/activities.dto.ts
@@ -1,18 +1,20 @@
+import { FinancialServiceProviderName } from '@121-service/src/financial-service-providers/enum/financial-service-provider-name.enum';
 import { MessageContentType } from '@121-service/src/notifications/enum/message-type.enum';
-import { NotificationType } from '@121-service/src/notifications/twilio.entity';
 import { TransactionStatusEnum } from '@121-service/src/payments/transactions/enums/transaction-status.enum';
 import { RegistrationStatusEnum } from '@121-service/src/registration/enum/registration-status.enum';
+import { LocalizedString } from '@121-service/src/shared/types/localized-string.type';
 
 export class ActivitiesDto {
   meta: {
     availableTypes: ActivityTypeEnum[];
     count: Partial<Record<ActivityTypeEnum, number>>;
   };
-  data: ActivityType[];
+  data: Activity[];
 }
 
-type ActivityType =
+export type Activity =
   | DataChangeActivity
+  | FinancialServiceProviderChangeActivity
   | MessageActivity
   | NoteActivity
   | StatusChangeActivity
@@ -24,15 +26,12 @@ enum ActivityTypeEnum {
   Note = 'note',
   DataChange = 'data-change',
   StatusChange = 'status-change',
-  FinancialServiceProviderChange = 'financial-service-provider-change', // ##TODO: Looks like this type is still missing in the rest of the code, e.g. extending from BaseActivity.
+  FinancialServiceProviderChange = 'financial-service-provider-change',
 }
 
 interface BaseActivity {
   id: string;
-  user: {
-    id: number;
-    username: string;
-  };
+  username?: string;
   created: Date;
   type: ActivityTypeEnum;
   attributes: Record<string, unknown>;
@@ -45,10 +44,9 @@ interface TransactionActivity extends BaseActivity {
     status: TransactionStatusEnum;
     amount: number;
     paymentDate: Date;
-    fsp: string; // Financial service provider enum name, like "intersolve-visa";
-    fspName: string; // Financial service provider name, like "Intersolve Visa";
-    errorMessage: null | string;
-    customData: unknown; // ##TODO: Do we want/need to expose customData via the API?
+    fsp: FinancialServiceProviderName;
+    fspName: LocalizedString;
+    errorMessage?: string;
   };
 }
 
@@ -66,7 +64,6 @@ interface MessageActivity extends BaseActivity {
     to: string;
     body: string;
     status: string;
-    type: NotificationType;
     mediaUrl: string | null;
     contentType: MessageContentType;
     errorCode: string | null;
@@ -84,9 +81,18 @@ interface StatusChangeActivity extends BaseActivity {
 interface DataChangeActivity extends BaseActivity {
   type: ActivityTypeEnum.DataChange;
   attributes: {
-    fieldName: string;
-    oldValue: string;
-    newValue: string;
-    reason: string; // Reason for the data change
+    fieldName: string | null;
+    oldValue: string | null;
+    newValue: string | null;
+    reason: string | null; // Reason for the data change
+  };
+}
+
+export interface FinancialServiceProviderChangeActivity extends BaseActivity {
+  type: ActivityTypeEnum.FinancialServiceProviderChange;
+  attributes: {
+    oldValue: string | null;
+    newValue: string | null;
+    reason: string | null; // Reason for the data change
   };
 }
