aa5d3fd07b7b4b60be6b68047894726671767a97
Elwin Schmitz <eschmitz@redcross.nl>
2024-10-21 11:26:47 +0200

    fix: Enforce best-practices in e-mail contents, to prevent training users to be vulnerable for phishing-attacks
    - Use fully visible, plain URLs so users can copy-paste or "type them over", instead of blindly clicking-whats-there
    - Improve styling of password to prevent misspellings/typos
    - (auto-)fix formatting to prevent long, unreadable lines of code
    - Use single place for template + styling (instead of multiple + in Azure Logic-app config)

--- services/121-service/src/emails/templates/createNonSsoUserTemplate.ts
+++ services/121-service/src/emails/templates/createNonSsoUserTemplate.ts
@@ -1,28 +1,44 @@
-import { emailStyle } from '@121-service/src/emails/templates/style';
+import { emailBody } from '@121-service/src/emails/templates/body.helper';
 
 export const createNonSSOUserTemplate = (
   displayName: string,
   username: string,
   password: string,
-): { subject: string; body: string } => {
+): {
+  subject: string;
+  body: string;
+} => {
   const subject = '121 Portal account created';
 
-  const body = `
-  ${emailStyle()}
+  const loginUrl = process.env.REDIRECT_PORTAL_URL_HOST;
+  const changePasswordUrl = `${process.env.REDIRECT_PORTAL_URL_HOST}/user`;
+  const supportEmail = 'support@121.global';
 
-    <div>
-        <p>Dear ${displayName},</p>
-        <p>You have been added to the 121 Portal by the platform admin. Click <a href="${process.env.REDIRECT_PORTAL_URL_HOST}">here</a> to log in.</p>
-        <br>
-        <p>Username: ${username}</p>
-        <p>Password: ${password}</p>
-        <br>
-        <p>After logging in, please change your password here: ${process.env.REDIRECT_PORTAL_URL_HOST}/user</p>
-        <p>For assistance, if you were not expecting this email or believe it was sent to you by mistake, please contact <a href="mailto:support@121.global">support@121.global</a></p>
-        <p>Best regards,</p>
-        <p>121 Portal Team</p>
-    </div>
-  `;
+  const body = emailBody(`
+    <p>Dear ${displayName},</p>
+    <p>
+      You have been added to the 121 Portal by the platform admin.<br>
+      To log in, go to: <a href="${loginUrl}">${loginUrl}</a>
+    </p>
+    <p>
+      Username: ${username}<br>
+      Password: <code>${password}</code>
+    </p>
+    <p>
+      After logging in, please change your password on: <a href="${changePasswordUrl}">${changePasswordUrl}</a>
+    </p>
+    <p>
+      For assistance, if you were not expecting this email or believe it was sent to you by mistake,
+      please contact: <a href="mailto:${supportEmail}">${supportEmail}</a>
+    </p>
+    <p>
+      Best regards,<br>
+      121 Support Team
+    </p>
+  `);
 
-  return { subject, body };
+  return {
+    subject,
+    body,
+  };
 };
--- services/121-service/src/emails/templates/genericTemplate.ts
+++ services/121-service/src/emails/templates/genericTemplate.ts
@@ -1,43 +1,30 @@
+import { emailBody } from '@121-service/src/emails/templates/body.helper';
+
+/**
+ * Create a basic e-mail message, including default salutation and signature.
+ * @param subject The e-mail subject.
+ * @param bodyMessage Text-only message.
+ */
 export const genericTemplate = (
   subject: string,
   bodyMessage: string,
-): { subject: string; body: string } => {
-  const body = `
-    <title>${subject}</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #F3F4F6;
-        }
-        .header, .footer {
-            background-color: #0A2C5E;
-            color: #FFFFFF;
-            padding: 20px;
-            text-align: left;
-        }
-        .content {
-            padding: 20px;
-            margin: 20px;
-            background-color: #FFFFFF;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            margin-bottom: 40px;
-        }
-    </style>
+): {
+  subject: string;
+  body: string;
+} => {
+  const body = emailBody(`
+    <p>Dear madam/sir,</p>
+    <p>
+      ${bodyMessage}
+    </p>
+    <p>
+      Best regards,<br>
+      121 Support Team
+    </p>
+  `);
 
-    <div class="content">
-      <p>Dear madam/sir,</p>
-      <p>${bodyMessage}</p>
-      <p>Kind regards,</p>
-      <p>121 support</p>
-    </div>
-
-    <div class="footer">
-      &copy; 2024 121 Support, All rights reserved.
-    </div>
-  `;
-
-  return { subject, body };
+  return {
+    subject,
+    body,
+  };
 };
