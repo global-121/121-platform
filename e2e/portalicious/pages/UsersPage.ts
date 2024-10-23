e9ee8b528aefe4d7bf115ed7a4cbaf464ffbab18
piotrk39 <piotrk39@gmail.com>
2024-10-23 14:09:18 +0200

    Add e2e test and usersPage POM

---
+++ e2e/portalicious/pages/UsersPage.ts
@@ -0,0 +1,51 @@
+import { expect, Locator } from '@playwright/test';
+import { Page } from 'playwright';
+
+import BasePage from './BasePage';
+
+class UsersPage extends BasePage {
+  page: Page;
+  readonly tableRows: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+    this.tableRows = this.page.locator('table tbody tr');
+  }
+
+  async validateAssignedUsersNames(expectedAssignedUsers: string[]) {
+    const actualAssignedUsers = await this.tableRows.evaluateAll((rows) =>
+      rows.map((row) =>
+        row.querySelector('td:nth-child(1)').textContent.trim(),
+      ),
+    );
+
+    const sortedActualUsers = [...actualAssignedUsers].sort((a, b) =>
+      a.localeCompare(b),
+    );
+    const sortedExpectedUsers = [...expectedAssignedUsers].sort((a, b) =>
+      a.localeCompare(b),
+    );
+
+    expect(sortedActualUsers).toEqual(sortedExpectedUsers);
+  }
+
+  async validateAssignedUserEmails(expectedUserEmails: string[]) {
+    const actualUserEmails = await this.tableRows.evaluateAll((rows) =>
+      rows.map((row) =>
+        row.querySelector('td:nth-child(2)').textContent.trim(),
+      ),
+    );
+
+    const sortedActualUserEmails = [...actualUserEmails].sort((a, b) =>
+      a.localeCompare(b),
+    );
+    const sortedExpectedUserEmails = [...expectedUserEmails].sort((a, b) =>
+      a.localeCompare(b),
+    );
+
+    expect(sortedActualUserEmails).toEqual(sortedExpectedUserEmails);
+  }
+}
+
+export default UsersPage;
