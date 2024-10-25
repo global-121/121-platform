9e8776851cd173b895b2338c63fc0082248e896d
tijsziere <39266480+tijsziere@users.noreply.github.com>
2024-10-25 15:46:09 +0200

    change footer

--- interfaces/Portalicious/src/app/components/page-layout/components/footer/footer.component.ts
+++ interfaces/Portalicious/src/app/components/page-layout/components/footer/footer.component.ts
@@ -1,19 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { ButtonModule } from 'primeng/button';
-
-import { environment } from '~environment';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ToolbarModule],
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
-  apiDocumentationUrl = environment.url_121_service_api.replace(
-    '/api',
-    '/docs',
-  );
+  currentYear: number = new Date().getFullYear();
 }
+
