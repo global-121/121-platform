cf0538a4a2e2a1bb0044104be88ef674f6e67861
piotrk39 <piotrk39@gmail.com>
2024-10-25 15:04:30 +0300

    fix lint issue

--- e2e/portalicious/tests/Users/UsersLastLogin.spec.ts
+++ e2e/portalicious/tests/Users/UsersLastLogin.spec.ts
@@ -45,7 +45,7 @@ test('[30866] [Admin] View last login', async ({ page }) => {
   await test.step('Validate last login', async () => {
     await basePage.navigateToPage('Users');
     await users.validateRowTextContent({
-      email: process.env.USERCONFIG_121_SERVICE_EMAIL_USER_VIEW,
+      email: process.env.USERCONFIG_121_SERVICE_EMAIL_USER_VIEW!,
       textContent: loginTimeStamp,
     });
   });
