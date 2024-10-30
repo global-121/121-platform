350ad9d01ea8df361b2a7449c6c6ac9ac4261d58
jannisvisser <jannisvisser@redcross.nl>
2024-10-30 11:55:16 +0100

    fix: make seed mock script work again

--- services/121-service/src/scripts/sql/mock-visa-customers.sql
+++ services/121-service/src/scripts/sql/mock-visa-customers.sql
@@ -9,5 +9,5 @@ INSERT
 			,'mock-holderId'
 			,r.id
 	from "121-service".registration r 
-	left join "121-service".financial_service_provider f on r."fspId" = f.id 
-	where f.fsp = 'Intersolve-visa');
+	left join "121-service".program_financial_service_provider_configuration f on r."programFinancialServiceProviderConfigurationId" = f.id 
+	where f."financialServiceProviderName" = 'Intersolve-visa');
