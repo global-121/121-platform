d1c13f8bd8952881e139f1036667b62045c4b1b8
Domenico Gemoli <domenicogemoli@gmail.com>
2024-10-30 11:56:37 +0100

    portalicious: create payment button
    AB#30872

--- interfaces/Portalicious/src/app/pages/project-payments/project-payments.page.ts
+++ interfaces/Portalicious/src/app/pages/project-payments/project-payments.page.ts
@@ -1,11 +1,20 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
 
 import { PageLayoutComponent } from '~/components/page-layout/page-layout.component';
+import { ToastService } from '~/services/toast.service';
 
 @Component({
   selector: 'app-project-payments',
   standalone: true,
-  imports: [PageLayoutComponent],
+  imports: [PageLayoutComponent, ButtonModule],
+  providers: [ToastService],
   templateUrl: './project-payments.page.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,4 +22,13 @@ import { PageLayoutComponent } from '~/components/page-layout/page-layout.compon
 export class ProjectPaymentsPageComponent {
   // this is injected by the router
   projectId = input.required<number>();
+
+  private toastService = inject(ToastService);
+
+  onCreatePayment() {
+    this.toastService.showToast({
+      severity: 'warn',
+      detail: 'Functionality not implemented yet',
+    });
+  }
 }
