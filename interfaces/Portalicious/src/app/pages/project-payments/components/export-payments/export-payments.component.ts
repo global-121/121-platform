5bfc3474666b855ef872d6c15a8e86b6a3f4ac0b
Domenico Gemoli <dgemoli@redcross.nl>
2024-11-05 14:02:00 +0100

    portalicious: payment exports (#6009)
    AB#30871

---
+++ interfaces/Portalicious/src/app/pages/project-payments/components/export-payments/export-payments.component.ts
@@ -0,0 +1,158 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  ViewChild,
+} from '@angular/core';
+
+import {
+  injectMutation,
+  injectQuery,
+} from '@tanstack/angular-query-experimental';
+import { MenuItem } from 'primeng/api';
+
+import { ExportType } from '@121-service/src/metrics/enum/export-type.enum';
+import { PermissionEnum } from '@121-service/src/user/enum/permission.enum';
+
+import { ButtonMenuComponent } from '~/components/button-menu/button-menu.component';
+import { ConfirmationDialogComponent } from '~/components/confirmation-dialog/confirmation-dialog.component';
+import { PaymentApiService } from '~/domains/payment/payment.api.service';
+import { ProjectApiService } from '~/domains/project/project.api.service';
+import {
+  projectHasPhysicalCardSupport,
+  projectHasVoucherSupport,
+} from '~/domains/project/project.helper';
+import { LatestExportDateComponent } from '~/pages/project-registrations/components/latest-export-date/latest-export-date.component';
+import { AuthService } from '~/services/auth.service';
+import { ExportService } from '~/services/export.service';
+import { ToastService } from '~/services/toast.service';
+
+@Component({
+  selector: 'app-export-payments',
+  standalone: true,
+  imports: [
+    ConfirmationDialogComponent,
+    ButtonMenuComponent,
+    LatestExportDateComponent,
+  ],
+  providers: [ToastService],
+  templateUrl: './export-payments.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ExportPaymentsComponent {
+  projectId = input.required<number>();
+
+  private authService = inject(AuthService);
+  private exportService = inject(ExportService);
+  private paymentApiService = inject(PaymentApiService);
+  private projectApiService = inject(ProjectApiService);
+  private toastService = inject(ToastService);
+
+  project = injectQuery(this.projectApiService.getProject(this.projectId));
+  payments = injectQuery(this.paymentApiService.getPayments(this.projectId));
+
+  @ViewChild('exportlastPaymentsDialog')
+  private exportlastPaymentsDialog: ConfirmationDialogComponent;
+  @ViewChild('exportUnusedVouchersDialog')
+  private exportUnusedVouchersDialog: ConfirmationDialogComponent;
+  @ViewChild('exportDebitCardUsageDialog')
+  private exportDebitCardUsageDialog: ConfirmationDialogComponent;
+
+  ExportType = ExportType;
+
+  maxLastPaymentsNumber = computed(() => {
+    if (!this.payments.isSuccess()) {
+      return 0;
+    }
+
+    // At max 5 payments can be exported
+    return Math.min(this.payments.data().length, 5);
+  });
+
+  getMutationData = ({
+    type,
+    withPaymentRange = false,
+  }: {
+    type: ExportType;
+    withPaymentRange?: boolean;
+  }) => {
+    const payments = this.payments.data() ?? [];
+    const l = payments.length;
+
+    if (!withPaymentRange || l === 0) {
+      return { type };
+    }
+
+    const minPayment = payments[l - this.maxLastPaymentsNumber()].payment;
+    const maxPayment = payments[l - 1].payment;
+
+    return {
+      type,
+      minPayment,
+      maxPayment,
+    };
+  };
+
+  lastPaymentsExportLabel = computed(() => {
+    return $localize`:@@export-payments-last:Export last ${this.maxLastPaymentsNumber()} payment(s)`;
+  });
+
+  exportPaymentsMutation = injectMutation(() => ({
+    mutationFn: this.exportService.getExportListMutation(
+      this.projectId,
+      this.toastService,
+    ),
+    onSuccess: this.exportService.downloadExport(this.toastService),
+  }));
+
+  exportOptions = computed<MenuItem[]>(() => [
+    {
+      label: this.lastPaymentsExportLabel(),
+      visible:
+        this.maxLastPaymentsNumber() > 0 &&
+        this.authService.hasAllPermissions({
+          projectId: this.projectId(),
+          requiredPermissions: [
+            PermissionEnum.PaymentREAD,
+            PermissionEnum.PaymentTransactionREAD,
+            PermissionEnum.RegistrationPaymentExport,
+          ],
+        }),
+      command: () => {
+        this.exportlastPaymentsDialog.askForConfirmation();
+      },
+    },
+    {
+      label: $localize`:@@export-payments-unused-vouchers:Export unused vouchers`,
+      visible:
+        projectHasVoucherSupport(this.project.data()) &&
+        this.authService.hasPermission({
+          projectId: this.projectId(),
+          requiredPermission: PermissionEnum.PaymentVoucherExport,
+        }),
+      command: () => {
+        this.exportUnusedVouchersDialog.askForConfirmation();
+      },
+    },
+    {
+      label: $localize`:@@export-payments-debit-card-usage:Export debit card usage`,
+      visible:
+        this.maxLastPaymentsNumber() > 0 &&
+        projectHasPhysicalCardSupport(this.project.data()) &&
+        this.authService.hasPermission({
+          projectId: this.projectId(),
+          requiredPermission: PermissionEnum.FspDebitCardEXPORT,
+        }),
+      command: () => {
+        this.exportDebitCardUsageDialog.askForConfirmation();
+      },
+    },
+  ]);
+
+  hasExportOptions = computed(() =>
+    this.exportOptions().some((option) => option.visible),
+  );
+}
--- interfaces/Portalicious/src/app/pages/project-payments/project-payments.page.ts
+++ interfaces/Portalicious/src/app/pages/project-payments/project-payments.page.ts
@@ -1,19 +1,24 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   inject,
   input,
 } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 
+import { PermissionEnum } from '@121-service/src/user/enum/permission.enum';
+
 import { PageLayoutComponent } from '~/components/page-layout/page-layout.component';
+import { ExportPaymentsComponent } from '~/pages/project-payments/components/export-payments/export-payments.component';
+import { AuthService } from '~/services/auth.service';
 import { ToastService } from '~/services/toast.service';
 
 @Component({
   selector: 'app-project-payments',
   standalone: true,
-  imports: [PageLayoutComponent, ButtonModule],
+  imports: [PageLayoutComponent, ButtonModule, ExportPaymentsComponent],
   providers: [ToastService],
   templateUrl: './project-payments.page.html',
   styles: ``,
@@ -23,6 +28,7 @@ export class ProjectPaymentsPageComponent {
   // this is injected by the router
   projectId = input.required<number>();
 
+  private authService = inject(AuthService);
   private toastService = inject(ToastService);
 
   onCreatePayment() {
@@ -31,4 +37,15 @@ export class ProjectPaymentsPageComponent {
       detail: 'Functionality not implemented yet',
     });
   }
+
+  canExport = computed(() =>
+    this.authService.hasAllPermissions({
+      projectId: this.projectId(),
+      requiredPermissions: [
+        PermissionEnum.RegistrationPersonalEXPORT,
+        PermissionEnum.PaymentREAD,
+        PermissionEnum.PaymentTransactionREAD,
+      ],
+    }),
+  );
 }
