10d317e2da0d40c949857b9c18ba49c1cfaf155d
Ruben <vandervalk@geoit.nl>
2024-11-08 16:33:34 +0100

    Get label from fsp configurations

--- interfaces/Portal/src/app/components/registration-activity-overview/registration-activity-overview.component.ts
+++ interfaces/Portal/src/app/components/registration-activity-overview/registration-activity-overview.component.ts
@@ -9,6 +9,7 @@ import {
   RegistrationActivity,
   RegistrationActivityType,
 } from 'src/app/models/registration-activity.model';
+import { TranslatableString } from 'src/app/models/translatable-string.model';
 import { PastPaymentsService } from 'src/app/services/past-payments.service';
 import { RegistrationActivityService } from 'src/app/services/registration-activity.service';
 import { PaymentUtils } from 'src/app/shared/payment.utils';
@@ -261,29 +262,17 @@ export class RegistrationActivityOverviewComponent implements OnInit {
 
         if (change.type === EventType.financialServiceProviderChange) {
           if (description.oldValue) {
-            try {
-              description.oldValue = JSON.parse(description.oldValue);
-              description.oldValue = this.translatableString.get(
-                description.oldValue,
-              );
-            } catch (error) {
-              description.oldValue = this.translatableString.get(
-                description.oldValue,
-              );
-            }
+            const label = this.getLabelForFspConfigChange(description.oldValue);
+            description.oldValue = label
+              ? this.translatableString.get(label)
+              : this.translatableString.get(description.oldValue);
           }
 
           if (description.newValue) {
-            try {
-              description.newValue = JSON.parse(description.newValue);
-              description.newValue = this.translatableString.get(
-                description.newValue,
-              );
-            } catch (error) {
-              description.newValue = this.translatableString.get(
-                description.newValue,
-              );
-            }
+            const label = this.getLabelForFspConfigChange(description.newValue);
+            description.newValue = label
+              ? this.translatableString.get(label)
+              : this.translatableString.get(description.newValue);
           }
 
           this.activityOverview.push({
@@ -310,6 +299,14 @@ export class RegistrationActivityOverviewComponent implements OnInit {
     }
 
     this.activityOverview.sort((a, b) => (b.date > a.date ? 1 : -1));
+  }
+
+  private getLabelForFspConfigChange(
+    name: string,
+  ): string | TranslatableString {
+    return this.program.financialServiceProviderConfigurations.find(
+      (fspConfig) => fspConfig.name === name,
+    )?.label;
   }
 
   private getSubLabelText(change: any, attribute: Attribute): string {
