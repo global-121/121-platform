56f9e6017b59ecc4f6dc9f43141864f16d04e115
Elwin Schmitz <ik@elwin.nu>
2024-11-11 15:55:01 +0100

    feat: Add config to enable support for Russian

--- interfaces/Portal/src/main.ts
+++ interfaces/Portal/src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 // Import locale data for formatting dates, times, numbers, etc.
-// Make sure to include any language-codes drom the ENV-variable: NG_LOCALES
+// Make sure to include any language-codes from the ENV-variable: NG_LOCALES
 import '@angular/common/locales/global/ar';
 import '@angular/common/locales/global/es';
 import '@angular/common/locales/global/fr';
 import '@angular/common/locales/global/nl';
+import '@angular/common/locales/global/ru';
 
 if (environment.production) {
   enableProdMode();
