e15d736cb9ab2727896208a8d104d9fcfe441b21
jannisvisser <jannisvisser@redcross.nl>
2024-11-13 13:41:17 +0100

    fix: automatic fixes based on eslint upgrade

--- services/121-service/src/payments/dto/split-payment-lists.dto.ts
+++ services/121-service/src/payments/dto/split-payment-lists.dto.ts
@@ -1,6 +1,6 @@
 import { FinancialServiceProviders } from '@121-service/src/financial-service-providers/enum/financial-service-provider-name.enum';
 import { PaPaymentDataDto } from '@121-service/src/payments/dto/pa-payment-data.dto';
 
-export type SplitPaymentListDto = {
-  [key in FinancialServiceProviders]?: PaPaymentDataDto[];
-};
+export type SplitPaymentListDto = Partial<
+  Record<FinancialServiceProviders, PaPaymentDataDto[]>
+>;
