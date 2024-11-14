01d65862458aaba57224ea1ace8c12dd6d1cb53f
arsforza <asforza@redcross.nl>
2024-11-14 17:08:56 +0100

    feat: create payment page header AB#31322

---
+++ interfaces/Portalicious/src/app/components/page-layout/components/payment-header/payment-header.component.ts
@@ -0,0 +1,53 @@
+import { DatePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  LOCALE_ID,
+} from '@angular/core';
+
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { SkeletonModule } from 'primeng/skeleton';
+
+import { BreadcrumbsTitleComponent } from '~/components/page-layout/components/breadcrumbs-title/breadcrumbs-title.component';
+import { PageLayoutTitleAndActionsComponent } from '~/components/page-layout/components/page-layout-title-and-actions/page-layout-title-and-actions.component';
+import { PaymentApiService } from '~/domains/payment/payment.api.service';
+import { Locale } from '~/utils/locale';
+
+@Component({
+  selector: 'app-payment-header',
+  standalone: true,
+  imports: [
+    SkeletonModule,
+    PageLayoutTitleAndActionsComponent,
+    BreadcrumbsTitleComponent,
+  ],
+  templateUrl: './payment-header.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaymentHeaderComponent {
+  readonly paymentApiService = inject(PaymentApiService);
+  private locale = inject<Locale>(LOCALE_ID);
+
+  projectId = input.required<number>();
+  paymentId = input.required<number>();
+
+  payments = injectQuery(this.paymentApiService.getPayments(this.projectId));
+
+  paymentDate = computed(() => {
+    if (!this.paymentId() || !this.projectId() || this.payments.isPending()) {
+      return '';
+    }
+
+    const date = this.payments
+      .data()
+      ?.find(
+        (payment) => payment.payment === Number(this.paymentId()),
+      )?.paymentDate;
+
+    return new DatePipe(this.locale).transform(date, 'short') ?? '';
+  });
+}
