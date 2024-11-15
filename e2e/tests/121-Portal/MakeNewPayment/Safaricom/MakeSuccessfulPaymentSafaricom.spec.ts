c73ed9ffabe630bc94ca94c07d4e926b0ccc2cb7
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2024-11-15 11:49:03 +0100

    fix

--- e2e/tests/121-Portal/MakeNewPayment/Safaricom/MakeSuccessfulPaymentSafaricom.spec.ts
+++ e2e/tests/121-Portal/MakeNewPayment/Safaricom/MakeSuccessfulPaymentSafaricom.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@playwright/test';
 
+import { FinancialServiceProviders } from '@121-service/src/financial-service-providers/enum/financial-service-provider-name.enum';
 import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
 import KRCSProgram from '@121-service/src/seed-data/program/program-krcs-turkana.json';
 import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
@@ -18,7 +19,6 @@ import TableModule from '@121-e2e/pages/Table/TableModule';
 
 import { AppRoutes } from '../../../../../interfaces/Portal/src/app/app-routes.enum';
 import englishTranslations from '../../../../../interfaces/Portal/src/assets/i18n/en.json';
-import { FinancialServiceProviders } from '@121-service/src/financial-service-providers/enum/financial-service-provider-name.enum';
 
 const krcsProgramTitle = KRCSProgram.titlePortal.en;
 const paymentLabel = englishTranslations.page.program.tab.payment.label;
