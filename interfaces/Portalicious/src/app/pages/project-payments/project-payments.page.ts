20dd543a09b7fed06c89fbed44656773d7e803e7
Domenico Gemoli <domenicogemoli@gmail.com>
2024-11-19 14:19:33 +0100

    portalicious: create payment popup
    AB#31328

--- interfaces/Portalicious/src/app/pages/project-payments/project-payments.page.ts
+++ interfaces/Portalicious/src/app/pages/project-payments/project-payments.page.ts
@@ -7,17 +7,16 @@ import {
 } from '@angular/core';
 
 import { injectQuery } from '@tanstack/angular-query-experimental';
-import { ButtonModule } from 'primeng/button';
 
 import { PermissionEnum } from '@121-service/src/user/enum/permission.enum';
 
 import { CardGridComponent } from '~/components/card-grid/card-grid.component';
 import { PageLayoutComponent } from '~/components/page-layout/page-layout.component';
 import { PaymentApiService } from '~/domains/payment/payment.api.service';
+import { CreatePaymentComponent } from '~/pages/project-payments/components/create-payment/create-payment.component';
 import { ExportPaymentsComponent } from '~/pages/project-payments/components/export-payments/export-payments.component';
 import { PaymentSummaryCardComponent } from '~/pages/project-payments/components/payment-summary-card/payment-summary-card.component';
 import { AuthService } from '~/services/auth.service';
-import { ToastService } from '~/services/toast.service';
 
 @Component({
   selector: 'app-project-payments',
@@ -26,29 +25,25 @@ import { ToastService } from '~/services/toast.service';
     PageLayoutComponent,
     PaymentSummaryCardComponent,
     ExportPaymentsComponent,
-    ButtonModule,
     CardGridComponent,
+    CreatePaymentComponent,
   ],
-  providers: [ToastService],
   templateUrl: './project-payments.page.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectPaymentsPageComponent {
-  private paymentApiService = inject(PaymentApiService);
-
   // this is injected by the router
   projectId = input.required<number>();
 
   private authService = inject(AuthService);
-  private toastService = inject(ToastService);
+  private paymentApiService = inject(PaymentApiService);
 
-  onCreatePayment() {
-    this.toastService.showToast({
-      severity: 'warn',
-      detail: 'Functionality not implemented yet',
-    });
-  }
+  payments = injectQuery(this.paymentApiService.getPayments(this.projectId));
+
+  paymentsSorted = computed(() =>
+    this.payments.data()?.sort((a, b) => b.payment - a.payment),
+  );
 
   canExport = computed(() =>
     this.authService.hasAllPermissions({
@@ -61,9 +56,10 @@ export class ProjectPaymentsPageComponent {
     }),
   );
 
-  payments = injectQuery(this.paymentApiService.getPayments(this.projectId));
-
-  paymentsSorted = computed(() =>
-    this.payments.data()?.sort((a, b) => b.payment - a.payment),
+  canCreatePayment = computed(() =>
+    this.authService.hasPermission({
+      projectId: this.projectId(),
+      requiredPermission: PermissionEnum.PaymentCREATE,
+    }),
   );
 }
