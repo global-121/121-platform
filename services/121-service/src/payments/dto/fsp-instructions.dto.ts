c47a99dce039a6795d9c80fd062b8a882d015c17
Domenico Gemoli <domenicogemoli@gmail.com>
2024-11-27 14:28:40 +0100

    feat: refactor FSPInstructions and add payment status endpoint
    AB#31608 AB#31609
    
    Whilst working on Portalicious I realised there was logic in the Portal
    and in the backend that isn't being used anywhere.
    
    - removes unused ExportFileType
    - removes unnecessary CsvInstructions
    - updates the Portal and service code accordingly
    
    Also adding a new endpoint that makes polling for this specific information easier
    - adds programs/:programId/payments/:payment/status

--- services/121-service/src/payments/dto/fsp-instructions.dto.ts
+++ services/121-service/src/payments/dto/fsp-instructions.dto.ts
@@ -1,13 +1,5 @@
 import { ExcelFspInstructions } from '@121-service/src/payments/fsp-integration/excel/dto/excel-fsp-instructions.dto';
 
-export type CsvInstructions = ExcelFspInstructions[];
-
 export class FspInstructions {
-  public data?: CsvInstructions | string;
-  public fileType?: ExportFileType;
-}
-
-export enum ExportFileType {
-  csv = 'csv',
-  excel = 'excel',
+  public data: ExcelFspInstructions[];
 }
