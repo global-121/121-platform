133f7cdaf2c089a6a75046becb9b7672b7ef6de4
jannisvisser <jannisvisser@redcross.nl>
2024-11-29 17:21:00 +0100

    fix: make sure test works with nodemon + ping for service to be back up

--- k6/tests/retryFailedJobsOnStartupDuringQueueProcessing.js
+++ k6/tests/retryFailedJobsOnStartupDuringQueueProcessing.js
@@ -1,4 +1,5 @@
 import { check, sleep } from 'k6';
+import http from 'k6/http';
 
 import loginModel from '../models/login.js';
 import paymentsModel from '../models/payments.js';
@@ -8,7 +9,7 @@ const resetPage = new resetModel();
 const paymentsPage = new paymentsModel();
 const loginPage = new loginModel();
 
-const duplicateNumber = 7;
+const duplicateNumber = 7; // This leads to 128 registrations
 const programId = 3;
 const paymentId = 3;
 const maxTimeoutAttempts = 400;
@@ -19,6 +20,11 @@ export const options = {
   duration: '40m',
   iterations: 1,
 };
+
+function isServiceUp() {
+  const response = http.get('http://localhost:3000/api/health/health'); // Replace with your health check endpoint
+  return response.status === 200;
+}
 
 export default function () {
   // reset db
@@ -50,11 +56,16 @@ export default function () {
     },
   });
 
-  sleep(5); // wait long enough so that jobs are added to the queue
+  sleep(5); // wait long enough so that jobs are added to the queue, but not so long that all are processed already
 
   resetPage.kill121Service();
 
-  sleep(5); // wait long enough so that 121-service is back up
+  // Wait for the service to restart and become available
+  let serviceUp = false;
+  while (!serviceUp) {
+    sleep(1); // Wait for 1 second before pinging again
+    serviceUp = isServiceUp();
+  }
 
   // Monitor that 100% of payments is successful and then stop the test
   const monitorPayment = paymentsPage.getPaymentResults(
