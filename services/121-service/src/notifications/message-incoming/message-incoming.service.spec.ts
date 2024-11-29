942de282e82e2e90671c402051facbb7ab00036b
jannisvisser <jannisvisser@redcross.nl>
2024-11-29 13:55:40 +0100

    fix: unit tests

--- services/121-service/src/notifications/message-incoming/message-incoming.service.spec.ts
+++ services/121-service/src/notifications/message-incoming/message-incoming.service.spec.ts
@@ -1,33 +1,32 @@
 import { TestBed } from '@automock/jest';
-import { Queue } from 'bull';
 
 import { MessageIncomingService } from '@121-service/src/notifications/message-incoming/message-incoming.service';
 import {
   TwilioIncomingCallbackDto,
   TwilioStatusCallbackDto,
 } from '@121-service/src/notifications/twilio.dto';
-import {
-  ProcessNameMessage,
-  QueueNameMessageCallBack,
-} from '@121-service/src/shared/enum/queue-process.names.enum';
-import { getQueueName } from '@121-service/src/utils/unit-test.helpers';
+import { QueueRegistryService } from '@121-service/src/queue-registry/queue-registry.service';
+import { ProcessNameMessage } from '@121-service/src/shared/enum/queue-process.names.enum';
 
 describe('MessageIncomingService', () => {
   let messageIncomingService: MessageIncomingService;
-  let messageStatusCallbackQueue: jest.Mocked<Queue>;
-  let messageIncommingQueue: jest.Mocked<Queue>;
+  let queueRegistryService: QueueRegistryService;
 
   beforeEach(() => {
-    const { unit, unitRef } = TestBed.create(MessageIncomingService).compile();
+    const { unit, unitRef } = TestBed.create(MessageIncomingService)
+      .mock(QueueRegistryService)
+      .using({
+        messageStatusCallbackQueue: {
+          add: jest.fn(),
+        },
+        messageIncomingCallbackQueue: {
+          add: jest.fn(),
+        },
+      })
+      .compile();
 
     messageIncomingService = unit;
-    messageStatusCallbackQueue = unitRef.get(
-      getQueueName(QueueNameMessageCallBack.status),
-    );
-
-    messageIncommingQueue = unitRef.get(
-      getQueueName(QueueNameMessageCallBack.incomingMessage),
-    );
+    queueRegistryService = unitRef.get(QueueRegistryService);
   });
 
   it('should be defined', () => {
@@ -42,11 +41,12 @@ describe('MessageIncomingService', () => {
     await messageIncomingService.addSmsStatusCallbackToQueue(testCallbackData);
 
     // Assert
-    expect(messageStatusCallbackQueue.add).toHaveBeenCalledTimes(1);
-    expect(messageStatusCallbackQueue.add).toHaveBeenCalledWith(
-      ProcessNameMessage.sms,
-      testCallbackData,
-    );
+    expect(
+      queueRegistryService.messageStatusCallbackQueue.add,
+    ).toHaveBeenCalledTimes(1);
+    expect(
+      queueRegistryService.messageStatusCallbackQueue.add,
+    ).toHaveBeenCalledWith(ProcessNameMessage.sms, testCallbackData);
   });
 
   it('should add WhatsApp status callback to queue', async () => {
@@ -59,11 +59,12 @@ describe('MessageIncomingService', () => {
     );
 
     // Assert
-    expect(messageStatusCallbackQueue.add).toHaveBeenCalledTimes(1);
-    expect(messageStatusCallbackQueue.add).toHaveBeenCalledWith(
-      ProcessNameMessage.whatsapp,
-      testCallbackData,
-    );
+    expect(
+      queueRegistryService.messageStatusCallbackQueue.add,
+    ).toHaveBeenCalledTimes(1);
+    expect(
+      queueRegistryService.messageStatusCallbackQueue.add,
+    ).toHaveBeenCalledWith(ProcessNameMessage.whatsapp, testCallbackData);
   });
 
   it('should add incoming WhatsApp to queue', async () => {
@@ -76,8 +77,12 @@ describe('MessageIncomingService', () => {
     );
 
     // Assert
-    expect(messageIncommingQueue.add).toHaveBeenCalledTimes(1);
-    expect(messageIncommingQueue.add).toHaveBeenCalledWith(
+    expect(
+      queueRegistryService.messageIncomingCallbackQueue.add,
+    ).toHaveBeenCalledTimes(1);
+    expect(
+      queueRegistryService.messageIncomingCallbackQueue.add,
+    ).toHaveBeenCalledWith(
       ProcessNameMessage.whatsapp,
       testIncommingWhatsappData,
     );
--- services/121-service/src/payments/fsp-integration/safaricom/safaricom.service.spec.ts
+++ services/121-service/src/payments/fsp-integration/safaricom/safaricom.service.spec.ts
@@ -1,11 +1,8 @@
-import { getQueueToken } from '@nestjs/bull';
-import { Test, TestingModule } from '@nestjs/testing';
-import { Queue } from 'bull';
+import { TestBed } from '@automock/jest';
 import { Redis } from 'ioredis';
 
 import { SafaricomTimeoutCallbackDto } from '@121-service/src/payments/fsp-integration/safaricom/dtos/safaricom-timeout-callback.dto';
 import { SafaricomTransferCallbackDto } from '@121-service/src/payments/fsp-integration/safaricom/dtos/safaricom-transfer-callback.dto';
-import { SafaricomCallbackQueueNames } from '@121-service/src/payments/fsp-integration/safaricom/enum/safaricom-callback-queue-names.enum';
 import { DoTransferParams } from '@121-service/src/payments/fsp-integration/safaricom/interfaces/do-transfer-params.interface';
 import { TransferReturnType } from '@121-service/src/payments/fsp-integration/safaricom/interfaces/transfer-return-type.interface';
 import { SafaricomTransferScopedRepository } from '@121-service/src/payments/fsp-integration/safaricom/repositories/safaricom-transfer.scoped.repository';
@@ -15,6 +12,7 @@ import {
   getRedisSetName,
   REDIS_CLIENT,
 } from '@121-service/src/payments/redis/redis-client';
+import { QueueRegistryService } from '@121-service/src/queue-registry/queue-registry.service';
 import { JobNames } from '@121-service/src/shared/enum/job-names.enum';
 
 const mockedDoTransferParams: DoTransferParams = {
@@ -25,69 +23,37 @@ const mockedDoTransferParams: DoTransferParams = {
 };
 
 describe('SafaricomService', () => {
-  let service: SafaricomService;
+  let safaricomService: SafaricomService;
   let safaricomApiService: SafaricomApiService;
   let safaricomTransferScopedRepository: SafaricomTransferScopedRepository;
-
   let redisClient: Redis;
-  let safaricomTransferCallbackQueue: Queue;
-  let safaricomTimeoutCallbackQueue: Queue;
+  let queueRegistryService: QueueRegistryService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        SafaricomService,
-        {
-          provide: SafaricomApiService,
-          useValue: {
-            transfer: jest.fn(),
-          },
+    const { unit, unitRef } = TestBed.create(SafaricomService)
+      .mock(QueueRegistryService)
+      .using({
+        safaricomTransferCallbackQueue: {
+          add: jest.fn(),
         },
-        {
-          provide: SafaricomTransferScopedRepository,
-          useValue: {
-            update: jest.fn(),
-          },
+        safaricomTimeoutCallbackQueue: {
+          add: jest.fn(),
         },
-        {
-          provide: REDIS_CLIENT,
-          useValue: {
-            sadd: jest.fn(),
-          },
-        },
-        {
-          provide: getQueueToken(SafaricomCallbackQueueNames.transfer),
-          useValue: {
-            add: jest.fn(),
-          },
-        },
-        {
-          provide: getQueueToken(SafaricomCallbackQueueNames.timeout),
-          useValue: {
-            add: jest.fn(),
-          },
-        },
-      ],
-    }).compile();
+      })
+      .compile();
 
-    service = module.get<SafaricomService>(SafaricomService);
-    safaricomApiService = module.get<SafaricomApiService>(SafaricomApiService);
-    safaricomTransferScopedRepository =
-      module.get<SafaricomTransferScopedRepository>(
-        SafaricomTransferScopedRepository,
-      );
-    safaricomTransferCallbackQueue = module.get(
-      getQueueToken(SafaricomCallbackQueueNames.transfer),
+    safaricomService = unit;
+    safaricomApiService = unitRef.get(SafaricomApiService);
+    queueRegistryService = unitRef.get(QueueRegistryService);
+    safaricomTransferScopedRepository = unitRef.get(
+      SafaricomTransferScopedRepository,
     );
-    safaricomTimeoutCallbackQueue = module.get(
-      getQueueToken(SafaricomCallbackQueueNames.timeout),
-    );
-    redisClient = module.get(REDIS_CLIENT);
+    redisClient = unitRef.get(REDIS_CLIENT);
   });
 
   describe('sendPayment', () => {
     it('should throw an error when called', async () => {
-      await expect(service.sendPayment([], 1, 1)).rejects.toThrow(
+      await expect(safaricomService.sendPayment([], 1, 1)).rejects.toThrow(
         'Method should not be called anymore.',
       );
     });
@@ -104,7 +70,7 @@ describe('SafaricomService', () => {
         .spyOn(safaricomApiService, 'transfer')
         .mockResolvedValue(transferResult);
 
-      await service.doTransfer(mockedDoTransferParams);
+      await safaricomService.doTransfer(mockedDoTransferParams);
 
       expect(safaricomApiService.transfer).toHaveBeenCalledWith({
         transferAmount: mockedDoTransferParams.transferAmount,
@@ -136,22 +102,20 @@ describe('SafaricomService', () => {
 
       jest.spyOn(redisClient, 'sadd').mockResolvedValue(1);
       jest
-        .spyOn(safaricomTransferCallbackQueue, 'add')
+        .spyOn(queueRegistryService.safaricomTransferCallbackQueue, 'add')
         .mockResolvedValue(mockJob as any);
 
-      await service.processTransferCallback(mockCallback);
+      await safaricomService.processTransferCallback(mockCallback);
 
-      expect(safaricomTransferCallbackQueue.add).toHaveBeenCalledWith(
-        JobNames.default,
-        {
-          originatorConversationId:
-            mockCallback.Result.OriginatorConversationID,
-          mpesaConversationId: mockCallback.Result.ConversationID,
-          mpesaTransactionId: mockCallback.Result.TransactionID,
-          resultCode: mockCallback.Result.ResultCode,
-          resultDescription: mockCallback.Result.ResultDesc,
-        },
-      );
+      expect(
+        queueRegistryService.safaricomTransferCallbackQueue.add,
+      ).toHaveBeenCalledWith(JobNames.default, {
+        originatorConversationId: mockCallback.Result.OriginatorConversationID,
+        mpesaConversationId: mockCallback.Result.ConversationID,
+        mpesaTransactionId: mockCallback.Result.TransactionID,
+        resultCode: mockCallback.Result.ResultCode,
+        resultDescription: mockCallback.Result.ResultDesc,
+      });
 
       expect(redisClient.sadd).toHaveBeenCalledWith(
         getRedisSetName(mockJob.data.programId),
@@ -184,18 +148,16 @@ describe('SafaricomService', () => {
 
       jest.spyOn(redisClient, 'sadd').mockResolvedValue(1);
       jest
-        .spyOn(safaricomTimeoutCallbackQueue, 'add')
+        .spyOn(queueRegistryService.safaricomTimeoutCallbackQueue, 'add')
         .mockResolvedValue(mockJob as any);
 
-      await service.processTimeoutCallback(mockTimeoutCallback);
+      await safaricomService.processTimeoutCallback(mockTimeoutCallback);
 
-      expect(safaricomTimeoutCallbackQueue.add).toHaveBeenCalledWith(
-        JobNames.default,
-        {
-          originatorConversationId:
-            mockTimeoutCallback.OriginatorConversationID,
-        },
-      );
+      expect(
+        queueRegistryService.safaricomTimeoutCallbackQueue.add,
+      ).toHaveBeenCalledWith(JobNames.default, {
+        originatorConversationId: mockTimeoutCallback.OriginatorConversationID,
+      });
 
       expect(redisClient.sadd).toHaveBeenCalledWith(
         getRedisSetName(mockJob.data.programId),
