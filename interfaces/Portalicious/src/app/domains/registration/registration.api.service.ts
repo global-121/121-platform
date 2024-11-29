7405a58a698011b595218b7d09f6999d8d57f06c
Domenico Gemoli <dgemoli@redcross.nl>
2024-11-29 16:48:42 +0100

    portalicious: fix remaining endpoints (#6196)
    AB#31660

--- interfaces/Portalicious/src/app/domains/registration/registration.api.service.ts
+++ interfaces/Portalicious/src/app/domains/registration/registration.api.service.ts
@@ -50,7 +50,7 @@ export class RegistrationApiService extends DomainApiService {
 
   getImportTemplate(projectId: Signal<number>) {
     return this.generateQueryOptions<string[]>({
-      path: [...BASE_ENDPOINT(projectId), 'import-template'],
+      path: [...BASE_ENDPOINT(projectId), 'import', 'template'],
     });
   }
 
