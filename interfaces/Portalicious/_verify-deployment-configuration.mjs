5c68ef9e63c1285e2e608bf879eba4f05a8aefea
Elwin Schmitz <eschmitz@redcross.nl>
2024-12-02 16:34:11 +0100

    ci: Verify actual deployment contains the configuration

---
+++ interfaces/Portalicious/_verify-deployment-configuration.mjs
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+/**
+ * See the "Deployment"-section of the interfaces/README.md-file for more information.
+ */
+
+import { config } from 'dotenv';
+import test from 'node:test';
+import { ok, match, doesNotMatch } from 'node:assert/strict';
+
+// Load environment-variables from .env file
+config({
+  debug: process.env.DEBUG,
+  override: process.env.DEBUG,
+});
+
+const url = process.argv[2]?.replace('--url=', '');
+
+if (!url || !url.startsWith('https')) {
+  console.error('Invalid URL argument.');
+  console.info(
+    'Provide a valid URL as argument using: ` --url=https://example.org`',
+  );
+  process.exit(1);
+}
+
+console.info('Verifying deployment configuration for URL:', url);
+const response = await fetch(url);
+
+const csp = response.headers.get('Content-Security-Policy');
+console.info('Content-Security-Policy in use:', csp);
+
+test('Response-Headers contain a Content-Security-Policy', () => {
+  ok(
+    response.headers.get('Content-Security-Policy'),
+    'Contain a Content-Security-Policy',
+  );
+});
+
+test('Content-Security-Policy set for Azure Entra SSO', () => {
+  const connectSrcCondition =
+    /connect-src[^;]* https:\/\/login\.microsoftonline\.com/;
+  const frameSrcCondition =
+    /frame-src[^;]* https:\/\/login\.microsoftonline\.com/;
+
+  if (process.env.USE_SSO_AZURE_ENTRA === 'true') {
+    match(csp, connectSrcCondition);
+    match(csp, frameSrcCondition);
+  } else {
+    doesNotMatch(csp, connectSrcCondition);
+    doesNotMatch(csp, frameSrcCondition);
+  }
+});
+
+test('Content-Security-Policy set for loading as iframe in Twilio Flex', () => {
+  const frameAncestorsCondition =
+    /frame-ancestors[^;]* https:\/\/flex\.twilio\.com/;
+
+  if (process.env.USE_IN_TWILIO_FLEX_IFRAME === 'true') {
+    match(csp, frameAncestorsCondition);
+  } else {
+    doesNotMatch(csp, frameAncestorsCondition);
+  }
+});
+
+test('Content-Security-Policy set to load PowerBI dashboard(s) in iframe', () => {
+  const frameSrcCondition = /frame-src[^;]* https:\/\/app\.powerbi\.com/;
+
+  if (process.env.USE_IN_TWILIO_FLEX_IFRAME === 'true') {
+    match(csp, frameSrcCondition);
+  } else {
+    doesNotMatch(csp, frameSrcCondition);
+  }
+});
