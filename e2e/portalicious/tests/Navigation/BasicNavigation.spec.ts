10360cbc1f2b329aea413e85eacb4b8f83e47e23
Domenico Gemoli <domenicogemoli@gmail.com>
2024-12-03 10:44:43 +0100

    portalicious: reload registrations page without page hanging
    AB#31830

--- e2e/portalicious/tests/Navigation/BasicNavigation.spec.ts
+++ e2e/portalicious/tests/Navigation/BasicNavigation.spec.ts
@@ -1,13 +1,25 @@
 import { expect, test } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
-import { resetDB } from '@121-service/test/helpers/utility.helper';
+import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
+import {
+  getAccessToken,
+  resetDB,
+} from '@121-service/test/helpers/utility.helper';
+import { registrationsPV } from '@121-service/test/registrations/pagination/pagination-data';
 
 import HomePage from '@121-e2e/portalicious/pages/HomePage';
 import LoginPage from '@121-e2e/portalicious/pages/LoginPage';
+import RegistrationsPage from '@121-e2e/portalicious/pages/RegistrationsPage';
+
+const projectId = 2;
+const projectTitle = 'NLRC Direct Digital Aid Program (PV)';
 
 test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.test);
+  await resetDB(SeedScript.nlrcMultiple);
+
+  const accessToken = await getAccessToken();
+  await seedIncludedRegistrations(registrationsPV, projectId, accessToken);
 
   // Login
   const loginPage = new LoginPage(page);
@@ -32,17 +44,26 @@ test('Navigation from sidebar', async ({ page }) => {
 test('Navigation from program header', async ({ page }) => {
   const homePage = new HomePage(page);
 
-  const projectTitle = 'Cash program Westeros';
-
   await page.goto('/en-GB/projects');
   await page.getByRole('link', { name: projectTitle }).click();
   await page.waitForURL((url) =>
-    url.pathname.startsWith('/en-GB/project/1/registrations'),
+    url.pathname.startsWith(`/en-GB/project/${projectId}/registrations`),
   );
   await expect(await homePage.logo).toHaveText(`121 Portal${projectTitle}`);
 
   await homePage.navigateToProgramPage('Monitoring');
   await page.waitForURL((url) =>
-    url.pathname.startsWith('/en-GB/project/1/monitoring'),
+    url.pathname.startsWith(`/en-GB/project/${projectId}/monitoring`),
   );
+});
+
+test('Reload registrations page', async ({ page }) => {
+  const registrationsPage = new RegistrationsPage(page);
+
+  await page.goto('/en-GB/projects');
+  await page.getByRole('link', { name: projectTitle }).click();
+  await registrationsPage.waitForLoaded(registrationsPV.length);
+
+  await page.reload();
+  await registrationsPage.waitForLoaded(registrationsPV.length);
 });
