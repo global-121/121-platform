61324e41e9ff481d8b7733b7298704ad64d8c374
jannisvisser <jannisvisser@redcross.nl>
2024-12-04 11:11:28 +0100

    fix: only retry recent failed jobs

--- services/121-service/src/queue-registry/queue-registry.service.ts
+++ services/121-service/src/queue-registry/queue-registry.service.ts
@@ -93,8 +93,10 @@ export class QueueRegistryService implements OnModuleInit {
   private async retryFailedJobs(): Promise<void> {
     for (const queue of this.allQueues) {
       const failedJobs = await queue.getFailed();
-      const missingProcessHandlerJobs = failedJobs.filter((job) =>
-        job.failedReason?.includes('Missing process handler for job type'),
+      const missingProcessHandlerJobs = failedJobs.filter(
+        (job) =>
+          job.failedReason?.includes('Missing process handler for job type') && // Only retry for this specific error message, as we know the job processing has never started and is therefore safe to retry (jobs are not idempotent)
+          job.timestamp >= Date.now() - 1000 * 60 * 5, // Only retry jobs that failed during because of the current start-up, and not older ones. Only 5s + a little bit should be needed, this is some extra buffer.
       );
       if (!missingProcessHandlerJobs.length) {
         continue;
@@ -102,13 +104,8 @@ export class QueueRegistryService implements OnModuleInit {
       console.log(
         `Found ${failedJobs.length} failed jobs because of missing process handler for queue ${queue.name}. Retrying now.`,
       );
-      for (const job of failedJobs) {
-        // Only retry for this specific error message, as we know the job processing has never started and is therefore safe to retry (jobs are not idempotent)
-        if (
-          job.failedReason?.includes('Missing process handler for job type')
-        ) {
-          await job.retry();
-        }
+      for (const job of missingProcessHandlerJobs) {
+        await job.retry();
       }
     }
   }
