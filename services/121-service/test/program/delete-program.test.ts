8e305155f8411f39492b332cde4869525965ba65
Ruben <vandervalk@geoit.nl>
2024-12-10 15:16:31 +0100

    Also delete cbe program
    Signed-off-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/test/program/delete-program.test.ts
+++ services/121-service/test/program/delete-program.test.ts
@@ -4,6 +4,7 @@ import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
 import {
   deleteProgram,
   getProgram,
+  startCbeValidationProcess,
 } from '@121-service/test/helpers/program.helper';
 import { seedPaidRegistrations } from '@121-service/test/helpers/registration.helper';
 import {
@@ -12,28 +13,66 @@ import {
 } from '@121-service/test/helpers/utility.helper';
 import {
   programIdOCW,
+  programIdPV,
+  registrationCbe,
   registrationsOCW,
+  registrationsPV,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
 describe('Delete program', () => {
   let accessToken: string;
 
-  beforeEach(async () => {
+  it('should delete a nlrc programs', async () => {
+    // Arrange
     await resetDB(SeedScript.nlrcMultiple);
     accessToken = await getAccessToken();
+
+    // Create some test data which should be cascaded deleted
+    await seedPaidRegistrations(registrationsOCW, programIdOCW);
+    await seedPaidRegistrations(registrationsPV, programIdPV);
+
+    // Act + Assert
+    const secretDto = { secret: process.env.RESET_SECRET! };
+
+    const deleteResponseOCW = await deleteProgram(
+      programIdOCW,
+      accessToken,
+      secretDto,
+    );
+    expect(deleteResponseOCW.statusCode).toBe(HttpStatus.NO_CONTENT);
+
+    const deleteResponsePV = await deleteProgram(
+      programIdPV,
+      accessToken,
+      secretDto,
+    );
+    expect(deleteResponsePV.statusCode).toBe(HttpStatus.NO_CONTENT);
+
+    const getProgramResponseOCW = await getProgram(programIdOCW, accessToken);
+    expect(getProgramResponseOCW.statusCode).toBe(HttpStatus.NOT_FOUND);
+
+    const getProgramResponsePV = await getProgram(programIdPV, accessToken);
+    expect(getProgramResponsePV.statusCode).toBe(HttpStatus.NOT_FOUND);
   });
 
-  it('should delete a program', async () => {
-    // arrange
-    // Create some related test data
-    await seedPaidRegistrations(registrationsOCW, programIdOCW);
+  it('should delete eth programs', async () => {
+    const programIdEth = 1;
+    await resetDB(SeedScript.ethJointResponse);
+    accessToken = await getAccessToken();
 
-    // act
-    const response = await deleteProgram(programIdOCW, accessToken, {
-      secret: process.env.RESET_SECRET!,
-    });
-    expect(response.statusCode).toBe(HttpStatus.NO_CONTENT);
-    const getProgramResponse = await getProgram(programIdOCW, accessToken);
-    expect(getProgramResponse.statusCode).toBe(HttpStatus.NOT_FOUND);
+    await seedPaidRegistrations([registrationCbe], programIdEth);
+    await startCbeValidationProcess(programIdEth, accessToken);
+
+    // Act + Assert
+    const secretDto = { secret: process.env.RESET_SECRET! };
+    const deleteResponseEth = await deleteProgram(
+      programIdEth,
+      accessToken,
+      secretDto,
+    );
+    expect(deleteResponseEth.statusCode).toBe(HttpStatus.NO_CONTENT);
+
+    const getProgramResponseEth = await getProgram(programIdEth, accessToken);
+    expect(getProgramResponseEth.statusCode).toBe(HttpStatus.NOT_FOUND);
   });
 });
