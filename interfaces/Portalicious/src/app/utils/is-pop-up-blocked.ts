21b9ac2a41cb1062726e269fc943070ea2f1fc7f
Peter Smallenbroek <PeterSmallenbroek@users.noreply.github.com>
2024-12-11 15:12:02 +0100

    Portalicious: add allow popup error (#6278)
    chore: add allow popup error

---
+++ interfaces/Portalicious/src/app/utils/is-pop-up-blocked.ts
@@ -0,0 +1,26 @@
+/**
+ * Checks if the browser is blocking pop-up windows.
+ *
+ * This function attempts to open a small pop-up window. If the window cannot be opened,
+ * it means that pop-ups are blocked by the browser.
+ *
+ * @returns {boolean} - Returns true if pop-ups are blocked, otherwise false.
+ */
+export function isPopupBlocked(): boolean {
+  const testPopup = window.open(
+    'about:blank',
+    '_blank',
+    'width=100,height=100',
+  );
+
+  if (
+    !testPopup ||
+    testPopup.closed ||
+    typeof testPopup.closed === 'undefined'
+  ) {
+    return true; // Popup is blocked
+  } else {
+    testPopup.close();
+    return false; // Popup is not blocked
+  }
+}
