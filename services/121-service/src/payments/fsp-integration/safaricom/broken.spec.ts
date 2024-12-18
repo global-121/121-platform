e659d6a1acc192c6be88d0c60a03753c9a375637
jannisvisser <jannisvisser@redcross.nl>
2024-12-18 11:59:32 +0100

    chore: cleanup

--- services/121-service/src/payments/fsp-integration/safaricom/broken.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Broken!', () => {
-  it('should pass this test', () => {
-    expect(true).toBe(true);
-  });
-
-  it('should pass another test', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
