17c89e9d3026d47088ab557bb45aa498c4ab4816
Ruben <vandervalk@geoit.nl>
2024-12-23 13:50:47 +0100

    lint
    Signed-off-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/src/program-financial-service-provider-configurations/entities/program-financial-service-provider-configuration.entity.ts
+++ services/121-service/src/program-financial-service-provider-configurations/entities/program-financial-service-provider-configuration.entity.ts
@@ -13,8 +13,8 @@ import { FinancialServiceProviders } from '@121-service/src/financial-service-pr
 import { TransactionEntity } from '@121-service/src/payments/transactions/transaction.entity';
 import { ProgramFinancialServiceProviderConfigurationPropertyEntity } from '@121-service/src/program-financial-service-provider-configurations/entities/program-financial-service-provider-configuration-property.entity';
 import { ProgramEntity } from '@121-service/src/programs/program.entity';
-import { LocalizedString } from '@121-service/src/shared/types/localized-string.type';
 import { RegistrationEntity } from '@121-service/src/registration/registration.entity';
+import { LocalizedString } from '@121-service/src/shared/types/localized-string.type';
 
 @Unique('programFinancialServiceProviderConfigurationUnique', [
   'programId',
