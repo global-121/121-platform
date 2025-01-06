c563a77bea9c6113e19121e69ebfffa3010e7cf0
Domenico Gemoli <dgemoli@redcross.nl>
2025-01-06 19:11:09 +0100

    portalicious: personal information attributes service (#6339)
    AB#32370 AB#31064

--- interfaces/Portalicious/src/app/pages/project-registration-personal-information/project-registration-personal-information.page.ts
+++ interfaces/Portalicious/src/app/pages/project-registration-personal-information/project-registration-personal-information.page.ts
@@ -1,45 +1,28 @@
-import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   computed,
   inject,
   input,
+  signal,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { injectQuery } from '@tanstack/angular-query-experimental';
-import { CardModule } from 'primeng/card';
-import { InputTextModule } from 'primeng/inputtext';
-import { SelectButtonModule } from 'primeng/selectbutton';
+import { SkeletonModule } from 'primeng/skeleton';
 
-import { GenericRegistrationAttributes } from '@121-service/src/registration/enum/registration-attribute.enum';
+import { RegistrationAttributeTypes } from '@121-service/src/registration/enum/registration-attribute.enum';
+import { LocalizedString } from '@121-service/src/shared/types/localized-string.type';
 
 import {
   DataListComponent,
   DataListItem,
 } from '~/components/data-list/data-list.component';
 import { RegistrationPageLayoutComponent } from '~/components/registration-page-layout/registration-page-layout.component';
-import { ProjectApiService } from '~/domains/project/project.api.service';
-import { projectHasInclusionScore } from '~/domains/project/project.helper';
-import {
-  getGenericAttributeDataListItem,
-  getValueForGenericAttribute,
-  projectAttributeToDataListItem,
-} from '~/domains/project/project-attribute.helpers';
-import { RegistrationApiService } from '~/domains/registration/registration.api.service';
+import { RegistrationAttributeService } from '~/services/registration-attribute.service';
 
 @Component({
   selector: 'app-project-registration-personal-information',
-  imports: [
-    CardModule,
-    CommonModule,
-    FormsModule,
-    SelectButtonModule,
-    InputTextModule,
-    DataListComponent,
-    RegistrationPageLayoutComponent,
-  ],
+  imports: [DataListComponent, RegistrationPageLayoutComponent, SkeletonModule],
   templateUrl: './project-registration-personal-information.page.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -49,72 +32,62 @@ export class ProjectRegistrationPersonalInformationPageComponent {
   readonly projectId = input.required<string>();
   readonly registrationId = input.required<string>();
 
-  readonly registrationApiService = inject(RegistrationApiService);
-  readonly projectApiService = inject(ProjectApiService);
+  readonly registrationAttributeService = inject(RegistrationAttributeService);
 
-  projectAttributes = injectQuery(
-    this.projectApiService.getProjectAttributes({
-      projectId: this.projectId,
-      includeProgramRegistrationAttributes: true,
-      includeTemplateDefaultAttributes: false,
-    }),
-  );
-
-  registration = injectQuery(
-    this.registrationApiService.getRegistrationById(
-      this.projectId,
-      this.registrationId,
+  registrationAttributes = injectQuery(
+    this.registrationAttributeService.getRegistrationAttributes(
+      signal({
+        projectId: this.projectId,
+        registrationId: this.registrationId,
+      }),
     ),
   );
 
-  project = injectQuery(this.projectApiService.getProject(this.projectId));
-
-  attributeList = computed<DataListItem[]>(() => {
-    const list: DataListItem[] = [];
-    if (!this.projectAttributes.isSuccess() || !this.registration.isSuccess()) {
-      return list;
-    }
-
-    const genericAttributeNames =
-      this.genericAttributeNamesForPersonalInformation();
-    for (const attributeName of genericAttributeNames) {
-      const value = getValueForGenericAttribute(
-        this.registration.data()[attributeName],
-        attributeName,
-      );
-      const genericAttributeListItem = getGenericAttributeDataListItem(
-        attributeName,
-        value,
-      );
-      list.push(genericAttributeListItem);
-    }
-    for (const attribute of this.projectAttributes.data()) {
-      const value = this.registration.data()[attribute.name];
-      const dataListItem = projectAttributeToDataListItem(attribute, value);
-      if (dataListItem) {
-        list.push(dataListItem);
-      }
-    }
-    return list;
-  });
-
-  private genericAttributeNamesForPersonalInformation = computed<
-    GenericRegistrationAttributes[]
-  >(() => {
-    const genericAttributeNames: GenericRegistrationAttributes[] = [
-      GenericRegistrationAttributes.preferredLanguage,
-      GenericRegistrationAttributes.programFinancialServiceProviderConfigurationLabel,
-      GenericRegistrationAttributes.paymentAmountMultiplier,
-    ];
-    if (this.project.data()?.enableMaxPayments) {
-      genericAttributeNames.concat([
-        GenericRegistrationAttributes.maxPayments,
-        GenericRegistrationAttributes.paymentCountRemaining,
-      ]);
-    }
-    if (projectHasInclusionScore(this.project.data())) {
-      genericAttributeNames.push(GenericRegistrationAttributes.inclusionScore);
-    }
-    return genericAttributeNames;
-  });
+  dataList = computed<DataListItem[]>(() =>
+    (this.registrationAttributes.data() ?? []).map(
+      ({ type, value, ...attribute }) => {
+        switch (type) {
+          case RegistrationAttributeTypes.multiSelect:
+            throw new Error('multiSelect not supported');
+          case RegistrationAttributeTypes.numeric:
+            return {
+              ...attribute,
+              type: 'number',
+              value: value as number,
+            };
+          case RegistrationAttributeTypes.numericNullable:
+            return {
+              ...attribute,
+              type: 'number',
+              value: value as null | number,
+            };
+          case RegistrationAttributeTypes.date:
+            return {
+              ...attribute,
+              type: 'date',
+              value: value as Date,
+            };
+          case RegistrationAttributeTypes.boolean:
+            return {
+              ...attribute,
+              type: 'boolean',
+              value: value as boolean,
+            };
+          case RegistrationAttributeTypes.dropdown:
+            return {
+              ...attribute,
+              type: 'options',
+              value: value as string | string[],
+            };
+          case RegistrationAttributeTypes.tel:
+          case RegistrationAttributeTypes.text:
+            return {
+              ...attribute,
+              type: 'text',
+              value: value as LocalizedString | string,
+            };
+        }
+      },
+    ),
+  );
 }
