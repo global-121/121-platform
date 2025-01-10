2efb7ca5af1755754e570da2ebfd16489719b062
jannisvisser <jannisvisser@redcross.nl>
2025-01-10 10:24:39 +0100

    refactor: get balance via fspconfigid of registration AB#32518 (#6358)
    * refactor: get balance via fspconfigid of registration AB#32518
    
    * refactor: improve performance of bulk calls by not repeating queries AB#32518
    
    * refactor: get via transaction AB#32518
    
    * test: add API test get-balance + small cleanup AB#32517
    
    * fix: improve query AB#32518
    
    * chore: add note

--- services/121-service/src/program-financial-service-provider-configurations/program-financial-service-provider-configurations.repository.ts
+++ services/121-service/src/program-financial-service-provider-configurations/program-financial-service-provider-configurations.repository.ts
@@ -65,6 +65,32 @@ export class ProgramFinancialServiceProviderConfigurationRepository extends Repo
     return response;
   }
 
+  public async getUsernamePasswordPropertiesByVoucherId(
+    intersolveVoucherId: number,
+  ): Promise<UsernamePasswordInterface> {
+    const programFspConfig = await this.baseRepository
+      .createQueryBuilder('configuration')
+      .leftJoin('configuration.transactions', 'transactions')
+      .innerJoin('transactions.latestTransaction', 'latestTransaction')
+      .leftJoin('latestTransaction.registration', 'registration')
+      .leftJoin('registration.images', 'images')
+      .leftJoin('images.voucher', 'voucher')
+      .where('voucher.id = :intersolveVoucherId', {
+        intersolveVoucherId,
+      })
+      .andWhere('voucher.payment = transactions.payment') // TODO: REFACTOR: this filter is needed as it is not taken care of by the joins above. Better to refactor the entity relations here, probably together with whole Voucher refactor. Also look at module responsiblity then.
+      .select('configuration.id AS id')
+      .getRawOne(); // use getRawOne (+select) instead of getOne for performance reasons
+
+    if (!programFspConfig) {
+      throw new Error(
+        `ProgramFspConfig not found based onintersolveVoucherId ${intersolveVoucherId}`,
+      );
+    }
+
+    return this.getUsernamePasswordProperties(programFspConfig.id);
+  }
+
   // This methods specfically does not throw as it also used to check if the property exists
   public async getPropertyValueByName({
     programFinancialServiceProviderConfigurationId,
---
+++ services/121-service/test/voucher/get-balance.test.ts
@@ -0,0 +1,75 @@
+import { TransactionStatusEnum } from '@121-service/src/payments/transactions/enums/transaction-status.enum';
+import { RegistrationStatusEnum } from '@121-service/src/registration/enum/registration-status.enum';
+import { SeedScript } from '@121-service/src/scripts/seed-script.enum';
+import { waitFor } from '@121-service/src/utils/waitFor.helper';
+import {
+  getTransactionsIntersolveVoucher,
+  getVoucherBalance,
+} from '@121-service/test/helpers/intersolve-voucher.helper';
+import { doPayment } from '@121-service/test/helpers/program.helper';
+import {
+  awaitChangePaStatus,
+  importRegistrations,
+} from '@121-service/test/helpers/registration.helper';
+import {
+  getAccessToken,
+  resetDB,
+} from '@121-service/test/helpers/utility.helper';
+import {
+  programIdPV,
+  registrationPV5,
+} from '@121-service/test/registrations/pagination/pagination-data';
+
+describe('Get Intersolve voucher balance', () => {
+  let accessToken: string;
+
+  const payment = 1;
+  const amount = 22;
+
+  beforeEach(async () => {
+    await waitFor(1_000);
+    await resetDB(SeedScript.nlrcMultiple);
+    accessToken = await getAccessToken();
+    await waitFor(3_000);
+  });
+
+  it('should succesfully get balance', async () => {
+    // Arrange
+    await importRegistrations(programIdPV, [registrationPV5], accessToken);
+    await awaitChangePaStatus(
+      programIdPV,
+      [registrationPV5.referenceId],
+      RegistrationStatusEnum.included,
+      accessToken,
+    );
+    const paymentReferenceIds = [registrationPV5.referenceId];
+    await doPayment(
+      programIdPV,
+      payment,
+      amount,
+      paymentReferenceIds,
+      accessToken,
+    );
+
+    // make sure to wait for the transaction to be completed
+    const getTransactionsBody = await getTransactionsIntersolveVoucher(
+      programIdPV,
+      payment,
+      registrationPV5.referenceId,
+      accessToken,
+    );
+
+    // Act
+    const getVoucherBalanceResponse = await getVoucherBalance(
+      programIdPV,
+      payment,
+      registrationPV5.referenceId,
+      accessToken,
+    );
+
+    // Assert
+    expect(getTransactionsBody[0].status).toBe(TransactionStatusEnum.success);
+    expect(getVoucherBalanceResponse.status).toBe(200);
+    expect(getVoucherBalanceResponse.text).toBe('12.5'); // This is the number our mock gives back
+  });
+});
