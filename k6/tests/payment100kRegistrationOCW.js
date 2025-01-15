96592c8b40ad8178f48663dd7f2d7aa8aeeb6288
piotrk39 <piotrk39@gmail.com>
2025-01-15 16:57:40 +0100

    Test k6 tests

--- k6/tests/payment100kRegistrationOCW.js
+++ k6/tests/payment100kRegistrationOCW.js
@@ -1,25 +1,32 @@
-import { check, sleep } from 'k6';
+import { check, fail, sleep } from 'k6';
 
 import { registrationVisa } from '../helpers/registration-default.data.js';
 import InitializePaymentModel from '../models/initalize-payment.js';
 
 const initializePayment = new InitializePaymentModel();
 
-const duplicateNumber = 17; // '17' leads to 131k registrations
+const duplicateNumber = 10; // '17' leads to 131k registrations
 const resetScript = 'nlrc-multiple';
 const programId = 3;
 const paymentId = 3;
-const maxTimeoutAttempts = 200;
-const minPassRatePercentage = 10;
+const maxTimeoutAttempts = 1;
+const minPassRatePercentage = 5;
 
 export const options = {
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
   },
   vus: 1,
-  duration: '20m',
+  duration: '30s',
   iterations: 1,
 };
+
+function checkAndFail(response, checks) {
+  const result = check(response, checks);
+  if (!result) {
+    fail('One or more checks failed');
+  }
+}
 
 export default function () {
   const monitorPayment = initializePayment.initializePayment(
@@ -31,12 +38,15 @@ export default function () {
     maxTimeoutAttempts,
     minPassRatePercentage,
   );
-  check(monitorPayment, {
+  checkAndFail(monitorPayment, {
     'Payment progressed successfully status 200': (r) => {
       if (r.status != 200) {
         const responseBody = JSON.parse(r.body);
+        console.log('responseBody: ', responseBody);
+        console.log('status: ', r.status);
         console.log(responseBody.error || r.status);
       }
+      console.log('status: ', r.status);
       return r.status == 200;
     },
   });
