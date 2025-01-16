dd3ccad099802628c94a73b9b4179f54a306f635
piotrk39 <piotrk39@gmail.com>
2025-01-16 09:26:31 +0100

    add tests

--- k6/tests/payment100kRegistrationOCW.js
+++ k6/tests/payment100kRegistrationOCW.js
@@ -1,52 +1,47 @@
-import { check, fail } from 'k6';
+import { check, sleep } from 'k6';
 
-// const initializePayment = new InitializePaymentModel();
+import { registrationVisa } from '../helpers/registration-default.data.js';
+import InitializePaymentModel from '../models/initalize-payment.js';
 
-// const duplicateNumber = 17; // '17' leads to 131k registrations
-// const resetScript = 'nlrc-multiple';
-// const programId = 3;
-// const paymentId = 3;
-// const maxTimeoutAttempts = 1;
-// const minPassRatePercentage = 10;
-// const amount = 11.11; // Using an amount with cents. To ensure we handle javascript floating point precision issues
+const initializePayment = new InitializePaymentModel();
+
+const duplicateNumber = 17; // '17' leads to 131k registrations
+const resetScript = 'nlrc-multiple';
+const programId = 3;
+const paymentId = 3;
+const maxTimeoutAttempts = 200;
+const minPassRatePercentage = 10;
+const amount = 11.11; // Using an amount with cents. To ensure we handle javascript floating point precision issues
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.001'], // http errors should be less than 1%
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
   },
   vus: 1,
-  duration: '3m',
+  duration: '20m',
   iterations: 1,
 };
 
 export default function () {
-  // Manual check that always fails
-  check(null, {
-    'Manual failure check': () => {
-      console.log('This is a manual check that always fails');
-      fail('bla');
-      return false;
+  const monitorPayment = initializePayment.initializePayment(
+    resetScript,
+    programId,
+    registrationVisa,
+    duplicateNumber,
+    paymentId,
+    maxTimeoutAttempts,
+    minPassRatePercentage,
+    amount,
+  );
+  check(monitorPayment, {
+    'Payment progressed successfully status 200': (r) => {
+      if (r.status != 200) {
+        const responseBody = JSON.parse(r.body);
+        console.log(responseBody.error || r.status);
+      }
+      return r.status == 200;
     },
   });
-  // const monitorPayment = initializePayment.initializePayment(
-  //   resetScript,
-  //   programId,
-  //   registrationVisa,
-  //   duplicateNumber,
-  //   paymentId,
-  //   maxTimeoutAttempts,
-  //   minPassRatePercentage,
-  //   amount,
-  // );
-  // check(monitorPayment, {
-  //   'Payment progressed successfully status 200': (r) => {
-  //     if (r.status != 200) {
-  //       const responseBody = JSON.parse(r.body);
-  //       console.log(responseBody.error || r.status);
-  //     }
-  //     return r.status == 200;
-  //   },
-  // });
 
-  // sleep(1);
+  sleep(1);
 }
