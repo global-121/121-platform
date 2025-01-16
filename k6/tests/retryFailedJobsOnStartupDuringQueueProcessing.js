28c203c70b94bb4f428cdfdd37b8aeda54f9250f
piotrk39 <piotrk39@gmail.com>
2025-01-16 18:31:19 +0100

    Update all the tests and rise the threshold

---
+++ k6/tests/retryFailedJobsOnStartupDuringQueueProcessing.js
@@ -0,0 +1,103 @@
+import { check, fail, sleep } from 'k6';
+import http from 'k6/http';
+import { Counter } from 'k6/metrics';
+
+import loginModel from '../models/login.js';
+import paymentsModel from '../models/payments.js';
+import resetModel from '../models/reset.js';
+
+const resetPage = new resetModel();
+const paymentsPage = new paymentsModel();
+const loginPage = new loginModel();
+
+const duplicateNumber = 7; // This leads to 128 registrations
+const programId = 3;
+const paymentId = 3;
+const maxTimeoutAttempts = 400;
+const minPassRatePercentage = 100;
+const amount = 10;
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    failed_checks: ['count<1'], // fail the test if any check fails
+  },
+  vus: 1,
+  duration: '60m',
+  iterations: 1,
+};
+
+const failedChecks = new Counter('failed_checks');
+
+function checkAndFail(response, checks) {
+  const result = check(response, checks);
+  if (!result) {
+    failedChecks.add(1);
+    fail('One or more checks failed');
+  }
+}
+
+function isServiceUp() {
+  const response = http.get('http://localhost:3000/api/health/health'); // Replace with your health check endpoint
+  return response.status === 200;
+}
+
+export default function () {
+  // reset db
+  const reset = resetPage.resetDBMockRegistrations(duplicateNumber);
+  check(reset, {
+    'Reset succesfull status was 202': (r) => r.status == 202,
+  });
+
+  // login
+  const login = loginPage.login();
+  check(login, {
+    'Login succesfull status was 200': (r) => r.status == 201,
+    'Login time is less than 200ms': (r) => {
+      if (r.timings.duration >= 200) {
+        console.log(`Login time was ${r.timings.duration}ms`);
+      }
+      return r.timings.duration < 200;
+    },
+  });
+
+  // Do the payment
+  const doPayment = paymentsPage.createPayment(programId, amount);
+  checkAndFail(doPayment, {
+    'Payment successfully done status 202': (r) => {
+      if (r.status != 202) {
+        console.log(r.body);
+      }
+      return r.status == 202;
+    },
+  });
+
+  sleep(5); // wait long enough so that jobs are added to the queue, but not so long that all are processed already
+
+  resetPage.kill121Service();
+
+  // Wait for the service to restart and become available
+  let serviceUp = false;
+  while (!serviceUp) {
+    sleep(1); // Wait for 1 second before pinging again
+    serviceUp = isServiceUp();
+  }
+
+  // Monitor that 100% of payments is successful and then stop the test
+  const monitorPayment = paymentsPage.getPaymentResults(
+    programId,
+    maxTimeoutAttempts,
+    paymentId,
+    duplicateNumber,
+    minPassRatePercentage,
+  );
+  checkAndFail(monitorPayment, {
+    'Payment progressed successfully status 200': (r) => {
+      if (r.status != 200) {
+        const responseBody = JSON.parse(r.body);
+        console.log(responseBody.error || r.status);
+      }
+      return r.status == 200;
+    },
+  });
+}
