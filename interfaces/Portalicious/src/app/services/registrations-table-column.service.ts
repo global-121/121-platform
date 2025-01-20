60bef207ff39ced930816b5eae783f1ecb1ab408
Peter Smallenbroek <PeterSmallenbroek@users.noreply.github.com>
2025-01-20 16:12:00 +0100

    portalicious: filters from backend (#6381)

---
+++ interfaces/Portalicious/src/app/services/registrations-table-column.service.ts
@@ -0,0 +1,190 @@
+import { inject, Injectable, Signal, signal } from '@angular/core';
+
+import {
+  QueryClient,
+  queryOptions,
+} from '@tanstack/angular-query-experimental';
+
+import { RegistrationAttributeTypes } from '@121-service/src/registration/enum/registration-attribute.enum';
+import { RegistrationStatusEnum } from '@121-service/src/registration/enum/registration-status.enum';
+
+import { getChipDataByRegistrationStatus } from '~/components/colored-chip/colored-chip.helper';
+import {
+  QueryTableColumn,
+  QueryTableColumnType,
+} from '~/components/query-table/query-table.component';
+import { ProjectApiService } from '~/domains/project/project.api.service';
+import {
+  REGISTRATION_STATUS_LABELS,
+  registrationLink,
+} from '~/domains/registration/registration.helper';
+import { Registration } from '~/domains/registration/registration.model';
+import {
+  NormalizedRegistrationAttribute,
+  RegistrationAttributeService,
+} from '~/services/registration-attribute.service';
+import { TranslatableStringService } from '~/services/translatable-string.service';
+
+export const FILTERABLE_ATTRIBUTES_LABELS: Record<string, string> = {
+  paymentCount: $localize`:@@payment-count:Number of payments`,
+  paymentCountRemaining: $localize`:@@payment-count-remaining:Remaining payments`,
+  maxPayments: $localize`:@@max-payments:Max payments`,
+  lastMessageStatus: $localize`:@@last-message-status:Last message status`,
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegistrationsTableColumnService {
+  private readonly queryClient = inject(QueryClient);
+
+  private readonly projectApiService = inject(ProjectApiService);
+  private readonly registrationAttributeService = inject(
+    RegistrationAttributeService,
+  );
+  private translatableStringService = inject(TranslatableStringService);
+
+  getColumns(projectId: Signal<string>) {
+    return () => {
+      return queryOptions({
+        queryKey: ['filterableAttributes', projectId],
+        queryFn: async () => {
+          const project = await this.queryClient.fetchQuery(
+            this.projectApiService.getProject(projectId)(),
+          );
+          const registrationAttributes = await this.queryClient.fetchQuery(
+            this.registrationAttributeService.getRegistrationAttributes(
+              signal({
+                projectId,
+              }),
+            )(),
+          );
+
+          // Hardcoded columns first
+          const columns: QueryTableColumn<Registration>[] = [
+            {
+              field: 'registrationProgramId',
+              header: $localize`Reg. #`,
+              getCellText: (registration) =>
+                `Reg. #${registration.registrationProgramId.toString()}`,
+              getCellRouterLink: (registration) =>
+                registrationLink({
+                  projectId: projectId(),
+                  registrationId: registration.id,
+                }),
+              type: QueryTableColumnType.NUMERIC,
+            },
+            {
+              field: 'name',
+              header: $localize`:@@registration-full-name:Name`,
+              getCellRouterLink: (registration) =>
+                registrationLink({
+                  projectId: projectId(),
+                  registrationId: registration.id,
+                }),
+              fieldForFilter: 'fullName',
+              fieldForSort: 'fullName',
+            },
+            {
+              field: 'status',
+              header: $localize`:@@registration-status:Status`,
+              type: QueryTableColumnType.MULTISELECT,
+              options: Object.values(RegistrationStatusEnum)
+                .filter((status) => status !== RegistrationStatusEnum.deleted)
+                .map((status) => ({
+                  label: REGISTRATION_STATUS_LABELS[status],
+                  value: status,
+                })),
+              getCellChipData: (registration) =>
+                getChipDataByRegistrationStatus(registration.status),
+            },
+            {
+              field: 'programFinancialServiceProviderConfigurationName',
+              header: $localize`FSP`,
+              type: QueryTableColumnType.MULTISELECT,
+              options:
+                project.programFinancialServiceProviderConfigurations.map(
+                  (config) => ({
+                    label:
+                      this.translatableStringService.translate(config.label) ??
+                      '',
+                    value: config.name,
+                  }),
+                ),
+            },
+            {
+              field: 'registrationCreated',
+              fieldForFilter: 'registrationCreatedDate',
+              header: $localize`:@@registration-created:Registration created`,
+              type: QueryTableColumnType.DATE,
+              defaultHidden: true,
+            },
+          ];
+
+          if (project.filterableAttributes) {
+            for (const filterableGroup of project.filterableAttributes) {
+              if (filterableGroup.group === 'paAttributes') {
+                for (const filterableAttribute of filterableGroup.filters) {
+                  const foundAttribute = registrationAttributes.find(
+                    (ra) => ra.name === filterableAttribute.name,
+                  );
+                  if (foundAttribute) {
+                    columns.push({
+                      field: foundAttribute.name,
+                      header:
+                        this.translatableStringService.translate(
+                          foundAttribute.label,
+                        ) ?? foundAttribute.name,
+                      type: this.mapAttributeTypeToQueryTableColumnType(
+                        foundAttribute,
+                      ),
+                      options:
+                        foundAttribute.options?.map((option) => ({
+                          label: option.label ?? '',
+                          value: option.value,
+                        })) ?? [],
+                      defaultHidden: true,
+                    });
+                  }
+                }
+              } else {
+                for (const filterableAttribute of filterableGroup.filters) {
+                  columns.push({
+                    field: filterableAttribute.name,
+                    header:
+                      FILTERABLE_ATTRIBUTES_LABELS[filterableAttribute.name],
+                    defaultHidden: true,
+                    type: filterableAttribute.isInteger
+                      ? QueryTableColumnType.NUMERIC
+                      : QueryTableColumnType.TEXT,
+                  });
+                }
+              }
+            }
+          }
+
+          return columns;
+        },
+      });
+    };
+  }
+
+  private mapAttributeTypeToQueryTableColumnType(
+    attribute: NormalizedRegistrationAttribute,
+  ) {
+    switch (attribute.type) {
+      case RegistrationAttributeTypes.tel:
+      case RegistrationAttributeTypes.text:
+      case RegistrationAttributeTypes.boolean:
+        return QueryTableColumnType.TEXT;
+      case RegistrationAttributeTypes.numeric:
+      case RegistrationAttributeTypes.numericNullable:
+        return QueryTableColumnType.NUMERIC;
+      case RegistrationAttributeTypes.date:
+        return QueryTableColumnType.DATE;
+      case RegistrationAttributeTypes.dropdown:
+      case RegistrationAttributeTypes.multiSelect:
+        return QueryTableColumnType.MULTISELECT;
+    }
+  }
+}
