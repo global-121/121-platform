59e2bf80bab294816db93c5e9f8b2978d12f9e57
piotrk39 <piotrk39@gmail.com>
2025-01-23 12:28:14 +0100

    remove fail checks

--- k6/tests/payment100kRegistrationOCW.js
+++ k6/tests/payment100kRegistrationOCW.js
@@ -16,7 +16,6 @@ const amount = 11.11;
 export const options = {
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    failed_checks: ['count<1'], // fail the test if any check fails
   },
   vus: 1,
   duration: '80m',
