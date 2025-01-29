273209deebb429868cde62f760b7905fb95b0625
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-01-29 16:12:44 +0100

    Fix allow empty string values for nonRequired dropdows (#6430)
    * Fix allow empty string values for nonRequired dropdows
    
    * Updated integration test on bulk update
    
    ---------
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/test/helpers/assert.helper.ts
+++ services/121-service/test/helpers/assert.helper.ts
@@ -1,7 +1,6 @@
 import { MessageTemplateEntity } from '@121-service/src/notifications/message-template/message-template.entity';
 import { RegistrationStatusEnum } from '@121-service/src/registration/enum/registration-status.enum';
 import { RegistrationEntity } from '@121-service/src/registration/registration.entity';
-import { LocalizedString } from '@121-service/src/shared/types/localized-string.type';
 
 export function processMessagePlaceholders(
   messageTemplates: MessageTemplateEntity[],
@@ -20,30 +19,4 @@ export function processMessagePlaceholders(
   );
 
   return processedTemplate;
-}
-
-export function assertRegistrationBulkUpdate(
-  patchData: Record<
-    string,
-    string | undefined | boolean | number | null | LocalizedString
-  >,
-  updatedRegistration: Record<
-    string,
-    string | undefined | boolean | number | null
-  >,
-  originalRegistration: Record<
-    string,
-    string | undefined | boolean | number | null
-  >,
-): void {
-  for (const key in patchData) {
-    expect(JSON.stringify(updatedRegistration[key])).toBe(
-      JSON.stringify(patchData[key]),
-    );
-  }
-  for (const key in originalRegistration) {
-    if (patchData[key] === undefined && key !== 'name') {
-      expect(updatedRegistration[key]).toStrictEqual(originalRegistration[key]);
-    }
-  }
 }
