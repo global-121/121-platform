16ed5789569851a49d1849d6baa3d6ee298c70a2
PeterSmallenbroek <peter.smallenbroek@gmail.com>
2025-01-30 10:36:00 +0100

    fix

--- services/121-service/src/payments/reconciliation/intersolve-voucher-reconciliation/intersolve-voucher-reconciliation.module.ts
+++ services/121-service/src/payments/reconciliation/intersolve-voucher-reconciliation/intersolve-voucher-reconciliation.module.ts
@@ -7,6 +7,7 @@ import { IntersolveVoucherReconciliationController } from '@121-service/src/paym
 import { IntersolveVoucherReconciliationService } from '@121-service/src/payments/reconciliation/intersolve-voucher-reconciliation/intersolve-voucher-reconciliation.service';
 import { ProgramFinancialServiceProviderConfigurationsModule } from '@121-service/src/program-financial-service-provider-configurations/program-financial-service-provider-configurations.module';
 import { ProgramModule } from '@121-service/src/programs/programs.module';
+import { AzureLogService } from '@121-service/src/shared/services/azure-log.service';
 import { createScopedRepositoryProvider } from '@121-service/src/utils/scope/createScopedRepositoryProvider.helper';
 
 @Module({
@@ -19,6 +20,7 @@ import { createScopedRepositoryProvider } from '@121-service/src/utils/scope/cre
   providers: [
     IntersolveVoucherReconciliationService,
     createScopedRepositoryProvider(IntersolveVoucherEntity),
+    AzureLogService,
   ],
   controllers: [IntersolveVoucherReconciliationController],
 })
