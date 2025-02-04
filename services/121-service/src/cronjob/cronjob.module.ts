f7a0955805b65da86505ba98714715645f0d0480
Diderik van Wingerden <diderik@diderikvanwingerden.com>
2025-02-04 17:54:45 +0100

    Add refactor comment

--- services/121-service/src/cronjob/cronjob.module.ts
+++ services/121-service/src/cronjob/cronjob.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { CronjobController } from '@121-service/src/cronjob/cronjob.controller';
 import { CronjobService } from '@121-service/src/cronjob/cronjob.service';
 
+// TODO: REFACTOR: Rename to CronJobsModule (plural), including service, controller, file names, ...
 @Module({
   imports: [],
   providers: [CronjobService],
