5d6d8ac2bd975f347c095e116acf18d49fd278f7
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-02-11 17:54:16 +0100

    Remove last used date child wallet (#6503)
    Co-authored-by: Ruben <vandervalk@geoit.nl>

---
+++ services/121-service/src/migration/1739279566475-remove-last-used-child-wallet.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class RemoveLastUsedChildWallet1739279566475
+  implements MigrationInterface
+{
+  name = 'RemoveLastUsedChildWallet1739279566475';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."intersolve_visa_child_wallet" DROP COLUMN "lastUsedDate"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."intersolve_visa_child_wallet" ADD "lastUsedDate" TIMESTAMP`,
+    );
+  }
+}
