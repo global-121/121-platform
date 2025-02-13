7df61b9edfb8c5df1d178c6b97a222c1e23b1eef
piotrk39 <piotrk39@gmail.com>
2025-02-13 10:12:31 +0100

    Add the tests base

---
+++ e2e/portalicious/tests/ViewPayment/RetryPaymentWithExcelFsp.spec.ts
@@ -0,0 +1,106 @@
+import { test } from '@playwright/test';
+import { format } from 'date-fns';
+
+import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
+import NLRCProgramPV from '@121-service/src/seed-data/program/program-nlrc-pv.json';
+import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
+import {
+  getAccessToken,
+  resetDB,
+} from '@121-service/test/helpers/utility.helper';
+import {
+  programIdPV,
+  registrationsPvExcel,
+} from '@121-service/test/registrations/pagination/pagination-data';
+
+import LoginPage from '@121-e2e/portalicious/pages/LoginPage';
+import PaymentsPage from '@121-e2e/portalicious/pages/PaymentsPage';
+import RegistrationsPage from '@121-e2e/portalicious/pages/RegistrationsPage';
+
+// Export Excel FSP payment list
+const amount = NLRCProgramPV.fixedTransferValue;
+const fullName = registrationsPvExcel[0].fullName;
+const addressStreet = registrationsPvExcel[0].addressStreet;
+const addressHouseNumber = registrationsPvExcel[0].addressHouseNumber;
+const addressPostalCode = registrationsPvExcel[0].addressPostalCode;
+
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.nlrcMultiple);
+  const accessToken = await getAccessToken();
+  await seedIncludedRegistrations(
+    registrationsPvExcel,
+    programIdPV,
+    accessToken,
+  );
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto('/');
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+});
+
+test('[32304] Retry payments should put failed transactions back in pending and download the payment instructions file for those pending transactions', async ({
+  page,
+}) => {
+  const paymentsPage = new PaymentsPage(page);
+  const registrationsPage = new RegistrationsPage(page);
+
+  const projectTitle = NLRCProgramPV.titlePortal.en;
+  const numberOfPas = registrationsPvExcel.length;
+  const defaultTransferValue = amount;
+  const defaultMaxTransferValue = registrationsPvExcel.reduce((output, pa) => {
+    return output + pa.paymentAmountMultiplier * defaultTransferValue;
+  }, 0);
+  const financialServiceProviders: string[] = ['Excel Payment Instructions'];
+
+  const lastPaymentDate = `${format(new Date(), 'dd/MM/yyyy')}`;
+
+  await test.step('Navigate to Program payments', async () => {
+    await paymentsPage.selectProgram(projectTitle);
+
+    await paymentsPage.navigateToProgramPage('Payments');
+  });
+
+  await test.step('Create payment', async () => {
+    await paymentsPage.createPayment();
+    await paymentsPage.validateExcelFspInstructions();
+  });
+
+  await test.step('Do payment', async () => {
+    await paymentsPage.validatePaymentSummary({
+      fsp: financialServiceProviders,
+      registrationsNumber: numberOfPas,
+      currency: '€',
+      paymentAmount: defaultMaxTransferValue,
+    });
+    await paymentsPage.startPayment();
+    // Assert redirection to payment overview page
+    await page.waitForURL((url) =>
+      url.pathname.startsWith('/en-GB/project/2/payments/1'),
+    );
+    // Assert payment overview page by payment date/ title
+    await paymentsPage.validatePaymentsDetailsPageByDate(lastPaymentDate);
+    await paymentsPage.navigateToProgramPage('Payments');
+  });
+
+  await test.step('Download payment instructions', async () => {
+    await paymentsPage.openPaymentByDate({ date: lastPaymentDate });
+    await paymentsPage.selectPaymentExportOption({
+      option: 'Export FSP payment list',
+    });
+    await registrationsPage.exportAndAssertExcelFspList(
+      0,
+      {
+        amount,
+        fullName,
+        addressStreet,
+        addressHouseNumber,
+        addressPostalCode,
+      },
+      { condition: true, rowCount: 4 },
+    );
+  });
+});
