726c8ae8b60cf569f7b5275156240d2790c21a66
arsforza <asforza@redcross.nl>
2025-02-14 13:57:12 +0100

    fix: add migration for programCustomAttribute export

--- services/121-service/src/migration/1739294207739-RenamePeopleAffected.ts
+++ services/121-service/src/migration/1739294207739-RenamePeopleAffected.ts
@@ -1,4 +1,7 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { EntityManager, MigrationInterface, QueryRunner } from 'typeorm';
+
+import { ExportType } from '@121-service/src/metrics/enum/export-type.enum';
+import { ProgramRegistrationAttributeEntity } from '@121-service/src/programs/program-registration-attribute.entity';
 
 export class RenamePeopleAffected1739294207739 implements MigrationInterface {
   name = 'RenamePeopleAffected1739294207739';
@@ -7,11 +10,38 @@ export class RenamePeopleAffected1739294207739 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "121-service"."program_registration_attribute" ALTER COLUMN "export" SET DEFAULT '["all-registrations","included"]'`,
     );
+    await this.migrateData(queryRunner.manager);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "121-service"."program_registration_attribute" ALTER COLUMN "export" SET DEFAULT '["all-people-affected","included"]'`,
     );
+  }
+
+  private async migrateData(manager: EntityManager): Promise<void> {
+    const oldExportTypeString = 'all-people-affected';
+    const newExportTypeString = 'all-registrations';
+
+    const programRegistrationAttributeRepo = manager.getRepository(
+      ProgramRegistrationAttributeEntity,
+    );
+    const programRegistrationAttributes = await programRegistrationAttributeRepo
+      .createQueryBuilder('program_registration_attribute')
+      .getMany();
+    for (const programCustomAttribute of programRegistrationAttributes) {
+      const stringArray: string[] = [...programCustomAttribute.export];
+
+      if (!stringArray.includes(oldExportTypeString)) {
+        continue;
+      }
+
+      stringArray[stringArray.indexOf(oldExportTypeString)] =
+        newExportTypeString;
+
+      programCustomAttribute.export = stringArray as ExportType[];
+    }
+
+    await programRegistrationAttributeRepo.save(programRegistrationAttributes);
   }
 }
