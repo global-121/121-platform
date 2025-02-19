b93c3991a97c65aa6a711b26070db08243cf33fa
Domenico Gemoli <dgemoli@redcross.nl>
2025-02-19 13:43:09 +0100

    feat: add konami code to easter egg (#6532)

--- interfaces/Portalicious/src/app/app.component.ts
+++ interfaces/Portalicious/src/app/app.component.ts
@@ -5,12 +5,14 @@ import {
   OnDestroy,
   OnInit,
 } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
+import KonamiCode from 'konami-code-js';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { Subscription } from 'rxjs';
 
+import { AppRoutes } from '~/app.routes';
 import { AuthService } from '~/services/auth.service';
 import { ToastService } from '~/services/toast.service';
 
@@ -25,11 +27,16 @@ import { ToastService } from '~/services/toast.service';
 })
 export class AppComponent implements OnInit, OnDestroy {
   private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
   private authSubscriptions: Subscription[] = [];
   toastKey = ToastService.TOAST_KEY;
 
   ngOnInit() {
     this.authSubscriptions = this.authService.initializeSubscriptions();
+    new KonamiCode(() => {
+      void this.router.navigate(['/', AppRoutes.snake]);
+    });
   }
 
   ngOnDestroy(): void {
