2d02e23089d7e2a0112be0f0ef72e852a289b980
Domenico Gemoli <dgemoli@redcross.nl>
2025-02-27 15:35:35 +0100

    chore: add E2E tests for duplicate registrations (#6552)
    * chore: add E2E tests for duplicate registrations
    
    * PR review

---
+++ e2e/portalicious/pages/RegistrationPersonalInformationPage.ts
@@ -0,0 +1,40 @@
+import { Locator, Page } from 'playwright';
+import { expect } from 'playwright/test';
+
+import RegistrationBasePage from './RegistrationBasePage';
+
+class RegistrationPersonalInformationPage extends RegistrationBasePage {
+  readonly editInformationButton: Locator;
+  readonly editInformationReasonField: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.editInformationButton = this.page.getByRole('button', {
+      name: 'Edit information',
+    });
+    this.editInformationReasonField = this.page.getByLabel(
+      'Write a reason for the update',
+    );
+  }
+
+  async editRegistration({
+    field,
+    value,
+    reason = 'E2E test',
+  }: {
+    field: string;
+    value: string;
+    reason?: string;
+  }) {
+    await this.editInformationButton.click();
+    await this.page.getByLabel(field).fill(value);
+    await this.page.getByRole('button', { name: 'Save' }).click();
+    await this.editInformationReasonField.fill(reason);
+    await this.dialog.getByRole('button', { name: 'Save' }).click();
+
+    // this re-appears after the save has been successful
+    await expect(this.editInformationButton).toBeVisible();
+  }
+}
+
+export default RegistrationPersonalInformationPage;
