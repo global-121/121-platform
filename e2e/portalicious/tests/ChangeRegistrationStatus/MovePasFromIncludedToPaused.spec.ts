3911e5c087a822512a86b510b5ae5deac18ca4d5
piotrk39 <piotrk39@gmail.com>
2025-03-06 13:53:25 +0100

    Add test from included to paused

--- e2e/portalicious/tests/ChangeRegistrationStatus/MovePasFromIncludedToPaused.spec.ts
+++ e2e/portalicious/tests/ChangeRegistrationStatus/MovePasFromIncludedToPaused.spec.ts
@@ -22,9 +22,8 @@ import LoginPage from '@121-e2e/portalicious/pages/LoginPage';
 import RegistrationsPage from '@121-e2e/portalicious/pages/RegistrationsPage';
 
 const projectTitle = 'NLRC Direct Digital Aid Program (PV)';
-
-const toastMessageIncluded =
-  'The status of 1 registration(s) is being changed to "Included" successfully. The status change can take up to a minute to process.';
+const toastMessage =
+  'The status of 1 registration(s) is being changed to "Paused" successfully. The status change can take up to a minute to process.';
 
 test.beforeEach(async ({ page }) => {
   await resetDB(SeedScript.nlrcMultiple);
@@ -45,7 +44,7 @@ test.beforeEach(async ({ page }) => {
   await basePage.selectProgram(projectTitle);
 });
 
-test('[31209] Move PA(s) from status "Validated" to "Included"', async ({
+test('[31212] Move PA(s) from status "Included" to "Paused"', async ({
   page,
 }) => {
   const accessToken = await getAccessToken();
@@ -53,45 +52,31 @@ test('[31209] Move PA(s) from status "Validated" to "Included"', async ({
   const registrations = new RegistrationsPage(page);
   const tableComponent = new TableComponent(page);
 
-  await test.step('Change status of all registrations to "Validated"', async () => {
+  await test.step('Apply filter on "Registration Status" column', async () => {
     await changeBulkRegistrationStatus({
       programId: 2,
-      status: RegistrationStatusEnum.validated,
+      status: RegistrationStatusEnum.included,
       accessToken,
     });
   });
 
-  await test.step('Search for the registration with status "Validated"', async () => {
-    await tableComponent.filterColumnByDropDownSelection({
-      columnName: 'Registration Status',
-      selection: 'Validated',
-    });
-  });
-
-  await test.step('Validate the status of the registration', async () => {
-    await registrations.validateStatusOfFirstRegistration({
-      status: 'Validated',
-    });
-  });
-
-  await test.step('Change status of first selected registration to "Validated"', async () => {
+  await test.step('Change status of first selected registration to "Paused"', async () => {
     await tableComponent.changeStatusOfRegistrationInTable({
-      status: 'Include',
+      status: 'Pause',
     });
-    await basePage.validateToastMessage(toastMessageIncluded);
+    await basePage.validateToastMessage(toastMessage);
   });
 
-  await test.step('Search for the registration with status "Validated"', async () => {
-    await tableComponent.clearAllFilters();
+  await test.step('Search for the registration with status "Paused"', async () => {
     await tableComponent.filterColumnByDropDownSelection({
       columnName: 'Registration Status',
-      selection: 'Included',
+      selection: 'Paused',
     });
   });
 
   await test.step('Validate the status of the registration', async () => {
     await registrations.validateStatusOfFirstRegistration({
-      status: 'Included',
+      status: 'Paused',
     });
   });
 });
