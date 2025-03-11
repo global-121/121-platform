fb2f7dfd80f9973b1c58757c56cfb3ffaa34923c
Ruben <vandervalk@geoit.nl>
2025-03-11 12:02:47 +0100

    Dynamicall use registration program id in title

--- e2e/portalicious/tests/RegistrationPage/ViewRegistrationSummary.spec.ts
+++ e2e/portalicious/tests/RegistrationPage/ViewRegistrationSummary.spec.ts
@@ -2,34 +2,35 @@ import { expect, test } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import {
-  getRegistrationIdByReferenceId,
+  searchRegistrationByReferenceId,
   seedPaidRegistrations,
 } from '@121-service/test/helpers/registration.helper';
 import {
   getAccessToken,
   resetDB,
 } from '@121-service/test/helpers/utility.helper';
-import {
-  programIdPV,
-  registrationPvScoped,
-} from '@121-service/test/registrations/pagination/pagination-data';
+import { registrationPvScoped } from '@121-service/test/registrations/pagination/pagination-data';
 
 import LoginPage from '@121-e2e/portalicious/pages/LoginPage';
 import RegistrationActivityLogPage from '@121-e2e/portalicious/pages/RegistrationActivityLogPage';
 
 const projectId = 2;
 let registrationId: number;
+let registrationProgramId: number;
 
 test.beforeAll(async ({}) => {
   await resetDB(SeedScript.nlrcMultiple);
 
   const accessToken = await getAccessToken();
   await seedPaidRegistrations([registrationPvScoped], projectId);
-  registrationId = await getRegistrationIdByReferenceId({
-    programId: programIdPV,
-    referenceId: registrationPvScoped.referenceId,
+  const searchRegistrationResponse = await searchRegistrationByReferenceId(
+    registrationPvScoped.referenceId,
+    projectId,
     accessToken,
-  });
+  );
+  const registration = searchRegistrationResponse.body.data[0];
+  registrationId = registration.id;
+  registrationProgramId = registration.registrationProgramId;
 });
 
 test('User should see a summary of a registration', async ({ page }) => {
@@ -46,8 +47,7 @@ test('User should see a summary of a registration', async ({ page }) => {
 
   await test.step('Validate registration title', async () => {
     const title = await activityLogPage.getRegistrationTitle();
-    const registrationNumber = registrationId; // Assuming registrationId is the registration number
-    const expectedTitle = `Reg. #${registrationNumber} - ${registrationPvScoped.fullName}`;
+    const expectedTitle = `Reg. #${registrationProgramId} - ${registrationPvScoped.fullName}`;
     expect(title).toBe(expectedTitle);
   });
 
