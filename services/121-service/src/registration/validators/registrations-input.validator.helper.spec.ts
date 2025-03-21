cefea8b0a16c3108b3cde716eeb46241c869b0c1
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-03-21 17:13:05 +0100

    Allow non required boolean values to be empty (#6602)
    * Allow non required boolean values to be empty
    
    * remove forgotten console.log
    
    Signed-off-by: Ruben <vandervalk@geoit.nl>
    
    * Run tests  concurently
    
    ---------
    
    Signed-off-by: Ruben <vandervalk@geoit.nl>
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/src/registration/validators/registrations-input.validator.helper.spec.ts
+++ services/121-service/src/registration/validators/registrations-input.validator.helper.spec.ts
@@ -1,73 +1,38 @@
 import { RegistrationsInputValidatorHelpers } from '@121-service/src/registration/validators/registrations-input.validator.helper';
 
 describe('RegistrationsInputValidatorHelpers', () => {
-  describe('stringToBoolean', () => {
-    it('should convert "true", "yes", and "1" to true', () => {
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('true')).toBe(
-        true,
-      );
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('yes')).toBe(
-        true,
-      );
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('1')).toBe(true);
-    });
+  describe('inputToBoolean', () => {
+    it.concurrent.each([['true'], ['yes'], ['1'], [1], [true]])(
+      'should return true for input "%s"',
+      async (input) => {
+        expect(RegistrationsInputValidatorHelpers.inputToBoolean(input)).toBe(
+          true,
+        );
+      },
+    );
 
-    it('should convert "false", "no", "0", "", and null to false', () => {
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('false')).toBe(
-        false,
-      );
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('no')).toBe(
-        false,
-      );
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('0')).toBe(
-        false,
-      );
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('')).toBe(false);
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean(null)).toBe(
-        false,
-      );
-    });
-
-    it('should return undefined for unrecognized strings if no default value is provided', () => {
-      expect(
-        RegistrationsInputValidatorHelpers.inputToBoolean('unrecognized'),
-      ).toBeUndefined();
-    });
-
-    it('should return the default value for unrecognized strings if provided', () => {
-      expect(
-        RegistrationsInputValidatorHelpers.inputToBoolean('unrecognized', true),
-      ).toBe(true);
-      expect(
-        RegistrationsInputValidatorHelpers.inputToBoolean(
-          'unrecognized',
-          false,
-        ),
-      ).toBe(false);
-    });
-
-    it('should return the default value for undefined input if provided', () => {
-      expect(
-        RegistrationsInputValidatorHelpers.inputToBoolean(undefined, true),
-      ).toBe(true);
-      expect(
-        RegistrationsInputValidatorHelpers.inputToBoolean(undefined, false),
-      ).toBe(false);
-    });
-
-    it('should return undefined for undefined input if no default value is provided', () => {
-      expect(
-        RegistrationsInputValidatorHelpers.inputToBoolean(undefined),
-      ).toBeUndefined();
-    });
-
-    it('should handle boolean input by returning it directly', () => {
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('true')).toBe(
-        true,
-      );
-      expect(RegistrationsInputValidatorHelpers.inputToBoolean('false')).toBe(
+    it.concurrent.each([
+      ['false'],
+      ['no'],
+      ['0'],
+      [''],
+      [0],
+      [2],
+      ['unrecognized'],
+      [false],
+    ])('should return false for input "%s"', async (input) => {
+      expect(RegistrationsInputValidatorHelpers.inputToBoolean(input)).toBe(
         false,
       );
     });
+
+    it.concurrent.each([[null], [undefined]])(
+      'should return undefined for input %s',
+      async (input) => {
+        expect(
+          RegistrationsInputValidatorHelpers.inputToBoolean(input),
+        ).toBeUndefined();
+      },
+    );
   });
 });
