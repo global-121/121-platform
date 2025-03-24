a03e14fcf3a3098ef9a16df2136d3db631d7a992
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-03-24 08:37:35 +0100

    Test.e2e view debit cards (#6626)
    * e2e dialog debit card
    
    * e2e - apply review feedback
    
    * remove commented out code
    
    * put login in own steps - less noise when running tests
    
    * show example of regex
    
    * fix typos
    
    * improve step name
    
    * test improve
    
    ---------
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>
    Co-authored-by: Niels Bom <nbom@redcross.nl>

---
+++ e2e/portalicious/pages/RegistrationDebitCardPage.ts
@@ -0,0 +1,30 @@
+import { Locator } from 'playwright';
+
+import DataListComponent from '../components/DataListComponent';
+import RegistrationBasePage from './RegistrationBasePage';
+
+class RegistrationDebitCardPage extends RegistrationBasePage {
+  async getCurrentDebitCardElement(): Promise<Locator> {
+    return this.page.getByTestId('wallet-current-card-list');
+  }
+
+  async getCurrentDebitCardDataList(): Promise<Record<string, string>> {
+    return new DataListComponent(
+      await this.getCurrentDebitCardElement(),
+    ).getData();
+  }
+
+  async getPauseCardButton(): Promise<Locator> {
+    return this.page.getByRole('button', { name: 'Pause card' });
+  }
+
+  async getUnpauseCardButton(): Promise<Locator> {
+    return this.page.getByRole('button', { name: 'Unpause card' });
+  }
+
+  async getReplaceCardButton(): Promise<Locator> {
+    return this.page.getByRole('button', { name: 'Replace card' });
+  }
+}
+
+export default RegistrationDebitCardPage;
