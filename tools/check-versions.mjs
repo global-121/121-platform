e71ba44c2f1b492970710a3a6faa1d98e9256c65
Elwin Schmitz <eschmitz@redcross.nl>
2025-04-01 15:46:00 +0200

    feat: Add small CLI-tool to replace the "shields.io"-badges

---
+++ tools/check-versions.mjs
@@ -0,0 +1,52 @@
+import { parse } from 'node-html-parser';
+
+const IGNORED_INSTANCES = ['test', 'staging'];
+
+async function getInstancesUrls() {
+  const response = await fetch('https://status.121.global');
+  const data = await response.text();
+  const root = parse(data);
+
+  const instances = root
+    .querySelectorAll('.instance:not([id|=mock])')
+    .map((instance) => instance.id)
+    .filter((instance) => !IGNORED_INSTANCES.includes(instance));
+
+  return instances;
+}
+
+async function getVersionFromUrl(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.message)
+    .catch(() => '💥');
+}
+
+function col(value) {
+  return value.toString().padEnd(21, ' ');
+}
+
+async function main() {
+  console.log(`Checking what's currently live...\n`);
+
+  const instances = await getInstancesUrls();
+
+  console.log(
+    ` ${col('')} |  ${col('API')}  |  ${col('Portal')}  |`,
+    `\n`,
+    `----------------------|-------------------------|-------------------------|`,
+  );
+
+  for (const instance of instances) {
+    const api = await getVersionFromUrl(
+      `https://${instance}.121.global/api/health/version`,
+    );
+    const portal = await getVersionFromUrl(
+      `https://portal.${instance}.121.global/VERSION.json`,
+    );
+
+    console.log(` ${col(instance)} |  ${col(api)}  |  ${col(portal)}  |`);
+  }
+}
+
+main();
