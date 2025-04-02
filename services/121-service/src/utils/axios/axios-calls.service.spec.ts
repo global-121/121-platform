a535d76e06f1288a79c11d2da9e315b2d332603c
Niels Bom <nbom@redcross.nl>
2025-04-02 14:01:04 +0200

    improve axios-calls, add test

---
+++ services/121-service/src/utils/axios/axios-calls.service.spec.ts
@@ -0,0 +1,21 @@
+import { AxiosCallsService } from '@121-service/src/utils/axios/axios-calls.service';
+
+describe('AxiosCallsService', () => {
+  it('should log a useful message when authentication fails', async () => {
+    // Arrange
+    const axiosCallsService = new AxiosCallsService();
+    jest.spyOn(axiosCallsService, 'loginAsAdmin').mockResolvedValue({
+      headers: {
+        'set-cookie': undefined,
+      },
+    });
+
+    // Act
+    const promiseGetAccessToken = axiosCallsService.getAccessToken();
+
+    // Assert
+    await expect(promiseGetAccessToken).rejects.toThrow(
+      "Error while extracting access token from cookies: TypeError: Cannot read properties of undefined (reading 'find')",
+    );
+  });
+});
