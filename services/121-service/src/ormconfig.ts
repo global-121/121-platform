8db42b7e4a750930eb0a49dff132b4d3ad857ea1
Elwin Schmitz <eschmitz@redcross.nl>
2025-04-03 11:10:11 +0200

    refactor: Use ALL 3 CA-certificates from App Service container for DB-connection
    AB#35073
    
    Related #6613
    Related AB#34276

--- services/121-service/src/ormconfig.ts
+++ services/121-service/src/ormconfig.ts
@@ -1,5 +1,29 @@
-import fs from 'fs';
+import { readFileSync } from 'node:fs';
+import { TlsOptions } from 'node:tls';
 import { DataSourceOptions } from 'typeorm';
+
+const createSSLConfig = (): boolean | TlsOptions => {
+  if (process.env.NODE_ENV === 'development') {
+    return false;
+  }
+  // To make a secure connection to an "Azure Database for PostgreSQL flexible server" in local development:
+  // - See: https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/concepts-networking-ssl-tls#configure-ssl-on-the-client
+  // - Download the 3 certificates mentioned (or extract them from an App Service instance) into the local `./cert`-folder,
+  // - Set: certsPath to: './cert'
+  // - Disable the NODE_ENV check above, or set the ENV-variable to 'production' in the `services/.env`-file
+  const certsPath = '/etc/ssl/certs';
+  const azureConnectionCACertificates = [
+    readFileSync(`${certsPath}/DigiCert_Global_Root_CA.pem`).toString(),
+    readFileSync(
+      `${certsPath}/Microsoft_RSA_Root_Certificate_Authority_2017.pem`,
+    ).toString(),
+    readFileSync(`${certsPath}/DigiCert_Global_Root_G2.pem`).toString(),
+  ].join('\n\n');
+
+  return {
+    ca: azureConnectionCACertificates,
+  };
+};
 
 export const ORMConfig: DataSourceOptions = {
   type: 'postgres',
@@ -21,10 +45,5 @@ export const ORMConfig: DataSourceOptions = {
   // UNCOMMENT the following lines to enable query-logging for debugging
   // logging: ['query'],
   // logger: 'advanced-console',
-  ssl:
-    process.env.NODE_ENV === 'development'
-      ? undefined
-      : {
-          ca: fs.readFileSync('cert/DigiCertGlobalRootCA.crt.pem').toString(),
-        },
+  ssl: createSSLConfig(),
 };
