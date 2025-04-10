c8f7070e268570358bcf646455558fd52512e3cc
piotrk39 <piotrk39@gmail.com>
2025-04-10 13:18:26 +0200

    Add test base

--- e2e/portalicious/pages/RegistrationActivityLogPage.ts
+++ e2e/portalicious/pages/RegistrationActivityLogPage.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import { Page } from 'playwright';
+import { Locator, Page } from 'playwright';
 
 import TableComponent from '@121-e2e/portalicious/components/TableComponent';
 
@@ -7,10 +7,20 @@ import RegistrationBasePage from './RegistrationBasePage';
 
 class RegistrationActivityLogPage extends RegistrationBasePage {
   readonly table: TableComponent;
+  readonly personalInformationTab: Locator;
+  readonly editInformationButton: Locator;
+  readonly saveButton: Locator;
 
   constructor(page: Page) {
     super(page);
     this.table = new TableComponent(page);
+    this.personalInformationTab = this.page.getByRole('tab', {
+      name: 'Personal Information',
+    });
+    this.editInformationButton = this.page.getByRole('button', {
+      name: 'Edit information',
+    });
+    this.saveButton = this.page.getByRole('button', { name: 'Save' });
   }
 
   async validateLastMessageSent(message: string) {
@@ -25,6 +35,35 @@ class RegistrationActivityLogPage extends RegistrationBasePage {
     const sentMessageText = await this.page.getByText(message).innerText();
     expect(sentMessageText).toBe(message);
     await dropdownButton.click();
+  }
+
+  async navigateToPersonalInformation() {
+    await this.personalInformationTab.click();
+  }
+
+  async clickEditInformationButton() {
+    await this.editInformationButton.click();
+  }
+
+  async saveChanges() {
+    await this.saveButton.click();
+    await this.page.getByLabel('Write a reason for the update').fill('Test');
+    await this.saveButton.nth(1).click();
+  }
+
+  async selectDropdownOption({
+    dropdownName,
+    option,
+  }: {
+    dropdownName: string;
+    option: string;
+  }) {
+    const dropwdown = this.page
+      .getByTestId(`edit-personal-information-${dropdownName}`)
+      .getByRole('button');
+    await dropwdown.click();
+    await this.page.getByLabel('Preferred Language').fill(option);
+    await this.page.getByRole('option', { name: option }).click();
   }
 }
 
