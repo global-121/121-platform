89711410c10481cf7be9bfeb5bd81bd1fe260f75
piotrk39 <piotrk39@gmail.com>
2025-04-11 16:00:10 +0200

    Add parametrized tests and pi fields library

---
+++ e2e/portalicious/components/PersonalInformationFields.ts
@@ -0,0 +1,66 @@
+export const textInputs = [
+  {
+    fieldName: 'Partner Organization',
+    textInputIdName: 'namePartnerOrganization',
+    textInputValue: 'Red Panda',
+  },
+  {
+    fieldName: 'Address postal code',
+    textInputIdName: 'addressPostalCode',
+    textInputValue: '1234GH',
+  },
+  {
+    fieldName: 'Scope',
+    textInputIdName: 'scope',
+    textInputValue: 'amsterdam.west',
+    count: 2,
+  },
+  {
+    fieldName: 'Name',
+    textInputIdName: 'name',
+    textInputValue: 'Hovik Karayan',
+  },
+  {
+    fieldName: 'Full Name',
+    textInputIdName: 'fullName',
+    textInputValue: 'Hovik Karayan',
+  },
+  {
+    fieldName: 'Address street',
+    textInputIdName: 'addressStreet',
+    textInputValue: 'Parklaan',
+  },
+  {
+    fieldName: 'Address house number addition',
+    textInputIdName: 'addressHouseNumberAddition',
+    textInputValue: 'D',
+  },
+  {
+    fieldName: 'Address city',
+    textInputIdName: 'addressCity',
+    textInputValue: 'Amsterdam',
+  },
+];
+
+export const numberInputs = [
+  {
+    fieldName: 'Max. payments:',
+    numberInputIdName: 'maxPayments',
+    numberInputValue: 5,
+  },
+  {
+    fieldName: 'Address house number:',
+    numberInputIdName: 'addressHouseNumber',
+    numberInputValue: 123,
+  },
+  {
+    fieldName: 'Transfer value multiplier:',
+    numberInputIdName: 'paymentAmountMultiplier',
+    numberInputValue: 2,
+  },
+  {
+    fieldName: 'Remaining payments:',
+    numberInputIdName: 'paymentCountRemaining',
+    numberInputValue: 5,
+  },
+];
