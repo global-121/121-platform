25ca5a7827678f80397e344765941d0d40c60742
Ruben <vandervalk@geoit.nl>
2025-04-14 16:32:34 +0200

    migrate for multiple roles

--- services/121-service/src/migration/1743597529999-created-ignore-duplication-permission.ts
+++ services/121-service/src/migration/1743597529999-created-ignore-duplication-permission.ts
@@ -24,24 +24,35 @@ export class CreatedIgnoreDuplicationPermission1743597529999
 
     const permissionId = permissionIdQuery[0].id;
 
-    const cvaRoleIdQuery = await queryRunner.query(
-      `SELECT "id" FROM "121-service"."user_role" where "role" = '${DefaultUserRole.CvaManager}'`,
-    );
+    const rolesToAssign = [
+      DefaultUserRole.CvaManager,
+      DefaultUserRole.Admin,
+      DefaultUserRole.ProgramAdmin,
+    ];
 
-    if (cvaRoleIdQuery.length === 0) {
-      return;
-    }
-
-    const cvaRoleId = cvaRoleIdQuery[0].id;
-
-    const roleAssignment = await queryRunner.query(
-      `SELECT "userRoleId" FROM "121-service"."user_role_permissions_permission" where "userRoleId" = ${cvaRoleId} and "permissionId" = ${permissionId}`,
-    );
-
-    if (roleAssignment.length === 0)
-      await queryRunner.query(
-        `INSERT INTO "121-service"."user_role_permissions_permission" ("userRoleId", "permissionId") VALUES (${cvaRoleId}, ${permissionId})`,
+    // Loop through each role and assign the permission
+    for (const role of rolesToAssign) {
+      const roleIdQuery = await queryRunner.query(
+        `SELECT "id" FROM "121-service"."user_role" where "role" = '${role}'`,
       );
+
+      if (roleIdQuery.length === 0) {
+        // Skip if this role doesn't exist
+        continue;
+      }
+
+      const roleId = roleIdQuery[0].id;
+
+      const roleAssignment = await queryRunner.query(
+        `SELECT "userRoleId" FROM "121-service"."user_role_permissions_permission" where "userRoleId" = ${roleId} and "permissionId" = ${permissionId}`,
+      );
+
+      if (roleAssignment.length === 0) {
+        await queryRunner.query(
+          `INSERT INTO "121-service"."user_role_permissions_permission" ("userRoleId", "permissionId") VALUES (${roleId}, ${permissionId})`,
+        );
+      }
+    }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function -- Down migration not implemented
