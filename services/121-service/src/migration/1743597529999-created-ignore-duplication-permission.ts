7c668454a8b01fa96fb309da745c0ebd7d17e6a7
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-04-15 09:19:19 +0200

    migrate for multiple roles (#6734)
    * migrate for multiple roles
    
    * removed enum
    
    ---------
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/src/migration/1743597529999-created-ignore-duplication-permission.ts
+++ services/121-service/src/migration/1743597529999-created-ignore-duplication-permission.ts
@@ -1,7 +1,5 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-import { DefaultUserRole } from '@121-service/src/user/user-role.enum';
-
 export class CreatedIgnoreDuplicationPermission1743597529999
   implements MigrationInterface
 {
@@ -24,24 +22,31 @@ export class CreatedIgnoreDuplicationPermission1743597529999
 
     const permissionId = permissionIdQuery[0].id;
 
-    const cvaRoleIdQuery = await queryRunner.query(
-      `SELECT "id" FROM "121-service"."user_role" where "role" = '${DefaultUserRole.CvaManager}'`,
-    );
+    const rolesToAssign = ['cva-manager', 'admin', 'program-admin'];
 
-    if (cvaRoleIdQuery.length === 0) {
-      return;
-    }
-
-    const cvaRoleId = cvaRoleIdQuery[0].id;
-
-    const roleAssignment = await queryRunner.query(
-      `SELECT "userRoleId" FROM "121-service"."user_role_permissions_permission" where "userRoleId" = ${cvaRoleId} and "permissionId" = ${permissionId}`,
-    );
-
-    if (roleAssignment.length === 0)
-      await queryRunner.query(
-        `INSERT INTO "121-service"."user_role_permissions_permission" ("userRoleId", "permissionId") VALUES (${cvaRoleId}, ${permissionId})`,
+    // Loop through each role and assign the permission
+    for (const role of rolesToAssign) {
+      const roleIdQuery = await queryRunner.query(
+        `SELECT "id" FROM "121-service"."user_role" where "role" = '${role}'`,
       );
+
+      if (roleIdQuery.length === 0) {
+        // Skip if this role doesn't exist
+        continue;
+      }
+
+      const roleId = roleIdQuery[0].id;
+
+      const roleAssignment = await queryRunner.query(
+        `SELECT "userRoleId" FROM "121-service"."user_role_permissions_permission" where "userRoleId" = ${roleId} and "permissionId" = ${permissionId}`,
+      );
+
+      if (roleAssignment.length === 0) {
+        await queryRunner.query(
+          `INSERT INTO "121-service"."user_role_permissions_permission" ("userRoleId", "permissionId") VALUES (${roleId}, ${permissionId})`,
+        );
+      }
+    }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function -- Down migration not implemented
