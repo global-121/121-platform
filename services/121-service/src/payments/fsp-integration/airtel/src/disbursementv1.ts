887ab701d2ee5d207f0c5022c43453dae395908a
Niels Bom <nbom@redcross.nl>
2025-05-07 15:18:15 +0200

    refactor: rename

--- services/121-service/src/payments/fsp-integration/airtel/src/disbursementv1.ts
+++ services/121-service/src/payments/fsp-integration/airtel/src/disbursementv1.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { encryptPin } from '@121-service/src/payments/fsp-integration/airtel/src/util.ts';
+import { encryptPinV1 } from '@121-service/src/payments/fsp-integration/airtel/src/util.ts';
 import { getAccessToken } from '@121-service/src/payments/fsp-integration/airtel/src/util.ts';
 import { generateRandomId } from '@121-service/src/payments/fsp-integration/airtel/src/util.ts';
 import { postTransaction } from '@121-service/src/payments/fsp-integration/airtel/src/util.ts';
@@ -12,7 +12,7 @@ const CLIENT_SECRET = `${process.env.CLIENT_SECRET}`;
 const pin = `${process.env.ZAMBIA_DISBURSEMENT_PIN}`;
 const rsaPublicKeyForPinEncryption = `${process.env.PIN_RSA_ENCRYPTION_PUBLIC_KEY_V1}`;
 // RSA encryption is relatively slow, so only do this encryption once per app-startup.
-const pinEncrypted = encryptPin(pin, rsaPublicKeyForPinEncryption);
+const pinEncrypted = encryptPinV1(pin, rsaPublicKeyForPinEncryption);
 
 const example_body = {
   payee: {
