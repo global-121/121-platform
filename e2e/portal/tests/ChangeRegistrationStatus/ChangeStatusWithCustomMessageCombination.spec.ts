90a8f26a3e438742c8c4d96b2214ef7d519705fe
piotrk39 <piotrk39@gmail.com>
2025-05-07 18:19:31 +0200

    Add base test

---
+++ e2e/portal/tests/ChangeRegistrationStatus/ChangeStatusWithCustomMessageCombination.spec.ts
@@ -0,0 +1,49 @@
+import test from '@playwright/test';
+
+import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
+import { seedRegistrations } from '@121-service/test/helpers/registration.helper';
+import { resetDB } from '@121-service/test/helpers/utility.helper';
+import {
+  programIdPV,
+  registrationPV5,
+} from '@121-service/test/registrations/pagination/pagination-data';
+
+import TableComponent from '@121-e2e/portal/components/TableComponent';
+import BasePage from '@121-e2e/portal/pages/BasePage';
+import LoginPage from '@121-e2e/portal/pages/LoginPage';
+import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
+
+const toastMessage =
+  'The status of 1 registration(s) is being changed to "Declined" successfully. The status change can take up to a minute to process.';
+// Arrange
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.nlrcMultiple);
+
+  await seedRegistrations([registrationPV5], programIdPV);
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto('/');
+  await loginPage.login(
+    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  );
+  // Navigate to program
+  const basePage = new BasePage(page);
+  await basePage.selectProgram('NLRC Direct Digital Aid Program (PV)');
+});
+
+test('[35840] Change status of registration with custom message', async ({
+  page,
+}) => {
+  const registrations = new RegistrationsPage(page);
+  const tableComponent = new TableComponent(page);
+  // Act
+  await test.step('Change status of first selected registration to "Declined"', async () => {
+    await tableComponent.changeRegistrationStatusWithCustomMessage({
+      status: 'Decline',
+      message: 'Test custom message to change the status of registration',
+    });
+    await registrations.validateToastMessageAndClose(toastMessage);
+  });
+});
