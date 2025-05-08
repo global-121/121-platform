6baf73d23dabb53f5aad5c3bc31dac522ac8a58f
piotrk39 <piotrk39@gmail.com>
2025-05-08 16:02:07 +0200

    Finalize the test with negatice validation

--- e2e/portal/tests/ChangeRegistrationStatus/ChangeStatusWithCustomMessageCombination.spec.ts
+++ e2e/portal/tests/ChangeRegistrationStatus/ChangeStatusWithCustomMessageCombination.spec.ts
@@ -1,4 +1,4 @@
-import test from '@playwright/test';
+import { type Page, test } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import { seedRegistrations } from '@121-service/test/helpers/registration.helper';
@@ -6,10 +6,10 @@ import { resetDB } from '@121-service/test/helpers/utility.helper';
 import {
   programIdPV,
   registrationPV5,
+  registrationPV6,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
 import TableComponent from '@121-e2e/portal/components/TableComponent';
-import BasePage from '@121-e2e/portal/pages/BasePage';
 import LoginPage from '@121-e2e/portal/pages/LoginPage';
 import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
 
@@ -18,26 +18,20 @@ const toastMessage =
 const customMessage =
   'Test custom message to change the status of registration';
 // Arrange
-test.beforeEach(async ({ page }) => {
+const reset = async () => {
   await resetDB(SeedScript.nlrcMultiple);
+  await seedRegistrations([registrationPV5, registrationPV6], programIdPV);
+};
 
-  await seedRegistrations([registrationPV5], programIdPV);
-
-  // Login
+const login = async (page: Page, email?: string, password?: string) => {
   const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login(
-    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
-    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
-  );
+  await page.goto(`/`);
+  await loginPage.login(email, password);
   // Navigate to program
-  const basePage = new BasePage(page);
-  await basePage.selectProgram('NLRC Direct Digital Aid Program (PV)');
-});
+  await loginPage.selectProgram('NLRC Direct Digital Aid Program (PV)');
+};
 
-test('[35840] Change status of registration with custom message', async ({
-  page,
-}) => {
+const navigateToRegistrationsAndResetFilters = async (page: Page) => {
   const registrations = new RegistrationsPage(page);
   const tableComponent = new TableComponent(page);
   // Act
@@ -49,13 +43,58 @@ test('[35840] Change status of registration with custom message', async ({
     await registrations.validateToastMessageAndClose(toastMessage);
   });
 
-  await test.step('Find and validate custom message', async () => {
-    await registrations.goToRegistrationByName({
-      registrationName: registrationPV5.fullName,
+  test.afterEach(async () => {
+    await navigateToRegistrationsAndResetFilters(page);
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  // Act
+  test('[35840] Change status of registration with custom message', async () => {
+    const registrations = new RegistrationsPage(page);
+    const tableComponent = new TableComponent(page);
+    await test.step('Change status of first selected registration and write a custom message', async () => {
+      await tableComponent.changeRegistrationStatusByNameWithCustomMessage({
+        registrationName: registrationPV5.fullName,
+        status: 'Decline',
+        message: customMessage,
+        customMessage: true,
+      });
+      await registrations.validateToastMessageAndWait(toastMessage);
     });
-    await tableComponent.validateMessageActivityByTypeAndText({
-      notificationType: 'Custom message',
-      message: customMessage,
+
+    await test.step('Find and validate custom message', async () => {
+      await registrations.goToRegistrationByName({
+        registrationName: registrationPV5.fullName,
+      });
+      await tableComponent.validateMessageActivityByTypeAndText({
+        notificationType: 'Custom message',
+        message: customMessage,
+      });
+    });
+  });
+
+  test('[35849] Change status of registration without custom message', async () => {
+    const registrations = new RegistrationsPage(page);
+    const tableComponent = new TableComponent(page);
+    // Act
+    await test.step('Change status of first selected registration and write a custom message', async () => {
+      await tableComponent.changeRegistrationStatusByNameWithCustomMessage({
+        registrationName: registrationPV6.fullName,
+        status: 'Decline',
+        message: customMessage,
+        customMessage: false,
+      });
+      await registrations.validateToastMessageAndWait(toastMessage);
+    });
+
+    await test.step('Find and validate custom message', async () => {
+      await registrations.goToRegistrationByName({
+        registrationName: registrationPV6.fullName,
+      });
+      await tableComponent.validatActivityNotPresentByType('Custom message');
     });
   });
 });
