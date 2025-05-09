6d4e99bd29aab83b43b12f36d83c732b189390e6
piotrk39 <piotrk39@gmail.com>
2025-05-09 10:58:01 +0200

    Add tests for templated messages

--- e2e/portal/tests/ChangeRegistrationStatus/ChangeStatusWithTemplatedMessageCombination.spec.ts
+++ e2e/portal/tests/ChangeRegistrationStatus/ChangeStatusWithTemplatedMessageCombination.spec.ts
@@ -14,9 +14,18 @@ import LoginPage from '@121-e2e/portal/pages/LoginPage';
 import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
 
 const toastMessage =
-  'The status of 1 registration(s) is being changed to "Declined" successfully. The status change can take up to a minute to process.';
-const customMessage =
-  'Test custom message to change the status of registration';
+  'The status of 1 registration(s) is being changed to "Included" successfully. The status change can take up to a minute to process.';
+const templateMessage = `Dit is een bericht van het Rode Kruis.
+
+Bedankt voor je inschrijving. Je ontvangt vanaf nu elke dinsdag een Albert Heijn waardebon via WhatsApp. Je ontvangt de waardebonnen zo lang je op de lijst staat van .
+
+Het Rode Kruis kan je ook informatie geven over bijvoorbeeld medische hulp, voedsel of veiligheid. Kijk op onze website:
+
+https://helpfulinformation.redcross.nl/
+
+of stel je vraag via WhatsApp:
+
+https://wa.me/3197010286964`;
 // Arrange
 const reset = async () => {
   await resetDB(SeedScript.nlrcMultiple);
@@ -33,7 +42,15 @@ const login = async (page: Page, email?: string, password?: string) => {
 
 const navigateToRegistrationsAndResetFilters = async (page: Page) => {
   const registrations = new RegistrationsPage(page);
-  const tableComponent = new TableComponent(page);
+
+  await registrations.navigateToProgramPage('Registrations');
+  // Checkboxes are still selected after navigating back to registrations page
+  // Thus reload of the page let's validateif this is correct behavior
+  await page.reload();
+};
+
+test.describe('Change status of registration with and without templated message', () => {
+  let page: Page;
 
   test.beforeAll(async ({ browser }) => {
     await reset();
@@ -43,17 +60,6 @@ const navigateToRegistrationsAndResetFilters = async (page: Page) => {
       process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
       process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
     );
-  });
-  
-  // Act
-  await test.step('Change status of first selected registration and write a custom message', async () => {
-    await tableComponent.changeRegistrationStatusByNameWithMessage({
-      registrationName: registrationPV5.fullName,
-      status: 'Decline',
-      customMessage: true,
-      message: customMessage,
-    });
-    await registrations.validateToastMessageAndClose(toastMessage);
   });
 
   test.afterEach(async () => {
@@ -65,49 +71,46 @@ const navigateToRegistrationsAndResetFilters = async (page: Page) => {
   });
 
   // Act
-  test('[35840] Change status of registration with custom message', async () => {
+  test('[35912] Change status of registration with templated message', async () => {
     const registrations = new RegistrationsPage(page);
     const tableComponent = new TableComponent(page);
-    await test.step('Change status of first selected registration and write a custom message', async () => {
+    await test.step('Change status of first selected registration and send templated message', async () => {
       await tableComponent.changeRegistrationStatusByNameWithMessage({
         registrationName: registrationPV5.fullName,
-        status: 'Decline',
-        message: customMessage,
-        customMessage: true,
+        status: 'Include',
+        templateMessage: true,
       });
       await registrations.validateToastMessageAndClose(toastMessage);
     });
 
-    await test.step('Find and validate custom message', async () => {
+    await test.step('Find and validate templated message', async () => {
       await registrations.goToRegistrationByName({
         registrationName: registrationPV5.fullName,
       });
       await tableComponent.validateMessageActivityByTypeAndText({
-        notificationType: 'Custom message',
-        message: customMessage,
+        notificationType: 'Inclusion',
+        message: templateMessage,
       });
     });
   });
 
-  test('[35849] Change status of registration without custom message', async () => {
+  test('[35913] Change status of registration without templated message', async () => {
     const registrations = new RegistrationsPage(page);
     const tableComponent = new TableComponent(page);
     // Act
-    await test.step('Change status of first selected registration and write a custom message', async () => {
+    await test.step('Change status of first selected registration and write a templated message', async () => {
       await tableComponent.changeRegistrationStatusByNameWithMessage({
         registrationName: registrationPV6.fullName,
-        status: 'Decline',
-        message: customMessage,
-        customMessage: false,
+        status: 'Include',
       });
       await registrations.validateToastMessageAndClose(toastMessage);
     });
 
-    await test.step('Find and validate custom message not present', async () => {
+    await test.step('Find and validate templated message not present', async () => {
       await registrations.goToRegistrationByName({
         registrationName: registrationPV6.fullName,
       });
-      await tableComponent.validateActivityNotPresentByType('Custom message');
+      await tableComponent.validateActivityNotPresentByType('Inclusion');
     });
   });
 });
