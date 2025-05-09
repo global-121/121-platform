de04e129ba77614c0aafa1cd4924c76faaa88934
Niels Bom <nbom@redcross.nl>
2025-05-09 12:44:32 +0200

    feat: Airtel mock service disbursement v3 (not complete)
    Not complete because their endpoint is giving errors.

--- services/mock-service/src/fsp-integration/airtel/airtel.dto.ts
+++ services/mock-service/src/fsp-integration/airtel/airtel.dto.ts
@@ -21,6 +21,7 @@ export class AirtelAuthenticateResponseBodyFailDto {
   public error: string;
 }
 
+// Disbursement V1
 class AirtelDisbursementV1PayloadPayeeDto {
   @ApiProperty({ example: '123456789' })
   public msisdn: string;
@@ -59,4 +60,52 @@ class AirtelDisbursementV1ResponseSuccessBodyDataDto {
 export class AirtelDisbursementV1ResponseSuccessBodyDto {
   @ApiProperty()
   public body: AirtelDisbursementV1ResponseSuccessBodyDataDto;
+}
+
+// Disbursement V3
+class AirtelDisbursementV3PayloadPayeeDto {
+  @ApiProperty({ example: '123456789' })
+  public msisdn: string;
+  @ApiProperty({ example: 'NORMAL' })
+  public wallet_type: string;
+}
+
+class AirtelDisbursementV3PayloadTransactionDto {
+  @ApiProperty({ example: 0.1 })
+  public amount: number;
+  @ApiProperty({ example: uuid() })
+  public id: string;
+  @ApiProperty({ example: 'B2C' })
+  public type: string;
+}
+
+export class AirtelDisbursementV3PayloadDto {
+  @ApiProperty()
+  public payee: AirtelDisbursementV3PayloadPayeeDto;
+  @ApiProperty({
+    example: uuid(),
+    type: 'string',
+  })
+  @ApiProperty({ example: uuid() })
+  public reference: string;
+  @ApiProperty({ example: 'not-really-encrypted' })
+  public pin: string;
+  @ApiProperty()
+  public transaction: AirtelDisbursementV3PayloadTransactionDto;
+}
+
+class AirtelDisbursementV3ResponseSuccessBodyDataTransactionDto {
+  //
+  @ApiProperty({ example: 'CI250506.1824.H00006' })
+  public reference_id: string;
+}
+
+class AirtelDisbursementV3ResponseSuccessBodyDataDto {
+  @ApiProperty()
+  public payee: AirtelDisbursementV3ResponseSuccessBodyDataTransactionDto;
+}
+
+export class AirtelDisbursementV3ResponseSuccessBodyDto {
+  @ApiProperty()
+  public body: AirtelDisbursementV3ResponseSuccessBodyDataDto;
 }
