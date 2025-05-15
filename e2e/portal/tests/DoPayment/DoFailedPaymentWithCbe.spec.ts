7ec322cb8108e5c7ae5ada6eef46dd5bab3d9e48
piotrk39 <piotrk39@gmail.com>
2025-05-15 11:08:56 +0200

    Add failed payment test for cbe

--- e2e/portal/tests/DoPayment/DoFailedPaymentWithCbe.spec.ts
+++ e2e/portal/tests/DoPayment/DoFailedPaymentWithCbe.spec.ts
@@ -2,30 +2,26 @@ import { test } from '@playwright/test';
 import { format } from 'date-fns';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import KRCSProgram from '@121-service/src/seed-data/program/program-safaricom.json';
+import CbeProgram from '@121-service/src/seed-data/program/program-cbe.json';
 import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
 import {
   getAccessToken,
   resetDB,
 } from '@121-service/test/helpers/utility.helper';
 import {
-  programIdSafaricom,
-  registrationsSafaricom,
+  programIdCbe,
+  registrationsCbe,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
 import LoginPage from '@121-e2e/portal/pages/LoginPage';
 import PaymentsPage from '@121-e2e/portal/pages/PaymentsPage';
 
 test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.safaricomProgram);
+  await resetDB(SeedScript.cbeProgram);
+  // Full name is set to 'error' to create a failed payment
+  registrationsCbe[0].fullName = 'error';
   const accessToken = await getAccessToken();
-  // Phone number is set to 254000000000 to create a failed payment
-  registrationsSafaricom[0].phoneNumber = '254000000000';
-  await seedIncludedRegistrations(
-    registrationsSafaricom,
-    programIdSafaricom,
-    accessToken,
-  );
+  await seedIncludedRegistrations(registrationsCbe, programIdCbe, accessToken);
 
   // Login
   const loginPage = new LoginPage(page);
@@ -36,17 +32,14 @@ test.beforeEach(async ({ page }) => {
   );
 });
 
-test('[36096] Do failed payment for Safaricom fsp', async ({ page }) => {
+test('[36101] Do failed payment for Cbe fsp', async ({ page }) => {
   const paymentsPage = new PaymentsPage(page);
-  const projectTitle = KRCSProgram.titlePortal.en;
-  const numberOfPas = registrationsSafaricom.length;
-  const defaultTransferValue = KRCSProgram.fixedTransferValue;
-  const defaultMaxTransferValue = registrationsSafaricom.reduce(
-    (output, pa) => {
-      return output + pa.paymentAmountMultiplier * defaultTransferValue;
-    },
-    0,
-  );
+  const projectTitle = CbeProgram.titlePortal.en;
+  const numberOfPas = registrationsCbe.length;
+  const defaultTransferValue = CbeProgram.fixedTransferValue;
+  const defaultMaxTransferValue = registrationsCbe.reduce((output, pa) => {
+    return output + pa.paymentAmountMultiplier * defaultTransferValue;
+  }, 0);
   const lastPaymentDate = `${format(new Date(), 'dd/MM/yyyy')}`;
 
   await test.step('Navigate to Program payments', async () => {
@@ -60,9 +53,7 @@ test('[36096] Do failed payment for Safaricom fsp', async ({ page }) => {
     await paymentsPage.startPayment();
     // Assert redirection to payment overview page
     await page.waitForURL((url) =>
-      url.pathname.startsWith(
-        `/en-GB/project/${programIdSafaricom}/payments/1`,
-      ),
+      url.pathname.startsWith(`/en-GB/project/${programIdCbe}/payments/1`),
     );
     // Assert payment overview page by payment date/ title
     await paymentsPage.validatePaymentsDetailsPageByDate(lastPaymentDate);
@@ -79,7 +70,7 @@ test('[36096] Do failed payment for Safaricom fsp', async ({ page }) => {
       registrationsNumber: numberOfPas,
       successfulTransfers: 0,
       failedTransfers: numberOfPas,
-      currency: KRCSProgram.currency,
+      currency: CbeProgram.currency,
     });
     // DO NOT MAKE IT A RULE!!!
     // Only in this case we need to reload the page to get the updated data of the successful payments.
@@ -91,7 +82,7 @@ test('[36096] Do failed payment for Safaricom fsp', async ({ page }) => {
       registrationsNumber: numberOfPas,
       successfulTransfers: 0,
       failedTransfers: numberOfPas,
-      currency: KRCSProgram.currency,
+      currency: CbeProgram.currency,
     });
   });
 });
