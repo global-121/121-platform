c9df1b0f60da96634ec0a596ca0aacd38e5b128b
Niels Bom <nbom@redcross.nl>
2025-05-19 13:51:40 +0200

    feat: Airtel FSP - add Airtel to queues registry

--- services/121-service/src/queues-registry/queues-registry.module.ts
+++ services/121-service/src/queues-registry/queues-registry.module.ts
@@ -49,6 +49,19 @@ import { AzureLogService } from '@121-service/src/shared/services/azure-log.serv
       },
     }),
     BullModule.registerQueue({
+      name: TransactionJobQueueNames.airtel,
+      processors: [
+        {
+          path: 'src/transaction-job-processors/processors/transaction-job-airtel.processor.ts',
+        },
+      ],
+      // ## TODO: consider whether this is right.
+      limiter: {
+        max: 20, // Max number of jobs processed
+        duration: 1000, // per duration in milliseconds
+      },
+    }),
+    BullModule.registerQueue({
       name: TransactionJobQueueNames.commercialBankEthiopia,
       processors: [
         {
