3e8376b40a46f108da6ed0623f73598e812655db
arsforza <asforza@redcross.nl>
2025-05-19 08:04:49 +0200

    add tests for permissions

--- e2e/portal/tests/ViewPayments/ExportPayments.spec.ts
+++ e2e/portal/tests/ViewPayments/ExportPayments.spec.ts
@@ -1,4 +1,5 @@
-import { test } from '@playwright/test';
+import { Page, test } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
@@ -11,6 +12,39 @@ import { registrationsOCW } from '@121-service/test/registrations/pagination/pag
 import LoginPage from '@121-e2e/portal/pages/LoginPage';
 import PaymentsPage from '@121-e2e/portal/pages/PaymentsPage';
 
+import HomePage from '../../pages/HomePage';
+
+const login = async ({
+  page,
+  email,
+  password,
+}: {
+  page: Page;
+  email: string | undefined;
+  password: string | undefined;
+}) => {
+  const loginPage = new LoginPage(page);
+  await page.goto(`/`);
+  await loginPage.login(email ?? '', password);
+};
+
+const navigateToPaymentsPage = async (paymentsPage: PaymentsPage) => {
+  const projectTitle = 'NLRC OCW Program';
+  await test.step('Navigate to Program payments', async () => {
+    await paymentsPage.selectProgram(projectTitle);
+    await paymentsPage.navigateToProgramPage('Payments');
+  });
+};
+
+const createFivePayments = async (paymentsPage: PaymentsPage) => {
+  for (let i = 0; i < 5; i++) {
+    await paymentsPage.createPayment();
+    await paymentsPage.startPayment();
+    await paymentsPage.navigateToProgramPage('Payments');
+    await paymentsPage.dismissToast();
+  }
+};
+
 test.beforeEach(async ({ page }) => {
   await resetDB(SeedScript.nlrcMultiple);
   const programIdOCW = 3;
@@ -19,32 +53,21 @@ test.beforeEach(async ({ page }) => {
   const accessToken = await getAccessToken();
   await seedIncludedRegistrations(registrationsOCW, OcwProgramId, accessToken);
 
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login(
-    process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
-    process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
-  );
+  await login({
+    page,
+    email: process.env.USERCONFIG_121_SERVICE_EMAIL_ADMIN,
+    password: process.env.USERCONFIG_121_SERVICE_PASSWORD_ADMIN,
+  });
+
+  const paymentsPage = new PaymentsPage(page);
+
+  await navigateToPaymentsPage(paymentsPage);
+
+  await createFivePayments(paymentsPage);
 });
 
 test('[35621] ExportFivePayments', async ({ page }) => {
   const paymentsPage = new PaymentsPage(page);
-
-  const projectTitle = 'NLRC OCW Program';
-  await test.step('Navigate to Program payments', async () => {
-    await paymentsPage.selectProgram(projectTitle);
-    await paymentsPage.navigateToProgramPage('Payments');
-  });
-
-  await test.step('Create 5 payments', async () => {
-    for (let i = 0; i < 5; i++) {
-      await paymentsPage.createPayment();
-      await paymentsPage.startPayment();
-      await paymentsPage.navigateToProgramPage('Payments');
-      await paymentsPage.dismissToast();
-    }
-  });
 
   await test.step('Validate export payment button', async () => {
     await paymentsPage.exportButton.waitFor({ state: 'visible' });
@@ -59,5 +82,41 @@ test('[35621] ExportFivePayments', async ({ page }) => {
       expectedRowCount: 25,
       excludedColumns: ['timestamp'],
     });
+  });
+});
+
+test('[36125] View available actions for admin', async ({ page }) => {
+  const paymentsPage = new PaymentsPage(page);
+
+  await test.step('Validate export options', async () => {
+    await paymentsPage.exportButton.waitFor({ state: 'visible' });
+
+    await paymentsPage.exportButton.click();
+
+    const exportOptions = await page.getByRole('menuitem').all();
+    await expect(exportOptions.length).toBe(3);
+  });
+});
+
+test('[36126] View available actions for a "view only" user', async ({
+  page,
+}) => {
+  const homePage = new HomePage(page);
+  await homePage.selectAccountOption('Logout');
+
+  await login({
+    page,
+    email: process.env.USERCONFIG_121_SERVICE_EMAIL_USER_VIEW,
+    password: process.env.USERCONFIG_121_SERVICE_PASSWORD_USER_VIEW,
+  });
+
+  const paymentsPage = new PaymentsPage(page);
+
+  await navigateToPaymentsPage(paymentsPage);
+
+  await test.step('Validate hidden buttons', async () => {
+    await paymentsPage.exportButton.waitFor({ state: 'hidden' });
+
+    await paymentsPage.createPaymentButton.waitFor({ state: 'hidden' });
   });
 });
