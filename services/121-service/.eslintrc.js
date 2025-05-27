244fd8bb6aedbc2396ffd9dbbb080f77d155385f
Elwin Schmitz <eschmitz@redcross.nl>
2025-05-27 16:11:40 +0200

    WIP: no-commit-lint-rule just yet

--- services/121-service/.eslintrc.js
+++ services/121-service/.eslintrc.js
@@ -62,7 +62,6 @@ module.exports = {
         'n/no-extraneous-import': [0], // Managed by TypeScript
         'n/no-missing-import': [0, { ignoreTypeImport: true }], // Disabled to allow for path-aliases via tsconfig.json/below
         'n/no-missing-require': [0], // Disabled to allow for path-aliases via tsconfig.json/below
-        'n/no-process-env': 'error',
         'no-relative-import-paths/no-relative-import-paths': [
           'warn',
           {
