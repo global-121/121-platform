08a16b84dfefc49f3be1949e99e4b68079e38f08
jannisvisser <jannisvisser@redcross.nl>
2025-06-18 16:42:26 +0200

    fix: revert callback dto change AB#36710

--- services/121-service/src/payments/reconciliation/onafriq-reconciliation/onafriq-reconciliation.controller.ts
+++ services/121-service/src/payments/reconciliation/onafriq-reconciliation/onafriq-reconciliation.controller.ts
@@ -2,12 +2,11 @@ import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SkipThrottle } from '@nestjs/throttler';
 
-import { OnafriqTransactionCallbackDto as OnafriqTransactionCallbackDto } from '@121-service/src/payments/reconciliation/onafriq-reconciliation/dtos/onafriq-transaction-callback.dto';
+import { OnafriqTransactionCallbackDto } from '@121-service/src/payments/reconciliation/onafriq-reconciliation/dtos/onafriq-transaction-callback.dto';
 import { OnafriqReconciliationService } from '@121-service/src/payments/reconciliation/onafriq-reconciliation/onafriq-reconciliation.service';
 
 @ApiTags('financial-service-providers/onafriq')
 @Controller('financial-service-providers/onafriq')
-@Controller()
 export class OnafriqReconciliationController {
   public constructor(
     private onafriqReconciliationService: OnafriqReconciliationService,
