98ba80824c13da2be315bd32672dd3b70bdc6663
jannisvisser <jannisvisser@redcross.nl>
2025-06-18 16:42:26 +0200

    fix: revert callback dto change AB#36710

--- services/121-service/src/payments/reconciliation/onafriq-reconciliation/dtos/onafriq-transaction-callback.dto.ts
+++ services/121-service/src/payments/reconciliation/onafriq-reconciliation/dtos/onafriq-transaction-callback.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 class OnafriqTransactionCallbackStatus {
   @ApiProperty()
+  @IsNotEmpty() // REFACTOR: Ideally we don't want to block callbacks, but without these decorators, the callback is actually blocked. Solve differently.
   readonly code: string;
   @ApiProperty()
+  @IsNotEmpty()
   readonly message: string;
 }
 
 export class OnafriqTransactionCallbackDto {
   @ApiProperty()
+  @IsNotEmpty()
   readonly thirdPartyTransId: string;
   @ApiProperty()
+  @IsNotEmpty()
   readonly status: OnafriqTransactionCallbackStatus;
 }
