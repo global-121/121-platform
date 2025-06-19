6e6224e11c462a8b420c5169e01160ab9b5ee280
Elwin Schmitz <eschmitz@redcross.nl>
2025-06-19 13:44:21 +0200

    test: Enable lint-rule to prevent using process.env ever again

--- services/121-service/.eslintrc.js
+++ services/121-service/.eslintrc.js
@@ -64,6 +64,7 @@ module.exports = {
         'prettier/prettier': ['error', { endOfLine: 'auto' }],
         'n/no-extraneous-import': [0], // Managed by TypeScript
         'n/no-missing-import': [0, { ignoreTypeImport: true }], // Disabled to allow for path-aliases via tsconfig.json/below
+        'n/no-process-env': 'error',
         'no-relative-import-paths/no-relative-import-paths': [
           'warn',
           {
