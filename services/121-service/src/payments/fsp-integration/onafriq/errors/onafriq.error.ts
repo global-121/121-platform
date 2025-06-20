b71cb611cac65d8884044cc499ef91e7d4990c36
jannisvisser <jannisvisser@redcross.nl>
2025-06-20 09:13:58 +0200

    fix: process PR comments overall + API tests AB#36752

--- services/121-service/src/payments/fsp-integration/onafriq/errors/onafriq.error.ts
+++ services/121-service/src/payments/fsp-integration/onafriq/errors/onafriq.error.ts
@@ -1,15 +1,11 @@
 import { OnafriqApiResponseStatusType } from '@121-service/src/payments/fsp-integration/onafriq/enum/onafriq-api-response-status-type.enum';
+type TypeGenericOrDuplicateTransId =
+  | OnafriqApiResponseStatusType.genericError
+  | OnafriqApiResponseStatusType.duplicateThirdPartyTransIdError;
 
 export class OnafriqError extends Error {
-  type:
-    | OnafriqApiResponseStatusType.genericError
-    | OnafriqApiResponseStatusType.duplicateThirdPartyTransIdError;
-  constructor(
-    message: string,
-    type:
-      | OnafriqApiResponseStatusType.genericError
-      | OnafriqApiResponseStatusType.duplicateThirdPartyTransIdError,
-  ) {
+  type: TypeGenericOrDuplicateTransId;
+  constructor(message: string, type: TypeGenericOrDuplicateTransId) {
     super(message);
     this.name = 'OnafriqApiError';
     this.type = type;
