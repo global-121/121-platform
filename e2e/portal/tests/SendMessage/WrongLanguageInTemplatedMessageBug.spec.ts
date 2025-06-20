ea9da2af73c400f2a1977ff6ffca565ad6ece816
piotrk39 <piotrk39@gmail.com>
2025-06-20 10:39:38 +0200

    Add a base test

--- e2e/portal/tests/SendMessage/WrongLanguageInTemplatedMessageBug.spec.ts
+++ e2e/portal/tests/SendMessage/WrongLanguageInTemplatedMessageBug.spec.ts
@@ -2,10 +2,8 @@ import { test } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
+import { resetDB } from '@121-service/test/helpers/utility.helper';
+import { getAccessToken } from '@121-service/test/helpers/utility.helper';
 import { registrationsPV } from '@121-service/test/registrations/pagination/pagination-data';
 
 import BasePage from '@121-e2e/portal/pages/BasePage';
@@ -37,7 +35,9 @@ test.beforeEach(async ({ page }) => {
   );
 });
 
-test('[31076] Send templated message', async ({ page }) => {
+test('[36775] Bug: Only English was enabled in templated messages', async ({
+  page,
+}) => {
   const basePage = new BasePage(page);
   const registrations = new RegistrationsPage(page);
   const activityLog = new RegistrationActivityLogPage(page);
