27f5610c2878eb1e3b2c278eff592f82e1fdff23
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-06-25 10:18:03 +0200

    Update sequence of programFspConfig tables (#6965)
    Co-authored-by: Ruben <vandervalk@geoit.nl>

---
+++ services/121-service/src/migration/1750777175366-program-fsp-config-missing-sequence.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class ProgramFspConfigMissingSequence1750777175366
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `select
+        setval(
+        '"121-service".program_fsp_configuration_property_id_seq',
+        (
+        select
+          MAX(id) + 1
+        from
+          "121-service".program_fsp_configuration_property pfcp )
+      );
+      `,
+    );
+    await queryRunner.query(
+      `select
+        setval(
+        '"121-service".program_fsp_configuration_id_seq',
+        (
+        select
+          MAX(id) + 1
+        from
+          "121-service".program_fsp_configuration pfc )
+      );`,
+    );
+  }
+
+  public async down(_queryRunner: QueryRunner): Promise<void> {
+    console.log('We only move forward, there is no going back!');
+  }
+}
