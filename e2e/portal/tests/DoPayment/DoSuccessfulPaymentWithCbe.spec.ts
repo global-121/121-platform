a92dde1c8eb7c0d792be934aece1ecb490db938c
Piotr Krajewski <52535860+Piotrk39@users.noreply.github.com>
2025-06-30 09:59:22 +0200

    Add E2E test for: "Export debit card usage" (#6967)
    * Update test to handle snapshot validation with excluded 'issueDate' column
    
    * Add test with updated reporter
    
    * turn off reporting
    
    * Extend timeout for payment with CBE fsp
    
    * Change tests to use ExportData component and update tests
    
    * Update snapshot and import component
    
    * Update tests based on PR review
    
    * Update e2e/portal/tests/ViewPayments/ExportDebitCardUsageUnsuccessfully.spec.ts
    
    Co-authored-by: RubenGeo <34537157+RubenGeo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: RubenGeo <34537157+RubenGeo@users.noreply.github.com>

--- e2e/portal/tests/DoPayment/DoSuccessfulPaymentWithCbe.spec.ts
+++ e2e/portal/tests/DoPayment/DoSuccessfulPaymentWithCbe.spec.ts
@@ -58,7 +58,7 @@ test('[36081] Do successful payment for Cbe fsp', async ({ page }) => {
       programId: programIdCbe,
       paymentReferenceIds: registrationsCbe.map((r) => r.referenceId),
       accessToken,
-      maxWaitTimeMs: 30_000,
+      maxWaitTimeMs: 40_000,
       completeStatusses: [TransactionStatusEnum.success],
     });
     // Assert redirection to payment overview page
