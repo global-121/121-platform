e53d630f8e297c8aa83f5d39c6dd68e3e8042a23
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-07-01 15:09:38 +0200

    Await status change, improve test flakiness (#6998)
    * Await status change, improve test flakiness
    
    * waitForStatusChangeToComplete return early on error
    
    ---------
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/test/helpers/registration.helper.ts
+++ services/121-service/test/helpers/registration.helper.ts
@@ -1,3 +1,4 @@
+import { HttpStatus } from '@nestjs/common';
 import { isMatch } from 'lodash';
 import * as request from 'supertest';
 
@@ -270,7 +271,7 @@ export function getRegistrations({
     .send();
 }
 
-export async function changeRegistrationStatus({
+async function changeRegistrationStatus({
   programId,
   referenceIds,
   status,
@@ -341,6 +342,10 @@ export async function awaitChangeRegistrationStatus({
     accessToken,
     options,
   });
+  // If the changeRegistrationStatus throws an error, it means that the status change is not allowed/succesful so we don't need to wait for it
+  if (result.status !== HttpStatus.ACCEPTED) {
+    return result;
+  }
 
   await waitForStatusChangeToComplete(
     programId,
