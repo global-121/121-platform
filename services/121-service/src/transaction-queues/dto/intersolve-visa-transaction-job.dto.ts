5f199f058015e8e71cc118aa5b76c0c2d5597fa3
Niels Bom <nbom@redcross.nl>
2025-07-01 17:18:41 +0200

    refactor - add more props to SharedTransactionJobDto

--- services/121-service/src/transaction-queues/dto/intersolve-visa-transaction-job.dto.ts
+++ services/121-service/src/transaction-queues/dto/intersolve-visa-transaction-job.dto.ts
@@ -2,14 +2,11 @@ import { SharedTransactionJobDto } from '@121-service/src/transaction-queues/dto
 
 export interface IntersolveVisaTransactionJobDto
   extends SharedTransactionJobDto {
-  readonly referenceId: string;
   readonly transactionAmountInMajorUnit: number; // This is in the major unit of the currency, for example whole euros
-  readonly bulkSize: number;
   readonly name: string;
   readonly addressStreet: string;
   readonly addressHouseNumber: string;
   readonly addressHouseNumberAddition: string;
   readonly addressPostalCode?: string;
   readonly addressCity: string;
-  readonly phoneNumber: string;
 }
--- services/121-service/src/transaction-queues/dto/onafriq-transaction-job.dto.ts
+++ services/121-service/src/transaction-queues/dto/onafriq-transaction-job.dto.ts
@@ -1,10 +1,7 @@
 import { SharedTransactionJobDto } from '@121-service/src/transaction-queues/dto/shared-transaction-job.dto';
 
 export interface OnafriqTransactionJobDto extends SharedTransactionJobDto {
-  readonly referenceId: string;
   readonly transactionAmount: number;
-  readonly bulkSize: number;
-  readonly phoneNumber: string;
   readonly firstName: string;
   readonly lastName: string;
 }
