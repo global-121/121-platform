dcf096250cc960b8f51bc0533fe8c76f42112d4c
Elwin Schmitz <eschmitz@redcross.nl>
2025-07-02 09:12:06 +0200

    test: Remove unnecessary/ineffective tests for 'manual' actions only

--- services/121-service/src/payments/fsp-integration/onafriq/services/onafriq.api.service.spec.ts
+++ services/121-service/src/payments/fsp-integration/onafriq/services/onafriq.api.service.spec.ts
@@ -88,41 +88,4 @@ describe('OnafriqApiService', () => {
       expect(result.status).toBe(OnafriqApiResponseStatusType.success);
     });
   });
-
-  describe('subscribeWebhook', () => {
-    it('should call post twice and return the second response on success', async () => {
-      const firstResponse = {
-        status: HttpStatus.OK,
-        statusText: 'OK',
-        data: { message: 'Success' },
-      };
-      const secondResponse = {
-        status: HttpStatus.OK,
-        statusText: 'OK',
-        data: { message: 'Success' },
-      };
-      (customHttpService.post as jest.Mock)
-        .mockResolvedValueOnce(firstResponse)
-        .mockResolvedValueOnce(secondResponse);
-
-      const result = await onafriqApiService.subscribeWebhook();
-      expect(customHttpService.post).toHaveBeenCalledTimes(2);
-      expect(result).toEqual(secondResponse);
-    });
-
-    it('should return after first post if not 200 or message not Success', async () => {
-      const firstResponse = {
-        status: HttpStatus.BAD_REQUEST,
-        statusText: 'Bad Request',
-        data: { message: 'Error' },
-      };
-      (customHttpService.post as jest.Mock).mockResolvedValueOnce(
-        firstResponse,
-      );
-
-      const result = await onafriqApiService.subscribeWebhook();
-      expect(customHttpService.post).toHaveBeenCalledTimes(1);
-      expect(result).toEqual(firstResponse);
-    });
-  });
 });
