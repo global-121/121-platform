021e1658d4d6f3e5073da761bf5f450a36137774
Niels Bom <nbom@redcross.nl>
2025-07-07 11:05:32 +0200

    refactor - remove unused test helpers

--- services/121-service/src/utils/unit-test.helpers.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Get the name of a Queue to be mocked
- *
- * @param name Of the queue to be mocked
- * @returns Queue name to be used for mocking
- */
-export function getQueueName(name: string | undefined): string {
-  return `BullQueue_${name}`;
-}
