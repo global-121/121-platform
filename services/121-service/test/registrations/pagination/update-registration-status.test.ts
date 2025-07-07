d36e275a90ab711ec9500906e957c45141c69be0
jannisvisser <jannisvisser@redcross.nl>
2025-07-07 11:09:33 +0200

    fix: return error if changeRegistrationStatus fails

--- services/121-service/test/registrations/pagination/update-registration-status.test.ts
+++ services/121-service/test/registrations/pagination/update-registration-status.test.ts
@@ -4,6 +4,7 @@ import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import { waitForStatusUpdateToComplete } from '@121-service/test/helpers/program.helper';
 import {
   awaitChangeRegistrationStatus,
+  changeRegistrationStatus,
   getEvents,
   getRegistrations,
   importRegistrations,
@@ -169,7 +170,7 @@ describe('change the status of a set of registrations', () => {
 
       for (const status of statusesThatRequireReason) {
         // Act
-        const updateStatusResponse = await awaitChangeRegistrationStatus({
+        const updateStatusResponse = await changeRegistrationStatus({
           programId: programIdOCW,
           referenceIds,
           status,
