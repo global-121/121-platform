c299dc88d315d31f203e0bbc5ac90944c2ec186d
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-07-15 16:47:31 +0200

    Simplify block visa using sms instead of whatsapp (#7049)
    * Simplify block visa using sms instead of whatsapp
    
    * Remove duplicate comment
    
    ---------
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/test/visa-card/block-visa-card.test.ts
+++ services/121-service/test/visa-card/block-visa-card.test.ts
@@ -20,6 +20,8 @@ import {
   resetDB,
 } from '@121-service/test/helpers/utility.helper';
 
+const noWhatsappSetup = null; // Do not use WhatsApp for this test to avoid race conditions - our mock server replies 'yes' on pending whatsapp message a bit too fast
+
 describe('(Un)Block visa debit card', () => {
   let accessToken: string;
 
@@ -33,9 +35,9 @@ describe('(Un)Block visa debit card', () => {
     const testRegistration = {
       ...registrationVisa,
       referenceId: 'test-registration-visa--block-card',
+      whatsappPhoneNumber: noWhatsappSetup,
     };
     await seedPaidRegistrations([testRegistration], programIdVisa);
-
     const visaWalletResponseBefore = await getVisaWalletsAndDetails(
       programIdVisa,
       testRegistration.referenceId,
@@ -56,7 +58,7 @@ describe('(Un)Block visa debit card', () => {
       programId: programIdVisa,
       referenceIds: [testRegistration.referenceId],
       accessToken,
-      minimumNumberOfMessagesPerReferenceId: 4,
+      minimumNumberOfMessagesPerReferenceId: 2,
     });
 
     const visaWalletResponseAfter = await getVisaWalletsAndDetails(
@@ -89,6 +91,7 @@ describe('(Un)Block visa debit card', () => {
     const testRegistration = {
       ...registrationVisa,
       referenceId: 'test-registration-visa--unblock-card',
+      whatsappPhoneNumber: noWhatsappSetup,
     };
     await seedPaidRegistrations([testRegistration], programIdVisa);
 
@@ -106,14 +109,6 @@ describe('(Un)Block visa debit card', () => {
       testRegistration.referenceId,
     );
 
-    // wait for "block card" message to be processed before proceeding - to avoid race conditions
-    await waitForMessagesToComplete({
-      programId: programIdVisa,
-      referenceIds: [testRegistration.referenceId],
-      accessToken,
-      minimumNumberOfMessagesPerReferenceId: 4,
-    });
-
     // Act
     const unblockVisaResponse = await unblockVisaCard(
       programIdVisa,
@@ -127,7 +122,7 @@ describe('(Un)Block visa debit card', () => {
       programId: programIdVisa,
       referenceIds: [testRegistration.referenceId],
       accessToken,
-      minimumNumberOfMessagesPerReferenceId: 6,
+      minimumNumberOfMessagesPerReferenceId: 3,
     });
 
     const visaWalletResponseAfter = await getVisaWalletsAndDetails(
