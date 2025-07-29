692b90b6b996f8dc2b68b22ad592cc5dfd430b04
tijsziere <39266480+tijsziere@users.noreply.github.com>
2025-07-29 09:05:50 +0200

    update test & exports in jsons

--- services/121-service/test/registrations/update-registration.test.ts
+++ services/121-service/test/registrations/update-registration.test.ts
@@ -216,7 +216,7 @@ describe('Update attribute of PA', () => {
   it('should update scope within current users scope', async () => {
     // Arrange
     accessToken = await setupNlrcEnvironment();
-    const newScope = 'utrecht.houten';
+    const newScope = 'turkana.north';
     const reason = 'automated test';
     const updateDto = {
       scope: newScope,
