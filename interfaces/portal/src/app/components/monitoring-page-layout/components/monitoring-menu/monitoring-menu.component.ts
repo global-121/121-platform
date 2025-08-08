1b5d72a4550a1b9d289d1e9abd869a92ad68a884
Domenico Gemoli <domenicogemoli@gmail.com>
2025-08-08 11:21:13 +0200

    feat: project files

--- interfaces/portal/src/app/components/monitoring-page-layout/components/monitoring-menu/monitoring-menu.component.ts
+++ interfaces/portal/src/app/components/monitoring-page-layout/components/monitoring-menu/monitoring-menu.component.ts
@@ -2,14 +2,18 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  inject,
   input,
 } from '@angular/core';
 
 import { MenuItem } from 'primeng/api';
 import { TabsModule } from 'primeng/tabs';
 
+import { PermissionEnum } from '@121-service/src/user/enum/permission.enum';
+
 import { AppRoutes } from '~/app.routes';
 import { TabsMenuComponent } from '~/components/tabs-menu/tabs-menu.component';
+import { AuthService } from '~/services/auth.service';
 
 @Component({
   selector: 'app-monitoring-menu',
@@ -20,11 +24,22 @@ import { TabsMenuComponent } from '~/components/tabs-menu/tabs-menu.component';
 export class MonitoringMenuComponent {
   readonly projectId = input.required<string>();
 
+  readonly authService = inject(AuthService);
+
   readonly navMenuItems = computed<MenuItem[]>(() => [
     {
       label: $localize`:@@page-title-project-monitoring-powerbi:PowerBI`,
       routerLink: `/${AppRoutes.project}/${this.projectId().toString()}/${AppRoutes.projectMonitoring}/${AppRoutes.projectMonitoringPowerBI}`,
       icon: 'pi pi-chart-line',
+    },
+    {
+      label: $localize`:@@page-title-project-monitoring-files:Files`,
+      routerLink: `/${AppRoutes.project}/${this.projectId().toString()}/${AppRoutes.projectMonitoring}/${AppRoutes.projectMonitoringFiles}`,
+      icon: 'pi pi-file',
+      visible: this.authService.hasPermission({
+        projectId: this.projectId(),
+        requiredPermission: PermissionEnum.ProgramAttachmentsREAD,
+      }),
     },
   ]);
 }
