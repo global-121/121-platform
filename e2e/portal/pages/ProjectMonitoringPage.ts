331699f2a5d9a4439ff0fa449291cffd16785043
Domenico Gemoli <dgemoli@redcross.nl>
2025-08-13 10:38:00 +0200

    feat: upload files on a project level (#7076)
    * feat: project files
    
    * Adds tests for payment attachments upload (#7107)
    
    ---------
    
    Co-authored-by: Piotr Krajewski <52535860+Piotrk39@users.noreply.github.com>

--- e2e/portal/pages/ProjectMonitoringPage.ts
+++ e2e/portal/pages/ProjectMonitoringPage.ts
@@ -1,4 +1,5 @@
 import { expect } from '@playwright/test';
+import { promises as fs } from 'fs';
 import { Locator, Page } from 'playwright';
 
 import BasePage from './BasePage';
@@ -12,6 +13,13 @@ class ProjectMonitoring extends BasePage {
   readonly projectDescriptionTile: Locator;
   readonly metricTileComponent: Locator;
   readonly monitoringIframe: Locator;
+  readonly importFileButton: Locator;
+  readonly uploadFileButton: Locator;
+  readonly formError: Locator;
+  readonly downloadOption: Locator;
+  readonly deleteOption: Locator;
+  readonly deleteConfirmationCheckbox: Locator;
+  readonly deleteFileButton: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -27,6 +35,21 @@ class ProjectMonitoring extends BasePage {
     );
     this.metricTileComponent = this.page.getByTestId('metric-tile-component');
     this.monitoringIframe = this.page.getByTestId('monitoring-iframe');
+    this.importFileButton = this.page.getByRole('button', {
+      name: 'Import file',
+    });
+    this.uploadFileButton = this.page.getByRole('button', {
+      name: 'Upload file',
+    });
+    this.formError = this.page.getByTestId('form-error');
+    this.downloadOption = this.page.getByRole('menuitem', { name: 'Download' });
+    this.deleteOption = this.page.getByRole('menuitem', { name: 'Delete' });
+    this.deleteConfirmationCheckbox = this.page.getByRole('checkbox', {
+      name: 'I understand this action can not be undone.',
+    });
+    this.deleteFileButton = this.page.getByRole('button', {
+      name: 'Delete file',
+    });
   }
 
   async assertMonitoringTabElements({
@@ -71,6 +94,62 @@ class ProjectMonitoring extends BasePage {
       peopleRegistered.toString(),
     );
     await expect(includedTileLocator).toHaveText(peopleIncluded.toString());
+  }
+
+  async selectTab({ tabName }: { tabName: string }) {
+    const tabLocator = this.page.getByRole('tablist').getByText(tabName);
+    await tabLocator.click();
+  }
+
+  async uploadAttachment({
+    filePath,
+    filename,
+  }: {
+    filePath: string;
+    filename: string;
+  }) {
+    await this.page.waitForLoadState('networkidle');
+    await this.uploadFileButton.waitFor({ state: 'visible' });
+    await this.uploadFileButton.click();
+    await this.chooseAndUploadFile(filePath);
+    await this.page
+      .getByPlaceholder('Name the file for easy identification')
+      .fill(filename);
+    await this.importFileButton.click();
+  }
+
+  async validateFormError({ errorText }: { errorText: string }) {
+    await this.page.waitForLoadState('networkidle');
+    await this.formError.waitFor();
+    const errorString = await this.formError.textContent();
+    expect(await this.formError.isVisible()).toBe(true);
+    expect(errorString).toContain(errorText);
+  }
+
+  async downloadAttachmentByName({
+    fileName,
+    snapshotName,
+  }: {
+    fileName: string;
+    snapshotName: string;
+  }) {
+    await this.page
+      .getByRole('row', { name: fileName })
+      .locator('button')
+      .click();
+    const filePath = await this.downloadFile(this.downloadOption.click());
+    const fileBuffer = await fs.readFile(filePath);
+    expect(fileBuffer).toMatchSnapshot(snapshotName);
+  }
+
+  async deleteAttachmentByName({ fileName }: { fileName: string }) {
+    await this.page
+      .getByRole('row', { name: fileName })
+      .locator('button')
+      .click();
+    await this.deleteOption.click();
+    await this.deleteConfirmationCheckbox.click();
+    await this.deleteFileButton.click();
   }
 }
 
---
+++ interfaces/portal/src/app/pages/project-monitoring-files/project-monitoring-files.page.ts
@@ -0,0 +1,208 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  signal,
+  viewChild,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import {
+  injectMutation,
+  injectQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+
+import { PermissionEnum } from '@121-service/src/user/enum/permission.enum';
+
+import { ConfirmationDialogComponent } from '~/components/confirmation-dialog/confirmation-dialog.component';
+import { FormErrorComponent } from '~/components/form-error/form-error.component';
+import { MonitoringPageLayoutComponent } from '~/components/monitoring-page-layout/monitoring-page-layout.component';
+import {
+  QueryTableColumn,
+  QueryTableColumnType,
+  QueryTableComponent,
+} from '~/components/query-table/query-table.component';
+import { ProjectApiService } from '~/domains/project/project.api.service';
+import { PROJECT_ATTACHMENT_FILE_TYPE_LABELS } from '~/domains/project/project.helper';
+import {
+  ProjectAttachment,
+  ProjectAttachmentFileType,
+} from '~/domains/project/project.model';
+import { MonitoringUploadFileDialogComponent } from '~/pages/project-monitoring-files/components/monitoring-upload-file-dialog/monitoring-upload-file-dialog.component';
+import { TableCellFileTypeComponent } from '~/pages/project-monitoring-files/components/table-cell-file-type.component';
+import { AuthService } from '~/services/auth.service';
+import { DownloadService } from '~/services/download.service';
+import { ToastService } from '~/services/toast.service';
+import {
+  generateFieldErrors,
+  genericFieldIsRequiredValidationMessage,
+} from '~/utils/form-validation';
+import { getUniqueUserOptions } from '~/utils/unique-users';
+
+type DeleteFileFormGroup =
+  (typeof ProjectMonitoringFilesPageComponent)['prototype']['deleteFileFormGroup'];
+
+@Component({
+  selector: 'app-monitoring-files',
+  imports: [
+    QueryTableComponent,
+    ButtonModule,
+    ConfirmationDialogComponent,
+    CheckboxModule,
+    FormErrorComponent,
+    ReactiveFormsModule,
+    MonitoringUploadFileDialogComponent,
+    MonitoringPageLayoutComponent,
+  ],
+  templateUrl: './project-monitoring-files.page.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ToastService],
+})
+export class ProjectMonitoringFilesPageComponent {
+  readonly projectId = input.required<string>();
+
+  readonly authService = inject(AuthService);
+  readonly downloadService = inject(DownloadService);
+  readonly queryClient = inject(QueryClient);
+  readonly projectApiService = inject(ProjectApiService);
+  readonly toastService = inject(ToastService);
+
+  projectAttachments = injectQuery(
+    this.projectApiService.getProjectAttachments(this.projectId),
+  );
+
+  readonly selectedFile = signal<null | ProjectAttachment>(null);
+
+  readonly deleteFileConfirmationDialog =
+    viewChild.required<ConfirmationDialogComponent>(
+      'deleteFileConfirmationDialog',
+    );
+
+  deleteFileFormGroup = new FormGroup({
+    confirmAction: new FormControl<boolean>(false, {
+      nonNullable: true,
+      // eslint-disable-next-line @typescript-eslint/unbound-method -- https://github.com/typescript-eslint/typescript-eslint/issues/1929#issuecomment-618695608
+      validators: [Validators.requiredTrue],
+    }),
+  });
+
+  deleteFileFormFieldErrors = generateFieldErrors<DeleteFileFormGroup>(
+    this.deleteFileFormGroup,
+    {
+      confirmAction: genericFieldIsRequiredValidationMessage,
+    },
+  );
+
+  deleteFileMutation = injectMutation(() => ({
+    mutationFn: (attachmentId: number) =>
+      this.projectApiService.removeProjectAttachment({
+        projectId: this.projectId,
+        attachmentId,
+      }),
+    onSuccess: () => {
+      void this.projectApiService.invalidateCache(this.projectId);
+      this.toastService.showToast({
+        detail: $localize`File deleted successfully`,
+      });
+    },
+  }));
+
+  readonly columns = computed<QueryTableColumn<ProjectAttachment>[]>(() => [
+    {
+      field: 'fileType',
+      header: $localize`File type`,
+      component: TableCellFileTypeComponent,
+      type: QueryTableColumnType.MULTISELECT,
+      options: Object.values(ProjectAttachmentFileType).map((type) => ({
+        label:
+          PROJECT_ATTACHMENT_FILE_TYPE_LABELS[
+            type as ProjectAttachmentFileType
+          ],
+        value: type,
+      })),
+    },
+    {
+      field: 'filename',
+      header: $localize`File name`,
+    },
+    {
+      // TODO: AB#30792 TField should also support "leaves" such as "user.name" or "user.address.city"
+      // @ts-expect-error the typing of query-table does not support "leaves" but the functionality does
+      field: 'user.username',
+      header: $localize`Imported by`,
+      type: QueryTableColumnType.MULTISELECT,
+      options: getUniqueUserOptions(this.projectAttachments.data() ?? []),
+    },
+    {
+      field: 'created',
+      header: $localize`Date and time`,
+      type: QueryTableColumnType.DATE,
+    },
+  ]);
+
+  downloadAttachmentMutation = injectMutation(() => ({
+    mutationFn: (file: ProjectAttachment) =>
+      this.projectApiService.downloadProjectAttachment({
+        projectId: this.projectId,
+        attachmentId: file.id,
+      }),
+    onSuccess: (file, { filename }) => {
+      this.downloadService.downloadFile({ file, filename });
+    },
+  }));
+
+  readonly contextMenuItems = computed<MenuItem[]>(() => [
+    {
+      label: $localize`:@@generic-download:Download`,
+      icon: 'pi pi-download',
+      command: () => {
+        const selectedFile = this.selectedFile();
+        if (!selectedFile) {
+          // Should never happen, but keeps TS happy
+          return;
+        }
+
+        this.toastService.showToast({
+          severity: 'info',
+          summary: $localize`Downloading file`,
+          showSpinner: true,
+        });
+
+        this.downloadAttachmentMutation.mutate(selectedFile);
+      },
+    },
+    {
+      label: $localize`:@@generic-delete:Delete`,
+      icon: 'pi pi-trash text-red-500',
+      command: () => {
+        this.deleteFileFormGroup.reset();
+        this.deleteFileConfirmationDialog().askForConfirmation({
+          resetMutation: true,
+        });
+      },
+      visible: this.authService.hasPermission({
+        projectId: this.projectId(),
+        requiredPermission: PermissionEnum.ProgramAttachmentsDELETE,
+      }),
+    },
+  ]);
+
+  readonly canUploadFiles = computed(() =>
+    this.authService.hasPermission({
+      projectId: this.projectId(),
+      requiredPermission: PermissionEnum.ProgramAttachmentsCREATE,
+    }),
+  );
+}
