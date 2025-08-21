cb4033ecb982f8eb4d489800487665b646bb0f13
Jannis Visser <jannisvisser@redcross.nl>
2025-08-21 16:40:17 +0200

    chore: controller only in development

--- services/121-service/src/app.module.ts
+++ services/121-service/src/app.module.ts
@@ -1,5 +1,6 @@
 import { BullModule } from '@nestjs/bull';
 import { Module, OnApplicationBootstrap } from '@nestjs/common';
+import { Type } from '@nestjs/common';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { MulterModule } from '@nestjs/platform-express';
 import { ScheduleModule } from '@nestjs/schedule';
@@ -38,6 +39,16 @@ import { TransactionJobsModule } from '@121-service/src/transaction-jobs/transac
 import { TransactionQueuesModule } from '@121-service/src/transaction-queues/transaction-queues.module';
 import { TypeOrmModule } from '@121-service/src/typeorm.module';
 import { DevelopmentController } from '@121-service/src/utils/test-helpers/test.controller';
+
+const getControllers = (): Type<unknown>[] => {
+  const baseControllers: Type<unknown>[] = [AppController];
+
+  if (env.NODE_ENV === 'development') {
+    baseControllers.push(DevelopmentController);
+  }
+
+  return baseControllers;
+};
 
 @Module({
   // Note: no need to import just any (new) Module in ApplicationModule, when another Module already imports it
@@ -92,7 +103,7 @@ import { DevelopmentController } from '@121-service/src/utils/test-helpers/test.
     SafaricomReconciliationModule,
     OnafriqReconciliationModule,
   ],
-  controllers: [AppController, DevelopmentController],
+  controllers: getControllers(),
   providers: [
     {
       provide: APP_GUARD,
