b3f217a952762177bac6dbfe739e1b1102f18b99
arsforza <asforza@recross.nl>
2025-08-22 20:41:15 +0200

    feat: add data to dashboard

--- interfaces/portal/src/app/domains/metric/metric.model.ts
+++ interfaces/portal/src/app/domains/metric/metric.model.ts
@@ -1,8 +1,18 @@
 import { ProgramStats } from '@121-service/src/metrics/dto/program-stats.dto';
+import { RegistrationStatusStats } from '@121-service/src/metrics/dto/registrationstatus-stats.dto';
+import { RegistrationCountByDate } from '@121-service/src/metrics/dto/registration-count-by-date.dto';
+import {
+  AggregatePerMonth,
+  AggregatePerPayment,
+} from '@121-service/src/metrics/dto/payment-aggregate.dto';
 
 import { Dto } from '~/utils/dto-type';
 
 export type ProjectMetrics = Dto<ProgramStats>;
+export type ProjectRegistrationStatusStats = Dto<RegistrationStatusStats>;
+export type ProjectAggregatePerPayment = Dto<AggregatePerPayment>;
+export type ProjectAggregatePerMonth = Dto<AggregatePerMonth>;
+export type ProjectRegistrationCountByDate = Dto<RegistrationCountByDate>;
 
 export interface SummaryMetric {
   value: null | number | string | undefined;
