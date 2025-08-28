edc40dffc7f17f458885f4dd75baa67c1e91e985
Ruben <vandervalk@geoit.nl>
2025-08-28 17:06:43 +0200

    update api test for clarity

--- services/121-service/test/visa-card/visa-customer-information-update.test.ts
+++ services/121-service/test/visa-card/visa-customer-information-update.test.ts
@@ -24,7 +24,11 @@ describe('Update registration data of registration with visa customer', () => {
     await waitFor(2_000);
   });
 
-  it('should update phone number before and after changing FSP configuration from Visa', async () => {
+  // When user update certain fields of a registration with a visa customer the 121-platform should update the visa customer with API calls to Intersolve
+  // It's very hard to properly create automated tests for this functionality as the end result are API request to Intersolve.
+  // When running API tests we have MOCK_INTERSOLVE=true in our env, which mean we make our API requests to our mock-service, which pretends to be Intersolve.
+  // We have no way to easily validate if the mock service is properly called, so we only check if no errors are thrown when updating a registration with a visa customer.
+  it('should get a success response when updating registration data of a registration with a visa customer', async () => {
     // Arrange
     await seedPaidRegistrations([registrationVisa], programIdVisa);
 
@@ -49,6 +53,7 @@ describe('Update registration data of registration with visa customer', () => {
       accessToken,
     );
 
+    // Even if a registrations FSP is changed, if a visa customer exists, the visa customer is still updated
     const responseUpdateAfterFspChange = await updateRegistration(
       programIdVisa,
       registrationVisa.referenceId,
