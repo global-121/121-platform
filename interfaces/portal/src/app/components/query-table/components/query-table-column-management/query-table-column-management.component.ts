a6c6d6d7d1c3e2ffa3c38091750fd2903bb4e4a8
Domenico Gemoli <domenicogemoli@gmail.com>
2025-09-03 14:58:56 +0200

    chore: refactor table management

--- interfaces/portal/src/app/components/query-table/components/query-table-column-management/query-table-column-management.component.ts
+++ interfaces/portal/src/app/components/query-table/components/query-table-column-management/query-table-column-management.component.ts
@@ -5,6 +5,7 @@ import {
   input,
   model,
   output,
+  signal,
 } from '@angular/core';
 import {
   FormControl,
@@ -13,11 +14,12 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 
-import { injectMutation } from '@tanstack/angular-query-experimental';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
+import { DrawerModule } from 'primeng/drawer';
+import { FocusTrapModule } from 'primeng/focustrap';
 
-import { FormSidebarComponent } from '~/components/form/form-sidebar.component';
+import { FormErrorComponent } from '~/components/form-error/form-error.component';
 import { QueryTableColumn } from '~/components/query-table/query-table.component';
 import { RtlHelperService } from '~/services/rtl-helper.service';
 
@@ -25,10 +27,12 @@ import { RtlHelperService } from '~/services/rtl-helper.service';
   selector: 'app-query-table-column-management',
   imports: [
     ButtonModule,
-    FormSidebarComponent,
     CheckboxModule,
     ReactiveFormsModule,
     FormsModule,
+    DrawerModule,
+    FormErrorComponent,
+    FocusTrapModule,
   ],
   templateUrl: './query-table-column-management.component.html',
   styles: ``,
@@ -45,6 +49,7 @@ export class QueryTableColumnManagementComponent<
   readonly resetColumnVisibility = output();
 
   readonly formVisible = model<boolean>(false);
+  readonly formError = signal<string | undefined>(undefined);
 
   formGroup = new FormGroup({
     selectedColumns: new FormControl<QueryTableColumn<TData>[]>([], {
@@ -52,33 +57,26 @@ export class QueryTableColumnManagementComponent<
     }),
   });
 
-  updateColumnVisibility = injectMutation(() => ({
-    // We don't technically need a mutation here, but we're using one
-    // so that we can easily reuse the form-sidebar component
-    mutationFn: () => {
-      const { selectedColumns } = this.formGroup.getRawValue();
-
-      if (selectedColumns.length === 0) {
-        return Promise.reject(
-          new Error($localize`At least one column must be selected`),
-        );
-      }
-
-      return Promise.resolve(selectedColumns);
-    },
-    onSuccess: (selectedColumns) => {
-      const stateKey = this.selectedColumnsStateKey();
-      if (stateKey) {
-        localStorage.setItem(stateKey, JSON.stringify(selectedColumns));
-      }
-      this.visibleColumns.set(selectedColumns);
-      this.formVisible.set(false);
-    },
-  }));
   showColumnManagement() {
     this.formGroup.patchValue({
       selectedColumns: this.visibleColumns(),
     });
     this.formVisible.set(true);
+    this.formError.set(undefined);
+  }
+
+  onFormSubmit(): void {
+    const { selectedColumns } = this.formGroup.getRawValue();
+    if (selectedColumns.length === 0) {
+      this.formError.set($localize`At least one column must be selected`);
+      return;
+    }
+    this.formError.set(undefined);
+    const stateKey = this.selectedColumnsStateKey();
+    if (stateKey) {
+      localStorage.setItem(stateKey, JSON.stringify(selectedColumns));
+    }
+    this.visibleColumns.set(selectedColumns);
+    this.formVisible.set(false);
   }
 }
