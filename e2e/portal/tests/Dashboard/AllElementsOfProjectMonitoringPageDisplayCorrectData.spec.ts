68086d3ccabeb4f98b28f32b146051a631fc96d6
piotrk39 <piotrk39@gmail.com>
2025-09-10 14:23:09 +0200

    Add base test

---
+++ e2e/portal/tests/Dashboard/AllElementsOfProjectMonitoringPageDisplayCorrectData.spec.ts
@@ -0,0 +1,43 @@
+import { test } from '@playwright/test';
+
+import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
+import { seedPaidRegistrations } from '@121-service/test/helpers/registration.helper';
+import { resetDB } from '@121-service/test/helpers/utility.helper';
+import { registrationsOCW } from '@121-service/test/registrations/pagination/pagination-data';
+
+import LoginPage from '@121-e2e/portal/pages/LoginPage';
+import ProjectMonitoring from '@121-e2e/portal/pages/ProjectMonitoringPage';
+
+test.beforeEach(async ({ page }) => {
+  await resetDB(SeedScript.nlrcMultiple, __filename);
+  const programIdOCW = 3;
+  const OcwProgramId = programIdOCW;
+
+  await seedPaidRegistrations(registrationsOCW, OcwProgramId);
+
+  // Login
+  const loginPage = new LoginPage(page);
+  await page.goto('/');
+  await loginPage.login();
+});
+
+test('[30579] All elements of Monitoring Dashboard tab are displayed', async ({
+  page,
+}) => {
+  const projectMonitoring = new ProjectMonitoring(page);
+
+  const projectTitle = 'NLRC OCW program';
+
+  await test.step('Navigate to project`s monitoring page', async () => {
+    await projectMonitoring.selectProgram(projectTitle);
+    await projectMonitoring.navigateToProgramPage('Monitoring');
+    await projectMonitoring.selectTab({ tabName: 'Dashboard' });
+  });
+
+  await test.step('Check if all elements of Dashboard are displayed', async () => {
+    await projectMonitoring.assertValuesInMonitoringTab({
+      peopleIncluded: 5,
+      peopleRegistered: 5,
+    });
+  });
+});
