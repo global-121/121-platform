21544a0a34cb9b7b516cc7186dcac2ce2a18a70c
Niels Bom <nbom@redcross.nl>
2025-09-12 15:02:08 +0200

    WIP: create pipe for positive numbers

--- services/121-service/src/pipes/positive-integer.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-@Injectable()
-export class ValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    console.log('🚀 ~ ValidationPipe ~ transform ~ metadata:', metadata);
-    return value;
-  }
-}
