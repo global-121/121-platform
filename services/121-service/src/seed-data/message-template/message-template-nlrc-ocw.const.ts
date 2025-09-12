f0d74b79861ae7ca158144151ab9ee9a63658a40
Yuri <gewoonyuri@protonmail.com>
2025-09-12 09:23:26 +0200

    added test for sending message when registrations are imported

--- services/121-service/src/seed-data/message-template/message-template-nlrc-ocw.const.ts
+++ services/121-service/src/seed-data/message-template/message-template-nlrc-ocw.const.ts
@@ -95,7 +95,7 @@ export const messageTemplateNlrcOcw: SeedMessageTemplateConfig = {
     isSendMessageTemplate: false,
     isWhatsappTemplate: false,
     message: {
-      en: 'Welcome to our program. Whitin a couple of days your request will be validated.',
+      en: 'Welcome to our program. Within a couple of days your request will be validated.',
       nl: 'Welkom bij dit programma. Binnen enkele dagen zal uw verzoek worden behandeld.',
     },
   },
