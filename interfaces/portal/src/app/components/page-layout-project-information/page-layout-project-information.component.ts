655b752f31e183ff984cbaad252c6348b1e427cb
Niels Bom <nbom@redcross.nl>
2025-09-16 18:30:44 +0200

    WIP: add project-settings-information route and page

---
+++ interfaces/portal/src/app/components/page-layout-project-information/page-layout-project-information.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-layout-project-information',
+  imports: [],
+  templateUrl: './page-layout-project-information.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PageLayoutProjectInformationComponent {
+
+}
