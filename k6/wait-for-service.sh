acfe38200703cd4ebf825ffc2b3a0e05660afaa8
piotrk39 <piotrk39@gmail.com>
2025-09-17 20:20:06 +0200

    add sh script to workflow

---
+++ k6/wait-for-service.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+for i in {1..30}; do
+  if curl -s http://localhost:3000/api/health/health > /dev/null; then
+    echo "API is up!";
+    exit 0;
+  fi
+  echo "Waiting for API...";
+  sleep 2
+done
+echo "API did not start in time";
+exit 1
