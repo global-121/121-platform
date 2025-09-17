44edfcc8feb5762bae02799b03d5fc2ebfb93f75
Jannis Visser <jannisvisser@redcross.nl>
2025-09-17 11:30:17 +0200

    feat: update/create entities

--- services/121-service/src/payments/transactions/entities/transaction.entity.ts
+++ services/121-service/src/payments/transactions/entities/transaction.entity.ts
@@ -4,52 +4,35 @@ import {
   Index,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   OneToOne,
   Relation,
 } from 'typeorm';
 
 import { Base121AuditedEntity } from '@121-service/src/base-audited.entity';
 import { PaymentEntity } from '@121-service/src/payments/entities/payment.entity';
-import { LatestTransactionEntity } from '@121-service/src/payments/transactions/latest-transaction.entity';
+import { LatestTransactionEntity } from '@121-service/src/payments/transactions/entities/latest-transaction.entity';
+import { TransactionEventEntity } from '@121-service/src/payments/transactions/entities/transaction-event.entity';
 import { ProgramFspConfigurationEntity } from '@121-service/src/program-fsp-configurations/entities/program-fsp-configuration.entity';
 import { RegistrationEntity } from '@121-service/src/registration/entities/registration.entity';
-import { UserEntity } from '@121-service/src/user/entities/user.entity';
 
 @Entity('transaction')
 export class TransactionEntity extends Base121AuditedEntity {
-  @ManyToOne(() => UserEntity, { onDelete: 'NO ACTION' }) // Do not delete on deleting users, instead see catch in userService.delete()
-  @JoinColumn({ name: 'userId' })
-  public user: Relation<UserEntity>;
-
   @Column({ nullable: true, type: 'real' })
-  public amount: number | null;
+  public transferValue: number | null;
 
   @Column()
   @Index()
   public status: string;
 
-  @Column({ type: 'character varying', nullable: true })
-  public errorMessage: string | null;
-
   @ManyToOne((_type) => PaymentEntity, (payment) => payment.transactions, {
     onDelete: 'CASCADE',
   })
-  @JoinColumn({
-    name: 'paymentId',
-  })
+  @JoinColumn({ name: 'paymentId' })
   public payment: Relation<PaymentEntity>;
   @Index()
   @Column({ type: 'int' })
   public paymentId: number;
-
-  @Column('json', {
-    default: {},
-  })
-  public customData: Record<string, unknown>;
-
-  @Column({ default: 1 })
-  @Index()
-  public transactionStep: number;
 
   @ManyToOne(
     (_type) => ProgramFspConfigurationEntity,
@@ -75,6 +58,13 @@ export class TransactionEntity extends Base121AuditedEntity {
   @Column({ type: 'int', nullable: false })
   public registrationId: number;
 
+  @OneToMany(
+    () => TransactionEventEntity,
+    (transactionEvents) => transactionEvents.transactionId,
+  )
+  public transactionsEvents: Relation<TransactionEventEntity[]>;
+
+  // ##TODO: refactor this out, but leave in for now to avoid breaking changes
   @OneToOne(
     () => LatestTransactionEntity,
     (latestTransaction) => latestTransaction.transaction,
