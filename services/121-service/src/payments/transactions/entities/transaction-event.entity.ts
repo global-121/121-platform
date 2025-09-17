02c77aea32b70628c418d300896a7de31606e655
Jannis Visser <jannisvisser@redcross.nl>
2025-09-17 11:30:17 +0200

    feat: update/create entities

---
+++ services/121-service/src/payments/transactions/entities/transaction-event.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  Relation,
+} from 'typeorm';
+
+import { Base121AuditedEntity } from '@121-service/src/base-audited.entity';
+import { TransactionEntity } from '@121-service/src/payments/transactions/entities/transaction.entity';
+import { UserEntity } from '@121-service/src/user/entities/user.entity';
+
+@Entity('transaction_event')
+export class TransactionEventEntity extends Base121AuditedEntity {
+  @ManyToOne(() => UserEntity, { onDelete: 'NO ACTION' }) // Do not delete on deleting users, instead see catch in userService.delete()
+  @JoinColumn({ name: 'userId' })
+  public user: Relation<UserEntity>;
+
+  // ##TODO: also separately errorCode?
+  @Column({ type: 'character varying', nullable: true })
+  public errorMessage: string | null;
+
+  @Column()
+  @Index()
+  public type: string; //##TODO: make enum later?
+
+  @ManyToOne(
+    (_type) => TransactionEntity,
+    (transaction) => transaction.transactionsEvents,
+    { onDelete: 'CASCADE' },
+  )
+  @JoinColumn({ name: 'transactionId' })
+  public transaction: Relation<TransactionEntity>;
+  @Index()
+  @Column({ type: 'int' })
+  public transactionId: number;
+}
