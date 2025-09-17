cb425cb6a3369074b9e9ebfcf8ba7d7174592db7
Jannis Visser <jannisvisser@redcross.nl>
2025-09-17 19:44:23 +0200

    wip

--- services/121-service/src/payments/entities/payment.entity.ts
+++ services/121-service/src/payments/entities/payment.entity.ts
@@ -8,7 +8,6 @@ import {
 } from 'typeorm';
 
 import { Base121Entity } from '@121-service/src/base.entity';
-import { LatestTransactionEntity } from '@121-service/src/payments/transactions/entities/latest-transaction.entity';
 import { TransactionEntity } from '@121-service/src/payments/transactions/entities/transaction.entity';
 import { ProgramEntity } from '@121-service/src/programs/entities/program.entity';
 
@@ -16,12 +15,6 @@ import { ProgramEntity } from '@121-service/src/programs/entities/program.entity
 export class PaymentEntity extends Base121Entity {
   @OneToMany(() => TransactionEntity, (transactions) => transactions.paymentId)
   public transactions: Relation<TransactionEntity[]>;
-
-  @OneToMany(
-    () => LatestTransactionEntity,
-    (latestTransactions) => latestTransactions.payment,
-  )
-  public latestTransactions: Relation<LatestTransactionEntity[]>;
 
   @ManyToOne((_type) => ProgramEntity, (program) => program.payments, {
     onDelete: 'CASCADE',
--- services/121-service/src/payments/transactions/transactions.module.ts
+++ services/121-service/src/payments/transactions/transactions.module.ts
@@ -6,14 +6,14 @@ import { ActionsModule } from '@121-service/src/actions/actions.module';
 import { TwilioMessageEntity } from '@121-service/src/notifications/entities/twilio.entity';
 import { MessageQueuesModule } from '@121-service/src/notifications/message-queues/message-queues.module';
 import { MessageTemplateModule } from '@121-service/src/notifications/message-template/message-template.module';
-import { LatestTransactionEntity } from '@121-service/src/payments/transactions/entities/latest-transaction.entity';
 import { TransactionEntity } from '@121-service/src/payments/transactions/entities/transaction.entity';
-import { LatestTransactionRepository } from '@121-service/src/payments/transactions/repositories/latest-transaction.repository';
 import { TransactionRepository } from '@121-service/src/payments/transactions/transaction.repository';
 import { TransactionScopedRepository } from '@121-service/src/payments/transactions/transaction.scoped.repository';
+import { TransactionEventsModule } from '@121-service/src/payments/transactions/transaction-events/transaction-events.module';
 import { TransactionsService } from '@121-service/src/payments/transactions/transactions.service';
 import { ProgramEntity } from '@121-service/src/programs/entities/program.entity';
 import { RegistrationUtilsModule } from '@121-service/src/registration/modules/registration-utils/registration-utils.module';
+import { ProgramModule } from '@121-service/src/programs/programs.module';
 import { RegistrationScopedRepository } from '@121-service/src/registration/repositories/registration-scoped.repository';
 import { RegistrationEventsModule } from '@121-service/src/registration-events/registration-events.module';
 import { UserModule } from '@121-service/src/user/user.module';
@@ -24,7 +24,6 @@ import { createScopedRepositoryProvider } from '@121-service/src/utils/scope/cre
   imports: [
     TypeOrmModule.forFeature([
       ProgramEntity,
-      LatestTransactionEntity,
       TwilioMessageEntity,
       TransactionEntity,
     ]),
@@ -35,6 +34,8 @@ import { createScopedRepositoryProvider } from '@121-service/src/utils/scope/cre
     MessageTemplateModule,
     RegistrationUtilsModule,
     RegistrationEventsModule,
+    ProgramModule,
+    TransactionEventsModule,
   ],
   providers: [
     TransactionsService,
@@ -42,13 +43,11 @@ import { createScopedRepositoryProvider } from '@121-service/src/utils/scope/cre
     TransactionRepository,
     RegistrationScopedRepository,
     createScopedRepositoryProvider(TransactionEntity),
-    LatestTransactionRepository,
   ],
   exports: [
     TransactionsService,
     TransactionScopedRepository,
     TransactionRepository,
-    LatestTransactionRepository,
   ],
 })
 export class TransactionsModule {}
