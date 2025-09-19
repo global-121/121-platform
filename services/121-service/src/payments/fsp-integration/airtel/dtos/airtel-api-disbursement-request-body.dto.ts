2da931233c15d4c813b32a12c40d0dc9aa9d6202
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-09-19 08:52:44 +0200

    airtel do not add name (#7267)
    * airtel do not add name
    
    * remove from unit test
    
    ---------
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/src/payments/fsp-integration/airtel/dtos/airtel-api-disbursement-request-body.dto.ts
+++ services/121-service/src/payments/fsp-integration/airtel/dtos/airtel-api-disbursement-request-body.dto.ts
@@ -2,7 +2,6 @@ export interface AirtelApiDisbursementRequestBodyDto {
   readonly payee: {
     readonly currency: string;
     readonly msisdn: string;
-    readonly name: string;
   };
   reference: string;
   readonly pin: string;
