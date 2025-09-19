0ad1c84c47af319c4f15c40486ce3967dc795008
Jannis Visser <jannisvisser@redcross.nl>
2025-09-19 13:18:09 +0200

    fix callback query onafriq

--- services/121-service/src/payments/reconciliation/onafriq-reconciliation/onafriq-reconciliation.module.ts
+++ services/121-service/src/payments/reconciliation/onafriq-reconciliation/onafriq-reconciliation.module.ts
@@ -6,6 +6,7 @@ import { OnafriqReconciliationController } from '@121-service/src/payments/recon
 import { OnafriqReconciliationService as OnafriqReconciliationService } from '@121-service/src/payments/reconciliation/onafriq-reconciliation/onafriq-reconciliation.service';
 import { TransactionCallbackJobProcessorOnafriq } from '@121-service/src/payments/reconciliation/onafriq-reconciliation/processors/onafriq-transaction-callback-job.processor';
 import { RedisModule } from '@121-service/src/payments/redis/redis.module';
+import { TransactionEventEntity } from '@121-service/src/payments/transactions/entities/transaction-event.entity';
 import { TransactionEventsModule } from '@121-service/src/payments/transactions/transaction-events/transaction-events.module';
 import { TransactionsModule } from '@121-service/src/payments/transactions/transactions.module';
 import { QueuesRegistryModule } from '@121-service/src/queues-registry/queues-registry.module';
@@ -24,6 +25,7 @@ import { createScopedRepositoryProvider } from '@121-service/src/utils/scope/cre
     OnafriqReconciliationService,
     TransactionCallbackJobProcessorOnafriq,
     createScopedRepositoryProvider(OnafriqTransactionEntity),
+    createScopedRepositoryProvider(TransactionEventEntity),
   ],
   controllers: [OnafriqReconciliationController],
   exports: [OnafriqReconciliationService],
