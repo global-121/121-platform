571d33b8e6433b570a8b92c44d6ff8464e50ee89
Jannis Visser <jannisvisser@redcross.nl>
2025-09-24 17:06:20 +0200

    fix: interface setup

--- services/121-service/src/payments/interfaces/transaction-job-details.interface.ts
+++ services/121-service/src/payments/interfaces/transaction-job-details.interface.ts
@@ -1,4 +1,4 @@
-export class TransactionInputData {
+export class TransactionJobDetails {
   public readonly referenceId: string;
   public readonly transactionAmount: number;
   public readonly transactionId: number;
