e8167be197e26ad33240220b9574bcc808211d26
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2025-09-25 12:04:14 +0200

    Decmial payment amount (#7285)
    * Decmial payment amount
    
    * remove double comment
    
    ---------
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/test/registrations/calculate-payment-amount-multiplier.test.ts
+++ services/121-service/test/registrations/calculate-payment-amount-multiplier.test.ts
@@ -1,9 +1,13 @@
 import { HttpStatus } from '@nestjs/common';
 
+import { TransactionStatusEnum } from '@121-service/src/payments/transactions/enums/transaction-status.enum';
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
+import { getTransactions } from '@121-service/test/helpers/program.helper';
 import {
+  doPaymentAndWaitForCompletion,
   importRegistrations,
   searchRegistrationByReferenceId,
+  seedIncludedRegistrations,
 } from '@121-service/test/helpers/registration.helper';
 import {
   getAccessToken,
@@ -24,17 +28,17 @@ describe('Set/calculate payment amount multiplier', () => {
   });
 
   it('should automatically calculate payment amount based on formula', async () => {
-    await resetDB(SeedScript.testMultiple, __filename);
-    const nrOfDragons = 2;
-
     // Arrange
+    await resetDB(SeedScript.testMultiple, __filename);
+    const nrOfDragons = 2.5; // We are using half a dragon here to ensure decimal calculations also work
+    const amount = 10;
     const registrationWesterosCopy = { ...registrationWesteros1 };
     registrationWesterosCopy.dragon = nrOfDragons;
 
     // Act
-    const responseImport = await importRegistrations(
-      programIdWesteros,
+    await seedIncludedRegistrations(
       [registrationWesterosCopy],
+      programIdWesteros,
       accessToken,
     );
     const searchRegistrationResponse = await searchRegistrationByReferenceId(
@@ -44,9 +48,26 @@ describe('Set/calculate payment amount multiplier', () => {
     );
     const importedRegistration = searchRegistrationResponse.body.data[0];
 
+    const paymentId = await doPaymentAndWaitForCompletion({
+      programId: programIdWesteros,
+      referenceIds: [importedRegistration.referenceId],
+      amount,
+      accessToken,
+      completeStatusses: Object.values(TransactionStatusEnum),
+    });
+
+    const transactionsResponse = await getTransactions({
+      programId: programIdWesteros,
+      paymentId,
+      registrationReferenceId: importedRegistration.referenceId,
+
+      accessToken,
+    });
+    const transaction = transactionsResponse.body[0];
     // Assert
-    expect(responseImport.statusCode).toBe(HttpStatus.CREATED);
+
     expect(importedRegistration.paymentAmountMultiplier).toBe(nrOfDragons + 1);
+    expect(transaction.amount).toBe(amount * (nrOfDragons + 1));
   });
 
   it('should error if paymentAmountMultiplier is set while program has a formula', async () => {
@@ -75,8 +96,8 @@ describe('Set/calculate payment amount multiplier', () => {
   });
 
   it('should set paymentAmountMultiplier to 1 if program has no formula and paymentAmountMultiplier in import is not set', async () => {
-    await resetDB(SeedScript.nlrcMultiple, __filename);
     // Arrange
+    await resetDB(SeedScript.nlrcMultiple, __filename);
     const registrationPvCopy = {
       ...registrationPV5,
     };
@@ -101,8 +122,8 @@ describe('Set/calculate payment amount multiplier', () => {
   });
 
   it('should set paymentAmountMultiplier based paymentAmountMultiplier if program has no formula', async () => {
-    await resetDB(SeedScript.nlrcMultiple, __filename);
     // Arrange
+    await resetDB(SeedScript.nlrcMultiple, __filename);
     const paymentAmountMultiplier = 3;
     const registrationPvCopy = {
       ...registrationPV5,
