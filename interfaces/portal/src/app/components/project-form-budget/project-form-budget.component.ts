33768fc54e459e451734b2a8930c1ff64f2166c8
Domenico Gemoli <domenicogemoli@gmail.com>
2025-09-25 15:24:58 +0200

    chore: refactor project forms
    AB#38218

---
+++ interfaces/portal/src/app/components/project-form-budget/project-form-budget.component.ts
@@ -0,0 +1,122 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  input,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+
+import { CurrencyCode } from '@121-service/src/exchange-rates/enums/currency-code.enum';
+
+import { FormFieldWrapperComponent } from '~/components/form-field-wrapper/form-field-wrapper.component';
+import { PROJECT_FORM_TOOLTIPS } from '~/domains/project/project.helper';
+import { Project } from '~/domains/project/project.model';
+import {
+  generateFieldErrors,
+  genericFieldIsRequiredValidationMessage,
+} from '~/utils/form-validation';
+
+export type ProjectBudgetFormGroup =
+  (typeof ProjectFormBudgetComponent)['prototype']['formGroup'];
+
+@Component({
+  selector: 'app-project-form-budget',
+  imports: [
+    FormFieldWrapperComponent,
+    ReactiveFormsModule,
+    InputTextModule,
+    SelectModule,
+  ],
+  templateUrl: './project-form-budget.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProjectFormBudgetComponent {
+  readonly project = input<Project>();
+
+  readonly currencies = Object.values(CurrencyCode)
+    .map((code) => ({
+      label: code,
+      value: code,
+    }))
+    .sort((a, b) => a.label.localeCompare(b.label));
+
+  formGroup = new FormGroup({
+    budget: new FormControl<number | undefined>(undefined, {
+      nonNullable: true,
+      validators: [Validators.min(0)],
+    }),
+    currency: new FormControl<CurrencyCode | undefined>(undefined, {
+      nonNullable: true,
+      // eslint-disable-next-line @typescript-eslint/unbound-method -- https://github.com/typescript-eslint/typescript-eslint/issues/1929#issuecomment-618695608
+      validators: [Validators.required],
+    }),
+    distributionFrequency: new FormControl<string | undefined>(undefined, {
+      nonNullable: true,
+      validators: [],
+    }),
+    distributionDuration: new FormControl<number | undefined>(undefined, {
+      nonNullable: true,
+      validators: [Validators.min(0)],
+    }),
+    fixedTransferValue: new FormControl(0, {
+      nonNullable: true,
+      // eslint-disable-next-line @typescript-eslint/unbound-method -- https://github.com/typescript-eslint/typescript-eslint/issues/1929#issuecomment-618695608
+      validators: [Validators.required, Validators.min(0)],
+    }),
+  });
+
+  formFieldErrors = generateFieldErrors<ProjectBudgetFormGroup>(
+    this.formGroup,
+    {
+      budget: (control) => {
+        if (control.errors?.min) {
+          return $localize`This needs to be at least 0.`;
+        }
+        return;
+      },
+      currency: genericFieldIsRequiredValidationMessage,
+      distributionFrequency: genericFieldIsRequiredValidationMessage,
+      distributionDuration: (control) => {
+        if (control.errors?.min) {
+          return $localize`This needs to be at least 0.`;
+        }
+        return;
+      },
+      fixedTransferValue: (control) => {
+        if (control.errors?.min) {
+          return $localize`This needs to be at least 0.`;
+        }
+        return genericFieldIsRequiredValidationMessage(control);
+      },
+    },
+  );
+
+  updateFormGroup = effect(() => {
+    const projectData = this.project();
+
+    if (!projectData) {
+      return;
+    }
+
+    this.formGroup.setValue({
+      budget: projectData.budget,
+      currency: projectData.currency,
+      distributionFrequency: projectData.distributionFrequency ?? undefined,
+      distributionDuration: projectData.distributionDuration,
+      fixedTransferValue: projectData.fixedTransferValue ?? 0,
+    });
+  });
+
+  readonly tooltipCurrency = PROJECT_FORM_TOOLTIPS.currency;
+  readonly tooltipDistributionDuration =
+    PROJECT_FORM_TOOLTIPS.distributionDuration;
+}
---
+++ interfaces/portal/src/app/components/project-form-name/project-form-name.component.ts
@@ -0,0 +1,70 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  input,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+
+import { FormFieldWrapperComponent } from '~/components/form-field-wrapper/form-field-wrapper.component';
+import { Project } from '~/domains/project/project.model';
+import {
+  generateFieldErrors,
+  genericFieldIsRequiredValidationMessage,
+} from '~/utils/form-validation';
+
+export type ProjectNameFormGroup =
+  (typeof ProjectFormNameComponent)['prototype']['formGroup'];
+
+@Component({
+  selector: 'app-project-form-name',
+  imports: [
+    FormFieldWrapperComponent,
+    ReactiveFormsModule,
+    InputTextModule,
+    TextareaModule,
+  ],
+  templateUrl: './project-form-name.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProjectFormNameComponent {
+  readonly project = input<Project>();
+
+  formGroup = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      // eslint-disable-next-line @typescript-eslint/unbound-method -- https://github.com/typescript-eslint/typescript-eslint/issues/1929#issuecomment-618695608
+      validators: [Validators.required],
+    }),
+    description: new FormControl<string | undefined>(undefined, {
+      nonNullable: true,
+    }),
+  });
+
+  formFieldErrors = generateFieldErrors<ProjectNameFormGroup>(this.formGroup, {
+    name: genericFieldIsRequiredValidationMessage,
+    description: genericFieldIsRequiredValidationMessage,
+  });
+
+  updateFormGroup = effect(() => {
+    const projectData = this.project();
+
+    if (!projectData) {
+      return;
+    }
+
+    this.formGroup.setValue({
+      name: projectData.titlePortal?.en ?? '',
+      description: projectData.description?.en,
+    });
+  });
+}
