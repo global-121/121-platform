fe73091dfd3244f47a50f12d79086aba443d81da
arsforza <asforza@recross.nl>
2025-09-29 10:09:14 +0200

    add amount sent per month chart

---
+++ interfaces/portal/src/app/pages/project-monitoring-dashboard/components/amount-sent-per-month-chart/amount-sent-per-month-chart.component.ts
@@ -0,0 +1,103 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+} from '@angular/core';
+
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { ChartData } from 'chart.js';
+import { ChartModule } from 'primeng/chart';
+
+import { TransactionStatusEnum } from '@121-service/src/payments/transactions/enums/transaction-status.enum';
+
+import tailwindConfig from '~/../../tailwind.config';
+import { MetricApiService } from '~/domains/metric/metric.api.service';
+import {
+  getChartOptions,
+  shade,
+} from '~/pages/project-monitoring-dashboard/project-monitoring-dashboard.helper';
+
+const colors = tailwindConfig.theme.colors;
+
+const paymentColors = {
+  [TransactionStatusEnum.error]: colors.red[shade],
+  [TransactionStatusEnum.success]: colors.green[shade],
+  [TransactionStatusEnum.waiting]: colors.yellow[shade],
+};
+
+@Component({
+  selector: 'app-amount-sent-per-month-chart',
+  imports: [ChartModule],
+  templateUrl: './amount-sent-per-month-chart.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AmountSentPerMonthChartComponent {
+  private metricApiService = inject(MetricApiService);
+  readonly projectId = input.required<string>();
+
+  readonly getLabelFunction =
+    input.required<
+      (opts: { title: string; labels: string[]; data: number[] }) => string
+    >();
+
+  readonly title = $localize`Registrations by creation date (last 2 weeks)`;
+
+  readonly query = injectQuery(() => ({
+    ...this.metricApiService.getAmountSentByMonth({
+      projectId: this.projectId,
+      limitNumberOfPayments: 5,
+    })(),
+    enabled: !!this.projectId(),
+  }));
+
+  readonly queryData = computed(() => {
+    if (!this.query.isSuccess()) {
+      return {};
+    }
+
+    return this.query.data();
+  });
+
+  readonly labels = computed<string[]>(() =>
+    Object.keys(this.queryData()).sort(),
+  );
+
+  readonly data = computed(() => this.labels().map((k) => this.queryData()[k]));
+
+  chartOptions = getChartOptions({
+    title: this.title,
+    showLegend: true,
+  });
+
+  readonly chartData = computed<ChartData>(() => ({
+    labels: this.labels(),
+    datasets: [
+      {
+        label: TransactionStatusEnum.error,
+        data: this.data().map((a) => a.failed),
+        backgroundColor: paymentColors[TransactionStatusEnum.error],
+      },
+      {
+        label: TransactionStatusEnum.success,
+        data: this.data().map((a) => a[TransactionStatusEnum.success]),
+        backgroundColor: paymentColors[TransactionStatusEnum.success],
+      },
+      {
+        label: TransactionStatusEnum.waiting,
+        data: this.data().map((a) => a[TransactionStatusEnum.waiting]),
+        backgroundColor: paymentColors[TransactionStatusEnum.waiting],
+      },
+    ],
+  }));
+
+  readonly ariaLabel = computed(() =>
+    this.getLabelFunction()({
+      title: this.title,
+      labels: this.labels(),
+      data: this.chartData().datasets[0].data as number[],
+    }),
+  );
+}
