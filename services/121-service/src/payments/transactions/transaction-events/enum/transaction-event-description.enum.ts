98d756a74d5f64fbbcc309e387fd616e00b50e32
jannisvisser <jannisvisser@redcross.nl>
2025-09-29 14:43:29 +0200

    Switch Intersolve Voucher to new transaction-events flow (#7296)
    * draft full version intersolve-voucher with ##TODO's
    
    * rm console.log
    
    * refactor: latestEvent to transactions.service + other small ##TODO's
    
    * comments
    
    * refactor a bit

--- services/121-service/src/payments/transactions/transaction-events/enum/transaction-event-description.enum.ts
+++ services/121-service/src/payments/transactions/transaction-events/enum/transaction-event-description.enum.ts
@@ -5,7 +5,7 @@ export enum TransactionEventDescription {
   // Onafriq processing-step events
   onafriqRequestSent = 'Onafriq request sent',
   onafriqCallbackReceived = 'Onafriq callback received',
-  // Safaricom processing-step events // ##TODO: these are exactly the same as onafriq, can we generalize?
+  // Safaricom processing-step events // ##TODO: these are exactly the same as onafriq, can we generalize? Go over all these things in general at the end.
   safaricomRequestSent = 'Safaricom request sent',
   safaricomCallbackReceived = 'Safaricom callback received',
   // Nedbank processing-step events
@@ -17,4 +17,9 @@ export enum TransactionEventDescription {
   visaPaymentRequested = 'Visa payment requested',
   // Commercial Bank Ethiopia processing-step events
   commercialBankEthiopiaRequestSent = 'Commercial Bank Ethiopia request sent',
+  // Intersolve Voucher processing-step events
+  intersolveVoucherCreationRequest = 'Intersolve Voucher creation requested',
+  intersolveVoucherInitialMessageSent = 'Intersolve Voucher initial message sent',
+  intersolveVoucherVoucherMessageSent = 'Intersolve Voucher voucher message sent',
+  intersolveVoucherMessageCallback = 'Intersolve Voucher message callback received', // ##TODO: do we want to distinguish between callback on initial vs voucher message?
 }
