fc4c76f531a3296f0062654309c7f0f1209a8393
Elwin Schmitz <eschmitz@redcross.nl>
2025-09-30 14:17:51 +0200

    fix: no workaround needed?

--- services/mock-service/src/fsp-integration/airtel/airtel.mock.controller.ts
+++ services/mock-service/src/fsp-integration/airtel/airtel.mock.controller.ts
@@ -76,9 +76,7 @@ export class AirtelMockController {
   }
 
   @ApiOperation({ summary: 'Disbursement API v2' })
-  // The * is a wildcard is needed because the airtel API want the url to end with a slash.
-  // This is a a workaround because the Post decorator does not allow trailing slashes."
-  @Post('standard/v2/disbursements/*path')
+  @Post('standard/v2/disbursements')
   @HttpCode(200)
   @ApiHeader({
     name: 'X-Country',
