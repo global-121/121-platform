45e69ec408c19468dae218c2db91bb79153e0d03
Yuri <gewoonyuri@protonmail.com>
2025-10-06 14:03:08 +0200

    100 registrations per job

--- services/121-service/src/registration/dto/registration-update-job.dto.ts
+++ services/121-service/src/registration/dto/registration-update-job.dto.ts
@@ -1,7 +1,6 @@
 export class RegistrationsUpdateJobDto {
-  referenceId: string;
   programId: number;
-  data: Record<string, string | number | undefined | boolean>;
+  data: Record<string, string | number | undefined | boolean>[];
   request: { userId: number; scope?: string };
   reason: string;
 }
