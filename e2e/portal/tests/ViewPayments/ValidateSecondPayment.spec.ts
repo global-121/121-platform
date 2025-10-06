6214c43de843249289e35cc107c02fa96f80ee6d
Piotr Krajewski <52535860+Piotrk39@users.noreply.github.com>
2025-10-06 18:35:32 +0200

    Add E2E test for validating second payment (#7348)
    * Cherry pick from PR#7343
    
    * Update setup of the test
    
    * Remove unused payment completion await

--- e2e/portal/tests/ViewPayments/ValidateSecondPayment.spec.ts
+++ e2e/portal/tests/ViewPayments/ValidateSecondPayment.spec.ts
@@ -3,7 +3,10 @@ import { format } from 'date-fns';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import NLRCProgram from '@121-service/src/seed-data/program/program-nlrc-ocw.json';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
+import {
+  doPaymentAndWaitForCompletion,
+  seedIncludedRegistrations,
+} from '@121-service/test/helpers/registration.helper';
 import {
   getAccessToken,
   resetDB,
@@ -14,13 +17,29 @@ import {
 } from '@121-service/test/registrations/pagination/pagination-data';
 
 import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import PaymentPage from '@121-e2e/portal/pages/PaymentPage';
 import PaymentsPage from '@121-e2e/portal/pages/PaymentsPage';
+
+const transferValueForSecondPayment = 10;
 
 test.beforeEach(async ({ page }) => {
   await resetDB(SeedScript.nlrcMultiple, __filename);
   const accessToken = await getAccessToken();
   await seedIncludedRegistrations(registrationsVisa, programIdOCW, accessToken);
+  // do 1st payment
+  await doPaymentAndWaitForCompletion({
+    programId: programIdOCW,
+    amount: 25,
+    referenceIds: registrationsVisa.map((reg) => reg.referenceId),
+    accessToken,
+  });
+
+  // do 2nd payment
+  await doPaymentAndWaitForCompletion({
+    programId: programIdOCW,
+    amount: transferValueForSecondPayment,
+    referenceIds: registrationsVisa.map((reg) => reg.referenceId),
+    accessToken,
+  });
 
   // Login
   const loginPage = new LoginPage(page);
@@ -28,45 +47,31 @@ test.beforeEach(async ({ page }) => {
   await loginPage.login();
 });
 
-test('[31970] Do successful payment for Visa fsp', async ({ page }) => {
-  const paymentPage = new PaymentPage(page);
+test('[38338] Validate second payment is correctly displayed on payment card', async ({
+  page,
+}) => {
   const paymentsPage = new PaymentsPage(page);
   const projectTitle = NLRCProgram.titlePortal.en;
   const numberOfPas = registrationsVisa.length;
-  const defaultTransferValue = NLRCProgram.fixedTransferValue;
   const defaultMaxTransferValue = registrationsVisa.reduce((output, pa) => {
-    return output + pa.paymentAmountMultiplier * defaultTransferValue;
+    return output + pa.paymentAmountMultiplier * transferValueForSecondPayment;
   }, 0);
   const lastPaymentDate = `${format(new Date(), 'dd/MM/yyyy')}`;
 
-  await test.step('Navigate to Program payments', async () => {
+  await test.step('Navigate to payments', async () => {
     await paymentsPage.selectProgram(projectTitle);
-
     await paymentsPage.navigateToProgramPage('Payments');
   });
 
-  await test.step('Do payment', async () => {
-    await paymentsPage.createPayment();
-    await paymentsPage.startPayment();
-    // Assert redirection to payment overview page
-    await page.waitForURL((url) =>
-      url.pathname.startsWith(`/en-GB/project/${programIdOCW}/payments/1`),
-    );
-    // Assert payment overview page by payment date/ title
-    await paymentPage.validatePaymentsDetailsPageByDate(lastPaymentDate);
-  });
-
-  await test.step('Validate payment card', async () => {
-    await paymentPage.validateToastMessageAndClose('Payment created.');
-    await paymentPage.waitForPaymentToComplete();
-    await paymentPage.navigateToProgramPage('Payments');
+  await test.step('Validate 2nd payment card', async () => {
     await paymentsPage.validatePaymentCard({
       date: lastPaymentDate,
       paymentAmount: defaultMaxTransferValue,
       registrationsNumber: numberOfPas,
       successfulTransfers: defaultMaxTransferValue,
       failedTransfers: 0,
-      projectId: programIdOCW,
+      projectId: 3,
+      paymentId: 2,
     });
   });
 });
