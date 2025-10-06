185d857408a17fbf4ea1a6e4f27da3282836c51c
Yuri <gewoonyuri@protonmail.com>
2025-10-06 11:08:25 +0200

    WIP: refactor registration update

---
+++ services/121-service/src/registration-update-jobs/registration-update-jobs.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { EmailsModule } from '@121-service/src/emails/emails.module';
+import { RegistrationsModule } from '@121-service/src/registration/registrations.module';
+
+@Module({
+  imports: [RegistrationsModule, EmailsModule],
+  providers: [],
+})
+export class RegistrationUpdateJobsModule {}
