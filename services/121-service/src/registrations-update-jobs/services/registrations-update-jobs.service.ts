cca277e01f735cf00c3da48296b860cfd33cb360
Yuri <gewoonyuri@protonmail.com>
2025-10-06 14:36:33 +0200

    updated refactor for batch processing

--- services/121-service/src/registrations-update-jobs/services/registrations-update-jobs.service.ts
+++ services/121-service/src/registrations-update-jobs/services/registrations-update-jobs.service.ts
@@ -15,38 +15,31 @@ export class RegistrationsUpdateJobsService {
   constructor(private readonly registrationsService: RegistrationsService) {}
 
   public async processRegistrationsUpdateJob(
-    registrationsUpdateJob: RegistrationsUpdateJobDto[],
+    job: RegistrationsUpdateJobDto,
   ): Promise<RegistrationsUpdateJobResult[]> {
     const results: RegistrationsUpdateJobResult[] = [];
 
-    for (const registration of registrationsUpdateJob) {
-      const result = await this.processRegistrationUpdate(registration);
-      if (result) results.push(result);
+    for (const record of job.data) {
+      const dto: UpdateRegistrationDto = {
+        data: record,
+        reason: job.reason,
+      };
+      try {
+        await this.registrationsService.validateInputAndUpdateRegistration({
+          programId: job.programId,
+          referenceId: record.referenceId as string,
+          updateRegistrationDto: dto,
+          userId: job.request.userId,
+        });
+      } catch (error) {
+        results.push({
+          referenceId: record.referenceId as string,
+          data: record,
+          error: error.message,
+        });
+      }
     }
 
     return results;
-  }
-
-  private async processRegistrationUpdate(
-    job: RegistrationsUpdateJobDto,
-  ): Promise<RegistrationsUpdateJobResult | undefined> {
-    const dto: UpdateRegistrationDto = {
-      data: job.data,
-      reason: job.reason,
-    };
-    try {
-      await this.registrationsService.validateInputAndUpdateRegistration({
-        programId: job.programId,
-        referenceId: job.referenceId,
-        updateRegistrationDto: dto,
-        userId: job.request.userId,
-      });
-    } catch (error) {
-      return {
-        referenceId: job.referenceId,
-        data: job.data,
-        error: error.message,
-      };
-    }
   }
 }
