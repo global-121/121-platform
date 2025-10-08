a4fba118a3229e94a012d79596f30a05467d2419
piotrk39 <piotrk39@gmail.com>
2025-10-08 18:18:34 +0200

    Add Extra test and refactor POM

--- e2e/portal/tests/Dashboard/ProjectMonitoring/AllElementsOfProjectMonitoringPageDisplayCorrectData.spec.ts
+++ e2e/portal/tests/Dashboard/ProjectMonitoring/AllElementsOfProjectMonitoringPageDisplayCorrectData.spec.ts
@@ -1,21 +1,51 @@
 import { test } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import { seedPaidRegistrations } from '@121-service/test/helpers/registration.helper';
-import { resetDB } from '@121-service/test/helpers/utility.helper';
-import { registrationsOCW } from '@121-service/test/registrations/pagination/pagination-data';
+import NLRCProgramPv from '@121-service/src/seed-data/program/program-nlrc-pv.json';
+import {
+  doPaymentAndWaitForCompletion,
+  seedIncludedRegistrations,
+} from '@121-service/test/helpers/registration.helper';
+import {
+  getAccessToken,
+  resetDB,
+} from '@121-service/test/helpers/utility.helper';
+import {
+  programIdOCW,
+  programIdPV,
+  registrationsOCW,
+  registrationsVoucher,
+} from '@121-service/test/registrations/pagination/pagination-data';
 
 import BasePage from '@121-e2e/portal/pages/BasePage';
 import LoginPage from '@121-e2e/portal/pages/LoginPage';
 import ProjectMonitoring from '@121-e2e/portal/pages/ProjectMonitoringPage';
 
+const defaultTransferValue = 25;
+
 test.beforeEach(async ({ page }) => {
   await resetDB(SeedScript.nlrcMultiple, __filename);
-  const programIdOCW = 3;
-  const OcwProgramId = programIdOCW;
-
-  await seedPaidRegistrations(registrationsOCW, OcwProgramId);
-
+  const accessToken = await getAccessToken();
+  await seedIncludedRegistrations(registrationsOCW, programIdOCW, accessToken);
+  await seedIncludedRegistrations(
+    registrationsVoucher,
+    programIdPV,
+    accessToken,
+  );
+  // do payment for NLRC OCW
+  await doPaymentAndWaitForCompletion({
+    programId: programIdOCW,
+    amount: defaultTransferValue,
+    referenceIds: registrationsOCW.map((reg) => reg.referenceId),
+    accessToken,
+  });
+  // do payment for NLRC PV
+  await doPaymentAndWaitForCompletion({
+    programId: programIdPV,
+    amount: defaultTransferValue,
+    referenceIds: registrationsVoucher.map((reg) => reg.referenceId),
+    accessToken,
+  });
   // Login
   const loginPage = new LoginPage(page);
   await page.goto('/');
@@ -36,12 +66,21 @@ test('[30579] All elements of Monitoring page display correct data for OCW', asy
   });
 
   await test.step('Check if all elements are displayed', async () => {
+    const defaultMaxTransferValue = registrationsOCW.reduce((output, pa) => {
+      return output + pa.paymentAmountMultiplier * defaultTransferValue;
+    }, 0);
+
     await projectMonitoring.assertMonitoringTabElements({
       shouldHaveIframe: true,
     });
     await projectMonitoring.assertValuesInMonitoringTab({
       peopleIncluded: 5,
       peopleRegistered: 5,
+      lastPaymentAmount: `€${defaultMaxTransferValue.toString()}`,
+      cashDisbursed: `€${defaultMaxTransferValue.toString()}`,
+      remainingBudget: `-€${defaultMaxTransferValue.toString()}`,
+      paymentsDone: 1,
+      newRegistrations: 0,
     });
   });
 });
@@ -60,12 +99,24 @@ test('[30326] All elements of Monitoring page display correct data for NLRC', as
   });
 
   await test.step('Check if all elements are displayed', async () => {
+    const defaultMaxTransferValue = registrationsVoucher.reduce(
+      (output, pa) => {
+        return output + pa.paymentAmountMultiplier * defaultTransferValue;
+      },
+      0,
+    );
+    const remainingBudget = NLRCProgramPv.budget - defaultMaxTransferValue;
+
     await projectMonitoring.assertMonitoringTabElements({
       shouldHaveIframe: false,
     });
     await projectMonitoring.assertValuesInMonitoringTab({
-      peopleIncluded: 0,
-      peopleRegistered: 0,
+      peopleIncluded: 2,
+      peopleRegistered: 2,
+      cashDisbursed: `€${defaultMaxTransferValue.toString()}`,
+      remainingBudget: `€${remainingBudget.toLocaleString()}`,
+      paymentsDone: 1,
+      newRegistrations: 0,
     });
   });
 });
