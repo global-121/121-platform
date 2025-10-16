6ab123dec1c2f1c532b79f5b97a2385b0201de8f
Domenico Gemoli <domenicogemoli@gmail.com>
2025-10-16 11:44:25 +0200

    Separate component for configuration-list

---
+++ interfaces/portal/src/app/pages/project-settings-fsps/components/fsp-configuration-list/fsp-configuration-list.component.ts
@@ -0,0 +1,66 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  model,
+  output,
+} from '@angular/core';
+
+import { injectQuery } from 'node_modules/@tanstack/angular-query-experimental/inject-query';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+import { Fsps } from '@121-service/src/fsps/enums/fsp-name.enum';
+import { FSP_SETTINGS } from '@121-service/src/fsps/fsp-settings.const';
+
+import { CardWithLinkComponent } from '~/components/card-with-link/card-with-link.component';
+import { FormErrorComponent } from '~/components/form-error/form-error.component';
+import { SkeletonInlineComponent } from '~/components/skeleton-inline/skeleton-inline.component';
+import { FspConfigurationApiService } from '~/domains/fsp-configuration/fsp-configuration.api.service';
+import { FspConfiguration } from '~/domains/fsp-configuration/fsp-configuration.model';
+import { FspConfigurationCardComponent } from '~/pages/project-settings-fsps/components/fsp-configuration-card/fsp-configuration-card.component';
+import { TranslatableStringPipe } from '~/pipes/translatable-string.pipe';
+
+@Component({
+  selector: 'app-fsp-configuration-list',
+  imports: [
+    CardModule,
+    ButtonModule,
+    SkeletonInlineComponent,
+    FormErrorComponent,
+    FspConfigurationCardComponent,
+    CardWithLinkComponent,
+    TranslatableStringPipe,
+  ],
+  templateUrl: './fsp-configuration-list.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FspConfigurationListComponent {
+  readonly projectId = input.required<string>();
+  readonly forceShowNewFspList = model.required<boolean>();
+  readonly addFspConfiguration = output<Fsps>();
+  readonly reconfigureFspConfiguration = output<FspConfiguration>();
+
+  readonly fspConfigurationApiService = inject(FspConfigurationApiService);
+
+  fspConfigurations = injectQuery(
+    this.fspConfigurationApiService.getFspConfigurations(this.projectId),
+  );
+
+  readonly FSP_SETTINGS = FSP_SETTINGS;
+  readonly Fsps = Fsps;
+
+  readonly showNewFspList = computed(
+    () =>
+      this.forceShowNewFspList() || this.fspConfigurations.data()?.length === 0,
+  );
+
+  hasFspConfiguration(fspName: Fsps) {
+    return this.fspConfigurations
+      .data()
+      ?.some((fspConfiguration) => fspConfiguration.fspName === fspName);
+  }
+}
