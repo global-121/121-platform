4f2a2bac8f9e3e07412f3de4dd6b869acd3a4b02
Yuri <ywilmering@redcross.nl>
2025-10-23 12:23:09 +0200

    stripping user email template input

--- services/121-service/src/user/user-emails/user-email-templates/user-email-templates.service.ts
+++ services/121-service/src/user/user-emails/user-email-templates/user-email-templates.service.ts
@@ -6,6 +6,7 @@ import { UserEmailTemplate } from '@121-service/src/user/user-emails/user-email-
 import { buildTemplateAccountcreated } from '@121-service/src/user/user-emails/user-email-templates/templates/account-created.template';
 import { buildTemplateAccountCreatedSSO } from '@121-service/src/user/user-emails/user-email-templates/templates/account-created-sso.template';
 import { buildTemplatePasswordReset } from '@121-service/src/user/user-emails/user-email-templates/templates/password-reset.template';
+import { stripHtmlTags } from '@121-service/src/utils/strip-html-tags.helper';
 
 @Injectable()
 export class UserEmailTemplatesService {
@@ -14,21 +15,42 @@ export class UserEmailTemplatesService {
     userEmailTemplateInput: UserEmailTemplateInput,
   ): UserEmailTemplate {
     let emailTemplate: UserEmailTemplate;
+    const sanitizedUserEmailTemplateInput = this.sanitizeUserEmailTemplateInput(
+      userEmailTemplateInput,
+    );
 
     switch (userEmailTemplateType) {
       case UserEmailTemplateType.accountCreated:
-        emailTemplate = buildTemplateAccountcreated(userEmailTemplateInput);
+        emailTemplate = buildTemplateAccountcreated(
+          sanitizedUserEmailTemplateInput,
+        );
         break;
       case UserEmailTemplateType.accountCreatedForSSO:
-        emailTemplate = buildTemplateAccountCreatedSSO(userEmailTemplateInput);
+        emailTemplate = buildTemplateAccountCreatedSSO(
+          sanitizedUserEmailTemplateInput,
+        );
         break;
       case UserEmailTemplateType.passwordReset:
-        emailTemplate = buildTemplatePasswordReset(userEmailTemplateInput);
+        emailTemplate = buildTemplatePasswordReset(
+          sanitizedUserEmailTemplateInput,
+        );
         break;
       default:
-        throw new Error(`Unsupported email type: ${userEmailTemplateType}`);
+        throw new Error(
+          `Unsupported email type: ${sanitizedUserEmailTemplateInput}`,
+        );
     }
 
     return emailTemplate;
+  }
+
+  private sanitizeUserEmailTemplateInput(
+    userEmailTemplateInput: UserEmailTemplateInput,
+  ): UserEmailTemplateInput {
+    const { displayName } = userEmailTemplateInput;
+    return {
+      ...userEmailTemplateInput,
+      displayName: stripHtmlTags(displayName),
+    };
   }
 }
