2a354fb47e5437568635e5c69f82a95237366cdb
Yuri <ywilmering@redcross.nl>
2025-10-23 10:34:57 +0200

    added test to test attachmentless email

--- services/121-service/src/registrations-update-jobs/dev-test-job-email.spec.ts
+++ services/121-service/src/registrations-update-jobs/dev-test-job-email.spec.ts
@@ -6,12 +6,15 @@ import { RegistrationsUpdateJobDto } from '@121-service/src/registration/dto/reg
 import { RegistrationsService } from '@121-service/src/registration/services/registrations.service';
 import { RegistrationsUpdateJobsService } from '@121-service/src/registrations-update-jobs/registrations-update-jobs.service';
 import { UserService } from '@121-service/src/user/user.service';
+import { UserEmailTemplateType } from '@121-service/src/user/user-emails/enum/user-email-template-type.enum';
 import { UserEmailsModule } from '@121-service/src/user/user-emails/user-emails.module';
+import { UserEmailsService } from '@121-service/src/user/user-emails/user-emails.service';
 
 // This test will make a real email request if validation fails and env vars are set
 // Remove before merge
 describe('RegistrationsUpdateJobsService - integration with real email', () => {
   let registrationsUpdateJobsService: RegistrationsUpdateJobsService;
+  let userEmailsService: UserEmailsService;
 
   beforeEach(async () => {
     const mockRegistrationsService = {
@@ -39,6 +42,7 @@ describe('RegistrationsUpdateJobsService - integration with real email', () => {
     registrationsUpdateJobsService = moduleRef.get(
       RegistrationsUpdateJobsService,
     );
+    userEmailsService = moduleRef.get(UserEmailsService);
   });
 
   it('should send a real email on failed validations', async () => {
@@ -67,6 +71,24 @@ describe('RegistrationsUpdateJobsService - integration with real email', () => {
 
     await expect(
       registrationsUpdateJobsService.processRegistrationsUpdateJob(job),
+    ).resolves.not.toThrow();
+  });
+
+  it('should send a real account created email', async () => {
+    if (!env.MY_EMAIL_ADDRESS) {
+      console.log('Skipping real email test - MY_EMAIL_ADDRESS not set');
+      return;
+    }
+
+    await expect(
+      userEmailsService.sendUserEmail({
+        userEmailTemplateInput: {
+          email: env.MY_EMAIL_ADDRESS,
+          displayName: 'Test User',
+          password: 'TestPassword123!',
+        },
+        userEmailTemplateType: UserEmailTemplateType.accountCreated,
+      }),
     ).resolves.not.toThrow();
   });
 });
