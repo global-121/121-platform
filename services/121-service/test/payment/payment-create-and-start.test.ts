2c3b53fd320807f5db7fb8ab228c04e7e0830b4b
jannisvisser <jannisvisser@redcross.nl>
2025-11-03 12:15:30 +0100

    feat: facilitate 2nd start + api-test (#7436)
    * feat: facilitate 2nd start + api-test
    
    * process comments
    
    * wait for payment in progress to be false

--- services/121-service/test/payment/payment-create-and-start.test.ts
+++ services/121-service/test/payment/payment-create-and-start.test.ts
@@ -1,19 +1,21 @@
 import { HttpStatus } from '@nestjs/common';
 
+import { PaymentEvent } from '@121-service/src/payments/payment-events/enums/payment-event.enum';
 import { TransactionStatusEnum } from '@121-service/src/payments/transactions/enums/transaction-status.enum';
 import { RegistrationStatusEnum } from '@121-service/src/registration/enum/registration-status.enum';
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import { waitFor } from '@121-service/src/utils/waitFor.helper';
 import {
+  createAndStartPayment,
   createPayment,
+  getPaymentEvents,
   getTransactions,
   startPayment,
+  waitForPaymentNotInProgress,
   waitForPaymentTransactionsToComplete,
 } from '@121-service/test/helpers/program.helper';
 import {
   awaitChangeRegistrationStatus,
   getRegistrations,
-  importRegistrations,
   seedIncludedRegistrations,
   waitForRegistrationToHaveUpdatedPaymentCount,
 } from '@121-service/test/helpers/registration.helper';
@@ -131,65 +133,168 @@ describe('Start and create a payment separately', () => {
     expect(registrationAfterStart!.paymentCount).toBe(1);
   });
 
-  it('should not start transactions of non-included registrations', async () => {
-    // Arrange
-    const registrations = [registrationPV5, registrationPV6];
-    await importRegistrations(programId, registrations, accessToken);
-    await awaitChangeRegistrationStatus({
-      programId,
-      referenceIds: registrations.map((r) => r.referenceId),
-      status: RegistrationStatusEnum.included,
-      accessToken,
+  describe('payment start', () => {
+    let paymentId: number;
+    beforeEach(async () => {
+      const registrations = [registrationPV5, registrationPV6];
+      await seedIncludedRegistrations(registrations, programId, accessToken);
+      // only create, not start yet
+      const createPaymentResponse = await createPayment({
+        programId,
+        transferValue,
+        referenceIds: registrations.map((r) => r.referenceId),
+        accessToken,
+      });
+      paymentId = createPaymentResponse.body.id;
+      await waitForPaymentTransactionsToComplete({
+        programId,
+        paymentReferenceIds: registrations.map((r) => r.referenceId),
+        accessToken,
+        maxWaitTimeMs: 20_000,
+        completeStatusses: [TransactionStatusEnum.created],
+      });
+
+      // Make one registration non-included
+      await awaitChangeRegistrationStatus({
+        programId,
+        referenceIds: [registrationPV5.referenceId],
+        status: RegistrationStatusEnum.declined,
+        accessToken,
+      });
     });
-    // only create, not start yet
-    const createPaymentResponse = await createPayment({
+
+    it('should not start transactions of non-included registrations', async () => {
+      // Arrange > beforeEach
+
+      // Act
+      await startPayment({
+        programId,
+        paymentId,
+        accessToken,
+      });
+      // Wait for payment not in progress anymore instead of using waitForPaymentTransactionsToComplete. As we cannot just wait for the other transaction to complete, as we should give the time in theory for the declined transaction to also process, even though the assertion is that it shouldn't.
+      await waitForPaymentNotInProgress({
+        programId,
+        accessToken,
+      });
+
+      // await waitFor(1_000); /
+
+      // Assert
+      const getTransactionsResponse = await getTransactions({
+        programId,
+        paymentId,
+        accessToken,
+      });
+      const transactions = getTransactionsResponse.body;
+      const unstartedTransactions = transactions.filter(
+        (t: any) => t.status === TransactionStatusEnum.created,
+      );
+      const startedTransactions = transactions.filter(
+        (t: any) => t.status !== TransactionStatusEnum.created,
+      );
+
+      expect(unstartedTransactions.length).toBe(1);
+      expect(startedTransactions.length).toBe(1);
+    });
+
+    it('should facilitate 2nd payment start when created transactions present', async () => {
+      // Arrange > beforeEach
+      // start first payment
+      await startPayment({
+        programId,
+        paymentId,
+        accessToken,
+      });
+      // only wait for the transaction of the included registration to complete
+      await waitForPaymentTransactionsToComplete({
+        programId,
+        paymentReferenceIds: [registrationPV6].map((r) => r.referenceId),
+        accessToken,
+        maxWaitTimeMs: 10_000,
+      });
+      // include the other registration again
+      await awaitChangeRegistrationStatus({
+        programId,
+        referenceIds: [registrationPV5.referenceId],
+        status: RegistrationStatusEnum.included,
+        accessToken,
+      });
+
+      // Act - Start payment again
+      const startPaymentResponse = await startPayment({
+        programId,
+        paymentId,
+        accessToken,
+      });
+      await waitForPaymentTransactionsToComplete({
+        programId,
+        paymentReferenceIds: [registrationPV5, registrationPV6].map(
+          (r) => r.referenceId,
+        ),
+        accessToken,
+        maxWaitTimeMs: 10_000,
+      });
+
+      // Assert
+      expect(startPaymentResponse.status).toBe(HttpStatus.ACCEPTED);
+
+      const getTransactionsResponse = await getTransactions({
+        programId,
+        paymentId,
+        accessToken,
+      });
+      const transactions = getTransactionsResponse.body;
+      const startedTransactions = transactions.filter(
+        (t: any) => t.status !== TransactionStatusEnum.created,
+      );
+      expect(startedTransactions.length).toBe(2);
+
+      const paymentEvents = await getPaymentEvents({
+        programId,
+        paymentId,
+        accessToken,
+      });
+      const paymentStartedEvents = paymentEvents.body.data.filter(
+        (e) => e.type === PaymentEvent.started,
+      );
+      expect(paymentStartedEvents.length).toBe(2); // two starts
+    });
+  });
+
+  it('should not facilitate 2nd payment when no created transactions for included registrations left', async () => {
+    // Arrange
+    await seedIncludedRegistrations(
+      [registrationPV5, registrationPV6],
+      programId,
+      accessToken,
+    );
+    const doPaymentResponse = await createAndStartPayment({
       programId,
       transferValue,
-      referenceIds: registrations.map((r) => r.referenceId),
+      referenceIds: [registrationPV5, registrationPV6].map(
+        (r) => r.referenceId,
+      ),
       accessToken,
     });
-    const paymentId = createPaymentResponse.body.id;
     await waitForPaymentTransactionsToComplete({
       programId,
-      paymentReferenceIds: registrations.map((r) => r.referenceId),
+      paymentReferenceIds: [registrationPV5, registrationPV6].map(
+        (r) => r.referenceId,
+      ),
       accessToken,
-      maxWaitTimeMs: 20_000,
-      completeStatusses: [TransactionStatusEnum.created],
+      maxWaitTimeMs: 10_000,
     });
+    const paymentId = doPaymentResponse.body.id;
 
-    // Make one registration non-included
-    await awaitChangeRegistrationStatus({
-      programId,
-      referenceIds: [registrationPV5.referenceId],
-      status: RegistrationStatusEnum.declined,
-      accessToken,
-    });
-
-    // Act
-    await startPayment({
+    // Act - try to start payment again
+    const startPaymentResponse2 = await startPayment({
       programId,
       paymentId,
       accessToken,
     });
-    await waitFor(1_000); // small wait to ensure transactions are started. We cannot listen for specific non-created statuses here as one should stay on created
 
     // Assert
-    const getTransactionsResponse = await getTransactions({
-      programId,
-      paymentId,
-      accessToken,
-    });
-    const transactions = getTransactionsResponse.body;
-
-    const createdTransactions = transactions.filter(
-      (t: any) => t.status === TransactionStatusEnum.created,
-    );
-    const nonCreatedTransactions = transactions.filter(
-      (t: any) => t.status !== TransactionStatusEnum.created,
-    );
-
-    expect(createPaymentResponse.status).toBe(HttpStatus.ACCEPTED);
-    expect(createdTransactions.length).toBe(1);
-    expect(nonCreatedTransactions.length).toBe(1);
+    expect(startPaymentResponse2.status).toBe(HttpStatus.BAD_REQUEST);
   });
 });
