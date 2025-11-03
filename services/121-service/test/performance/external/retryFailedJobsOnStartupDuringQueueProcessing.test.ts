e8bdb6fa8cef33b4f3dbc61c6f181f55c03a7be3
piotrk39 <piotrk39@gmail.com>
2025-11-03 11:51:13 +0100

    Add retryFailedJobsOnStartupDuringQueueProcessing test

---
+++ services/121-service/test/performance/external/retryFailedJobsOnStartupDuringQueueProcessing.test.ts
@@ -0,0 +1,108 @@
+import { HttpStatus } from '@nestjs/common';
+import { env } from 'process';
+
+import { RegistrationStatusEnum } from '@121-service/src/registration/enum/registration-status.enum';
+import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
+import { registrationVisa } from '@121-service/src/seed-data/mock/visa-card.data';
+import { waitFor } from '@121-service/src/utils/waitFor.helper';
+import { doPayment } from '@121-service/test/helpers/program.helper';
+import {
+  changeRegistrationStatus,
+  duplicateRegistrations,
+  importRegistrations,
+  waitForStatusChangeToComplete,
+} from '@121-service/test/helpers/registration.helper';
+import {
+  getAccessToken,
+  resetDB,
+} from '@121-service/test/helpers/utility.helper';
+import {
+  getPaymentResults,
+  isServiceUp,
+  kill121Service,
+} from '@121-service/test/performance/helpers/performance.helper';
+import { programIdOCW } from '@121-service/test/registrations/pagination/pagination-data';
+
+const duplicateNumber = parseInt(env.DUPLICATE_NUMBER || '7'); // cronjob duplicate number should be 2^7 = 128
+const maxWaitTimeMs = 240_000; // 4 minutes
+const passRate = 100; // 100%
+const maxRetryDurationMs = 1_800_000; // 30 minutes
+const delayBetweenAttemptsMs = 5_000; // 5 seconds
+const transferValue = 25;
+const testTimeout = 3_600_000; // 60 minutes
+
+jest.setTimeout(testTimeout);
+describe('Retry Failed Jobs On Startup During Queue Processing', () => {
+  let accessToken: string;
+
+  it('Setup, do payment, kill service and restart', async () => {
+    // Arrange
+    const startTime = Date.now();
+    await resetDB(SeedScript.nlrcMultiple, __filename);
+    accessToken = await getAccessToken();
+    // Upload registration
+    const importRegistrationResponse = await importRegistrations(
+      programIdOCW,
+      [registrationVisa],
+      accessToken,
+    );
+    expect(importRegistrationResponse.statusCode).toBe(HttpStatus.CREATED);
+    // Change status of this registration to 'included'
+    const changeStatusResponse = await changeRegistrationStatus({
+      programId: programIdOCW,
+      status: RegistrationStatusEnum.included,
+      accessToken,
+    });
+    expect(changeStatusResponse.statusCode).toBe(HttpStatus.ACCEPTED);
+    // Wait for all status changes to be processed
+    await waitForStatusChangeToComplete({
+      programId: programIdOCW,
+      amountOfRegistrations: 1,
+      status: RegistrationStatusEnum.included,
+      maxWaitTimeMs,
+      accessToken,
+    });
+    // Duplicate registration to be 128
+    const duplicateRegistrationsResponse = await duplicateRegistrations({
+      powerNumberRegistration: duplicateNumber,
+      accessToken,
+      body: {
+        secret: env.RESET_SECRET,
+      },
+    });
+    expect(duplicateRegistrationsResponse.statusCode).toBe(HttpStatus.CREATED);
+
+    // Do payment
+    const doPaymentResponse = await doPayment({
+      programId: programIdOCW,
+      transferValue,
+      referenceIds: [],
+      accessToken,
+    });
+    expect(doPaymentResponse.statusCode).toBe(HttpStatus.ACCEPTED);
+    // Wait long enough so that jobs are added to the queue but not finished processing
+    await waitFor(5_000);
+    // Kill 121 service to simulate crash during queue processing
+    await kill121Service();
+    // Wait for the service to restart and become available
+    let serviceUp = false;
+    while (!serviceUp) {
+      await waitFor(1_000); // Wait for 1 second before pinging again
+      serviceUp = await isServiceUp();
+    }
+    // Assert
+    // Check payment results to have 100% success rate
+    await getPaymentResults({
+      programId: programIdOCW,
+      paymentId: 1,
+      accessToken,
+      totalAmountPowerOfTwo: duplicateNumber,
+      passRate,
+      maxRetryDurationMs,
+      delayBetweenAttemptsMs,
+      verbose: true,
+    });
+    const elapsedTime = Date.now() - startTime;
+    expect(elapsedTime).toBeLessThan(testTimeout);
+  });
+});
