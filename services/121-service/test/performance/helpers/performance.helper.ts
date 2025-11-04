0e364c18e51837e6a4385dcafee8c33b7b6ac835
piotrk39 <piotrk39@gmail.com>
2025-11-04 15:31:54 +0100

    Remove external integration test workflow add docker logs & move test suite

--- services/121-service/test/performance/helpers/performance.helper.ts
+++ services/121-service/test/performance/helpers/performance.helper.ts
@@ -1,6 +1,5 @@
 import * as request from 'supertest';
 
-import { env } from '@121-service/src/env';
 import { waitFor } from '@121-service/src/utils/waitFor.helper';
 import { getPaymentSummary } from '@121-service/test/helpers/program.helper';
 import { getServer } from '@121-service/test/helpers/utility.helper';
@@ -14,19 +13,6 @@ import {
   RegistrationStatusResult,
 } from '@121-service/test/performance/interfaces/registration-status.interface';
 import { StatusOverviewItem } from '@121-service/test/performance/interfaces/status-overview.interface';
-
-export async function kill121Service(): Promise<request.Response> {
-  return getServer().post('/test/kill-service').send({
-    secret: env.RESET_SECRET,
-  });
-}
-
-export async function isServiceUp(): Promise<boolean> {
-  return getServer()
-    .get('/health/health')
-    .then((response) => response.status === 200)
-    .catch(() => false);
-}
 
 export async function getPaymentResults({
   programId,
