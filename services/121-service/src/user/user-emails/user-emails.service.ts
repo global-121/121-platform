7f6cd172a3fe916a03bc6424e0fcfceb5e16bc88
Yuri <ywilmering@redcross.nl>
2025-11-05 16:11:31 +0100

    simplified the user emails service

--- services/121-service/src/user/user-emails/user-emails.service.ts
+++ services/121-service/src/user/user-emails/user-emails.service.ts
@@ -7,12 +7,9 @@ import { EmailData } from '@121-service/src/emails/interfaces/email-data.interfa
 import { EmailTemplate } from '@121-service/src/emails/interfaces/email-template.interface';
 import { AccountCreatedEvent } from '@121-service/src/user/events/account-created.event';
 import { PasswordResetEvent } from '@121-service/src/user/events/password-reset.event';
-import { UserEmailType } from '@121-service/src/user/user-emails/enum/user-email-type.enum';
-import { UserEmailInput } from '@121-service/src/user/user-emails/interfaces/user-email-input.interface';
 import { buildTemplateAccountCreated } from '@121-service/src/user/user-emails/templates/account-created.template';
 import { buildTemplateAccountCreatedSSO } from '@121-service/src/user/user-emails/templates/account-created-sso.template';
 import { buildTemplatePasswordReset } from '@121-service/src/user/user-emails/templates/password-reset.template';
-import { stripHtmlTags } from '@121-service/src/utils/strip-html-tags.helper';
 
 @Injectable()
 export class UserEmailsService {
@@ -23,90 +20,28 @@ export class UserEmailsService {
   public async handleUserEvent(
     payload: AccountCreatedEvent | PasswordResetEvent,
   ): Promise<void> {
-    let userEmailType: UserEmailType | undefined;
+    let emailTemplate: EmailTemplate | undefined;
+
     if (payload instanceof AccountCreatedEvent) {
-      userEmailType = env.USE_SSO_AZURE_ENTRA
-        ? UserEmailType.accountCreatedForSSO
-        : UserEmailType.accountCreated;
+      if (env.USE_SSO_AZURE_ENTRA) {
+        emailTemplate = buildTemplateAccountCreatedSSO(payload);
+      } else {
+        emailTemplate = buildTemplateAccountCreated(payload);
+      }
     } else if (payload instanceof PasswordResetEvent) {
-      userEmailType = UserEmailType.passwordReset;
+      emailTemplate = buildTemplatePasswordReset(payload);
     }
 
-    if (!userEmailType) {
-      throw new Error(
-        'UserEmailType could not be determined from event payload',
-      );
+    if (!emailTemplate) {
+      throw new Error('No email template found for the given event payload');
     }
 
-    await this.sendUserEmail({
-      userEmailInput: payload,
-      userEmailType,
-    });
-  }
-
-  public async sendUserEmail({
-    userEmailInput,
-    userEmailType,
-  }: {
-    userEmailInput: UserEmailInput;
-    userEmailType: UserEmailType;
-  }): Promise<void> {
-    const emailData: EmailData = this.buildUserEmailData({
-      userEmailType,
-      userEmailInput,
-    });
+    const emailData: EmailData = {
+      email: payload.email,
+      subject: emailTemplate.subject,
+      body: emailTemplate.body,
+    };
 
     await this.emailsService.sendEmail(emailData);
-  }
-
-  private buildUserEmailData({
-    userEmailType,
-    userEmailInput,
-  }: {
-    userEmailType: UserEmailType;
-    userEmailInput: UserEmailInput;
-  }): EmailData {
-    const { email } = userEmailInput;
-
-    const template: EmailTemplate = this.buildUserEmailTemplate({
-      userEmailType,
-      userEmailInput,
-    });
-
-    const userEmailData: EmailData = {
-      email,
-      subject: template.subject,
-      body: template.body,
-    };
-
-    return userEmailData;
-  }
-
-  private buildUserEmailTemplate({
-    userEmailType,
-    userEmailInput,
-  }: {
-    userEmailType: UserEmailType;
-    userEmailInput: UserEmailInput;
-  }): EmailTemplate {
-    let emailTemplate: EmailTemplate;
-    const sanitizedUserEmailInput = {
-      ...userEmailInput,
-      displayName: stripHtmlTags(userEmailInput.displayName),
-    };
-
-    switch (userEmailType) {
-      case UserEmailType.accountCreated:
-        emailTemplate = buildTemplateAccountCreated(sanitizedUserEmailInput);
-        break;
-      case UserEmailType.accountCreatedForSSO:
-        emailTemplate = buildTemplateAccountCreatedSSO(sanitizedUserEmailInput);
-        break;
-      case UserEmailType.passwordReset:
-        emailTemplate = buildTemplatePasswordReset(sanitizedUserEmailInput);
-        break;
-    }
-
-    return emailTemplate;
   }
 }
