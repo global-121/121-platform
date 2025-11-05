92792f69665235e790479c6f5686b921696de73a
piotrk39 <piotrk39@gmail.com>
2025-11-05 14:35:35 +0100

    Add documentation reference

--- services/121-service/test/restart/retryFailedJobsOnStartupDuringQueueProcessing.test.ts
+++ services/121-service/test/restart/retryFailedJobsOnStartupDuringQueueProcessing.test.ts
@@ -23,6 +23,9 @@ import {
   waitForServiceToBeUp,
 } from '@121-service/test/restart/helpers/restart.helper';
 
+// The test set up and it's enviroment differs a bit from all other integration and performance tests. The configuration of this test is explained in the wiki page linked below.
+// See: https://github.com/global-121/121-platform/wiki/Integration-tests/#exception-running-retryfailedjobsonstartupduringqueueprocessingtestts
+
 const duplicateNumber = 5; // 2^5 = 32
 const maxWaitTimeMs = 5_000; // 5 seconds
 const passRate = 100; // 100%
