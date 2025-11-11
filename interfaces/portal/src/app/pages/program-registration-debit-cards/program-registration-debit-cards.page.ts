e8c96c79d5b3d023a181b8f2b193677adfe4b125
Yuri <ywilmering@redcross.nl>
2025-11-11 17:27:10 +0100

    added stuff

--- interfaces/portal/src/app/pages/program-registration-debit-cards/program-registration-debit-cards.page.ts
+++ interfaces/portal/src/app/pages/program-registration-debit-cards/program-registration-debit-cards.page.ts
@@ -14,6 +14,7 @@ import {
 import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { Dialog } from 'primeng/dialog';
 
 import { VisaCardAction } from '@121-service/src/fsp-integrations/integrations/intersolve-visa/enums/intersolve-visa-card-action.enum';
 
@@ -41,6 +42,7 @@ import { ToastService } from '~/services/toast.service';
     ColoredChipComponent,
     FormDialogComponent,
     PageLayoutRegistrationComponent,
+    Dialog,
   ],
   providers: [ToastService],
   templateUrl: './program-registration-debit-cards.page.html',
@@ -153,6 +155,7 @@ export class ProgramRegistrationDebitCardsPageComponent {
     const allCards = this.walletWithCards.data()?.cards;
 
     if (!allCards) {
+      console.log('No cards found in wallet');
       return [];
     }
 
