dcb70cf97695e478636a0f78ba7a6fb7ac205bb8
Niels Bom <nbom@redcross.nl>
2025-11-13 15:23:50 +0100

    chore: update comment

--- services/121-service/src/shared/types/translation-partial.type.ts
+++ services/121-service/src/shared/types/translation-partial.type.ts
@@ -1,6 +1,10 @@
 import { Language } from '@121-service/src/shared/types/language.type';
 import { TranslationFull } from '@121-service/src/shared/types/translation-full.type';
 
+/**
+ * "Partial translation" means that there may be 0..n keys for languages in
+ * TLanguage.
+ */
 export type TranslationPartial<TLanguage extends Language> = Partial<
   TranslationFull<TLanguage>
 >;
