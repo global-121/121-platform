234cf3581f1a7af2fa7d059600760638d6d722b7
Jannis Visser <jannisvisser@redcross.nl>
2025-11-14 14:04:26 +0100

    fix index

--- services/121-service/src/migration/1763105665428-rename-transfer-transaction.ts
+++ services/121-service/src/migration/1763105665428-rename-transfer-transaction.ts
@@ -6,24 +6,26 @@ export class RenameTransferTransaction1763105665428
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       CREATE INDEX IF NOT EXISTS idx_transaction_event_description ON "121-service".transaction_event(description);
-
+    `);
+    await queryRunner.query(`
       UPDATE "121-service".transaction_event
       SET description = 'Transaction created'
       WHERE description = 'Transfer created';
-
+    `);
+    await queryRunner.query(`
       UPDATE "121-service".transaction_event
       SET description = 'Transaction started'
       WHERE description = 'Transfer started';
-
+    `);
+    await queryRunner.query(`
       UPDATE "121-service".transaction_event
       SET description = 'Transaction retried'
       WHERE description = 'Transfer retried';
-
+    `);
+    await queryRunner.query(`
       UPDATE "121-service".transaction_event
       SET description = 'Transaction approval'
       WHERE description = 'Transfer approval';
-
-      DROP INDEX IF EXISTS idx_transaction_event_description;
     `);
   }
 
