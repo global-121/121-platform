48c54a8efc9071edc24f38ad69d341474640006e
Jannis Visser <jannisvisser@redcross.nl>
2025-11-14 13:34:19 +0100

    update migration

--- services/121-service/src/migration/1763105665428-rename-transfer-transaction.ts
+++ services/121-service/src/migration/1763105665428-rename-transfer-transaction.ts
@@ -5,10 +5,22 @@ export class RenameTransferTransaction1763105665428
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            UPDATE "121-service".transaction_event
-            SET description = REPLACE(description, 'Transfer', 'Transaction')
-            WHERE description LIKE '%Transfer%';
-        `);
+      UPDATE "121-service".transaction_event
+      SET description = 'Transaction created'
+      WHERE description = 'Transfer created';
+
+      UPDATE "121-service".transaction_event
+      SET description = 'Transaction started'
+      WHERE description = 'Transfer started';
+
+      UPDATE "121-service".transaction_event
+      SET description = 'Transaction retried'
+      WHERE description = 'Transfer retried';
+
+      UPDATE "121-service".transaction_event
+      SET description = 'Transaction approval'
+      WHERE description = 'Transfer approval';
+    `);
   }
 
   public async down(_queryRunner: QueryRunner): Promise<void> {
