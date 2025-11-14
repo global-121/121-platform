eb4b483800a24bf23a317bcc0e4a26d7460047dc
Niels Bom <nbom@redcross.nl>
2025-11-14 15:52:26 +0100

    chore - improve comments

--- services/121-service/src/shared/enum/ui-language.enum.ts
+++ services/121-service/src/shared/enum/ui-language.enum.ts
@@ -1,6 +1,6 @@
 /**
- * Supported locales for the User Interface.
- * See: <https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes>
+ * Supported languages for the User Interface.
+ * See: https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
  * Make sure to use the 2-letter code from the "Set 1"-column.
  */
 export enum UILanguage {
