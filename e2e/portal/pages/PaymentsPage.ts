617b5942fbf3ff7ab36e02cbf5a089139e8b0584
piotrk39 <piotrk39@gmail.com>
2025-11-14 16:55:09 +0100

    Change assertion

--- e2e/portal/pages/PaymentsPage.ts
+++ e2e/portal/pages/PaymentsPage.ts
@@ -161,9 +161,12 @@ class PaymentsPage extends BasePage {
       .getByTitle(date)
       .textContent();
 
-    const includedRegistrationsElement = await card
-      .filter({ hasText: 'Included reg.' })
-      .textContent();
+    const includedRegistrationsLocator = card.filter({
+      hasText: 'Included reg.',
+    });
+    await includedRegistrationsLocator.waitFor({ state: 'visible' });
+    const includedRegistrationsElement =
+      await includedRegistrationsLocator.textContent();
 
     // Get the total amount element within the card
     const totalAmountElement = await card
