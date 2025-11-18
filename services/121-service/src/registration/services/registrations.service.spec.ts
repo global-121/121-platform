8fe4cba8dbd1e36899498a2e47c4c3c06fb39f4f
Yuri <ywilmering@redcross.nl>
2025-11-18 10:45:49 +0100

    tiny test cleanup

--- services/121-service/src/registration/services/registrations.service.spec.ts
+++ services/121-service/src/registration/services/registrations.service.spec.ts
@@ -35,7 +35,7 @@ describe('RegistrationsService', () => {
   let registrationEventsService: RegistrationEventsService;
 
   beforeEach(async () => {
-    const module: TestingModule §= await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: IntersolveVisaDataSynchronizationService,
