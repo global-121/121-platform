bd9c1e9bf805ee00e6e9f96b8395045eb11f687b
Ruben <vandervalk@geoit.nl>
2025-11-21 21:49:42 +0300

    unit test fix

--- services/121-service/src/utils/test-helpers/createQueryBuilderMock.helper.ts
+++ services/121-service/src/utils/test-helpers/createQueryBuilderMock.helper.ts
@@ -7,6 +7,7 @@ interface QueryBuilderMock {
   getMany?: () => any;
   getRawMany?: () => any;
   distinct?: () => QueryBuilderMock;
+  orderBy?: () => QueryBuilderMock;
 }
 
 export function generateMockCreateQueryBuilder(
@@ -20,6 +21,7 @@ export function generateMockCreateQueryBuilder(
     andWhere: () => mock,
     leftJoin: () => mock,
     distinct: () => mock,
+    orderBy: () => mock,
   };
 
   if (options.useGetMany) {
