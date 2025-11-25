f70f4a50df8a15c5987fdd1e0b0143c8a8824639
Niels Bom <nbom@redcross.nl>
2025-11-25 14:20:06 +0100

    refactor: rename variable

--- interfaces/portal/src/app/utils/get-linguonym.ts
+++ interfaces/portal/src/app/utils/get-linguonym.ts
@@ -17,15 +17,15 @@ export const getLinguonym = ({
     type: 'language',
   });
   // Unlikely but fallback to the language code itself.
-  let possibleResult: string | undefined;
+  let possibleLinguonym: string | undefined;
   try {
     // The database can contain language codes that are not standard.
     // Calling this method with invalid input will produce a RangeError.
     // We catch this and fall back to the original language code, ex: 'et_AM'.
-    possibleResult = names.of(languageToDisplayNameOf);
+    possibleLinguonym = names.of(languageToDisplayNameOf);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars -- Error variable not needed.
   } catch (_) {
     // do nothing
   }
-  return possibleResult ?? languageToDisplayNameOf;
+  return possibleLinguonym ?? languageToDisplayNameOf;
 };
