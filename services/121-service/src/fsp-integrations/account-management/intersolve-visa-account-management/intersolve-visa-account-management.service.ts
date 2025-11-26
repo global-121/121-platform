72b05c2a0d9b1fe4f0e857dce28d35a256949c62
Yuri <ywilmering@redcross.nl>
2025-11-26 07:35:37 +0100

    programId

--- services/121-service/src/fsp-integrations/account-management/intersolve-visa-account-management/intersolve-visa-account-management.service.ts
+++ services/121-service/src/fsp-integrations/account-management/intersolve-visa-account-management/intersolve-visa-account-management.service.ts
@@ -448,6 +448,7 @@ export class IntersolveVisaAccountManagementService {
     const registration =
       await this.registrationUtilsService.getRegistrationOrThrow({
         referenceId,
+        programId,
       });
 
     const registrationData = await this.getRegistrationData(registration);
