909274742efbd55fc4929877850f637e9d65930d
Yuri <ywilmering@redcross.nl>
2025-11-27 11:36:17 +0100

    remover unused file

--- services/121-service/src/registration/interfaces/registration-attribute-change-handler.interface.ts
+++
@@ -1,9 +0,0 @@
-import { RegistrationEntity } from '@121-service/src/registration/entities/registration.entity';
-
-export interface RegistrationAttributeChangeHandler {
-  handleAttributeChange(params: {
-    readonly registration: RegistrationEntity;
-    readonly attribute: string;
-    readonly value: string | number | string[] | boolean | null;
-  }): Promise<void>;
-}
