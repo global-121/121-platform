27456ef22d3e458cbef7ff634ab9b24f51df91e7
Jannis Visser <jannisvisser@redcross.nl>
2025-12-01 12:39:50 +0100

    drop view first in migration

--- services/121-service/src/migration/1763138063129-transaction-view-paginate.ts
+++ services/121-service/src/migration/1763138063129-transaction-view-paginate.ts
@@ -7,6 +7,13 @@ export class TransactionViewPaginate1763138063129
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `DROP VIEW IF EXISTS "121-service"."transaction_view"`,
+    );
+    await queryRunner.query(
+      `DELETE FROM "121-service"."typeorm_metadata" WHERE "schema" = $1 AND "type" = $2 AND "name" = $3`,
+      ['121-service', 'VIEW', 'transaction_view'],
+    );
+    await queryRunner.query(
       `CREATE VIEW "121-service"."transaction_view" AS SELECT "t"."id" AS "id", "t"."created" AS "created", "t"."updated" AS "updated", "t"."userId" AS "userId", "t"."transferValue" AS "transferValue", "t"."status" AS "status", "t"."paymentId" AS "paymentId", "t"."registrationId" AS "registrationId", "event"."errorMessage" AS "errorMessage", "event"."programFspConfigurationId" AS "programFspConfigurationId", "fspconfig"."fspName" AS "fspName", "fspconfig"."name" AS "programFspConfigurationName", "fspconfig"."label" AS "programFspConfigurationLabel", "registration"."registrationStatus" AS "registrationStatus", "registration"."referenceId" AS "registrationReferenceId", "registration"."registrationProgramId" AS "registrationProgramId", "registration"."scope" AS "registrationScope" FROM "121-service"."transaction" "t" INNER JOIN "121-service"."last_transaction_event" "lte" ON "t"."id" = "lte"."transactionId"  LEFT JOIN "121-service"."transaction_event" "event" ON "lte"."transactionEventId" = "event"."id"  LEFT JOIN "121-service"."program_fsp_configuration" "fspconfig" ON "event"."programFspConfigurationId" = "fspconfig"."id"  LEFT JOIN "121-service"."registration" "registration" ON "t"."registrationId" = "registration"."id"`,
     );
     await queryRunner.query(
