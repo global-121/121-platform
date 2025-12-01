68bf935b576a1f69a87e0ef16c09f868b64fe624
Yuri <ywilmering@redcross.nl>
2025-12-01 12:41:07 +0100

    using app dialog

--- interfaces/portal/src/app/pages/program-registration-debit-cards/program-registration-debit-cards.page.ts
+++ interfaces/portal/src/app/pages/program-registration-debit-cards/program-registration-debit-cards.page.ts
@@ -16,7 +16,6 @@ import {
 import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { Dialog } from 'primeng/dialog';
 
 import { VisaCardAction } from '@121-service/src/fsp-integrations/integrations/intersolve-visa/enums/intersolve-visa-card-action.enum';
 
@@ -30,7 +29,6 @@ import { FormDialogComponent } from '~/components/form-dialog/form-dialog.compon
 import { PageLayoutRegistrationComponent } from '~/components/page-layout-registration/page-layout-registration.component';
 import { ProgramApiService } from '~/domains/program/program.api.service';
 import { RegistrationApiService } from '~/domains/registration/registration.api.service';
-import { IssueCardDialogComponent } from '~/pages/program-registration-debit-cards/components/issue-card/issue-card.dialog.component';
 import { RtlHelperService } from '~/services/rtl-helper.service';
 import { ToastService } from '~/services/toast.service';
 
@@ -45,8 +43,6 @@ import { ToastService } from '~/services/toast.service';
     ColoredChipComponent,
     FormDialogComponent,
     PageLayoutRegistrationComponent,
-    Dialog,
-    IssueCardDialogComponent,
   ],
   providers: [ToastService],
   templateUrl: './program-registration-debit-cards.page.html',
@@ -63,6 +59,8 @@ export class ProgramRegistrationDebitCardsPageComponent {
   private readonly toastService = inject(ToastService);
   private readonly queryClient = inject(QueryClient);
   private readonly programApiService = inject(ProgramApiService);
+
+  readonly tokenCode = model('');
 
   registration = injectQuery(
     this.registrationApiService.getRegistrationById(
@@ -242,9 +240,30 @@ export class ProgramRegistrationDebitCardsPageComponent {
     },
   }));
 
-  readonly currencyCode = computed(() => this.program.data()?.currency);
+  linkCardMutation = injectMutation(() => ({
+    mutationFn: () => {
+      const referenceId = this.referenceId();
 
-  public readonly dialogVisible = model(false);
+      if (!referenceId) {
+        this.toastService.showGenericError();
+        throw new Error('ReferenceId is missing');
+      }
+
+      return this.registrationApiService.linkCardToRegistration({
+        programId: this.programId,
+        referenceId: this.referenceId,
+        tokenCode: this.tokenCode,
+      });
+    },
+    onSuccess: () => {
+      this.toastService.showToast({
+        detail: $localize`Card successfully linked`,
+      });
+      this.invalidateWalletQuery();
+    },
+  }));
+
+  readonly currencyCode = computed(() => this.program.data()?.currency);
 
   private invalidateWalletQuery() {
     void this.queryClient.invalidateQueries({
