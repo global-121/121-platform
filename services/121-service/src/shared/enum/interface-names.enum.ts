db72b73a06e368520a30f4bba2d3694b3f6fae98
Elwin Schmitz <eschmitz@redcross.nl>
2025-12-10 11:43:35 +0100

    feat: Add TelemetryProcessor to correlate Portal-requests to back-end (#7547)

--- services/121-service/src/shared/enum/interface-names.enum.ts
+++ services/121-service/src/shared/enum/interface-names.enum.ts
@@ -1,3 +1,5 @@
+export const INTERFACE_NAME_HEADER = 'x-121-interface';
+
 export enum InterfaceNames {
   portal = 'portal', // Hard-coded, used in: interfaces/portal/src/logout/logout.js (make sure to keep in sync)
 }
