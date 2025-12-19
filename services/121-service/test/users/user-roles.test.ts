f8608a0a53559c2cd4a8ab6bf5d4cd8892182ff0
Piotrk39 <piotrk39@gmail.com>
2025-12-19 14:44:09 +0100

    Update descriptions for tests and snapshots

--- services/121-service/test/users/user-roles.test.ts
+++ services/121-service/test/users/user-roles.test.ts
@@ -8,149 +8,129 @@ import {
   resetDB,
 } from '@121-service/test/helpers/utility.helper';
 
-describe('/ Users', () => {
-  describe('/ Roles', () => {
-    let accessToken: string;
+describe('/ Roles', () => {
+  let accessToken: string;
 
-    beforeEach(async () => {
-      await resetDB(SeedScript.testMultiple, __filename);
-      accessToken = await getAccessToken();
-    });
+  beforeEach(async () => {
+    await resetDB(SeedScript.testMultiple, __filename);
+    accessToken = await getAccessToken();
+  });
 
-    it('should create roles when using valid permissions', async () => {
-      // Arrange
+  it('should create roles when using valid permissions', async () => {
+    // Act
+    const response = await getServer()
+      .post('/roles')
+      .set('Cookie', [accessToken])
+      .send({
+        role: 'test-manager',
+        label: 'Do stuff with certain permissions',
+        description: 'This is a test role',
+        permissions: ['program.update', 'program:metrics.read'],
+      });
 
-      // Act
-      const response = await getServer()
-        .post('/roles')
-        .set('Cookie', [accessToken])
-        .send({
-          role: 'test-manager',
-          label: 'Do stuff with certain permissions',
-          description: 'This is a test role',
-          permissions: ['program.update', 'program:metrics.read'],
-        });
+    // Assert
+    expect(response.status).toBe(HttpStatus.CREATED);
+  });
 
-      // Assert
-      expect(response.status).toBe(HttpStatus.CREATED);
-    });
+  it('should not create a role when the role already exists', async () => {
+    // Arrange
+    const roleId = 'test-manager';
+    // Act
+    const response = await getServer()
+      .post('/roles')
+      .set('Cookie', [accessToken])
+      .send({
+        role: roleId,
+        label: 'Do stuff with certain permissions',
+        description: 'This is a test role',
+        permissions: ['program.update', 'program:metrics.read'],
+      });
+    // Assert
+    expect(response.status).toBe(HttpStatus.CREATED);
+    // Act
+    const response2 = await getServer()
+      .post('/roles')
+      .set('Cookie', [accessToken])
+      .send({
+        role: roleId,
+        label: 'Do stuff with certain permissions',
+        description: 'This is a test role',
+        permissions: ['program.update', 'program:metrics.read'],
+      });
+    // Assert
+    expect(response2.status).toBe(HttpStatus.CONFLICT);
+    expect(response2.text).toMatchSnapshot();
+  });
 
-    it('should not create a role when the role already exists', async () => {
-      // Arrange
-      const roleId = 'test-manager';
+  it('should not create a role when using a permission that does not exist', async () => {
+    // Arrange
+    const fakePermission = 'program.make-up-my-own-permission';
+    // Act
+    const response = await getServer()
+      .post('/roles')
+      .set('Cookie', [accessToken])
+      .send({
+        role: 'test-manager',
+        label: 'Do stuff with certain permissions',
+        description: 'This is a test role',
+        permissions: ['program.update', fakePermission, 'program:metrics.read'],
+      });
+    // Assert
+    expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    expect(response.text).toMatchSnapshot();
+  });
 
-      // Act
-      const response = await getServer()
-        .post('/roles')
-        .set('Cookie', [accessToken])
-        .send({
-          role: roleId,
-          label: 'Do stuff with certain permissions',
-          description: 'This is a test role',
-          permissions: ['program.update', 'program:metrics.read'],
-        });
+  it('should return all user roles', async () => {
+    // Act
+    const response = await getUserRoles(accessToken);
+    const rolesLength = response.body.length;
+    // Assert
+    expect(response.status).toBe(HttpStatus.OK);
+    expect(rolesLength).toBe(10); // all default roles
+  });
 
-      // Assert
-      expect(response.status).toBe(HttpStatus.CREATED);
+  it('should update a role by userRoleId', async () => {
+    // Arrange
+    const userId = 1;
+    const updateData = {
+      label: 'Updated user role label',
+      description: 'Updated user role description',
+    };
+    // Act
+    const updateUserRole = await getServer()
+      .put(`/roles/${userId}`)
+      .set('Cookie', [accessToken])
+      .send(updateData);
+    // Assert
+    expect(updateUserRole.status).toBe(HttpStatus.OK);
+    const getUserRole = await getUserRoles(accessToken);
 
-      // Act
-      const response2 = await getServer()
-        .post('/roles')
-        .set('Cookie', [accessToken])
-        .send({
-          role: roleId,
-          label: 'Do stuff with certain permissions',
-          description: 'This is a test role',
-          permissions: ['program.update', 'program:metrics.read'],
-        });
+    expect(getUserRole.status).toBe(HttpStatus.OK);
+    const role = getUserRole.body.find((r: { id: number }) => r.id === userId);
 
-      // Assert
-      expect(response2.status).toBe(HttpStatus.CONFLICT);
-      expect(response2.text).toMatchSnapshot();
-    });
+    expect(role.label).toBe(updateData.label);
+    expect(role.description).toBe(updateData.description);
+  });
 
-    it('should not create a role when using a permission that does not exist', async () => {
-      // Arrange
-      const fakePermission = 'program.make-up-my-own-permission';
-
-      // Act
-      const response = await getServer()
-        .post('/roles')
-        .set('Cookie', [accessToken])
-        .send({
-          role: 'test-manager',
-          label: 'Do stuff with certain permissions',
-          description: 'This is a test role',
-          permissions: [
-            'program.update',
-            fakePermission,
-            'program:metrics.read',
-          ],
-        });
-
-      // Assert
-      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
-      expect(response.text).toMatchSnapshot();
-    });
-
-    it('should return all user roles', async () => {
-      // Arrange
-
-      // Act
-      const response = await getUserRoles(accessToken);
-      const rolesLength = response.body.length;
-
-      // Assert
-      expect(response.status).toBe(HttpStatus.OK);
-      expect(rolesLength).toBe(10); // all default roles
-    });
-
-    it('should update a role by userRoleId', async () => {
-      // Arrange
-      const userId = 1;
-      const updateData = {
-        label: 'Updated user role label',
-        description: 'Updated user role description',
-      };
-      // Act
-      const updateUserRole = await getServer()
-        .put(`/roles/${userId}`)
-        .set('Cookie', [accessToken])
-        .send(updateData);
-
-      // Assert
-      expect(updateUserRole.status).toBe(HttpStatus.OK);
-      const getUserRole = await getUserRoles(accessToken);
-
-      expect(getUserRole.status).toBe(HttpStatus.OK);
-      const role = getUserRole.body.find(
-        (r: { id: number }) => r.id === userId,
-      );
-
-      expect(role.label).toBe(updateData.label);
-      expect(role.description).toBe(updateData.description);
-    });
-
-    it('should delete a role by userRoleId', async () => {
-      // Arrange
-      const userId = 2;
-      // Get user roles before delete
-      const getUserRoleBeforeDelete = await getUserRoles(accessToken);
-      expect(getUserRoleBeforeDelete.status).toBe(HttpStatus.OK);
-      const rolesLengthBeforeDelete = getUserRoleBeforeDelete.body.length;
-      // Act
-      // Delete user role
-      const deleteUserRole = await getServer()
-        .delete(`/roles/${userId}`)
-        .set('Cookie', [accessToken])
-        .send();
-      expect(deleteUserRole.status).toBe(HttpStatus.OK);
-      // Assert
-      // Get user roles after delete
-      const getUserRoleAfterDelete = await getUserRoles(accessToken);
-      expect(getUserRoleAfterDelete.status).toBe(HttpStatus.OK);
-      const rolesLengthAfterDelete = getUserRoleAfterDelete.body.length;
-      expect(rolesLengthAfterDelete).toBe(rolesLengthBeforeDelete - 1);
-    });
+  it('should delete a role by userRoleId', async () => {
+    // Arrange
+    const userId = 2;
+    // Get user roles before delete
+    const getUserRoleBeforeDelete = await getUserRoles(accessToken);
+    expect(getUserRoleBeforeDelete.status).toBe(HttpStatus.OK);
+    const rolesLengthBeforeDelete = getUserRoleBeforeDelete.body.length;
+    // Act
+    // Delete user role
+    const deleteUserRole = await getServer()
+      .delete(`/roles/${userId}`)
+      .set('Cookie', [accessToken])
+      .send();
+    expect(deleteUserRole.status).toBe(HttpStatus.OK);
+    // Assert
+    // Get user roles after delete
+    const getUserRoleAfterDelete = await getUserRoles(accessToken);
+    expect(getUserRoleAfterDelete.status).toBe(HttpStatus.OK);
+    const rolesLengthAfterDelete = getUserRoleAfterDelete.body.length;
+    expect(rolesLengthAfterDelete).toBe(rolesLengthBeforeDelete - 1);
   });
 });
--- services/121-service/test/users/users.test.ts
+++ services/121-service/test/users/users.test.ts
@@ -15,153 +15,146 @@ import {
 import { programIdPV } from '@121-service/test/registrations/pagination/pagination-data';
 
 describe('/ Users', () => {
-  describe('/ Roles', () => {
-    let accessToken: string;
+  let accessToken: string;
 
-    beforeEach(async () => {
-      await resetDB(SeedScript.testMultiple, __filename);
-      accessToken = await getAccessToken();
-    });
+  beforeEach(async () => {
+    await resetDB(SeedScript.testMultiple, __filename);
+    accessToken = await getAccessToken();
+  });
 
-    it('should logout user', async () => {
-      // Arrange
+  it('should logout user', async () => {
+    // Act
+    const logoutResponse = await logoutUser(accessToken);
+    // Assert
+    expect(logoutResponse.status).toBe(HttpStatus.CREATED);
+  });
 
-      // Act
-      const logoutResponse = await logoutUser(accessToken);
-      // Assert
-      expect(logoutResponse.status).toBe(HttpStatus.CREATED);
-    });
+  it('should change user password', async () => {
+    // Arrange
+    const changePasswordPayload = {
+      username: 'admin@example.org',
+      password: 'password',
+      newPassword: 'newPassword',
+    };
+    // Act
+    const changePasswordResponse = await getServer()
+      .post('/users/password')
+      .set('Cookie', [accessToken])
+      .send(changePasswordPayload);
+    expect(changePasswordResponse.status).toBe(HttpStatus.CREATED);
 
-    it('should change user password', async () => {
-      // Arrange
-      const changePasswordPayload = {
-        username: 'admin@example.org',
-        password: 'password',
-        newPassword: 'newPassword',
-      };
-      // Act
-      const changePasswordResponse = await getServer()
-        .post('/users/password')
-        .set('Cookie', [accessToken])
-        .send(changePasswordPayload);
-      expect(changePasswordResponse.status).toBe(HttpStatus.CREATED);
+    const logoutResponse = await logoutUser(accessToken);
+    expect(logoutResponse.status).toBe(HttpStatus.CREATED);
+    // Assert
+    const loginResponseAfterPasswordChange = await loginApi(
+      changePasswordPayload.username,
+      changePasswordPayload.newPassword,
+    );
+    expect(loginResponseAfterPasswordChange.status).toBe(HttpStatus.CREATED);
+  });
 
-      const logoutResponse = await logoutUser(accessToken);
-      expect(logoutResponse.status).toBe(HttpStatus.CREATED);
-      // Assert
-      const loginResponseAfterPasswordChange = await loginApi(
-        changePasswordPayload.username,
-        changePasswordPayload.newPassword,
-      );
-      expect(loginResponseAfterPasswordChange.status).toBe(HttpStatus.CREATED);
-    });
+  it('should delete user', async () => {
+    // Arrange
+    const userId = 2;
+    const userListBeforeDelete: string[] = [];
+    const userListAfterDelete: string[] = [];
+    // Act
+    const getAllUsersBeforeDelete = await getAllUsers(accessToken);
+    const usersLengthBeforeDelete = getAllUsersBeforeDelete.body.length;
 
-    it('should delete user', async () => {
-      // Arrange
-      const userId = 2;
-      const userListBeforeDelete: string[] = [];
-      const userListAfterDelete: string[] = [];
-      // Act
-      const getAllUsersBeforeDelete = await getAllUsers(accessToken);
-      const usersLengthBeforeDelete = getAllUsersBeforeDelete.body.length;
+    for (let i = 0; i < usersLengthBeforeDelete; i++) {
+      const user = getAllUsersBeforeDelete.body[i].username;
+      userListBeforeDelete.push(user);
+    }
 
-      for (let i = 0; i < usersLengthBeforeDelete; i++) {
-        const user = getAllUsersBeforeDelete.body[i].username;
-        userListBeforeDelete.push(user);
-      }
+    const deleteUserResponse = await getServer()
+      .delete(`/users/${userId}`)
+      .set('Cookie', [accessToken])
+      .send();
+    // Assert
+    expect(deleteUserResponse.status).toBe(HttpStatus.OK);
+    const getAllUsersAfterDelete = await getAllUsers(accessToken);
+    const usersLengthAfterDelete = getAllUsersAfterDelete.body.length;
 
-      const deleteUserResponse = await getServer()
-        .delete(`/users/${userId}`)
-        .set('Cookie', [accessToken])
-        .send();
-      // Assert
-      expect(deleteUserResponse.status).toBe(HttpStatus.OK);
-      const getAllUsersAfterDelete = await getAllUsers(accessToken);
-      const usersLengthAfterDelete = getAllUsersAfterDelete.body.length;
+    for (let i = 0; i < usersLengthAfterDelete; i++) {
+      const user = getAllUsersAfterDelete.body[i].username;
+      userListAfterDelete.push(user);
+    }
+    expect(usersLengthAfterDelete).toBe(usersLengthBeforeDelete - 1);
+    expect(userListAfterDelete).not.toEqual(userListBeforeDelete);
+  });
 
-      for (let i = 0; i < usersLengthAfterDelete; i++) {
-        const user = getAllUsersAfterDelete.body[i].username;
-        userListAfterDelete.push(user);
-      }
-      expect(usersLengthAfterDelete).toBe(usersLengthBeforeDelete - 1);
-      expect(userListAfterDelete).not.toEqual(userListBeforeDelete);
-    });
+  // NOTE: for this reset of password it seems like only the correct response status can be validated
+  it('should reset user password', async () => {
+    // Arrange
+    const resetPasswordPayload = { username: 'admin@example.org' };
+    // Act
+    const resetPasswordResponse = await getServer()
+      .patch('/users/password')
+      .set('Cookie', [accessToken])
+      .send(resetPasswordPayload);
+    // Assert
+    expect(resetPasswordResponse.status).toBe(HttpStatus.NO_CONTENT);
+  });
 
-    // NOTE: for this reset of password it seems like only the correct response status can be validated
-    it('should reset user password', async () => {
-      // Arrange
-      const resetPasswordPayload = { username: 'admin@example.org' };
-      // Act
-      const resetPasswordResponse = await getServer()
-        .patch('/users/password')
-        .set('Cookie', [accessToken])
-        .send(resetPasswordPayload);
-      // Assert
-      expect(resetPasswordResponse.status).toBe(HttpStatus.NO_CONTENT);
-    });
+  it('should update user properties (isOrganizationAdmin, displayName)', async () => {
+    // Arrange
+    const userId = 2;
+    const updateData = {
+      isOrganizationAdmin: true,
+      displayName: 'Updated Display Name',
+    };
+    // Act
+    const updateUserResponse = await getServer()
+      .patch(`/users/${userId}`)
+      .set('Cookie', [accessToken])
+      .send(updateData);
+    // Assert
+    expect(updateUserResponse.status).toBe(HttpStatus.OK);
+    const getUsersResponse = await getAllUsers(accessToken);
+    expect(getUsersResponse.status).toBe(HttpStatus.OK);
+    const updatedUser = getUsersResponse.body.find(
+      (r: { id: number }) => r.id === userId,
+    );
+    expect(updatedUser.isOrganizationAdmin).toBe(
+      updateData.isOrganizationAdmin,
+    );
+    expect(updatedUser.displayName).toBe(updateData.displayName);
+  });
 
-    it('should update user properties (isOrganizationAdmin, displayName)', async () => {
-      // Arrange
-      const userId = 2;
-      const updateData = {
-        isOrganizationAdmin: true,
-        displayName: 'Updated Display Name',
-      };
-      // Act
-      const updateUserResponse = await getServer()
-        .patch(`/users/${userId}`)
-        .set('Cookie', [accessToken])
-        .send(updateData);
-      // Assert
-      expect(updateUserResponse.status).toBe(HttpStatus.OK);
-      const getUsersResponse = await getAllUsers(accessToken);
-      expect(getUsersResponse.status).toBe(HttpStatus.OK);
-      const updatedUser = getUsersResponse.body.find(
-        (r: { id: number }) => r.id === userId,
-      );
-      expect(updatedUser.isOrganizationAdmin).toBe(
-        updateData.isOrganizationAdmin,
-      );
-      expect(updatedUser.displayName).toBe(updateData.displayName);
-    });
+  it('should get all users', async () => {
+    // Arrange
 
-    it('should get all users', async () => {
-      // Arrange
+    // Act
+    const getAllUsersResponse = await getAllUsers(accessToken);
+    const usersLength = getAllUsersResponse.body.length;
 
-      // Act
-      const getAllUsersResponse = await getAllUsers(accessToken);
-      const usersLength = getAllUsersResponse.body.length;
+    // Assert
+    expect(getAllUsersResponse.status).toBe(HttpStatus.OK);
+    expect(usersLength).toBe(10); // 1 user per default role
+  });
 
-      // Assert
-      expect(getAllUsersResponse.status).toBe(HttpStatus.OK);
-      expect(usersLength).toBe(10); // 1 user per default role
-    });
+  it('should get current user', async () => {
+    // Act
+    const getCurrentUserResponse = await getServer()
+      .get('/users/current')
+      .set('Cookie', [accessToken])
+      .send();
+    // Assert
+    const currentUser = getCurrentUserResponse.body.user;
 
-    it('should get current user', async () => {
-      // Arrange
-      // Act
-      const getCurrentUserResponse = await getServer()
-        .get('/users/current')
-        .set('Cookie', [accessToken])
-        .send();
-      // Assert
-      const currentUser = getCurrentUserResponse.body.user;
+    expect(getCurrentUserResponse.status).toBe(HttpStatus.OK);
+    expect(currentUser.username).toBe('admin@example.org');
+  });
 
-      expect(getCurrentUserResponse.status).toBe(HttpStatus.OK);
-      expect(currentUser.username).toBe('admin@example.org');
-    });
-
-    it('should return all users assigned to a program', async () => {
-      // Arrange
-
-      // Act
-      const fetchUsersFromPvProgram = await getAllUsersByProgramId(
-        accessToken,
-        programIdPV.toString(),
-      );
-      // Assert
-      expect(fetchUsersFromPvProgram.status).toBe(HttpStatus.OK);
-      expect(fetchUsersFromPvProgram.body.length).toBe(10);
-    });
+  it('should return all users assigned to a program', async () => {
+    // Act
+    const fetchUsersFromPvProgram = await getAllUsersByProgramId(
+      accessToken,
+      programIdPV.toString(),
+    );
+    // Assert
+    expect(fetchUsersFromPvProgram.status).toBe(HttpStatus.OK);
+    expect(fetchUsersFromPvProgram.body.length).toBe(10);
   });
 });
