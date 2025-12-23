83d4f8a225cd5a2715a805cfa8228221dd7fb3af
Jannis Visser <jannisvisser@redcross.nl>
2025-12-23 08:50:12 +0100

    rebase

--- services/121-service/src/user/approver/dto/approver-response.dto.ts
+++ services/121-service/src/user/approver/dto/approver-response.dto.ts
@@ -4,6 +4,9 @@ export class ApproverResponseDto {
   @ApiProperty({ example: 1 })
   public readonly id: number;
 
+  @ApiProperty({ example: 4 })
+  public readonly userId: number;
+
   @ApiProperty({ example: 'johndoe' })
   public readonly username: string | null;
 
