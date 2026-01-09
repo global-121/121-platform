5db3f530a620aaaf1da290faac3fc650d1ccb17f
Jannis Visser <jannisvisser@redcross.nl>
2026-01-09 08:02:22 +0100

    rebase fix

--- services/121-service/test/payment/fsp-integrations/do-payment-fsp-visa-debit/do-payment-fsp-visa-debit-error.test.ts
+++ services/121-service/test/payment/fsp-integrations/do-payment-fsp-visa-debit/do-payment-fsp-visa-debit-error.test.ts
@@ -436,7 +436,7 @@ describe('Do failing payment with FSP Visa Debit', () => {
     });
 
     // Act
-    const doPaymentResponse = await createAndStartPayment({
+    const doPaymentResponse = await doPayment({
       programId: programIdVisa,
       transferValue: 1000,
       referenceIds: [uniqueRegistration.referenceId],
