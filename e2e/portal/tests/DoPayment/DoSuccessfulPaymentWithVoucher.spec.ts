54be8005ce28253d7387cbcf33929409c81a7401
Piotrk39 <piotrk39@gmail.com>
2026-01-16 20:16:26 +0100

    Change last batch of tests with fixtures

--- e2e/portal/tests/DoPayment/DoSuccessfulPaymentWithVoucher.spec.ts
+++ e2e/portal/tests/DoPayment/DoSuccessfulPaymentWithVoucher.spec.ts
@@ -1,35 +1,22 @@
-import { test } from '@playwright/test';
 import { format } from 'date-fns';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import NLRCProgram from '@121-service/src/seed-data/program/program-nlrc-pv.json';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import {
   programIdPV,
   registrationsVoucher,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
+import { test } from '@121-e2e/portal/fixtures/fixture';
 import PaymentPage from '@121-e2e/portal/pages/PaymentPage';
 import PaymentsPage from '@121-e2e/portal/pages/PaymentsPage';
 
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations(
-    registrationsVoucher,
-    programIdPV,
-    accessToken,
-  );
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
+test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
+  await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.nlrcMultiple,
+    registrations: registrationsVoucher,
+    programId: programIdPV,
+  });
 });
 
 test('Do successful payment for Voucher fsp', async ({ page }) => {
