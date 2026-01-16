56e091fdcd4d4388c4a1287772e00a3d5fa4119f
Piotrk39 <piotrk39@gmail.com>
2026-01-16 20:16:26 +0100

    Change last batch of tests with fixtures

--- e2e/portal/tests/DoPayment/DoSuccessfulPaymentWithSafaricom.spec.ts
+++ e2e/portal/tests/DoPayment/DoSuccessfulPaymentWithSafaricom.spec.ts
@@ -1,35 +1,22 @@
-import { test } from '@playwright/test';
 import { format } from 'date-fns';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import KRCSProgram from '@121-service/src/seed-data/program/program-safaricom.json';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import {
   programIdSafaricom,
   registrationsSafaricom,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
+import { test } from '@121-e2e/portal/fixtures/fixture';
 import PaymentPage from '@121-e2e/portal/pages/PaymentPage';
 import PaymentsPage from '@121-e2e/portal/pages/PaymentsPage';
 
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.safaricomProgram, __filename);
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations(
-    registrationsSafaricom,
-    programIdSafaricom,
-    accessToken,
-  );
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
+test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
+  await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.safaricomProgram,
+    registrations: registrationsSafaricom,
+    programId: programIdSafaricom,
+  });
 });
 
 test('Do successful payment for Safaricom fsp', async ({ page }) => {
