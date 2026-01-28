1e368a882c8af78ca351ad411f6fdac1e14ef019
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2026-01-28 14:40:04 +0100

    process more comments

--- interfaces/portal/src/app/pipes/credit-card-number.pipe.spec.ts
+++ interfaces/portal/src/app/pipes/credit-card-number.pipe.spec.ts
@@ -9,33 +9,53 @@ describe('CreditCardNumberPipe', () => {
     TestBed.configureTestingModule({
       providers: [CreditCardNumberPipe],
     });
+    pipe = TestBed.inject(CreditCardNumberPipe);
   });
 
   it('create an instance', () => {
-    // Arrange
-    pipe = TestBed.inject(CreditCardNumberPipe);
     expect(pipe).toBeTruthy();
   });
 
-  it('should return a dashed credit card number when a number string is provided', () => {
-    // Arrange
-    pipe = TestBed.inject(CreditCardNumberPipe);
-    const creditCard19Digits = '1234567890123456789';
-    const expectedValue = '1234-5678-9012-3456-789';
-
-    // Assert
-    const dashedNumber = pipe.transform(creditCard19Digits);
-    expect(dashedNumber).toBe(expectedValue);
+  it('null defaults to empty string', () => {
+    expect(pipe.transform(null)).toBe('');
   });
 
-  it('should return the input value string is provided', () => {
-    // Arrange
-    pipe = TestBed.inject(CreditCardNumberPipe);
-    const creditCardString = 'Test-card';
-    const expectedValue = 'Test-card';
+  it('undefined defaults to empty string', () => {
+    expect(pipe.transform(undefined)).toBe('');
+  });
 
-    // Assert
-    const dashedNumber = pipe.transform(creditCardString);
-    expect(dashedNumber).toBe(expectedValue);
+  describe('when it gets a non-number string it should return the input value', () => {
+    it('for regular strings', () => {
+      expect(pipe.transform('Test-card')).toBe('Test-card');
+    });
+
+    it('for empty strings', () => {
+      expect(pipe.transform('')).toBe('');
+    });
+
+    it("when first character is zero, it's not considered to be a number", () => {
+      expect(pipe.transform('010')).toBe('010');
+    });
+  });
+
+  describe('when it gets a number string', () => {
+    it('should return a dashed credit card number', () => {
+      const creditCard19Digits = '1234567890123456789';
+      expect(pipe.transform(creditCard19Digits)).toBe(
+        '1234-5678-9012-3456-789',
+      );
+    });
+
+    it('does not dash a short number', () => {
+      expect(pipe.transform('1234')).toBe('1234');
+    });
+
+    it('starts adding dashes to numbers of length greater than 4', () => {
+      expect(pipe.transform('12345')).toBe('1234-5');
+    });
+
+    it('numbers in exponential notation are treated as strings', () => {
+      expect(pipe.transform('1.23456e+5')).toBe('1.23-456e-+5');
+    });
   });
 });
--- interfaces/portal/src/app/pipes/credit-card-number.pipe.ts
+++ interfaces/portal/src/app/pipes/credit-card-number.pipe.ts
@@ -13,19 +13,6 @@ export class CreditCardNumberPipe implements PipeTransform {
       return value;
     }
 
-    const chunkSize = 4;
-    const splitValue = value.split('');
-
-    const numberOfDashes =
-      value.length % chunkSize > 0
-        ? Math.floor(value.length / chunkSize)
-        : Math.floor(value.length / chunkSize) - 1;
-
-    for (let i = numberOfDashes; i > 0; i--) {
-      const dashIndex = chunkSize * i;
-      splitValue.splice(dashIndex, 0, '-');
-    }
-
-    return splitValue.join('');
+    return value.match(/.{1,4}/g)?.join('-') ?? value;
   }
 }
