36656dd9b4b1fe22993cc535c81914b9f7726f3e
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2026-02-09 16:54:35 +0100

    rm unused file

--- interfaces/portal/src/app/domains/kobo/kobo.model.ts
+++
@@ -1,5 +0,0 @@
-import { KoboEntity } from '@121-service/src/kobo/entities/kobo.entity';
-
-import { Dto } from '~/utils/dto-type';
-
-export type KoboIntegration = Dto<Omit<KoboEntity, 'program'>>;
