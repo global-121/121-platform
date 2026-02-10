ab50d6eb1875337c7fe571bb973140b1a455de44
Yuri <ywilmering@redcross.nl>
2026-02-10 16:36:45 +0100

    does everything still work?

--- services/121-service/src/fsp-integrations/shared/consts/public-fsp-configuration-properties.const.ts
+++
@@ -1,8 +0,0 @@
-import { FspConfigurationProperties } from '@121-service/src/fsp-integrations/shared/enum/fsp-configuration-properties.enum';
-import { Fsps } from '@121-service/src/fsp-integrations/shared/enum/fsp-name.enum';
-
-export const PublicFspConfigurationProperties: Partial<
-  Record<Fsps, FspConfigurationProperties[]>
-> = {
-  [Fsps.intersolveVisa]: [FspConfigurationProperties.cardDistributionByMail],
-};
--- services/121-service/src/fsp-integrations/shared/consts/secret-fsp-configuration-properties.const.ts
+++
@@ -1,9 +0,0 @@
-import { FspConfigurationProperties } from '@121-service/src/fsp-integrations/shared/enum/fsp-configuration-properties.enum';
-
-export const SecretFspConfigurationProperties: FspConfigurationProperties[] = [
-  FspConfigurationProperties.password,
-  FspConfigurationProperties.username,
-  // Onafriq
-  FspConfigurationProperties.passwordOnafriq,
-  FspConfigurationProperties.uniqueKeyOnafriq,
-];
