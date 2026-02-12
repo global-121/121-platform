7ffbe1ec4ec6ca7c946d64a1fb84d7edd456355a
Yuri <ywilmering@redcross.nl>
2026-02-12 14:58:21 +0100

    ts

---
+++ packages/shared/dist/enums/ui-language.enum.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UILanguage = void 0;
+/**
+ * Supported languages for the User Interface.
+ * See: https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
+ * Make sure to use the 2-letter code from the "Set 1"-column.
+ */
+var UILanguage;
+(function (UILanguage) {
+    UILanguage["ar"] = "ar";
+    UILanguage["en"] = "en";
+    UILanguage["nl"] = "nl";
+    UILanguage["es"] = "es";
+    UILanguage["fr"] = "fr";
+    UILanguage["sk"] = "sk";
+})(UILanguage || (exports.UILanguage = UILanguage = {}));
---
+++ packages/shared/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UILanguage = void 0;
+var ui_language_enum_1 = require("./enums/ui-language.enum");
+Object.defineProperty(exports, "UILanguage", { enumerable: true, get: function () { return ui_language_enum_1.UILanguage; } });
