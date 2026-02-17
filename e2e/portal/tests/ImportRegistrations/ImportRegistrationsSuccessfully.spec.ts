cbf5091191c13dcd625c15710c531c68dc8940d0
Piotrk39 <piotrk39@gmail.com>
2026-02-17 12:48:45 +0100

    Add missing fixtures to import registrations

--- e2e/portal/tests/ImportRegistrations/ImportRegistrationsSuccessfully.spec.ts
+++ e2e/portal/tests/ImportRegistrations/ImportRegistrationsSuccessfully.spec.ts
@@ -3,9 +3,7 @@ import path from 'node:path';
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import NLRCProgramPV from '@121-service/src/seed-data/program/program-nlrc-pv.json';
 
-import TableComponent from '@121-e2e/portal/components/TableComponent';
 import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
-import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
 
 test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
   await resetDBAndSeedRegistrations({
@@ -13,9 +11,10 @@ test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
   });
 });
 
-test('Successfully import registrations', async ({ page }) => {
-  const registrationsPage = new RegistrationsPage(page);
-  const table = new TableComponent(page);
+test('Successfully import registrations', async ({
+  registrationsPage,
+  tableComponent,
+}) => {
   const registrationsDataFilePath = path.resolve(
     __dirname,
     '../../../test-registration-data/test-registrations-PV.csv',
@@ -36,8 +35,8 @@ test('Successfully import registrations', async ({ page }) => {
 
   await test.step('Validate registrations are present in the table and the counts match', async () => {
     // Default display filter number
-    await table.validateWaitForTableRowCount({ expectedRowCount: 10 });
+    await tableComponent.validateWaitForTableRowCount({ expectedRowCount: 10 });
     // Uploaded records count
-    await table.validateAllRecordsCount(20);
+    await tableComponent.validateAllRecordsCount(20);
   });
 });
