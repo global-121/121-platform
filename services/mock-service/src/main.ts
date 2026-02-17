b9ae643962d03df4b062d5586ad4381658658e6d
Elwin Schmitz <eschmitz@redcross.nl>
2026-02-17 13:52:52 +0100

    fix: Make Prettier happy

--- services/mock-service/src/main.ts
+++ services/mock-service/src/main.ts
@@ -20,7 +20,8 @@ import {
 import { env } from '@mock-service/src/env';
 
 declare global {
-  // To define the global queue-collection without namespace, we need to define it using `var`. See: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-typescript-eslintno-namespace-andor-no-var-rules-about-declaring-global-variables
+  // To define the global queue-collection without namespace, we need to define it using `var`.
+  // See: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-typescript-eslintno-namespace-andor-no-var-rules-about-declaring-global-variables
   var queueCallbacks: Record<string, string>;
 }
 
