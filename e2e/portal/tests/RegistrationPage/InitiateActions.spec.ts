3f6ed032418739836375da26dc99a2e1598d2a3d
Piotrk39 <piotrk39@gmail.com>
2026-02-18 14:53:16 +0100

    Add fixtures to RegistrationPage

--- e2e/portal/tests/RegistrationPage/InitiateActions.spec.ts
+++ e2e/portal/tests/RegistrationPage/InitiateActions.spec.ts
@@ -1,58 +1,47 @@
-import { expect, test } from '@playwright/test';
-
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import {
-  getRegistrationIdByReferenceId,
-  seedIncludedRegistrations,
-} from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
+import { getRegistrationIdByReferenceId } from '@121-service/test/helpers/registration.helper';
 import {
   programIdPV,
   registrationPV5,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import RegistrationActivityLogPage from '@121-e2e/portal/pages/RegistrationActivityLogPage';
+import {
+  customSharedFixture as test,
+  expect,
+} from '@121-e2e/portal/fixtures/fixture';
 
 const programId = 2;
 let registrationId: number;
 
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
+test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
+  const { accessToken } = await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.nlrcMultiple,
+    registrations: [registrationPV5],
+    programId: programIdPV,
+  });
 
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations([registrationPV5], programIdPV, accessToken);
   registrationId = await getRegistrationIdByReferenceId({
     programId: programIdPV,
     referenceId: registrationPV5.referenceId,
     accessToken,
   });
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto(`/`);
-  await loginPage.login();
 });
 
 test('User can initiate registration status changes from registration page', async ({
   page,
+  registrationActivityLogPage,
 }) => {
-  const activityLogPage = new RegistrationActivityLogPage(page);
-
   const statusChangeActions = ['Validate', 'Include', 'Decline', 'Delete'];
 
   for (const actionName of statusChangeActions) {
     await test.step('Navigate to registration', async () => {
-      await activityLogPage.goto(
+      await registrationActivityLogPage.goto(
         `/program/${programId}/registrations/${registrationId}`,
       );
     });
 
     await test.step(`Initiate action: ${actionName}`, async () => {
-      await activityLogPage.initiateAction(actionName);
+      await registrationActivityLogPage.initiateAction(actionName);
 
       const dialog = page.getByRole('dialog');
       await expect(dialog).toBeVisible();
@@ -69,19 +58,20 @@ test('User can initiate registration status changes from registration page', asy
   }
 });
 
-test('User can open add note sidebar from action menu', async ({ page }) => {
-  const activityLogPage = new RegistrationActivityLogPage(page);
-
+test('User can open add note sidebar from action menu', async ({
+  page,
+  registrationActivityLogPage,
+}) => {
   const actionName = 'Add note';
 
   await test.step('Navigate to registration', async () => {
-    await activityLogPage.goto(
+    await registrationActivityLogPage.goto(
       `/program/${programId}/registrations/${registrationId}`,
     );
   });
 
   await test.step(`Initiate action: ${actionName}`, async () => {
-    await activityLogPage.initiateAction(actionName);
+    await registrationActivityLogPage.initiateAction(actionName);
     await page.waitForLoadState('domcontentloaded');
     await page.waitForLoadState('networkidle');
     const sidebarDescription = page.getByText(/You are about to/);
@@ -92,19 +82,20 @@ test('User can open add note sidebar from action menu', async ({ page }) => {
   });
 });
 
-test('User can open message dialog from action menu', async ({ page }) => {
-  const activityLogPage = new RegistrationActivityLogPage(page);
-
+test('User can open message dialog from action menu', async ({
+  page,
+  registrationActivityLogPage,
+}) => {
   const actionName = 'Message';
 
   await test.step('Navigate to registration', async () => {
-    await activityLogPage.goto(
+    await registrationActivityLogPage.goto(
       `/program/${programId}/registrations/${registrationId}`,
     );
   });
 
   await test.step(`Initiate action: ${actionName}`, async () => {
-    await activityLogPage.initiateAction(actionName);
+    await registrationActivityLogPage.initiateAction(actionName);
     await page.waitForLoadState('domcontentloaded');
     await page.waitForLoadState('networkidle');
 
