6ca137e3ed8a767159dfcc33c6423d120642a7bb
Piotrk39 <piotrk39@gmail.com>
2026-02-18 16:22:18 +0100

    Add fixtures to ViewAndManagePeopleAffected

--- e2e/portal/tests/ViewAndManagePeopleAffected/IgnoreDuplicates.spec.ts
+++ e2e/portal/tests/ViewAndManagePeopleAffected/IgnoreDuplicates.spec.ts
@@ -1,54 +1,36 @@
-import { expect, test } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import {
   programIdPV,
   registrationsPV,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import HomePage from '@121-e2e/portal/pages/HomePage';
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import RegistrationActivityLogPage from '@121-e2e/portal/pages/RegistrationActivityLogPage';
-import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
 
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
-
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations(registrationsPV, programIdPV, accessToken);
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
-});
-
-test('Ignore duplicates', async ({ page }) => {
-  const homePage = new HomePage(page);
-  const registrations = new RegistrationsPage(page);
-  const registrationActivityLogPage = new RegistrationActivityLogPage(page);
-
-  const programTitle = 'NLRC Direct Digital Aid Program (PV)';
-
+test('Ignore duplicates', async ({
+  registrationsPage,
+  registrationActivityLogPage,
+  resetDBAndSeedRegistrations,
+}) => {
+  await test.step('Setup and seed database', async () => {
+    await resetDBAndSeedRegistrations({
+      seedScript: SeedScript.nlrcMultiple,
+      registrations: registrationsPV,
+      programId: programIdPV,
+      navigateToPage: `/program/${programIdPV}/registrations`,
+    });
+  });
   const duplicateRegistrationA = registrationsPV[1]; // 'Jan Janssen'
   const duplicateRegistrationB = registrationsPV[2]; // 'Joost Herlembach'
 
-  await test.step('Select program', async () => {
-    await homePage.selectProgram(programTitle);
-  });
-
   await test.step('Wait for registrations to load', async () => {
     const allRegistrationsCount = registrationsPV.length;
-    await registrations.waitForLoaded(allRegistrationsCount);
+    await registrationsPage.waitForLoaded(allRegistrationsCount);
   });
 
   await test.step('Open registration page', async () => {
-    await registrations.goToRegistrationByName({
+    await registrationsPage.goToRegistrationByName({
       registrationName: duplicateRegistrationA.fullName,
     });
   });
@@ -62,11 +44,11 @@ test('Ignore duplicates', async ({ page }) => {
   await test.step('Ignore duplication', async () => {
     await registrationActivityLogPage.initiateAction('Ignore duplication');
 
-    const dialog = await registrationActivityLogPage.dialog;
+    const dialog = registrationActivityLogPage.dialog;
 
     await expect(dialog.getByText('Ignore duplication')).toBeVisible();
 
-    const editInformationReasonField = await dialog.getByLabel(
+    const editInformationReasonField = dialog.getByLabel(
       'Write a reason for the update',
     );
     await editInformationReasonField.fill('E2E test');
