44b602def04457356f0b088cbba50a3c379374e7
Piotrk39 <piotrk39@gmail.com>
2026-02-18 15:10:10 +0100

    Add fixtures to UpdateRegistrations

--- e2e/portal/tests/UpdateRegistrations/UpdateRegistrationsUnsuccessfully.spec.ts
+++ e2e/portal/tests/UpdateRegistrations/UpdateRegistrationsUnsuccessfully.spec.ts
@@ -1,43 +1,25 @@
-import { test } from '@playwright/test';
-
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import NLRCProgramPV from '@121-service/src/seed-data/program/program-nlrc-pv.json';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import {
   programIdPV,
   registrationPV5,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
-
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations([registrationPV5], programIdPV, accessToken);
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
-});
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
 
 const fakeReferenceId =
   'this-is-not-the-greatest-reference-id-in-the-world-this-is-just-a-tribute';
 
 test('Wrong CSV should trigger error (wrong data, column name etc.)', async ({
-  page,
+  resetDBAndSeedRegistrations,
+  registrationsPage,
 }) => {
-  const registrationsPage = new RegistrationsPage(page);
-
-  const programTitle = NLRCProgramPV.titlePortal.en;
-
-  await test.step('Select program', async () => {
-    await registrationsPage.selectProgram(programTitle);
+  await test.step('Setup', async () => {
+    await resetDBAndSeedRegistrations({
+      seedScript: SeedScript.nlrcMultiple,
+      registrations: [registrationPV5],
+      programId: programIdPV,
+      navigateToPage: `/program/${programIdPV}/registrations`,
+    });
   });
 
   await test.step('Select all registrations and open "Update registrations" dialog', async () => {
