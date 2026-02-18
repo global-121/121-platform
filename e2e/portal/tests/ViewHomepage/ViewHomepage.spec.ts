80dff1d1728ee03eaba007179a404fda9cda2da4
Piotrk39 <piotrk39@gmail.com>
2026-02-18 16:52:26 +0100

    Add fixtures to ViewHomepage

--- e2e/portal/tests/ViewHomepage/ViewHomepage.spec.ts
+++ e2e/portal/tests/ViewHomepage/ViewHomepage.spec.ts
@@ -1,32 +1,28 @@
-import { expect, test } from '@playwright/test';
-
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import { resetDB } from '@121-service/test/helpers/utility.helper';
 
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
+import {
+  customSharedFixture as test,
+  expect,
+} from '@121-e2e/portal/fixtures/fixture';
 
 /**
  * This test is here to just load the homepage and check if it loads without any errors to make sure we don't break the infrastructure that runs the tests.
  */
+test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
+  await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.testMultiple,
+    skipSeedRegistrations: true,
+    navigateToPage: '/',
+  });
+});
+
 test('Load Portal', async ({ page }) => {
-  await page.goto('/');
   expect(await page.title()).toBe('121 Portal'); // This is the title in the downloaded HTML, not the rendered Angular-app page-title
 });
 
 test('View All programs', async ({ page }) => {
-  // Arrange
-  await resetDB(SeedScript.testMultiple, __filename);
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
-
-  // Act
-  await page.goto('/');
-
-  await page.waitForURL((url) => url.pathname.startsWith('/en-GB/programs'));
-
   // Assert
+  await page.waitForURL((url) => url.pathname.startsWith('/en-GB/programs'));
   await expect(page.locator('h1')).toContainText('All programs');
   await expect(page.locator('a[href^="/en-GB/program/"]')).toHaveCount(2);
 });
