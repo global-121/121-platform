546b897f5e296fac94c89c1eb4b9b3749f1a1afe
Piotrk39 <piotrk39@gmail.com>
2026-02-18 13:28:18 +0100

    Add fixtures to redline tests

--- e2e/portal/tests/Redline/ViewRegistrationPageInRedline.spec.ts
+++ e2e/portal/tests/Redline/ViewRegistrationPageInRedline.spec.ts
@@ -1,24 +1,20 @@
-import { expect, test } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import { seedRegistrations } from '@121-service/test/helpers/registration.helper';
-import { resetDB } from '@121-service/test/helpers/utility.helper';
 import {
   programIdWesteros,
   registrationWesteros1,
   registrationWesteros2,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
 
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.testMultiple, __filename);
-
-  // Login
-  const loginPage = new LoginPage(page);
-
-  await page.goto('/');
-  await loginPage.login();
+test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
+  await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.testMultiple,
+    skipSeedRegistrations: true,
+  });
 });
 
 test('View Redline start-page (no search)', async ({ page }) => {
