94d446602204ae9cfd25ac4e3c63e6a09d9a66e3
Piotrk39 <piotrk39@gmail.com>
2026-02-18 16:36:29 +0100

    Add fixtures to ViewFilterAndSortRegistrations

--- e2e/portal/tests/ViewFilterAndSortRegistrations/FilterRegistrationsByText.spec.ts
+++ e2e/portal/tests/ViewFilterAndSortRegistrations/FilterRegistrationsByText.spec.ts
@@ -1,53 +1,39 @@
-import { expect, test } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 import { RegistrationStatusEnum } from '@121-service/src/registration/enum/registration-status.enum';
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import { seedRegistrationsWithStatus } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import {
   programIdPV,
   registrationsPV,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import TableComponent from '@121-e2e/portal/components/TableComponent';
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
 
 let registrationName: string;
 
-// Arrange
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
-  const accessToken = await getAccessToken();
+test('Filter registrations by text', async ({
+  registrationsPage,
+  tableComponent,
+  resetDBAndSeedRegistrations,
+}) => {
+  await test.step('Setup and seed database', async () => {
+    await resetDBAndSeedRegistrations({
+      seedScript: SeedScript.nlrcMultiple,
+      registrations: registrationsPV,
+      programId: programIdPV,
+      seedWithStatus: RegistrationStatusEnum.included,
+      navigateToPage: `/program/${programIdPV}/registrations`,
+    });
+  });
 
-  await seedRegistrationsWithStatus(
-    registrationsPV,
-    programIdPV,
-    accessToken,
-    RegistrationStatusEnum.included,
-  );
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
-  // Navigate to program
-  await loginPage.selectProgram('NLRC Direct Digital Aid Program (PV)');
-});
-
-test('Filter registrations by text', async ({ page }) => {
-  const registrations = new RegistrationsPage(page);
-  const tableComponent = new TableComponent(page);
   // Act & Assert
   await test.step('Filter Name column by text', async () => {
     await tableComponent.filterColumnByText({
       columnName: 'Name',
       filterText: 'Jan Janssen',
     });
-    registrationName = await registrations.getFirstRegistrationNameFromTable();
+    registrationName =
+      await registrationsPage.getFirstRegistrationNameFromTable();
     expect(registrationName).toBe('Jan Janssen');
     await tableComponent.clearAllFilters();
   });
@@ -58,7 +44,8 @@ test('Filter registrations by text', async ({ page }) => {
       // defaults to "contains" filter type
       filterText: '35557',
     });
-    registrationName = await registrations.getFirstRegistrationNameFromTable();
+    registrationName =
+      await registrationsPage.getFirstRegistrationNameFromTable();
     expect(registrationName).toBe('Jack Strong');
     await tableComponent.validateWaitForTableRowCount({ expectedRowCount: 1 });
     await tableComponent.clearAllFilters();
@@ -70,7 +57,8 @@ test('Filter registrations by text', async ({ page }) => {
       filterText: '14155235557',
       filterMode: 'Equal to',
     });
-    registrationName = await registrations.getFirstRegistrationNameFromTable();
+    registrationName =
+      await registrationsPage.getFirstRegistrationNameFromTable();
     expect(registrationName).toBe('Jack Strong');
     await tableComponent.validateWaitForTableRowCount({ expectedRowCount: 1 });
     await tableComponent.clearAllFilters();
@@ -82,7 +70,8 @@ test('Filter registrations by text', async ({ page }) => {
       filterText: '14155235557',
       filterMode: 'Not equal to',
     });
-    registrationName = await registrations.getFirstRegistrationNameFromTable();
+    registrationName =
+      await registrationsPage.getFirstRegistrationNameFromTable();
     expect(registrationName).toBe('Gemma Houtenbos');
     await tableComponent.validateWaitForTableRowCount({ expectedRowCount: 3 });
   });
