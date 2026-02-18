24ad614e264738b672762ec920c1c8d46228543b
Piotrk39 <piotrk39@gmail.com>
2026-02-18 15:02:36 +0100

    Add fixtures to SendMessage

--- e2e/portal/tests/SendMessage/SendTemplatedMessage.spec.ts
+++ e2e/portal/tests/SendMessage/SendTemplatedMessage.spec.ts
@@ -1,20 +1,10 @@
-import { test } from '@playwright/test';
-
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import {
   programIdPV,
   registrationsPV,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import BasePage from '@121-e2e/portal/pages/BasePage';
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import RegistrationActivityLogPage from '@121-e2e/portal/pages/RegistrationActivityLogPage';
-import RegistrationsPage from '@121-e2e/portal/pages/RegistrationsPage';
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
 
 const sendingMessageToast =
   'Closing this notification will not cancel message sending.';
@@ -23,50 +13,45 @@ const englishMessageTemplate =
 const dutchMessageTemplate =
   'Dit is een bericht van het Rode Kruis.\n\nBedankt voor je inschrijving. Je ontvangt vanaf nu elke dinsdag een Albert Heijn waardebon via WhatsApp. Je ontvangt de waardebonnen zo lang je op de lijst staat van .\n\nHet Rode Kruis kan je ook informatie geven over bijvoorbeeld medische hulp, voedsel of veiligheid. Kijk op onze website:\n\nhttps://helpfulinformation.redcross.nl/\n\nof stel je vraag via WhatsApp:\n\nhttps://wa.me/3197010286964';
 
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations(registrationsPV, programIdPV, accessToken);
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
-});
-
-test('Send templated message', async ({ page }) => {
-  const basePage = new BasePage(page);
-  const registrations = new RegistrationsPage(page);
-  const activityLog = new RegistrationActivityLogPage(page);
-
-  const programTitle = 'NLRC Direct Digital Aid Program (PV)';
-
-  await test.step('Select program', async () => {
-    await basePage.selectProgram(programTitle);
+test('Send templated message', async ({
+  resetDBAndSeedRegistrations,
+  registrationsPage,
+  registrationActivityLogPage,
+}) => {
+  await test.step('Setup', async () => {
+    await resetDBAndSeedRegistrations({
+      seedScript: SeedScript.nlrcMultiple,
+      registrations: registrationsPV,
+      programId: programIdPV,
+      navigateToPage: `/program/${programIdPV}/registrations`,
+    });
   });
 
   await test.step('Send templated message', async () => {
-    await registrations.selectAllRegistrations();
-    await registrations.selectBulkAction('Message');
-    await registrations.selectTemplatedMessage('Include');
-    await registrations.clickContinueToPreview();
-    await registrations.validateMessagePresent(
+    await registrationsPage.selectAllRegistrations();
+    await registrationsPage.selectBulkAction('Message');
+    await registrationsPage.selectTemplatedMessage('Include');
+    await registrationsPage.clickContinueToPreview();
+    await registrationsPage.validateMessagePresent(
       'This is a message from the Red Cross.',
     );
-    await registrations.sendMessage();
+    await registrationsPage.sendMessage();
 
-    await registrations.validateToastMessage(sendingMessageToast);
+    await registrationsPage.validateToastMessage(sendingMessageToast);
     // Validate English message
-    await registrations.goToRegistrationByName({
+    await registrationsPage.goToRegistrationByName({
       registrationName: 'Jack Strong',
     });
-    await activityLog.validateLastMessageSent(englishMessageTemplate);
+    await registrationActivityLogPage.validateLastMessageSent(
+      englishMessageTemplate,
+    );
     // Validate Dutch message
-    await page.goto('/');
-    await basePage.selectProgram(programTitle);
-    await registrations.goToRegistrationByName({
+    await registrationsPage.goto(`/program/${programIdPV}/registrations`);
+    await registrationsPage.goToRegistrationByName({
       registrationName: 'Gemma Houtenbos',
     });
-    await activityLog.validateLastMessageSent(dutchMessageTemplate);
+    await registrationActivityLogPage.validateLastMessageSent(
+      dutchMessageTemplate,
+    );
   });
 });
