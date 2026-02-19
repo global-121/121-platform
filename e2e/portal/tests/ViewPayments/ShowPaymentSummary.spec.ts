0ba087f910b9a1a3c2d9b2368ec911ed2121225c
Piotrk39 <piotrk39@gmail.com>
2026-02-19 13:02:10 +0100

    Add fixtures to ViewPayments

--- e2e/portal/tests/ViewPayments/ShowPaymentSummary.spec.ts
+++ e2e/portal/tests/ViewPayments/ShowPaymentSummary.spec.ts
@@ -1,47 +1,29 @@
-import { test } from '@playwright/test';
 import { expect } from '@playwright/test';
 
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
 import NLRCProgram from '@121-service/src/seed-data/program/program-nlrc-ocw.json';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import { registrationsOCW } from '@121-service/test/registrations/pagination/pagination-data';
 
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import PaymentsPage from '@121-e2e/portal/pages/PaymentsPage';
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
 
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
-  const programIdOCW = 3;
-  const OcwProgramId = programIdOCW;
+const programIdOCW = 3;
 
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations(registrationsOCW, OcwProgramId, accessToken);
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
+test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
+  await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.nlrcMultiple,
+    registrations: registrationsOCW,
+    programId: programIdOCW,
+    navigateToPage: `/program/${programIdOCW}/payments`,
+  });
 });
 
-test('Show payment summary', async ({ page }) => {
-  const paymentsPage = new PaymentsPage(page);
-
-  const programTitle = 'NLRC OCW Program';
+test('Show payment summary', async ({ paymentsPage }) => {
   const fsps: string[] = ['Albert Heijn voucher WhatsApp', 'Visa debit card'];
   const numberOfPas = registrationsOCW.length;
   const defaultTransferValue = NLRCProgram.fixedTransferValue;
   const defaultMaxTransferValue = registrationsOCW.reduce((output, pa) => {
     return output + pa.paymentAmountMultiplier * defaultTransferValue;
   }, 0);
-
-  await test.step('Navigate to Program payments', async () => {
-    await paymentsPage.selectProgram(programTitle);
-    await paymentsPage.navigateToProgramPage('Payments');
-  });
 
   await test.step('Create payment', async () => {
     await paymentsPage.createPayment({});
@@ -54,16 +36,7 @@ test('Show payment summary', async ({ page }) => {
   });
 });
 
-test('Validate empty payment page', async ({ page }) => {
-  const paymentsPage = new PaymentsPage(page);
-
-  const programTitle = 'NLRC OCW Program';
-
-  await test.step('Navigate to Program payments', async () => {
-    await paymentsPage.selectProgram(programTitle);
-    await paymentsPage.navigateToProgramPage('Payments');
-  });
-
+test('Validate empty payment page', async ({ paymentsPage }) => {
   await test.step('Validate empty payment summary', async () => {
     const isEmpty = await paymentsPage.isPaymentPageEmpty();
     expect(isEmpty).toBe(true);
