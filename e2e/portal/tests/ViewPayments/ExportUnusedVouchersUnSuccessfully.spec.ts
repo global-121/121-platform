c2a3154e31376d5986633076ef72f1892781a26f
Piotrk39 <piotrk39@gmail.com>
2026-02-19 13:02:10 +0100

    Add fixtures to ViewPayments

--- e2e/portal/tests/ViewPayments/ExportUnusedVouchersUnSuccessfully.spec.ts
+++ e2e/portal/tests/ViewPayments/ExportUnusedVouchersUnSuccessfully.spec.ts
@@ -1,54 +1,27 @@
-import { test } from '@playwright/test';
-
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import NLRCProgram from '@121-service/src/seed-data/program/program-nlrc-pv.json';
-import { doPayment } from '@121-service/test/helpers/program.helper';
-import { seedIncludedRegistrations } from '@121-service/test/helpers/registration.helper';
-import {
-  getAccessToken,
-  resetDB,
-} from '@121-service/test/helpers/utility.helper';
 import {
   programIdPV,
-  registrationPV5,
-  registrationPV6,
   registrationsVoucher,
 } from '@121-service/test/registrations/pagination/pagination-data';
 
-import ExportData from '@121-e2e/portal/components/ExportData';
-import LoginPage from '@121-e2e/portal/pages/LoginPage';
-import PaymentsPage from '@121-e2e/portal/pages/PaymentsPage';
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
 
-// Arrange
-test.beforeEach(async ({ page }) => {
-  await resetDB(SeedScript.nlrcMultiple, __filename);
-  const accessToken = await getAccessToken();
-  await seedIncludedRegistrations(
-    registrationsVoucher,
-    programIdPV,
-    accessToken,
-  );
-
-  await doPayment({
-    programId: programIdPV,
+test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
+  await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.nlrcMultiple,
+    seedPaidRegistrations: true,
     transferValue: 100,
-    referenceIds: [registrationPV5.referenceId, registrationPV6.referenceId],
-    accessToken,
+    registrations: registrationsVoucher,
+    programId: programIdPV,
+    navigateToPage: `/program/${programIdPV}/payments`,
   });
-
-  // Login
-  const loginPage = new LoginPage(page);
-  await page.goto('/');
-  await loginPage.login();
 });
 
-test('Export unused vouchers unsuccessfully', async ({ page }) => {
-  const paymentsPage = new PaymentsPage(page);
-  const exportDataComponent = new ExportData(page);
-
+test('Export unused vouchers unsuccessfully', async ({
+  paymentsPage,
+  exportDataComponent,
+}) => {
   // Act
-  await paymentsPage.selectProgram(NLRCProgram.titlePortal.en);
-  await paymentsPage.navigateToProgramPage('Payments');
   await paymentsPage.selectPaymentExportOption({ option: 'Unused vouchers' });
   // Click on Proceed button
   await exportDataComponent.clickProceedToExport();
