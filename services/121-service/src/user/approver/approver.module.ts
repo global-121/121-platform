21dcad559fe7396ee6d3ea5b935942ffa796ed3e
Yuri <ywilmering@redcross.nl>
2026-02-20 13:35:09 +0100

    moved payment-approval to payment domain

--- services/121-service/src/user/approver/approver.module.ts
+++ services/121-service/src/user/approver/approver.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { PaymentApprovalEntity } from '@121-service/src/payments/entities/payment-approval.entity';
+import { PaymentApprovalRepository } from '@121-service/src/payments/repositories/payment-approval.repository';
 import { ProgramAidworkerAssignmentEntity } from '@121-service/src/programs/entities/program-aidworker.entity';
 import { ApproverController } from '@121-service/src/user/approver/approver.controller';
 import { ApproverService } from '@121-service/src/user/approver/approver.service';
 import { ApproverEntity } from '@121-service/src/user/approver/entities/approver.entity';
-import { PaymentApprovalEntity } from '@121-service/src/user/approver/entities/payment-approval.entity';
-import { PaymentApprovalRepository } from '@121-service/src/user/approver/repositories/payment-approval.repository';
 
 @Module({
   imports: [
@@ -18,6 +18,6 @@ import { PaymentApprovalRepository } from '@121-service/src/user/approver/reposi
   ],
   providers: [ApproverService, PaymentApprovalRepository],
   controllers: [ApproverController],
-  exports: [ApproverService, PaymentApprovalRepository],
+  exports: [ApproverService],
 })
 export class ApproverModule {}
