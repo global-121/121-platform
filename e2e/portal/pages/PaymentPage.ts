6eec4631cd6408d8faa7422014256b854865686b
Piotrk39 <piotrk39@gmail.com>
2026-02-26 12:30:04 +0100

    Update  getByText method to get element using regEx

--- e2e/portal/pages/PaymentPage.ts
+++ e2e/portal/pages/PaymentPage.ts
@@ -309,7 +309,7 @@ class PaymentPage extends BasePage {
 
   async validateTransferHistoryDialogTitle({ title }: { title: string }) {
     await this.page.waitForSelector('role=dialog');
-    await expect(this.page.getByText('transaction history')).toContainText(
+    await expect(this.page.getByText(/transaction history/i)).toContainText(
       title,
     ); // the title does not contain 'transfer history' text therefore we check for 'transaction history' text which is always present and then check if the date and rest of the string is correct
   }
