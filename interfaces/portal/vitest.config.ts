8dfc7bb54ede7841c7c2d6d5dd96b43afb3a29d7
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2026-03-09 23:03:28 +0100

    fix knip in a better way

--- interfaces/portal/vitest.config.ts
+++ interfaces/portal/vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -11,13 +12,9 @@ export default defineConfig({
         },
       ],
     },
-    // coverage: {
-    //   reportsDirectory: path.join(__dirname, './coverage'),
-    //   reporter: [['lcov'], ['text-summary']],
-    // },
-    // TODO: setting coverage to false fixes knip. keep like this or solve in another way?
     coverage: {
-      enabled: false,
+      reportsDirectory: path.join(__dirname, './coverage'),
+      reporter: [['lcov'], ['text-summary']],
     },
   },
 });
