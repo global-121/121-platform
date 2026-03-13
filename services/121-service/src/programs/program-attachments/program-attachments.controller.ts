c63fff264e882890a459c18ebd52609fed627181
Piotrk39 <piotrk39@gmail.com>
2026-03-13 13:50:03 +0100

    Exchange api format with dto for rename file

--- services/121-service/src/programs/program-attachments/program-attachments.controller.ts
+++ services/121-service/src/programs/program-attachments/program-attachments.controller.ts
@@ -34,11 +34,9 @@ import { AuthenticatedUserGuard } from '@121-service/src/guards/authenticated-us
 import { CreateProgramAttachmentDto } from '@121-service/src/programs/program-attachments/dtos/create-program-attachment.dto';
 import { CreateProgramAttachmentResponseDto } from '@121-service/src/programs/program-attachments/dtos/create-program-attachment-response.dto';
 import { GetProgramAttachmentResponseDto } from '@121-service/src/programs/program-attachments/dtos/get-program-attachment-response.dto';
+import { RenameProgramAttachmentDto } from '@121-service/src/programs/program-attachments/dtos/rename-program-attachment.dto';
 import { ProgramAttachmentsService } from '@121-service/src/programs/program-attachments/program-attachments.service';
-import {
-  FILE_UPLOAD_WITH_FILENAME_API_FORMAT,
-  RENAME_FILE_API_FORMAT,
-} from '@121-service/src/shared/file-upload-api-format';
+import { FILE_UPLOAD_WITH_FILENAME_API_FORMAT } from '@121-service/src/shared/file-upload-api-format';
 import { ScopedUserRequest } from '@121-service/src/shared/scoped-user-request';
 import { PermissionEnum } from '@121-service/src/user/enum/permission.enum';
 import { RequestHelper } from '@121-service/src/utils/request-helper/request-helper.helper';
@@ -108,16 +106,15 @@ export class ProgramAttachmentsController {
   })
   @ApiParam({ name: 'programId', required: true, type: 'integer' })
   @ApiParam({ name: 'attachmentId', required: true, type: 'integer' })
-  @ApiBody(RENAME_FILE_API_FORMAT)
   @ApiResponse({
     status: HttpStatus.OK,
     description: 'Rename an attachment in a program',
   })
   @Patch('programs/:programId/attachments/:attachmentId')
   public async renameProgramAttachment(
-    @Body() body: CreateProgramAttachmentDto,
     @Param('programId', ParseIntPipe) programId: number,
     @Param('attachmentId', ParseIntPipe) attachmentId: number,
+    @Body() body: RenameProgramAttachmentDto,
     @Req() req: ScopedUserRequest,
   ): Promise<CreateProgramAttachmentResponseDto> {
     const userId = RequestHelper.getUserId(req);
@@ -125,7 +122,7 @@ export class ProgramAttachmentsController {
     return await this.programAttachmentsService.renameProgramAttachment({
       programId,
       attachmentId,
-      filename: body.filename,
+      filename: body.newFileName,
       userId,
     });
   }
