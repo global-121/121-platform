daef2a7a9ac56cac827f2f93f5ad7a113431b4bf
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2026-03-19 17:42:58 +0100

    fix locale test

--- interfaces/portal/src/app/utils/locale.spec.ts
+++ interfaces/portal/src/app/utils/locale.spec.ts
@@ -1,14 +1,23 @@
 import { enableProdMode } from '@angular/core';
 
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { UILanguage } from '@121-service/src/shared/enum/ui-language.enum';
 
 import { getLocaleForInitialization, Locale } from '~/utils/locale';
 
 describe('getLocaleForInitialization', () => {
+  const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+
   beforeEach(() => {
+    getItemSpy.mockClear();
+    localStorage.clear();
     enableProdMode();
+  });
+
+  afterEach(() => {
+    getItemSpy.mockClear();
+    localStorage.clear();
   });
 
   it('should throw an error when an invalid default locale is passed in', () => {
@@ -30,7 +39,7 @@ describe('getLocaleForInitialization', () => {
   });
 
   it('should default to the urlLocale whenever there is weirdness saved in local storage', () => {
-    vi.spyOn(window.localStorage, 'getItem').mockReturnValue('nonsense');
+    getItemSpy.mockReturnValue('nonsense');
 
     const localeInfo = getLocaleForInitialization({
       defaultLocale: 'en-GB',
@@ -41,7 +50,7 @@ describe('getLocaleForInitialization', () => {
   });
 
   it('should use the default locale when there is nothing saved in local storage', () => {
-    vi.spyOn(window.localStorage, 'getItem').mockReturnValue(null);
+    getItemSpy.mockReturnValue(null);
 
     let localeInfo = getLocaleForInitialization({
       defaultLocale: 'en-GB',
@@ -59,9 +68,7 @@ describe('getLocaleForInitialization', () => {
   });
 
   it('should prompt to change language when the local storage locale is out of sync with the url locale', () => {
-    vi.spyOn(window.localStorage, 'getItem').mockImplementation(
-      () => UILanguage.nl,
-    );
+    getItemSpy.mockImplementation(() => UILanguage.nl);
 
     const localeInfo = getLocaleForInitialization({
       defaultLocale: 'en-GB',
@@ -75,7 +82,7 @@ describe('getLocaleForInitialization', () => {
   });
 
   it('should prompt to change language when the local storage locale does not exist and the url locale does not match the default locale', () => {
-    vi.spyOn(window.localStorage, 'getItem').mockImplementation(() => null);
+    getItemSpy.mockImplementation(() => null);
 
     const localeInfo = getLocaleForInitialization({
       defaultLocale: UILanguage.nl,
