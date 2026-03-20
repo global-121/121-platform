e31059509acc32002b29788143f99a2d1bb66e42
Yuri <ywilmering@redcross.nl>
2026-03-20 11:09:18 +0100

    first draft

--- services/121-service/src/programs/program-attachments/program-attachment.repository.ts
+++ services/121-service/src/programs/program-attachments/program-attachment.repository.ts
@@ -1,5 +1,5 @@
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Equal, FindManyOptions, Like, Repository } from 'typeorm';
 
 import { ProgramAttachmentEntity } from '@121-service/src/programs/program-attachments/program-attachment.entity';
 
@@ -9,5 +9,48 @@ export class ProgramAttachmentRepository extends Repository<ProgramAttachmentEnt
     private readonly repository: Repository<ProgramAttachmentEntity>,
   ) {
     super(repository.target, repository.manager, repository.queryRunner);
+  }
+
+  public findManyScoped({
+    programId,
+    scope,
+    relations,
+  }: {
+    programId: number;
+    scope: string;
+    relations?: FindManyOptions<ProgramAttachmentEntity>['relations'];
+  }): Promise<ProgramAttachmentEntity[]> {
+    return this.find({
+      where: [
+        { programId: Equal(programId), scope: Like(`${scope}%`) },
+        { programId: Equal(programId), program: { enableScope: false } },
+      ],
+      relations,
+    });
+  }
+
+  public findOneScoped({
+    programId,
+    attachmentId,
+    scope,
+  }: {
+    programId: number;
+    attachmentId: number;
+    scope: string;
+  }): Promise<ProgramAttachmentEntity | null> {
+    return this.findOne({
+      where: [
+        {
+          programId: Equal(programId),
+          id: Equal(attachmentId),
+          scope: Like(`${scope}%`),
+        },
+        {
+          programId: Equal(programId),
+          id: Equal(attachmentId),
+          program: { enableScope: false },
+        },
+      ],
+    });
   }
 }
