a5971db1e5f474ef5a8a6352e669bc1618dbeb6a
Yuri <ywilmering@redcross.nl>
2026-03-23 15:31:37 +0100

    minor tidy-up

--- services/mock-service/src/email/email.mock.service.ts
+++ services/mock-service/src/email/email.mock.service.ts
@@ -1,16 +1,11 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 import { SendEmailRequestDto } from '@mock-service/src/email/dto/send-email-request.dto';
 import { SendEmailResponseDto } from '@mock-service/src/email/dto/send-email-response.dto';
 
 @Injectable()
 export class EmailMockService {
-  private readonly logger = new Logger(EmailMockService.name);
-
-  public sendEmail(body: SendEmailRequestDto): SendEmailResponseDto {
-    this.logger.log(
-      `Mock email sent to="${body.email}" subject="${body.subject}"`,
-    );
+  public sendEmail(_body: SendEmailRequestDto): SendEmailResponseDto {
     return { message: 'Email accepted' };
   }
 }
