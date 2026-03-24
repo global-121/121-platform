c7e66166b6928a6d472982f3a702af19573b8f9c
Elwin Schmitz <eschmitz@redcross.nl>
2026-03-24 09:43:27 +0100

    fix: Correct import-path + make config-object extendable

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,6 @@
-import baseConfig from 'packages/lint-staged.config.base.mjs';
+import baseConfig from './packages/lint-staged.config.base.mjs';
 
 // See: https://github.com/lint-staged/lint-staged#using-js-configuration-files
-export default baseConfig;
+export default {
+  ...baseConfig,
+};
