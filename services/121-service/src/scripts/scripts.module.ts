87469ce2a55ff0c441d0b92d0c1df021765d2d5c
Elwin Schmitz <eschmitz@redcross.nl>
2026-03-27 21:16:26 +0100

    fix: Remove 1 lint-rule exception + adjust types to only minimal for the default run-method

--- services/121-service/src/scripts/scripts.module.ts
+++ services/121-service/src/scripts/scripts.module.ts
@@ -8,10 +8,8 @@ import { ProgramFspConfigurationsModule } from '@121-service/src/program-fsp-con
 import { ProgramApprovalThresholdsModule } from '@121-service/src/programs/program-approval-thresholds/program-approval-thresholds.module';
 import { ProgramModule } from '@121-service/src/programs/programs.module';
 import { QueuesRegistryModule } from '@121-service/src/queues-registry/queues-registry.module';
-import { ApproverSeedMode } from '@121-service/src/scripts/enum/approval-seed-mode.enum';
 import { MockSeedFactoryService } from '@121-service/src/scripts/factories/mock-seed-factory.service';
 import { ScriptsController } from '@121-service/src/scripts/scripts.controller';
-import { SeedConfigurationDto } from '@121-service/src/scripts/seed-configuration.dto';
 import { SeedInit } from '@121-service/src/scripts/seed-init';
 import { SeedMultipleNLRCMockData } from '@121-service/src/scripts/seed-multiple-nlrc-mock';
 import { ScriptsService } from '@121-service/src/scripts/services/scripts.service';
@@ -45,21 +43,5 @@ import { AxiosCallsService } from '@121-service/src/utils/axios/axios-calls.serv
 export class ScriptsModule {}
 
 export interface InterfaceScript {
-  // eslint-disable-next-line @typescript-eslint/method-signature-style -- Allow this method to be overridden in: seed-multiple-nlrc-mock.ts
-  run({
-    isApiTests,
-    powerNrRegistrationsString,
-    nrPaymentsString,
-    powerNrMessagesString,
-  }: {
-    isApiTests?: boolean;
-    powerNrRegistrationsString?: string;
-    nrPaymentsString?: string;
-    powerNrMessagesString?: string;
-    includeRegistrationEvents?: boolean;
-    mockPv?: boolean;
-    mockOcw?: boolean;
-    seedConfig?: SeedConfigurationDto;
-    approverMode?: ApproverSeedMode;
-  }): Promise<void>;
+  run: ({ isApiTests }: { isApiTests?: boolean }) => Promise<void>;
 }
