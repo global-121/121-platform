5472828a1c50b41cd4cbbddc48a98404088ee3e8
Piotrk39 <piotrk39@gmail.com>
2026-03-27 13:38:32 +0100

    Add refactors based on PR review

--- services/121-service/src/fsp-integrations/integrations/mtn/mtn.module.ts
+++ services/121-service/src/fsp-integrations/integrations/mtn/mtn.module.ts
@@ -2,7 +2,7 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
 import { MtnController } from '@121-service/src/fsp-integrations/integrations/mtn/mtn.controller';
-import { MtnApiKeyHelperService } from '@121-service/src/fsp-integrations/integrations/mtn/services/mtn.api.key.helper.service';
+import { MtnApiKeyHelperService } from '@121-service/src/fsp-integrations/integrations/mtn/services/mtn.api.key.helper';
 import { MtnService } from '@121-service/src/fsp-integrations/integrations/mtn/services/mtn.service';
 import { CustomHttpService } from '@121-service/src/shared/services/custom-http.service';
 
