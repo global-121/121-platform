387665ef1d539eec98c69bee8c63ef80c4ac1880
Elwin Schmitz <eschmitz@redcross.nl>
2026-03-27 15:07:38 +0100

    docs: Add explanations to config-sets + README of shared-config package

--- packages/eslint-config-121-platform/index.mjs
+++ packages/eslint-config-121-platform/index.mjs
@@ -11,7 +11,8 @@ import globals from 'globals';
 /**
  * Shared ESLint configuration for all packages of the 121-platform.
  *
- * A few 'building blocks' are provided, which should be composed into a specific eslint.config.mjs-file for each package, depending on the environment/needs.
+ * A few 'building blocks' are provided, which should be composed into a specific configuration for each package,
+ * depending on the specific needs. It's NOT necessary or even recommended to use ALL configs together.
  *
  * @example - For a back-end service:
  * ```ts
@@ -21,6 +22,7 @@ import globals from 'globals';
  *     eslintConfig121Platform.configs.base,
  *     eslintConfig121Platform.configs.node,
  *     eslintConfig121Platform.configs.typescript,
+ *     eslintConfig121Platform.configs.services,
  *   ],
  * });
  * ```
@@ -40,6 +42,10 @@ import globals from 'globals';
  * @type {import('./index.d.ts').EslintConfig121Platform} */
 export default {
   configs: {
+    /**
+     * Baseline settings/rules for all packages, regardless of environment/language.
+     * To align the editor's experience across all packages.
+     */
     base: defineConfig({
       name: '121-platform/base',
       languageOptions: {
@@ -56,22 +62,33 @@ export default {
         '@eslint-community/eslint-comments/require-description': 'error',
       },
     }),
+    /**
+     * Only necessary to make _specific_ exceptions for old-style/CommonJS files, like some config-files.
+     */
     legacyNode: defineConfig({
       name: '121-platform/legacy-node',
       languageOptions: {
         globals: globals.node,
       },
     }),
+    /**
+     * Allow browser-specific properties/methods(like `window.*` or `document.*`) not Node.js-specific ones (which won't work in a browser).
+     */
     browser: defineConfig({
       name: '121-platform/browser',
       languageOptions: {
         globals: globals.browser,
       },
     }),
+    /**
+     * Node.js-specific settings/rules. (Not generic enough to be in the `recommended`-config)
+     */
     node: defineConfig({
       name: '121-platform/node',
       files: ['**/*.mjs', '**/*.ts'],
       languageOptions: {
+        ecmaVersion: 2022, // NOTE: Align with Node.js version from: `.node-version`-file
+        sourceType: 'module',
         globals: globals.nodeBuiltin,
       },
       plugins: {
@@ -83,6 +100,10 @@ export default {
         'n/no-path-concat': 'error',
       },
     }),
+    /**
+     * Recommended coding practices, for both (ESM) JavaScript and TypeScript.
+     * For use as application-code or command-line/build-scripts.
+     */
     recommended: defineConfig({
       name: '121-platform/recommended',
       files: ['**/*.mjs', '**/*.ts'],
@@ -127,6 +148,14 @@ export default {
         ],
       },
     }),
+    /**
+     * More 'advanced' recommended coding practices.
+     * These rules should be considered 'TODO:'-items to fix in the 121-service, so they can we added to the `recommended`-config.
+     *
+     * <!> Before adding a rule here:
+     * Check wether it is possible to add to the `recommended`-config first;
+     * Only add here if there are too many violations in the 121-service' code.
+     */
     recommendedNext: defineConfig({
       name: '121-platform/recommended-next',
       files: ['**/*.mjs', '**/*.ts'],
@@ -137,10 +166,19 @@ export default {
         'promise/prefer-await-to-then': 'error',
       },
     }),
+    /**
+     * Rules specific to JavaScript files
+     * Only when not relevant for TypeScript files, like rules that are already managed by the TypeScript compiler.
+     */
     javascript: defineConfig({
       name: '121-platform/javascript',
       files: ['**/*.mjs'],
     }),
+    /**
+     * Rules/options specific to TypeScript ONLY.
+     *
+     * This config is NOT meant to be used as 'only a preset' for TypeScript-files, but to set specific rules only relevant for TypeScript files and not JavaScript files.
+     */
     typescript: defineConfig({
       name: '121-platform/typescript',
       files: ['**/*.ts'],
@@ -171,6 +209,12 @@ export default {
       name: '121-platform/typescript-next',
       files: ['**/*.ts'],
     }),
+    /**
+     * Rules specific to all the 121-platform's services.
+     *
+     * These rules should be generic enough to be applied to all services,
+     * but not relevant for other types of packages (like the Portal, or build-scripts).
+     */
     services: defineConfig({
       name: '121-platform/services',
       plugins: {
@@ -192,6 +236,11 @@ export default {
         ],
       },
     }),
+    /**
+     * Required to be used LAST!
+     * After all other rules are applied, this config will ensure that the code is formatted by Prettier,
+     * Any formatting issues will be reported as lint-errors.
+     */
     final: defineConfig({
       name: '121-platform/final',
       files: ['**/*.js', '**/*.mjs', '**/*.ts', 'src/app/**/*.html'],
