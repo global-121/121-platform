857b3a796a8f6be165c26ca60b18f7f059fd8670
Elwin Schmitz <eschmitz@redcross.nl>
2026-04-02 20:43:24 +0200

    perf: Use caching for ESLint to speed up a lot of things (#8082)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
-import baseConfig from './packages/lint-staged.config.base.mjs';
+import { prettierOnly } from './packages/lint-staged.config.base.mjs';
 
 // See: https://github.com/lint-staged/lint-staged#using-js-configuration-files
 export default {
-  ...baseConfig,
+  ...prettierOnly,
 };
