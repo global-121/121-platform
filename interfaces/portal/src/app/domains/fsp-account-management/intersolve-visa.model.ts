175c5f14111b033c05885a5fdbafd1be1e6d0c68
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2026-04-23 15:57:11 +0200

    feat: export closed cards

--- interfaces/portal/src/app/domains/fsp-account-management/intersolve-visa.model.ts
+++ interfaces/portal/src/app/domains/fsp-account-management/intersolve-visa.model.ts
@@ -1,5 +1,7 @@
 import { IntersolveVisaWalletDto } from '@121-service/src/fsp-integrations/integrations/intersolve-visa/dtos/internal/intersolve-visa-wallet.dto';
+import { ExportVisaWalletClosure } from '@121-service/src/fsp-integrations/integrations/intersolve-visa/interfaces/export-visa-wallet-closure.interface';
 
 import { Dto } from '~/utils/dto-type';
 
 export type WalletWithCards = Dto<IntersolveVisaWalletDto>;
+export type RefundedDebitCardsExport = Dto<ExportVisaWalletClosure>;
