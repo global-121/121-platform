fc14d7e133a1a1ae0b5783751cf900b09be95047
Niels <nvoogt@redcross.nl>
2026-04-28 16:48:44 +0200

    Adding e2e tests

--- e2e/portal/pages/RegistrationDataPage.ts
+++ e2e/portal/pages/RegistrationDataPage.ts
@@ -5,6 +5,11 @@ import BasePage from './BasePage';
 class RegistrationDataPage extends BasePage {
   readonly addKoboToolboxButton: Locator;
   readonly continueButton: Locator;
+  readonly koboCardEllipsisMenu: Locator;
+  readonly koboCard: Locator;
+  readonly initiateImportButton: Locator;
+  readonly importDialog: Locator;
+  readonly closeImportDialog: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -12,6 +17,37 @@ class RegistrationDataPage extends BasePage {
       .getByTestId('card-with-link')
       .getByTitle('KoboToolbox');
     this.continueButton = this.page.getByRole('button', { name: 'Continue' });
+    this.importDialog = this.page.getByTestId(
+      'import-existing-kobo-registrations-dialog',
+    );
+    this.initiateImportButton = this.page.getByRole('button', {
+      name: 'Import registrations',
+    });
+
+    this.closeImportDialog = this.page.getByRole('button', {
+      name: 'Close',
+    });
+  }
+
+  async addKoboIntergration(koboIntegrationDetails: {
+    url: string;
+    apiKey: string;
+  }) {
+    await this.clickRegistrationDataSection();
+    await this.addKoboToolboxIntegration({
+      url: koboIntegrationDetails.url,
+      apiKey: koboIntegrationDetails.apiKey,
+    });
+    // Validate success message after adding Kobo integration with correct details
+    await this.validateKoboIntegration({
+      koboFormName: '25042025 Prototype Sprint',
+    });
+    // Click continue button to exit the form
+    await this.clickContinueButton();
+    // Validate toast message after exiting the form
+    await this.validateToastMessageAndClose(
+      'Kobo form successfully integrated.',
+    );
   }
 
   async clickRegistrationDataSection() {
@@ -54,6 +90,15 @@ class RegistrationDataPage extends BasePage {
     if (koboFormName) {
       await this.page.getByText(koboFormName).waitFor();
     }
+  }
+
+  async openImportExistingKoboRegistrationsDialog() {
+    const ellipsisMenuButton = await this.page.getByTestId(
+      'ellipsis-menu-button',
+    );
+
+    await ellipsisMenuButton.click();
+    await this.page.getByText('Import existing reg.').click();
   }
 }
 
---
+++ e2e/portal/tests/ProgramSettings/RegistrationData/ImportExistingKoboRegistrations.spec.ts
@@ -0,0 +1,51 @@
+import { env } from '@121-service/src/env';
+import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
+import {
+  programIdSafaricom,
+  registrationsSafaricom,
+} from '@121-service/test/registrations/pagination/pagination-data';
+
+import { customSharedFixture as test } from '@121-e2e/portal/fixtures/fixture';
+
+// KOBO INTEGRATION DETAILS
+const koboIntegrationDetails = {
+  url: `${env.MOCK_SERVICE_URL}/api/kobo/#/forms/success-asset/summary`,
+  apiKey: 'mock-token',
+};
+
+test('Import existing Kobo registrations', async ({
+  resetDBAndSeedRegistrations,
+  registrationDataPage,
+}) => {
+  await resetDBAndSeedRegistrations({
+    seedScript: SeedScript.safaricomProgram,
+    registrations: registrationsSafaricom,
+    programId: programIdSafaricom,
+    navigateToPage: `/program/${programIdSafaricom}/settings/registration-data`,
+  });
+
+  await test.step('Add Kobo integration', async () => {
+    await registrationDataPage.addKoboIntergration(koboIntegrationDetails);
+  });
+
+  await test.step('Import existing Kobo registrations', async () => {
+    await registrationDataPage.openImportExistingKoboRegistrationsDialog();
+    await registrationDataPage.initiateImportButton.click();
+  });
+
+  await test.step('Validate success message after importing existing Kobo registrations', async () => {
+    await registrationDataPage.importDialog.getByText('1 total submission(s)');
+    await registrationDataPage.importDialog.getByText(
+      'Imported successfully: 1',
+    );
+  });
+
+  await test.step('Skip importing existing Kobo registrations when there are no new registrations to import', async () => {
+    await registrationDataPage.closeImportDialog.click();
+    await registrationDataPage.openImportExistingKoboRegistrationsDialog();
+    await registrationDataPage.initiateImportButton.click();
+
+    await registrationDataPage.importDialog.getByText('1 total submission(s)');
+    await registrationDataPage.importDialog.getByText('Submissions skipped: 1');
+  });
+});
