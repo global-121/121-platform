887dcf444d7ad8b0a7a7eaeb6af8781589963d71
Piotrk39 <piotrk39@gmail.com>
2026-05-25 11:48:42 +0200

    Add PATCH endpoint to rename payment and corresponding DTO

---
+++ services/121-service/src/payments/dto/rename-payment.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
+
+export class RenamePaymentDto {
+  @ApiProperty({ example: 'New payment name' })
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/)
+  @MaxLength(60)
+  public readonly newName: string;
+}
