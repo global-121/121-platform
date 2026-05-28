a414716b6955a615e6630eecc38ee65748060113
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2026-05-28 15:03:37 +0200

    Align export debit card status with Portal interface for closed cards (#8292)
    Remove card blocked to card closed hack
    
    Co-authored-by: Ruben <vandervalk@geoit.nl>

--- services/121-service/src/fsp-integrations/integrations/intersolve-visa/enums/wallet-status-121.enum.ts
+++ services/121-service/src/fsp-integrations/integrations/intersolve-visa/enums/wallet-status-121.enum.ts
@@ -2,6 +2,7 @@ export enum VisaCard121Status {
   Active = 'Active',
   Issued = 'Issued',
   Blocked = 'Blocked',
+  Closed = 'Closed',
   Paused = 'Paused',
   SuspectedFraud = 'Suspected Fraud',
   Unknown = 'Unknown',
