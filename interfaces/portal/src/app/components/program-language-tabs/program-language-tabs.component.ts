f0428ddae4ecede2c4601fe5797b852badbb3267
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2026-05-28 08:45:35 +0200

    translate tabs

--- interfaces/portal/src/app/components/program-language-tabs/program-language-tabs.component.ts
+++ interfaces/portal/src/app/components/program-language-tabs/program-language-tabs.component.ts
@@ -1,4 +1,4 @@
-import { NgTemplateOutlet } from '@angular/common';
+import { NgTemplateOutlet, TitleCasePipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -14,13 +14,17 @@ import {
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { TabsModule } from 'primeng/tabs';
 
-import { UILanguage } from '../../../../../../services/121-service/src/shared/enum/ui-language.enum';
+import { RegistrationPreferredLanguage } from '@121-service/src/shared/enum/registration-preferred-language.enum';
+import { UILanguage } from '@121-service/src/shared/enum/ui-language.enum';
+import { Language } from '@121-service/src/shared/types/language.type';
 
 import { ProgramApiService } from '~/domains/program/program.api.service';
+import { getLinguonym } from '~/utils/get-linguonym';
 
+const LOCAL_STORAGE_LOCALE_KEY = 'preferredLanguage';
 @Component({
   selector: 'app-program-language-tabs',
-  imports: [TabsModule, NgTemplateOutlet],
+  imports: [TabsModule, NgTemplateOutlet, TitleCasePipe],
   templateUrl: './program-language-tabs.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -34,20 +38,51 @@ export class ProgramLanguageTabsComponent {
 
   readonly programApiService = inject(ProgramApiService);
 
+  readonly defaultLocale = localStorage.getItem(LOCAL_STORAGE_LOCALE_KEY);
+
   readonly program = injectQuery(
     this.programApiService.getProgram(this.programId),
   );
 
-  readonly programLanguages = computed(
-    () => this.program.data()?.languages ?? [],
-  );
+  readonly programLanguages = computed(() => {
+    if (!this.program.isSuccess()) {
+      return [];
+    }
+
+    const languages = this.program.data().languages;
+
+    // put the preferred language first
+    const reorderedLanguages: RegistrationPreferredLanguage[] = [];
+    const preferredLanguage = languages.find((l) => l === this.defaultLocale);
+    if (preferredLanguage) {
+      reorderedLanguages.push(preferredLanguage);
+    }
+    reorderedLanguages.push(
+      ...languages.filter((l) => l !== preferredLanguage),
+    );
+    return reorderedLanguages;
+  });
+
+  readonly getTabLabel = (language: Language) =>
+    computed(() => {
+      let languageToShowNameIn = UILanguage.en;
+      if (this.defaultLocale) {
+        languageToShowNameIn = this.defaultLocale as UILanguage;
+      }
+
+      return getLinguonym({
+        languageToDisplayNameOf: language,
+        languageToShowNameIn,
+      });
+    });
+
+  readonly isDefaultLanguage = (language: Language) =>
+    computed(() => language === this.defaultLocale);
 
   updateCurrentLanguage = effect(() => {
-    if (this.programLanguages().length === 1) {
-      const languageKey = this.programLanguages()[0];
-      const uiLanguage: UILanguage | undefined =
-        UILanguage[languageKey as keyof typeof UILanguage];
-      this.currentLanguage.set(uiLanguage);
-    }
+    const languageKey = this.programLanguages()[0];
+    const uiLanguage: UILanguage | undefined =
+      UILanguage[languageKey as keyof typeof UILanguage];
+    this.currentLanguage.set(uiLanguage);
   });
 }
