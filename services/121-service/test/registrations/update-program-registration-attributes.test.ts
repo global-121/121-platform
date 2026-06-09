f7a5da823cd157616b2f118cbd03c74f63e98a64
Andrea Sforza <63000678+arsforza@users.noreply.github.com>
2026-06-09 07:28:15 +0200

    add tests

---
+++ services/121-service/test/registrations/update-program-registration-attributes.test.ts
@@ -0,0 +1,82 @@
+import { HttpStatus } from '@nestjs/common';
+
+import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
+import { patchProgramRegistrationAttributesInBatch } from '@121-service/test/helpers/program.helper';
+import {
+  getAccessToken,
+  resetDB,
+} from '@121-service/test/helpers/utility.helper';
+
+const programIdOcw = 3;
+
+async function setupNlrcEnvironment() {
+  await resetDB({ seedScript: SeedScript.nlrcMultiple });
+  const accessToken = await getAccessToken();
+
+  return accessToken;
+}
+
+describe('Update program registration attributes in batch', () => {
+  let accessToken: string;
+
+  it('should successfully update multiple attributes', async () => {
+    // Arrange
+    accessToken = await setupNlrcEnvironment();
+    const attributesToUpdateSuccess = [
+      {
+        programRegistrationAttributeName: 'whatsappPhoneNumber',
+        updateProgramRegistrationAttribute: {
+          label: { en: 'WhatsApp Phone Number' },
+        },
+      },
+      {
+        programRegistrationAttributeName: 'addressPostalCode',
+        updateProgramRegistrationAttribute: {
+          label: { en: 'Address PostalCode' },
+          placeholder: { en: 'Postal code' },
+        },
+      },
+    ];
+
+    // Act
+    const response = await patchProgramRegistrationAttributesInBatch({
+      programId: programIdOcw,
+      accessToken,
+      attributesToUpdate: attributesToUpdateSuccess,
+    });
+
+    // Assert
+    expect(response.statusCode).toBe(HttpStatus.OK);
+
+    const updatedAttributes = response.body;
+
+    expect(updatedAttributes).toHaveLength(2);
+    expect(updatedAttributes[0]['label']['en']).toBe('WhatsApp Phone Number');
+    expect(updatedAttributes[1]['label']['en']).toBe('Address PostalCode');
+    expect(updatedAttributes[1]['placeholder']['en']).toBe('Postal code');
+  });
+
+  it('should fail on updating a non existing attribute name', async () => {
+    // Arrange
+    accessToken = await setupNlrcEnvironment();
+    const attributesToUpdateFail = [
+      {
+        programRegistrationAttributeName: 'attibuteFailNotFound',
+        updateProgramRegistrationAttribute: {
+          label: { en: 'WhatsApp Phone Number' },
+        },
+      },
+    ];
+
+    // Act
+
+    const response = await patchProgramRegistrationAttributesInBatch({
+      programId: programIdOcw,
+      accessToken,
+      attributesToUpdate: attributesToUpdateFail,
+    });
+
+    // Assert
+    expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
+  });
+});
