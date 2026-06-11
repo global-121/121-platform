fe48b43f84b77bfcd5e13bc36237fdb03f2fb56a
Niels <nvoogt@redcross.nl>
2026-06-11 15:55:41 +0200

    More work on the modal UX

--- interfaces/portal/src/app/pages/program-settings-registration-data/components/kobo-error-dialog/kobo-error-dialog.component.ts
+++ interfaces/portal/src/app/pages/program-settings-registration-data/components/kobo-error-dialog/kobo-error-dialog.component.ts
@@ -10,27 +10,32 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { Button } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
 import { group } from 'radashi';
 
 import {
+  KoboMissingFieldError,
+  KoboTypeMismatchError,
   KoboValidationError,
   KoboValidationErrorType,
 } from '@121-service/src/kobo/interfaces/kobo-validation-error.interface';
 
+import { QueryTableComponent } from '~/components/query-table/query-table.component';
 import { QueryTableColumn } from '~/components/query-table/query-table.types';
 import { ToastService } from '~/services/toast.service';
 
-interface ParsedFspAttributeError {
-  id: number;
-  attributeName: string;
-  error: string;
-  expectedTypes: string[];
-  actualType: string;
-}
-
 @Component({
   selector: 'app-kobo-error-dialog',
-  imports: [InputTextModule, ReactiveFormsModule, Dialog, Button],
+  imports: [
+    InputTextModule,
+    ReactiveFormsModule,
+    Dialog,
+    Button,
+    TableModule,
+    QueryTableComponent,
+    TagModule,
+  ],
   providers: [ToastService],
   templateUrl: './kobo-error-dialog.component.html',
   styles: ``,
@@ -38,15 +43,31 @@ interface ParsedFspAttributeError {
 })
 export class KoboErrorDialogComponent {
   readonly programId = input.required<number | string>();
-  readonly errors = input<KoboValidationError[]>([]);
+  readonly errors = input<[] | KoboValidationError[]>([]);
   readonly dialogVisible = model(false);
 
   readonly KoboValidationErrorType = KoboValidationErrorType;
 
-  readonly groupedErrors = computed(() => group(this.errors(), (e) => e.type));
+  // Group errors by type to display them in separate sections in the UI.
+  readonly groupedErrors = computed(() => {
+    if (this.errors().length) {
+      return group(this.errors(), (e) => e.type);
+    }
+    return [];
+  });
 
+  // Extract unique missing field names from the errors to display them in a list in the UI.
+  readonly uniqueMissingFields = computed(() => {
+    const missingFieldErrors = this.errors()
+      .filter((error) => error.type === KoboValidationErrorType.MissingField)
+      .map((error) => (error as KoboMissingFieldError).attributeName);
+    const uniqueFields = new Set(missingFieldErrors);
+    return Array.from(uniqueFields);
+  });
+
+  // Define the columns for the type mismatch errors table.
   readonly parsedErrorsColumns = computed<
-    QueryTableColumn<ParsedFspAttributeError>[]
+    QueryTableColumn<{ id: number } & KoboTypeMismatchError>[]
   >(() => [
     {
       field: 'attributeName',
@@ -59,16 +80,11 @@ export class KoboErrorDialogComponent {
     },
     {
       field: 'actualType',
-      header: $localize`Actual Type`,
-    },
-    {
-      field: 'error',
-      header: $localize`Error`,
+      header: $localize`Current Type`,
     },
   ]);
 
   show() {
-    console.log('Showing Kobo error dialog with errors:', this.errors());
     this.dialogVisible.set(true);
   }
 
