01e1a647b9615835e704252e49c87c845d71b00c
Niels <nvoogt@redcross.nl>
2026-06-11 16:39:01 +0200

    More work on the modal UX

--- interfaces/portal/src/app/pages/program-settings-registration-data/components/kobo-error-dialog/kobo-error-dialog.component.ts
+++ interfaces/portal/src/app/pages/program-settings-registration-data/components/kobo-error-dialog/kobo-error-dialog.component.ts
@@ -16,13 +16,10 @@ import { group } from 'radashi';
 
 import {
   KoboMissingFieldError,
-  KoboTypeMismatchError,
   KoboValidationError,
   KoboValidationErrorType,
 } from '@121-service/src/kobo/interfaces/kobo-validation-error.interface';
 
-import { QueryTableComponent } from '~/components/query-table/query-table.component';
-import { QueryTableColumn } from '~/components/query-table/query-table.types';
 import { ToastService } from '~/services/toast.service';
 
 @Component({
@@ -33,7 +30,6 @@ import { ToastService } from '~/services/toast.service';
     Dialog,
     Button,
     TableModule,
-    QueryTableComponent,
     TagModule,
   ],
   providers: [ToastService],
@@ -58,31 +54,16 @@ export class KoboErrorDialogComponent {
 
   // Extract unique missing field names from the errors to display them in a list in the UI.
   readonly uniqueMissingFields = computed(() => {
+    if (!this.errors().length) {
+      return [];
+    }
+
     const missingFieldErrors = this.errors()
       .filter((error) => error.type === KoboValidationErrorType.MissingField)
       .map((error) => (error as KoboMissingFieldError).attributeName);
     const uniqueFields = new Set(missingFieldErrors);
     return Array.from(uniqueFields);
   });
-
-  // Define the columns for the type mismatch errors table.
-  readonly parsedErrorsColumns = computed<
-    QueryTableColumn<{ id: number } & KoboTypeMismatchError>[]
-  >(() => [
-    {
-      field: 'attributeName',
-      header: $localize`Reference ID`,
-    },
-    {
-      field: 'expectedTypes',
-      header: $localize`Expected Types`,
-      getCellText: (item) => item.expectedTypes.join(', '),
-    },
-    {
-      field: 'actualType',
-      header: $localize`Current Type`,
-    },
-  ]);
 
   show() {
     this.dialogVisible.set(true);
