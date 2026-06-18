33bd476c099c1ce383902ff4c5b67e35e0086745
Niels <nvoogt@redcross.nl>
2026-06-18 17:47:23 +0200

    Working on Ruben comments - WIP

---
+++ services/121-service/src/kobo/enum/kobo-validation-error-base.ts
@@ -0,0 +1,13 @@
+/** @public */
+export enum KoboValidationErrorType {
+  missingField = 'missingField',
+  typeMismatch = 'typeMismatch',
+  invalidChoice = 'invalidChoice',
+  formConfiguration = 'formConfiguration',
+  forbiddenAttribute = 'forbiddenAttribute',
+  missingEnglishLanguage = 'missingEnglishLanguage',
+  matrixTypeFound = 'matrixTypeFound',
+  invalidLanguageCode = 'invalidLanguageCode',
+  selectOneNoChoices = 'selectOneNoChoices',
+  missingFullnameAttributes = 'missingFullnameAttributes',
+}
