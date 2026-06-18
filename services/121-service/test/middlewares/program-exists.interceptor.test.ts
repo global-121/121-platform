435ee2523d675337297a3af4c1a6d80fed1dfbb0
Elwin Schmitz <eschmitz@redcross.nl>
2026-06-18 08:51:04 +0200

    test: Use a real endpoint to test the Program-existence interceptor
    AB#42723

--- services/121-service/test/middlewares/program-exists.interceptor.test.ts
+++ services/121-service/test/middlewares/program-exists.interceptor.test.ts
@@ -1,10 +1,9 @@
 import { HttpStatus } from '@nestjs/common';
 
-import { CreateProgramFspConfigurationDto } from '@121-service/src/program-fsp-configurations/dtos/create-program-fsp-configuration.dto';
 import { SeedScript } from '@121-service/src/scripts/enum/seed-script.enum';
-import { postProgramFspConfiguration } from '@121-service/test/helpers/program-fsp-configuration.helper';
 import {
   getAccessToken,
+  getServer,
   resetDB,
 } from '@121-service/test/helpers/utility.helper';
 
@@ -19,13 +18,15 @@ describe('Program exist interceptor', () => {
   it('should throw an error if the program does not exist', async () => {
     // Act
     const nonExistingProgramId = 999999;
-    const result = await postProgramFspConfiguration({
-      programId: nonExistingProgramId,
-      body: new CreateProgramFspConfigurationDto(),
-      accessToken,
-    });
+    const result = await getServer()
+      .get(
+        `/programs/${nonExistingProgramId}/fsps/intersolve-visa/funding-wallet`,
+      )
+      .set('Cookie', [accessToken]);
 
     // Assert
-    expect(result.statusCode).toBe(HttpStatus.FORBIDDEN); // As the user will not have any permissions on the non-existing program, the request fails before the program has even been looked up.
+    expect(result.statusCode).toBe(HttpStatus.NOT_FOUND);
+    expect(result.body?.message).toContain('Program');
+    expect(result.body?.message).toContain(String(nonExistingProgramId));
   });
 });
