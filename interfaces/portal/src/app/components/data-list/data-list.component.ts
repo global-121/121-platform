70b551dbbf76eb704fc1949f456e09ac36fd2f88
Niels <nvoogt@redcross.nl>
2026-06-25 15:53:53 +0200

    refactor: Extract FSP tags into reusable component and display in budget settings

--- interfaces/portal/src/app/components/data-list/data-list.component.ts
+++ interfaces/portal/src/app/components/data-list/data-list.component.ts
@@ -50,6 +50,11 @@ export type DataListItem = {
       value?: null | string;
     }
   | {
+      type: 'component';
+      value?: Type<unknown>;
+      inputs?: Record<string, unknown>;
+    }
+  | {
       type: 'currency';
       value?: null | number;
       currencyCode?: null | string;
