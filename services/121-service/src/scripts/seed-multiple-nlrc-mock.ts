fb78a5057ee9558591f587b8ce3e84efe6dda7ee
Ruben <vandervalk@geoit.nl>
2026-06-29 11:05:47 +0200

    Improve reset error explanation for mock modes

--- services/121-service/src/scripts/seed-multiple-nlrc-mock.ts
+++ services/121-service/src/scripts/seed-multiple-nlrc-mock.ts
@@ -51,7 +51,7 @@ export class SeedMultipleNLRCMockData implements InterfaceScript<SeedMultipleNLR
   }: SeedMultipleNLRCRunParams): Promise<void> {
     if (env.INTERSOLVE_MODE !== FspMode.mock || !env.MOCK_TWILIO) {
       throw new HttpException(
-        `INTERSOLVE_MODE is not MOCK or MOCK_TWILIO is not set to true`,
+        `INTERSOLVE_MODE is not MOCK or MOCK_TWILIO is not set to true. Which is required for seeding NLRC multiple mock data. Please use another reset script, or update your environment variables. Current INTERSOLVE_MODE: ${env.INTERSOLVE_MODE}, MOCK_TWILIO: ${env.MOCK_TWILIO}`,
         HttpStatus.BAD_REQUEST,
       );
     }
