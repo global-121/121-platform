661c0b721d4ef527bf5194053def4b5c30c6fb70
Niels <nvoogt@redcross.nl>
2026-07-01 14:01:12 +0200

    Tempdump

---
+++ interfaces/portal/src/app/domains/fsp/fsp.api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { FspSettingsDto } from '@121-service/src/fsp-management/fsp-settings.dto';
+
+import { DomainApiService } from '~/domains/domain-api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FspApiService extends DomainApiService {
+  getAllFsps() {
+    return this.generateQueryOptions<FspSettingsDto[]>({
+      path: ['fsps'],
+    });
+  }
+}
