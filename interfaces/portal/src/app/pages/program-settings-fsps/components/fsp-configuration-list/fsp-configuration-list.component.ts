1fc189e2911fb342698df9868b7e406687e2e923
Niels <nvoogt@redcross.nl>
2026-07-02 10:19:33 +0200

    Finishing up

--- interfaces/portal/src/app/pages/program-settings-fsps/components/fsp-configuration-list/fsp-configuration-list.component.ts
+++ interfaces/portal/src/app/pages/program-settings-fsps/components/fsp-configuration-list/fsp-configuration-list.component.ts
@@ -1,26 +1,23 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
   inject,
   input,
   model,
   output,
 } from '@angular/core';
 
-import { injectQuery } from '@tanstack/angular-query-experimental';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 
 import { Fsps } from '@121-service/src/fsp-integrations/shared/enum/fsp-name.enum';
-import { FspConfigurationStates } from '@121-service/src/program-fsp-configurations/enum/fsp-configuration-states.enum';
 
 import {
   NotificationBannerComponent,
   NotificationBannerIcon,
 } from '~/components/notification-banner/notification-banner.component';
 import { SkeletonInlineComponent } from '~/components/skeleton-inline/skeleton-inline.component';
-import { FspConfigurationApiService } from '~/domains/fsp-configuration/fsp-configuration.api.service';
+import { injectFspConfigurations } from '~/domains/fsp-configuration/fsp-configuration.helper';
 import { FspConfiguration } from '~/domains/fsp-configuration/fsp-configuration.model';
 import { FspConfigurationCardComponent } from '~/pages/program-settings-fsps/components/fsp-configuration-card/fsp-configuration-card.component';
 import { AuthService } from '~/services/auth.service';
@@ -45,26 +42,17 @@ export class FspConfigurationListComponent {
   readonly addFspConfiguration = output<Fsps>();
   readonly reconfigureFspConfiguration = output<FspConfiguration>();
 
-  readonly fspConfigurationApiService = inject(FspConfigurationApiService);
+  readonly fspConfigurationsData = injectFspConfigurations({
+    programId: this.programId,
+  });
 
-  fspConfigurations = injectQuery(
-    this.fspConfigurationApiService.getFspConfigurations(this.programId),
-  );
+  readonly fspConfigurations = this.fspConfigurationsData.fspConfigurations;
+  readonly notAllFspsIntegrated =
+    this.fspConfigurationsData.notAllFspsIntegrated;
 
   readonly integrationRequiredBannerContent = {
     title: $localize`Integration required`,
     description: $localize`Integrate your FSPs before paying your registrations.`,
     icon: 'alert' as NotificationBannerIcon,
   };
-
-  readonly notAllFspsIntegrated = computed(
-    () =>
-      this.fspConfigurations
-        .data()
-        ?.some(
-          (fspConfiguration) =>
-            fspConfiguration.state ===
-            FspConfigurationStates.configurationPending,
-        ) ?? false,
-  );
 }
