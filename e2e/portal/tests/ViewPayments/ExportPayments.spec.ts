85f18651577ffd7995016e0f80cb31458b57b6f4
Piotrk39 <piotrk39@gmail.com>
2026-07-08 13:54:38 +0200

    Test for flakiness on ci

--- e2e/portal/tests/ViewPayments/ExportPayments.spec.ts
+++ e2e/portal/tests/ViewPayments/ExportPayments.spec.ts
@@ -21,57 +21,62 @@ test.beforeEach(async ({ resetDBAndSeedRegistrations }) => {
   });
 });
 
-test('ExportPayments', async ({ paymentsPage, exportDataComponent }) => {
-  const accessToken = await getAccessToken();
-  await test.step('Do payments', async () => {
-    for (let i = 0; i < 4; i++) {
-      await doPaymentAndWaitForCompletion({
-        programId: programIdOCW,
-        referenceIds: registrationsOCW.map((reg) => reg.referenceId),
-        transferValue: 25,
-        accessToken,
+for (let run = 1; run <= 20; run++) {
+  test(`ExportPayments (run ${run})`, async ({
+    paymentsPage,
+    exportDataComponent,
+  }) => {
+    const accessToken = await getAccessToken();
+    await test.step('Do payments', async () => {
+      for (let i = 0; i < 4; i++) {
+        await doPaymentAndWaitForCompletion({
+          programId: programIdOCW,
+          referenceIds: registrationsOCW.map((reg) => reg.referenceId),
+          transferValue: 25,
+          accessToken,
+        });
+      }
+      await paymentsPage.navigateToProgramPage('Payments');
+    });
+
+    await test.step('Validate export payment button', async () => {
+      await paymentsPage.exportButton.waitFor({ state: 'visible' });
+    });
+
+    await test.step('Export and validate file', async () => {
+      await paymentsPage.selectPaymentExportOption({
+        option: 'Payments',
       });
-    }
-    await paymentsPage.navigateToProgramPage('Payments');
-  });
 
-  await test.step('Validate export payment button', async () => {
-    await paymentsPage.exportButton.waitFor({ state: 'visible' });
-  });
+      await exportDataComponent.exportAndAssertData({
+        exactRowCount: 25, // defaults to export all payments, so 5 payments * 5 registrations
+        excludedColumns: ['id', 'created', 'updated', 'paymentDate'],
+        // Given that the payments are not consistently sorted,
+        // we need to sort them by registrationProgramId and payment
+        // to ensure the snapshot is stable.
+        sortFunction: (a: string[], b: string[], headerCells: string[]) => {
+          const registrationProgramIdIndex = headerCells.indexOf(
+            'registrationProgramId',
+          );
+          expect(registrationProgramIdIndex).toBeGreaterThan(-1);
+          const aId = a[registrationProgramIdIndex];
+          const bId = b[registrationProgramIdIndex];
 
-  await test.step('Export and validate file', async () => {
-    await paymentsPage.selectPaymentExportOption({
-      option: 'Payments',
-    });
+          if (aId !== bId) {
+            return parseInt(aId, 10) - parseInt(bId, 10);
+          }
 
-    await exportDataComponent.exportAndAssertData({
-      exactRowCount: 25, // defaults to export all payments, so 5 payments * 5 registrations
-      excludedColumns: ['id', 'created', 'updated', 'paymentDate'],
-      // Given that the payments are not consistently sorted,
-      // we need to sort them by registrationProgramId and payment
-      // to ensure the snapshot is stable.
-      sortFunction: (a: string[], b: string[], headerCells: string[]) => {
-        const registrationProgramIdIndex = headerCells.indexOf(
-          'registrationProgramId',
-        );
-        expect(registrationProgramIdIndex).toBeGreaterThan(-1);
-        const aId = a[registrationProgramIdIndex];
-        const bId = b[registrationProgramIdIndex];
+          const paymentIdIndex = headerCells.indexOf('paymentId');
+          expect(paymentIdIndex).toBeGreaterThan(-1);
+          const aPaymentId = a[paymentIdIndex];
+          const bPaymentId = b[paymentIdIndex];
 
-        if (aId !== bId) {
-          return parseInt(aId, 10) - parseInt(bId, 10);
-        }
-
-        const paymentIdIndex = headerCells.indexOf('paymentId');
-        expect(paymentIdIndex).toBeGreaterThan(-1);
-        const aPaymentId = a[paymentIdIndex];
-        const bPaymentId = b[paymentIdIndex];
-
-        return parseInt(aPaymentId, 10) - parseInt(bPaymentId, 10);
-      },
+          return parseInt(aPaymentId, 10) - parseInt(bPaymentId, 10);
+        },
+      });
     });
   });
-});
+}
 
 test('View available actions for admin', async ({ page, paymentsPage }) => {
   await test.step('Validate export options', async () => {
