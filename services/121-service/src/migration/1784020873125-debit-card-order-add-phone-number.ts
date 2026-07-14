88914dce023db928fdd64a933a4a898e83b0d236
Piotrk39 <piotrk39@gmail.com>
2026-07-14 11:31:25 +0200

    Update migration script

---
+++ services/121-service/src/migration/1784020873125-debit-card-order-add-phone-number.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class DebitCardOrderAddPhoneNumber1784020873125
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."intersolve_visa_card_order" ADD "addresseePhoneNumber" character varying`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "121-service"."intersolve_visa_card_order" DROP COLUMN "addresseePhoneNumber"`,
+    );
+  }
+}
