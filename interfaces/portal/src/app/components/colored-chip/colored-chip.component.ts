6b5be512db2375c95b5581e1ae5bbd452f87df67
Niels <nvoogt@redcross.nl>
2026-07-15 09:26:54 +0200

    Adds whitespace-nowrap to colored chip label

--- interfaces/portal/src/app/components/colored-chip/colored-chip.component.ts
+++ interfaces/portal/src/app/components/colored-chip/colored-chip.component.ts
@@ -30,34 +30,41 @@ export class ColoredChipComponent {
   readonly label = input.required<string>();
   readonly icon = input<string>();
   readonly tooltip = input<string>();
+  readonly wrapText = input(false);
 
   readonly styleClass = computed(() => {
-    let baseClass = '';
+    const baseClass: string[] = [];
 
     if (this.tooltip()) {
-      baseClass = 'cursor-help';
+      baseClass.push('cursor-help');
     }
+
+    if (!this.wrapText()) {
+      baseClass.push('whitespace-nowrap');
+    }
+
+    const classString = baseClass.join(' ');
 
     // Do not replace with something like `bg-${this.variant()}-100` as it would not work with tailwind's JIT compiler
     // https://tailwindcss.com/docs/just-in-time-mode
     const variant = this.variant();
     switch (variant) {
       case 'blue':
-        return `${baseClass} bg-blue-100 text-blue-700`;
+        return `${classString} bg-blue-100 text-blue-700`;
       case 'green':
-        return `${baseClass} bg-green-100 text-green-700`;
+        return `${classString} bg-green-100 text-green-700`;
       case 'purple':
-        return `${baseClass} bg-purple-100 text-purple-900`;
+        return `${classString} bg-purple-100 text-purple-900`;
       case 'red':
-        return `${baseClass} bg-red-100 text-red-700`;
+        return `${classString} bg-red-100 text-red-700`;
       case 'orange':
-        return `${baseClass} bg-orange-100 text-orange-700`;
+        return `${classString} bg-orange-100 text-orange-700`;
       case 'yellow':
-        return `${baseClass} bg-yellow-100 text-yellow-700`;
+        return `${classString} bg-yellow-100 text-yellow-700`;
       case 'grey':
-        return `${baseClass} bg-grey-100 text-grey-700`;
+        return `${classString} bg-grey-100 text-grey-700`;
       case 'contrast':
-        return `${baseClass} bg-grey-50 text-grey-900`;
+        return `${classString} bg-grey-50 text-grey-900`;
       default:
         variant satisfies never;
         return;
