3c895ed412b356ff37f1dd3808574a5b45eadc89
Niels <nvoogt@redcross.nl>
2026-08-04 16:42:57 +0200

    Finetuning some things

--- interfaces/portal/src/app/components/data-list/data-list.component.ts
+++ interfaces/portal/src/app/components/data-list/data-list.component.ts
@@ -25,7 +25,7 @@ import {
   ChipVariant,
   ColoredChipComponent,
 } from '~/components/colored-chip/colored-chip.component';
-import { ImageViewerTriggerComponent } from '~/components/image-viewer-trigger/image-viewer-trigger.component';
+import { ImageModalTriggerComponent } from '~/components/image/image-modal-trigger/image-modal-trigger.component';
 import { InfoTooltipComponent } from '~/components/info-tooltip/info-tooltip.component';
 import { TranslatableStringPipe } from '~/pipes/translatable-string.pipe';
 import { TranslatableStringService } from '~/services/translatable-string.service';
@@ -113,7 +113,7 @@ export type DataListItem = {
     RouterLink,
     InputTextModule,
     TagModule,
-    ImageViewerTriggerComponent,
+    ImageModalTriggerComponent,
   ],
   templateUrl: './data-list.component.html',
   styles: ``,
