d712b09502e45fa2bf60585b1f42c1b829379ecd
Niels <nvoogt@redcross.nl>
2026-08-06 14:45:47 +0200

    Fixing test

--- services/121-service/test/kobo/kobo-load-form-definition.test.ts
+++ services/121-service/test/kobo/kobo-load-form-definition.test.ts
@@ -129,6 +129,8 @@ describe('Import a Kobo form definition', () => {
       ...expectedAttributeNamesBeforeKobo,
       'How_are_you_today_select_one',
       'What_is_2_2_number',
+      'driversLicensePhoto',
+      'idPhoto',
       'photo',
     ];
 
