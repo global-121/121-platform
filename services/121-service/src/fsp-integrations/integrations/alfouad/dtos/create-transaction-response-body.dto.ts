3b1269cd1dbad5b81060fc04802543d88b4f5dd0
Yuri <ywilmering@redcross.nl>
2026-08-10 14:26:27 +0200

    authenticated requests

--- services/121-service/src/fsp-integrations/integrations/alfouad/dtos/create-transaction-response-body.dto.ts
+++ services/121-service/src/fsp-integrations/integrations/alfouad/dtos/create-transaction-response-body.dto.ts
@@ -1,8 +1,8 @@
-import { CreateTransferResult } from "@121-service/src/fsp-integrations/integrations/alfouad/interfaces/create-transfer-result.interface";
-
 export interface AlfouadApiCreateTransactionResponseBody {
   readonly State: string;
   readonly Message: string;
   readonly ErrorCode?: string | null;
-  readonly TransactionInfo?: CreateTransferResult | null;
+  readonly TransactionInfo?: {
+    readonly TransactionUID?: string;
+  } | null;
 }
