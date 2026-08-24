fad34702d56c74939c3e95290cd493368d007895
Yuri <ywilmering@redcross.nl>
2026-08-24 12:57:20 +0200

    leftover pr comments

--- services/121-service/src/fsp-integrations/integrations/alfouad/dtos/alfouad-api-create-transaction-response-body.dto.ts
+++ services/121-service/src/fsp-integrations/integrations/alfouad/dtos/alfouad-api-create-transaction-response-body.dto.ts
@@ -1,6 +1,6 @@
 import { AlfouadApiResponseState } from "@121-service/src/fsp-integrations/integrations/alfouad/enums/alfouad-api-response-state.enum";
 
-export interface AlfouadApiCreateTransactionResponseDto {
+export interface AlfouadApiCreateTransactionResponseBodyDto {
   readonly State: AlfouadApiResponseState;
   readonly Message: string;
   readonly ErrorCode?: string;
--- services/121-service/src/fsp-integrations/integrations/alfouad/dtos/alfouad-api-get-transaction-response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { AlfouadApiTransactionState } from "@121-service/src/fsp-integrations/integrations/alfouad/enums/alfouad-api-transaction-state.enum";
-
-export interface AlfouadApiGetTransactionResponseDto {
-  readonly State: AlfouadApiTransactionState;
-  readonly Message: string;
-  readonly ErrorCode?: string;
-}
