3492c1ce782bb8981d2195b9ce61badefecedf8d
Niels <nvoogt@redcross.nl>
2026-08-25 09:18:57 +0200

    Implemented new component in two locations according to issue

--- interfaces/portal/src/app/pages/program-settings-team/components/add-program-team-user-dialog/add-program-team-user-dialog.component.ts
+++ interfaces/portal/src/app/pages/program-settings-team/components/add-program-team-user-dialog/add-program-team-user-dialog.component.ts
@@ -27,6 +27,7 @@ import { SelectModule } from 'primeng/select';
 import { FormDialogComponent } from '~/components/form-dialog/form-dialog.component';
 import { FormFieldWrapperComponent } from '~/components/form-field-wrapper/form-field-wrapper.component';
 import { ManualLinkComponent } from '~/components/manual-link/manual-link.component';
+import { SelectNoResultsMessageComponent } from '~/components/select-empty-message/select-no-results-message.component';
 import { ProgramApiService } from '~/domains/program/program.api.service';
 import { ProgramUserWithRolesLabel } from '~/domains/program/program.model';
 import { RoleApiService } from '~/domains/role/role.api.service';
@@ -53,6 +54,7 @@ import { generateFieldErrors } from '~/utils/form-validation';
     InputTextModule,
     ReactiveFormsModule,
     ManualLinkComponent,
+    SelectNoResultsMessageComponent,
   ],
 })
 export class AddProgramTeamUserDialogComponent {
