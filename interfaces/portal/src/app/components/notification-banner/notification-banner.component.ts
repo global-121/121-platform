a981f62f4933473e5d817df7bb1f8d0a019530f0
Niels <nvoogt@redcross.nl>
2026-09-03 11:31:24 +0200

    Removed wrapperClass prop. Moved margin styles to wrapper divs

--- interfaces/portal/src/app/components/notification-banner/notification-banner.component.ts
+++ interfaces/portal/src/app/components/notification-banner/notification-banner.component.ts
@@ -37,7 +37,6 @@ export type NotificationBannerIcon =
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotificationBannerComponent {
-  readonly wrapperClass = input('');
   readonly content = input<NotificationBannerContent>();
   readonly color = input.required<NotificationBannerColor>();
 
