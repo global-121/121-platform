2c102bd7e56b108bdf9da57474e203f438f6647c
Elwin Schmitz <eschmitz@redcross.nl>
2026-09-10 12:28:53 +0200

    ci: Configure E2E-tests to be more strict, less forgiving

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   fullyParallel: false,
   // eslint-disable-next-line n/no-process-env -- This environment variable `CI` is NOT used in the 121-service, thus not managed via the env.ts file.
   forbidOnly: !!process.env.CI, // Fail the build on CI if you accidentally left test.only in the source code.
-  retries: 1,
+  retries: 0,
   reporter: [['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   workers: 1,
@@ -21,7 +21,7 @@ export default defineConfig({
   use: {
     // eslint-disable-next-line n/no-process-env -- This environment variable `BASE_URL` is NOT used in the 121-service, thus not managed via the env.ts file.
     baseURL: process.env.BASE_URL ?? 'http://localhost:8088',
-    video: 'on-first-retry',
+    video: 'retain-on-failure',
     screenshot: 'only-on-failure',
     headless: true,
     acceptDownloads: true,
@@ -41,7 +41,7 @@ export default defineConfig({
     viewport: null,
     ignoreHTTPSErrors: true,
     bypassCSP: false,
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
   projects: [
     {
